cde9a5c4549d0eee4a0aa5dff4cdc17978e3d1ed
Martin <bbaalmart@gmail.com>
2020-03-30 21:03:21 +0300

    holds the entire state tree of the app

---
+++ locate/src/store.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./redux/Join/reducers";
+
+const initialState = {};
+const middleware = [thunk];
+const store = createStore(
+  rootReducer
+  //   () => [],
+  //   initialState,
+  //   applyMiddleware(...middleware)
+  //   compose(
+  //     applyMiddleware(...middleware),
+  //     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  //   )
+);
+export default store;
