a023f4091dbb38bc83acb0370608a180eee96fbd
Martin <bbaalmart@gmail.com>
2020-03-31 07:27:50 +0300

    port number configuration for the API

--- locate/src/redux/Join/actions.js
+++ locate/src/redux/Join/actions.js
@@ -5,7 +5,7 @@ import { GET_ERRORS, SET_CURRENT_USER, USER_LOADING } from "./types";
 // Register User
 export const registerUser = (userData, history) => dispatch => {
   axios
-    .post("/api/v1/users/register", userData)
+    .post("http://localhost:3000/api/v1/users/register", userData)
     .then(res => history.push("/login")) // re-direct to login on successful register
     .catch(err =>
       dispatch({
@@ -17,7 +17,7 @@ export const registerUser = (userData, history) => dispatch => {
 // Login - get user token
 export const loginUser = userData => dispatch => {
   axios
-    .post("/api/v1/users/login", userData)
+    .post("http://localhost:3000/api/v1/users/loginUser", userData)
     .then(res => {
       // Save to localStorage
       // Set token to localStorage
@@ -33,7 +33,7 @@ export const loginUser = userData => dispatch => {
     .catch(err =>
       dispatch({
         type: GET_ERRORS,
-        payload: err.response.data
+        payload: err.response
       })
     );
 };
