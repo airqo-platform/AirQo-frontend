ffa8a8341f7ad655848f6d8075b154249fbf35dd
lilly-muyama <muyamalillian@gmail.com>
2020-04-03 15:34:53 +0300

    initial code for pie chart

--- analytics/src/views/Graphs/Graphs.js
+++ analytics/src/views/Graphs/Graphs.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { Grid} from '@material-ui/core';
 import { Display, Filters } from './components';
+//import CanvasJS from 'canvasjs';
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -45,3 +46,45 @@ const Graphs = () => {
 };
 
 export default Graphs;
+
+//var React = require('react');
+var Component = React.Component;
+var CanvasJSReact = require('canvasjs.react');
+var CanvasJS = CanvasJSReact.CanvasJS;
+var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+class App extends Component {
+	render() {
+		const options = {
+			exportEnabled: true,
+			animationEnabled: true,
+			title: {
+				text: "Website Traffic Sources"
+			},
+			data: [{
+				type: "pie",
+				startAngle: 75,
+				toolTipContent: "<b>{label}</b>: {y}%",
+				showInLegend: "true",
+				legendText: "{label}",
+				indexLabelFontSize: 16,
+				indexLabel: "{label} - {y}%",
+				dataPoints: [
+					{ y: 18, label: "Good" },
+					{ y: 49, label: "Moderate" },
+					{ y: 9, label: "UH4SG" },
+					{ y: 5, label: "Unhealthy" },
+					{ y: 19, label: "Very Unhealthy" }
+				]
+			}]
+		}
+		return (
+		<div>
+			<CanvasJSChart options = {options}
+				/* onRef={ref => this.chart = ref} */
+			/>
+			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
+		</div>
+		);
+	}
+}
+module.exports = App;                              
