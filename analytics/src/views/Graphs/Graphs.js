ecc6c049f3419b89c413525d63450b4c8b1076bb
lilly-muyama <muyamalillian@gmail.com>
2020-04-03 19:06:20 +0300

    testing

--- analytics/src/views/Graphs/Graphs.js
+++ analytics/src/views/Graphs/Graphs.js
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, Component } from 'react';
 import { makeStyles } from '@material-ui/styles';
 import { Grid} from '@material-ui/core';
 import { Display, Filters } from './components';
+import { Pie } from 'react-chartjs-2';
 //import CanvasJS from 'canvasjs';
 
 const useStyles = makeStyles(theme => ({
@@ -45,46 +46,37 @@ const Graphs = () => {
   );
 };
 
-export default Graphs;
+class PieChartComponent extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      labels: ['Good', 'Moderate', 'UH4SG', 'Unhealthy', 'Very Unhealthy', 'Hazardous', 'Other'],
+      datasets: [{
+        data: [100, 200, 300, 150, 320, 40, 2],
+        backgroundColor: ['green', 'yellow', 'orange', 'red', 'purple', 'maroon', 'grey']
+      }]
+    }
+  }
 
-//var React = require('react');
-var Component = React.Component;
-var CanvasJSReact = require('canvasjs.react');
-var CanvasJS = CanvasJSReact.CanvasJS;
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-class App extends Component {
-	render() {
-		const options = {
-			exportEnabled: true,
-			animationEnabled: true,
-			title: {
-				text: "Website Traffic Sources"
-			},
-			data: [{
-				type: "pie",
-				startAngle: 75,
-				toolTipContent: "<b>{label}</b>: {y}%",
-				showInLegend: "true",
-				legendText: "{label}",
-				indexLabelFontSize: 16,
-				indexLabel: "{label} - {y}%",
-				dataPoints: [
-					{ y: 18, label: "Good" },
-					{ y: 49, label: "Moderate" },
-					{ y: 9, label: "UH4SG" },
-					{ y: 5, label: "Unhealthy" },
-					{ y: 19, label: "Very Unhealthy" }
-				]
-			}]
-		}
-		return (
-		<div>
-			<CanvasJSChart options = {options}
-				/* onRef={ref => this.chart = ref} */
-			/>
-			{/*You can get reference to the chart instance as shown above using onRef. This allows you to access all chart properties and methods*/}
-		</div>
-		);
-	}
+  render(){
+    return (
+      <div>
+        <h1> Piechart showing air quality distribution</h1>
+        <Pie 
+           data ={{
+             labels: this.state.labels,
+             datasets: this.state.datasets
+           }}
+           height = '50%'
+        />
+        <br/>
+      </div>
+    )
+  }
 }
-module.exports = App;                              
+
+export default PieChartComponent;
+//export default Graphs;
+
+
+//module.exports = App;                              
