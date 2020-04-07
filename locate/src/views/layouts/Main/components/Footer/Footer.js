1a9ca89b7a6fb11b1004267a159226114c86c96d
Martin <bbaalmart@gmail.com>
2020-04-07 14:02:31 +0300

    the layout files for the dashboard

---
+++ locate/src/views/layouts/Main/components/Footer/Footer.js
@@ -0,0 +1,36 @@
+import React from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/styles";
+import { Typography, Link } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(4),
+  },
+}));
+
+const Footer = (props) => {
+  const { className, ...rest } = props;
+
+  const classes = useStyles();
+
+  return (
+    <div {...rest} className={clsx(classes.root, className)}>
+      <Typography variant="body1">
+        &copy;{" "}
+        <Link component="a" href="https://airqo.net" target="_blank">
+          AirQo
+        </Link>
+        . 2020
+      </Typography>
+      <Typography variant="caption">Air Quality Initiative</Typography>
+    </div>
+  );
+};
+
+Footer.propTypes = {
+  className: PropTypes.string,
+};
+
+export default Footer;
