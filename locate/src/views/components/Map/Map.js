af6ca4f9ed0131b6dc77cf30629e696b67017f21
lilly-muyama <muyamalillian@gmail.com>
2020-04-23 23:15:29 +0300

    added markers and other features

--- locate/src/views/components/Map/Map.js
+++ locate/src/views/components/Map/Map.js
@@ -2,16 +2,18 @@ import React from "react";
 import { connect } from "react-redux";
 import { mapRenderDefaults } from "../../../redux/Maps/actions";
 import propTypes from "prop-types";
-import {Map, FeatureGroup, Circle, TileLayer } from "react-leaflet";
+import {Map, FeatureGroup, LayerGroup, TileLayer, Marker, Popup} from "react-leaflet";
 import { EditControl } from "react-leaflet-draw";
 import axios from "axios";
 import L from 'leaflet';
+import { ElementClass } from "enzyme";
 
 class Maps extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      polygons: []
+      polygons: [],
+      markers: [[0.32, 32.598]]
      }
     };
   
@@ -35,7 +37,7 @@ class Maps extends React.Component {
       console.log(JSON.stringify(layer.toGeoJSON()));
 
       axios.post(
-        'http://127.0.0.1:4000/api/v1/map/parishes/trial',
+        'http://127.0.0.1:4000/api/v1/map/parishes',
         JSON.stringify(layer.toGeoJSON()),
         {headers:{'Content-Type':'application/json'}},
       )
@@ -46,7 +48,7 @@ class Maps extends React.Component {
           
           let myPolygons = [];
 
-          try{          
+          try{     
     
           myData.forEach(element => {
             if (element["properties.district"]){
@@ -56,6 +58,8 @@ class Maps extends React.Component {
                 "district":element["properties.district"],
                 "subcounty": element["properties.subcounty"],
                 "parish": element["properties.parish"],
+                "lat": element["properties.lat"],
+                "long":element["properties.long"],
                 "color": element["color"],
                 "fill_color": element["fill_color"],
                 "type": element.type
@@ -73,6 +77,8 @@ class Maps extends React.Component {
                   "district":element.properties.district,
                   "subcounty": element.properties.subcounty,
                   "parish": element.properties.parish,
+                  "lat": element.properties.lat,
+                  "long":element.properties.long,
                   "color": element.color,
                   "fill_color": element.fill_color,
                   "type": element.type
@@ -89,7 +95,6 @@ class Maps extends React.Component {
           this.setState({
             polygons: myPolygons
           })
-
         }
         catch(error){
         console.log('An error occured. Please try again')
@@ -99,10 +104,7 @@ class Maps extends React.Component {
     }
   };
 
-    
-
-  render() {
-  
+render() {
     return (
       <Map
         center={[this.props.mapDefaults.lat, this.props.mapDefaults.lng]}
@@ -112,7 +114,43 @@ class Maps extends React.Component {
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
           url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
         />
-        <FeatureGroup color="red" ref={ (reactFGref) => {this._onFeatureGroupReady(reactFGref);} }> }
+           
+        <LayerGroup>}
+           {this.state.polygons.map(location => (
+          <Marker
+            key={location.parish}
+            position={{ lat: location.properties.lat, lng: location.properties.long }}
+            icon = {new L.Icon({
+              iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-'+location.properties.color+'.png',
+              shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+              iconSize: [25, 41],
+              iconAnchor: [12, 41],
+              popupAnchor: [1, -34],
+              shadowSize: [41, 41]
+            })}
+            onMouseOver={(e) => {
+              e.target.openPopup();
+            }}
+            onMouseOut={(e) => {
+              e.target.closePopup();
+            }}
+          >
+            <Popup>
+              <span>
+              <span>
+                <b>DISTRICT: </b>{location.properties.district}, <br /> 
+                <b>SUBCOUNTY: </b>{location.properties.subcounty}, <br/>
+                <b>PARISH: </b>{location.properties.parish}, <br/>
+                <b>TYPE: </b>{location.properties.type}
+            </span>
+              </span>
+            </Popup>
+          </Marker>
+        ))}
+      </LayerGroup>
+      
+        
+        <FeatureGroup ref={ (reactFGref) => {this._onFeatureGroupReady(reactFGref);} }> 
           <EditControl
             position="topleft"
             onEdited={this._onEdited}
@@ -129,22 +167,22 @@ class Maps extends React.Component {
               circlemarker: false,
             }}
           />
-        </FeatureGroup>
+          </FeatureGroup>
       </Map>
     );
   }
-  
+
   _onFeatureGroupReady = (ref) => {
-    
-    console.log('_onFeatureGroupReady');
     if(ref===null) {
         return;
     }
     this._editableFG = ref; 
-    // populate the leaflet FeatureGroup with the geoJson layers
     if(this.state.polygons) {
-
       for (var i = 0; i < this.state.polygons.length; i++) {
+
+        //let leafletGeoJSON = new L.GeoJSON(this.state.polygons[i]);console.log(leafletGeoJSON);
+
+       try{
         let leafletGeoJSON = new L.GeoJSON(this.state.polygons[i], {
           onEachFeature: function (feature, layer) {
             let popup_string = "<b>DISTRICT: </b>"+feature['properties']['district']+ 
@@ -160,38 +198,21 @@ class Maps extends React.Component {
           });
         },
         style: {fillColor: this.state.polygons[i]['properties']['fill_color'], color: this.state.polygons[i]['properties']['color'], opacity:100},
-      
       });
-
         let leafletFG = this._editableFG.leafletElement;
         leafletGeoJSON.eachLayer( layer =>leafletFG.addLayer(layer));
       } 
-
-        
-    }else {
-        console.log('No polygons');
-                
+      catch(error){
+       console.log('An error occured and some polygons may not have been shown!')
+      }
+    }
+    //console.log(toString(count)+' invalid polygons in results')
+  }
+    else {
+        console.log('No polygons');           
     }
   }
-
 }
-
-/*function getGeoJson() {
-  return {
-
-      "type": "Feature",
-      "properties": {},
-      "geometry": {
-        "type": "Polygon",
-        "coordinates": [
-          [
-            [-122.45,37.79], [-122.42999999999999,37.79],[-122.42999999999999, 37.77],
-              [-122.45,37.77], [-122.45,37.79]
-          ]
-        ]
-      }
-  }
-}*/
 
 Maps.propTypes = {
   mapRenderDefaults: propTypes.func.isRequired,
