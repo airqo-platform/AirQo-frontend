08d99a3d9bd57161601216c739de43ca21a1ed15
danielogen <danielogen@gmail.com>
2020-05-09 14:30:33 +0300

    move locate model endpoint to constants

--- locate/src/config/constants.js
+++ locate/src/config/constants.js
@@ -15,6 +15,7 @@ const constants = {
   GET_USERS_URI: "https://airqo-250220.uc.r.appspot.com/api/v1/users/",
   SAVE_LOCATE_MAP: "http://localhost:4000/api/v1/map/savelocatemap",
   GET_LOCATE_MAP: "http://localhost:4000/api/v1/map/getlocatemap/",
+  RUN_LOCATE_MODEL: "http://127.0.0.1:4000/api/v1/map/parishes",
 };
 
 export default constants;
