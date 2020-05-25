b648a5c13941f86d3a0b3123aee0190921f3ea99
danielogen <danielogen@gmail.com>
2020-05-25 11:18:23 +0300

    fixed print-save pdf related issues

--- analytics/src/views/ReportTemplate/ReportTemplate.js
+++ analytics/src/views/ReportTemplate/ReportTemplate.js
@@ -1,6 +1,7 @@
 import React, { useRef } from "react";
 import { makeStyles } from "@material-ui/styles";
 import { Grid, Card, CardContent, Button } from "@material-ui/core";
+import Tooltip from "@material-ui/core/Tooltip";
 import Main from "./Main";
 import MainTest from "./MainTest";
 import ReactToPrint, { useReactToPrint } from "react-to-print";
@@ -28,15 +29,17 @@ const ReportTemplate = () => {
         <Grid item lg={12} sm={12} xl={12} xs={12}>
           <Card>
             <CardContent>
-              <Button
-                color="primary"
-                variant="contained"
-                endIcon={<PictureAsPdfSharpIcon />}
-                onClick={handlePrint}
-                alt="Generate Pdf"
-              >
-                {/* Generate Pdf */}
-              </Button>
+              <Tooltip title="Print OR Save As Pdf" placement="right" arrow>
+                <Button
+                  color="primary"
+                  variant="contained"
+                  endIcon={<PictureAsPdfSharpIcon />}
+                  onClick={handlePrint}
+                  alt="Generate Pdf"
+                >
+                  {/* Generate Pdf */}
+                </Button>
+              </Tooltip>
               <Main ref={componentRef} />
               {/* <MainTest ref={componentRef} /> */}
             </CardContent>
