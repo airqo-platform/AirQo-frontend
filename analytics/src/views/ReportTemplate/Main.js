30215ae1669dbb59e852c016d342f3a7ff18adb1
danielogen <danielogen@gmail.com>
2020-05-31 11:26:10 +0300

    manage dialog box state variable

--- analytics/src/views/ReportTemplate/Main.js
+++ analytics/src/views/ReportTemplate/Main.js
@@ -1,30 +1,30 @@
-import React, { Component } from 'react';
-import { EditorState, convertFromRaw, convertToRaw } from 'draft-js';
+import React, { Component } from "react";
+import { EditorState, convertFromRaw, convertToRaw } from "draft-js";
 import {
   Button,
   Dialog,
   DialogActions,
   DialogContent,
   DialogContentText,
-  TextField
-} from '@material-ui/core';
-import UpdateIcon from '@material-ui/icons/Update';
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import SaveIcon from '@material-ui/icons/Save';
-import FolderIcon from '@material-ui/icons/Folder';
-import Tooltip from '@material-ui/core/Tooltip';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import Avatar from '@material-ui/core/Avatar';
-import DeleteIcon from '@material-ui/icons/Delete';
-import DescriptionIcon from '@material-ui/icons/Description';
-import { Editor } from 'react-draft-wysiwyg';
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
-import '../../assets/scss/report.css';
-import axios from 'axios';
+  TextField,
+} from "@material-ui/core";
+import UpdateIcon from "@material-ui/icons/Update";
+import ArrowBackIcon from "@material-ui/icons/ArrowBack";
+import SaveIcon from "@material-ui/icons/Save";
+import FolderIcon from "@material-ui/icons/Folder";
+import Tooltip from "@material-ui/core/Tooltip";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import DialogTitle from "@material-ui/core/DialogTitle";
+import Avatar from "@material-ui/core/Avatar";
+import DeleteIcon from "@material-ui/icons/Delete";
+import DescriptionIcon from "@material-ui/icons/Description";
+import { Editor } from "react-draft-wysiwyg";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import "../../assets/scss/report.css";
+import axios from "axios";
 
 class Main extends Component {
   constructor(props) {
@@ -37,10 +37,10 @@ class Main extends Component {
       openSave: false,
       openConfirm: false,
       saved_report: [],
-      report_name: '',
+      report_name: "",
       report_body_default: {},
-      report_name_default: '',
-      response_msg: ''
+      report_name_default: "",
+      response_msg: "",
     };
     // bind to this
     this.handlePrevSavedOpen = this.handlePrevSavedOpen.bind(this);
@@ -55,10 +55,10 @@ class Main extends Component {
     this.onUpdateDraftCancel = this.onUpdateDraftCancel.bind(this);
   }
 
-  onEditorStateChange = editorState => {
+  onEditorStateChange = (editorState) => {
     // console.log(editorState)
     this.setState({
-      editorState
+      editorState,
     });
   };
 
@@ -66,37 +66,37 @@ class Main extends Component {
     // local: http://127.0.0.1:4000
     axios
       .get(
-        'https://analytcs-bknd-service-dot-airqo-250220.uc.r.appspot.com/api/v1/report/get_default_report_template'
+        "https://analytcs-bknd-service-dot-airqo-250220.uc.r.appspot.com/api/v1/report/get_default_report_template"
       )
-      .then(res => {
+      .then((res) => {
         let result = res.data[0];
         this.setState({
           editorState: EditorState.createWithContent(
             convertFromRaw(JSON.parse(JSON.stringify(result.report_body)))
-          )
+          ),
         });
         //console.log(result.report_body);
         this.setState({ report_body_default: result.report_body });
         this.setState({ report_name_default: result.report_name });
       })
-      .catch(e => {
+      .catch((e) => {
         console.log(e);
       });
   }
 
-  uploadImageCallBack = file => {
+  uploadImageCallBack = (file) => {
     return new Promise((resolve, reject) => {
       const xhr = new XMLHttpRequest(); // eslint-disable-line no-undef
-      xhr.open('POST', 'https://api.imgur.com/3/image');
-      xhr.setRequestHeader('Authorization', 'Client-ID 8d26ccd12712fca');
+      xhr.open("POST", "https://api.imgur.com/3/image");
+      xhr.setRequestHeader("Authorization", "Client-ID 8d26ccd12712fca");
       const data = new FormData(); // eslint-disable-line no-undef
-      data.append('image', file);
+      data.append("image", file);
       xhr.send(data);
-      xhr.addEventListener('load', () => {
+      xhr.addEventListener("load", () => {
         const response = JSON.parse(xhr.responseText);
         resolve(response);
       });
-      xhr.addEventListener('error', () => {
+      xhr.addEventListener("error", () => {
         const error = JSON.parse(xhr.responseText);
         reject(error);
       });
@@ -115,24 +115,24 @@ class Main extends Component {
     // make api call to save report
     axios
       .post(
-        'https://analytcs-bknd-service-dot-airqo-250220.uc.r.appspot.com/api/v1/report/save_monthly_report',
+        "https://analytcs-bknd-service-dot-airqo-250220.uc.r.appspot.com/api/v1/report/save_monthly_report",
         {
           user_id: this.state.user_id,
           report_name: this.state.report_name,
-          report_body: report_body
+          report_body: report_body,
         },
         {
           headers: {
-            'Content-Type': 'application/json'
-          }
+            "Content-Type": "application/json",
+          },
         }
       )
-      .then(res => {
+      .then((res) => {
         console.log(res);
         this.setState({ response_msg: res.data.message });
         this.setState({ openConfirm: true }); //
       })
-      .catch(e => console.log(e));
+      .catch((e) => console.log(e));
   };
 
   // This deals with save report dialog box
@@ -143,7 +143,7 @@ class Main extends Component {
     this.setState({ openSave: false });
   };
   // hooks the monthly report textfield input
-  changeHandler = e => {
+  changeHandler = (e) => {
     this.setState({ [e.target.name]: e.target.value });
   };
 
@@ -157,15 +157,15 @@ class Main extends Component {
   handlePrevSavedOpen = () => {
     axios
       .get(
-        'https://analytcs-bknd-service-dot-airqo-250220.uc.r.appspot.com/api/v1/report/get_monthly_report/' +
+        "https://analytcs-bknd-service-dot-airqo-250220.uc.r.appspot.com/api/v1/report/get_monthly_report/" +
           this.state.user_id
       )
-      .then(res => {
+      .then((res) => {
         this.setState({ saved_report: res.data });
         console.log(res.data);
         //console.log(this.state, "current user: ", this.props.auth.user._id);
       })
-      .catch(e => {
+      .catch((e) => {
         console.log(e);
       });
     this.setState({ openPrevSaved: true });
@@ -180,7 +180,7 @@ class Main extends Component {
     this.setState({
       editorState: EditorState.createWithContent(
         convertFromRaw(JSON.parse(JSON.stringify(report_body)))
-      )
+      ),
     });
     this.setState({ isDraftSelected: true });
     this.setState({ report_name: report_name });
@@ -193,38 +193,49 @@ class Main extends Component {
     // make api call to save report
     axios
       .delete(
-        'https://analytcs-bknd-service-dot-airqo-250220.uc.r.appspot.com/api/v1/report/delete_monthly_report/' +
+        "https://analytcs-bknd-service-dot-airqo-250220.uc.r.appspot.com/api/v1/report/delete_monthly_report/" +
           report_name
       )
-      .then(res => {
+      .then((res) => {
         console.log(res.data);
         this.setState({ response_msg: res.data.message });
         this.setState({ openConfirm: true }); //
       })
-      .catch(e => console.log(e));
+      .catch((e) => console.log(e));
+
+    // after success delete, return to default report template
+    this.setState({
+      editorState: EditorState.createWithContent(
+        convertFromRaw(
+          JSON.parse(JSON.stringify(this.state.report_body_default))
+        )
+      ),
+    });
+
+    this.setState({ isDraftSelected: false });
   };
 
   // handle updating draft report
   onUpdateDraft = () => {
     axios
       .post(
-        'https://analytcs-bknd-service-dot-airqo-250220.uc.r.appspot.com/api/v1/report/update_monthly_report/' +
+        "https://analytcs-bknd-service-dot-airqo-250220.uc.r.appspot.com/api/v1/report/update_monthly_report/" +
           this.state.report_name,
         {
-          report_body: convertToRaw(this.state.editorState.getCurrentContent())
+          report_body: convertToRaw(this.state.editorState.getCurrentContent()),
         },
         {
           headers: {
-            'Content-Type': 'application/json'
-          }
+            "Content-Type": "application/json",
+          },
         }
       )
-      .then(res => {
+      .then((res) => {
         console.log(res.data);
         this.setState({ response_msg: res.data.message });
         this.setState({ openConfirm: true }); //
       })
-      .catch(e => console.log(e));
+      .catch((e) => console.log(e));
   };
 
   //handle cancellation of report update
@@ -234,7 +245,7 @@ class Main extends Component {
         convertFromRaw(
           JSON.parse(JSON.stringify(this.state.report_body_default))
         )
-      )
+      ),
     });
 
     this.setState({ isDraftSelected: false });
@@ -244,15 +255,15 @@ class Main extends Component {
   render() {
     const { editorState } = this.state;
     const editor = {
-      height: 'auto',
-      width: '210mm',
-      margin: '0 auto',
-      textAlign: 'justify'
+      height: "auto",
+      width: "210mm",
+      margin: "0 auto",
+      textAlign: "justify",
     };
     const delete_report = {
-      paddingLeft: '3em',
-      color: 'red',
-      display: 'block'
+      paddingLeft: "3em",
+      color: "red",
+      display: "block",
     };
     console.log(this.state.user_id);
 
@@ -260,11 +271,7 @@ class Main extends Component {
       <div>
         {!this.state.isDraftSelected && (
           <div>
-            <Tooltip
-              title="Save draft report"
-              placement="right"
-              arrow
-            >
+            <Tooltip title="Save draft report" placement="right" arrow>
               <Button
                 color="primary"
                 variant="contained"
@@ -272,7 +279,7 @@ class Main extends Component {
                 onClick={this.handleSaveClick}
                 className="print"
               >
-                <style>{'@media print {.print{display: none;}}'}</style>
+                <style>{"@media print {.print{display: none;}}"}</style>
                 {/* save draft */}
               </Button>
             </Tooltip>
@@ -280,11 +287,7 @@ class Main extends Component {
         )}
         {this.state.isDraftSelected && (
           <div>
-            <Tooltip
-              title="Update draft report"
-              placement="right"
-              arrow
-            >
+            <Tooltip title="Update draft report" placement="right" arrow>
               <Button
                 color="primary"
                 variant="contained"
@@ -292,7 +295,7 @@ class Main extends Component {
                 onClick={this.onUpdateDraft}
                 className="print"
               >
-                <style>{'@media print {.print{display: none;}}'}</style>
+                <style>{"@media print {.print{display: none;}}"}</style>
                 {/* Load Draft */}
               </Button>
             </Tooltip>
@@ -300,11 +303,7 @@ class Main extends Component {
         )}
         {this.state.isDraftSelected && (
           <div>
-            <Tooltip
-              title="Go back to default report"
-              placement="right"
-              arrow
-            >
+            <Tooltip title="Go back to default report" placement="right" arrow>
               <Button
                 color="primary"
                 variant="contained"
@@ -312,18 +311,14 @@ class Main extends Component {
                 onClick={this.onUpdateDraftCancel}
                 className="print"
               >
-                <style>{'@media print {.print{display: none;}}'}</style>
+                <style>{"@media print {.print{display: none;}}"}</style>
                 {/* Load Draft */}
               </Button>
             </Tooltip>
           </div>
         )}
         <div>
-          <Tooltip
-            title="Open previous report"
-            placement="right"
-            arrow
-          >
+          <Tooltip title="Open previous report" placement="right" arrow>
             <Button
               color="primary"
               variant="contained"
@@ -331,7 +326,7 @@ class Main extends Component {
               onClick={this.handlePrevSavedOpen}
               className="print"
             >
-              <style>{'@media print {.print{display: none;}}'}</style>
+              <style>{"@media print {.print{display: none;}}"}</style>
               {/* Load Draft */}
             </Button>
           </Tooltip>
@@ -343,7 +338,7 @@ class Main extends Component {
             onEditorStateChange={this.onEditorStateChange}
             toolbarClassName="hidden-on-print"
             toolbar={{
-              inline: { inDropdown: true, className: 'hidden-on-print' },
+              inline: { inDropdown: true, className: "hidden-on-print" },
               list: { inDropdown: true },
               textAlign: { inDropdown: true },
               link: { inDropdown: true },
@@ -351,11 +346,11 @@ class Main extends Component {
               image: {
                 uploadCallback: this.uploadImageCallBack.bind(this),
                 alt: { present: false, mandatory: false },
-                previewImage: true
-              }
+                previewImage: true,
+              },
             }}
           />
-          <style>{'@media print {.hidden-on-print{display: none;}}'}</style>
+          <style>{"@media print {.hidden-on-print{display: none;}}"}</style>
         </div>
         <div>
           {/* Dialog for report */}
@@ -383,16 +378,10 @@ class Main extends Component {
               />
             </DialogContent>
             <DialogActions>
-              <Button
-                onClick={this.handleSaveClose}
-                color="primary"
-              >
+              <Button onClick={this.handleSaveClose} color="primary">
                 Cancel
               </Button>
-              <Button
-                onClick={this.saveReport}
-                color="primary"
-              >
+              <Button onClick={this.saveReport} color="primary">
                 Save
               </Button>
             </DialogActions>
@@ -411,10 +400,7 @@ class Main extends Component {
               </DialogContentText>
             </DialogContent>
             <DialogActions>
-              <Button
-                onClick={this.handleConfirmClose}
-                color="primary"
-              >
+              <Button onClick={this.handleConfirmClose} color="primary">
                 OK
               </Button>
             </DialogActions>
@@ -435,39 +421,40 @@ class Main extends Component {
               <DialogContentText id="alert-dialog-description">
                 <List>
                   {this.state.saved_report != null ? (
-                    this.state.saved_report.map(s => (
+                    this.state.saved_report.map((s) => (
                       <div>
-                        <ListItem
-                          key={s._id}
-                          href="#"
-                          onClick={this.onDraftReportSelected.bind(
-                            this,
-                            s.report_name,
-                            s.report_body
-                          )}
-                          component="a"
-                        >
+                        <ListItem>
                           <ListItemAvatar>
                             <Avatar>
                               <DescriptionIcon />
                             </Avatar>
                           </ListItemAvatar>
-                          <ListItemText
-                            primary={s.report_name}
-                            secondary={s.report_date}
-                          />
+                          <a
+                            key={s._id}
+                            href="#"
+                            onClick={this.onDraftReportSelected.bind(
+                              this,
+                              s.report_name,
+                              s.report_body
+                            )}
+                          >
+                            <ListItemText
+                              primary={s.report_name}
+                              secondary={s.report_date}
+                            />
+                          </a>
+                          <a
+                            href="#"
+                            style={delete_report}
+                            onClick={this.onDraftDelete.bind(
+                              this,
+                              s._id,
+                              s.report_name
+                            )}
+                          >
+                            <DeleteIcon />
+                          </a>
                         </ListItem>
-                        <a
-                          href="#"
-                          style={delete_report}
-                          onClick={this.onDraftDelete.bind(
-                            this,
-                            s._id,
-                            s.report_name
-                          )}
-                        >
-                          <DeleteIcon />
-                        </a>
                       </div>
                     ))
                   ) : (
@@ -487,10 +474,7 @@ class Main extends Component {
               </DialogContentText>
             </DialogContent>
             <DialogActions>
-              <Button
-                onClick={this.handlePrevSavedClose}
-                color="primary"
-              >
+              <Button onClick={this.handlePrevSavedClose} color="primary">
                 Close
               </Button>
             </DialogActions>
