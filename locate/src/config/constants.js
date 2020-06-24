86bbe9acf02c129eafe53e903bdfa5832d1119d3
Martin <bbaalmart@gmail.com>
2020-06-24 09:30:06 +0300

    Mechanism for switching environments

--- locate/src/config/constants.js
+++ locate/src/config/constants.js
@@ -1,34 +1,49 @@
 const prodConfig = {
-    VERIFY_TOKEN_URI: "https://airqo-250220.uc.r.appspot.com/api/v1/users/reset",
-    UPDATE_PWD_URI: "https://airqo-250220.uc.r.appspot.com/api/v1/users/updatePasswordViaEmail",
-    FORGOT_PWD_URI: "https://airqo-250220.uc.r.appspot.com/api/v1/users/forgotPassword",
-    LOGIN_USER_URI: "https://airqo-250220.uc.r.appspot.com/api/v1/users/loginUser",
-    REGISTER_USER_URI: "https://airqo-250220.uc.r.appspot.com/api/v1/users/registerUser",
-    REGISTER_CANDIDATE_URI: "https://airqo-250220.uc.r.appspot.com/api/v1/users/registerCandidate",
-    REJECT_USER_URI: "https://airqo-250220.uc.r.appspot.com/api/v1/users/deny",
-    ACCEPT_USER_URI: "https://airqo-250220.uc.r.appspot.com/api/v1/users/accept",
-    GET_USERS_URI: "https://airqo-250220.uc.r.appspot.com/api/v1/users/",
+  VERIFY_TOKEN_URI: "https://airqo-250220.uc.r.appspot.com/api/v1/users/reset",
+  UPDATE_PWD_URI:
+    "https://airqo-250220.uc.r.appspot.com/api/v1/users/updatePasswordViaEmail",
+  FORGOT_PWD_URI:
+    "https://airqo-250220.uc.r.appspot.com/api/v1/users/forgotPassword",
+  LOGIN_USER_URI:
+    "https://airqo-250220.uc.r.appspot.com/api/v1/users/loginUser",
+  REGISTER_USER_URI:
+    "https://airqo-250220.uc.r.appspot.com/api/v1/users/registerUser",
+  REGISTER_CANDIDATE_URI:
+    "https://airqo-250220.uc.r.appspot.com/api/v1/users/registerCandidate",
+  REJECT_USER_URI: "https://airqo-250220.uc.r.appspot.com/api/v1/users/deny",
+  ACCEPT_USER_URI: "https://airqo-250220.uc.r.appspot.com/api/v1/users/accept",
+  GET_USERS_URI: "https://airqo-250220.uc.r.appspot.com/api/v1/users/",
+  ADD_MAINTENANCE_URI: "",
+  GET_MAINTENANCE_URI: "",
+  GET_ISSUES_URI: "",
+  ADD_ISSUES_URI: "",
 };
 
 const devConfig = {
-    VERIFY_TOKEN_URI: "http://localhost:3000/api/v1/users/reset",
-    UPDATE_PWD_URI: "http://localhost:3000/api/v1/users/updatePasswordViaEmail",
-    FORGOT_PWD_URI: "http://localhost:3000/api/v1/users/forgotPassword",
-    LOGIN_USER_URI: "http://localhost:3000/api/v1/users/loginUser",
-    REGISTER_USER_URI: "http://localhost:3000/api/v1/users/registerUser",
-    REGISTER_CANDIDATE_URI: "http://localhost:3000/api/v1/users/registerCandidate",
-    REJECT_USER_URI: "http://localhost:3000/api/v1/users/deny",
-    ACCEPT_USER_URI: "http://localhost:3000/api/v1/users/accept",
-    GET_USERS_URI: "http://localhost:3000/api/v1/users/",
-}
+  VERIFY_TOKEN_URI: "http://localhost:3000/api/v1/users/reset",
+  UPDATE_PWD_URI: "http://localhost:3000/api/v1/users/updatePasswordViaEmail",
+  FORGOT_PWD_URI: "http://localhost:3000/api/v1/users/forgotPassword",
+  LOGIN_USER_URI: "http://localhost:3000/api/v1/users/loginUser",
+  REGISTER_USER_URI: "http://localhost:3000/api/v1/users/registerUser",
+  REGISTER_CANDIDATE_URI:
+    "http://localhost:3000/api/v1/users/registerCandidate",
+  REJECT_USER_URI: "http://localhost:3000/api/v1/users/deny",
+  ACCEPT_USER_URI: "http://localhost:3000/api/v1/users/accept",
+  GET_USERS_URI: "http://localhost:3000/api/v1/users/",
+  ADD_MAINTENANCE_URI:
+    "http://localhost:3000/api/v1/data/channels/maintenance/add",
+  GET_MAINTENANCE_URI: "http://localhost:3000/api/v1/data/channels/maintenance",
+  GET_ISSUES_URI: "http://localhost:3000/api/v1/data/channels/issues",
+  ADD_ISSUES_URI: "http://localhost:3000/api/v1/data/channels/issues/add",
+};
 
 function runConfig(env) {
-    switch (env) {
-        case "development":
-            return devConfig;
-        default:
-            return prodConfig;
-    }
+  switch (env) {
+    case "development":
+      return devConfig;
+    default:
+      return prodConfig;
+  }
 }
 
-export default {...runConfig(process.env.NODE_ENV) };
+export default { ...runConfig(process.env.NODE_ENV) };
