b256ec68ea52249403e6f888341eec1cbc56c91a
Sserunjogi Richard <sserurich@gmail.com>
2020-06-29 18:05:49 +0300

    resolved merge conflicts in analytics/src/views/Reporttemplate/main.js: accepted incoming changes from master

--- analytics/src/views/ReportTemplate/Main.js
+++ analytics/src/views/ReportTemplate/Main.js
@@ -1,40 +1,3 @@
-<<<<<<< HEAD
-import React, { Component } from "react";
-import { render } from "react-dom";
-import {
-  EditorState,
-  convertFromRaw,
-  convertToRaw,
-  ContentState,
-  convertFromHTML,
-} from "draft-js";
-import { Divider, Drawer, Button, Grid } from "@material-ui/core";
-import SaveIcon from "@material-ui/icons/Save";
-import CloudDownloadIcon from "@material-ui/icons/CloudDownload";
-import { Editor } from "react-draft-wysiwyg";
-import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
-import "../../assets/scss/report.css";
-import axios from "axios";
-
-// function uploadImageCallBack(file) {
-//   return new Promise((resolve, reject) => {
-//     const xhr = new XMLHttpRequest();
-//     xhr.open("POST", "https://api.imgur.com/3/image");
-//     xhr.setRequestHeader("Authorization", "Client-ID XXXXX");
-//     const data = new FormData();
-//     data.append("image", file);
-//     xhr.send(data);
-//     xhr.addEventListener("load", () => {
-//       const response = JSON.parse(xhr.responseText);
-//       resolve(response);
-//     });
-//     xhr.addEventListener("error", () => {
-//       const error = JSON.parse(xhr.responseText);
-//       reject(error);
-//     });
-//   });
-// }
-=======
 import React, { Component } from 'react';
 import { EditorState, convertFromRaw, convertToRaw } from 'draft-js';
 import {
@@ -62,22 +25,12 @@ import { Editor } from 'react-draft-wysiwyg';
 import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css';
 import '../../assets/scss/report.css';
 import axios from 'axios';
->>>>>>> master
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       editorState: EditorState.createEmpty(),
-<<<<<<< HEAD
-    };
-  }
-
-  onEditorStateChange = (editorState) => {
-    // console.log(editorState)
-    this.setState({
-      editorState,
-=======
       user_id: this.props.user_id,
       openPrevSaved: false,
       isDraftSelected: false,
@@ -106,29 +59,10 @@ class Main extends Component {
     // console.log(editorState)
     this.setState({
       editorState
->>>>>>> master
     });
   };
 
   componentDidMount() {
-<<<<<<< HEAD
-    axios.get("127.0.0.1:4000/api/v1/report/get_default_report_template").then().then();
-  }
-
-  uploadImageCallBack = (file) => {
-    return new Promise((resolve, reject) => {
-      const xhr = new XMLHttpRequest(); // eslint-disable-line no-undef
-      xhr.open("POST", "https://api.imgur.com/3/image");
-      xhr.setRequestHeader("Authorization", "Client-ID 8d26ccd12712fca");
-      const data = new FormData(); // eslint-disable-line no-undef
-      data.append("image", file);
-      xhr.send(data);
-      xhr.addEventListener("load", () => {
-        const response = JSON.parse(xhr.responseText);
-        resolve(response);
-      });
-      xhr.addEventListener("error", () => {
-=======
     // local: http://127.0.0.1:4000
     axios
       .get(
@@ -164,54 +98,12 @@ class Main extends Component {
         resolve(response);
       });
       xhr.addEventListener('error', () => {
->>>>>>> master
         const error = JSON.parse(xhr.responseText);
         reject(error);
       });
     });
   };
 
-<<<<<<< HEAD
-  render() {
-    const { editorState } = this.state;
-    const editor = {
-      height: "auto",
-      width: "210mm",
-      margin: "0 auto",
-      textAlign: "justify",
-    };
-
-    const rawState = JSON.stringify(
-      convertToRaw(editorState.getCurrentContent())
-    );
-    console.log(rawState);
-    return (
-      <>
-        <div>
-          <Button
-            color="primary"
-            variant="contained"
-            endIcon={<SaveIcon />}
-            onClick=""
-            className="print"
-          >
-            <style>{"@media print {.print{display: none;}}"}</style>
-            {/* Save Draft */}
-          </Button>
-        </div>
-        <div>
-          <Button
-            color="primary"
-            variant="contained"
-            endIcon={<CloudDownloadIcon />}
-            onClick=""
-            className="print"
-          >
-            <style>{"@media print {.print{display: none;}}"}</style>
-            {/* Load Draft */}
-          </Button>
-        </div>
-=======
   // save monthly report
   saveReport = () => {
     // hide the save draft report dialog
@@ -439,18 +331,13 @@ class Main extends Component {
           </Tooltip>
         </div>
 
->>>>>>> master
         <div style={editor}>
           <Editor
             editorState={editorState}
             onEditorStateChange={this.onEditorStateChange}
             toolbarClassName="hidden-on-print"
             toolbar={{
-<<<<<<< HEAD
-              inline: { inDropdown: true, className: "hidden-on-print" },
-=======
               inline: { inDropdown: true, className: 'hidden-on-print' },
->>>>>>> master
               list: { inDropdown: true },
               textAlign: { inDropdown: true },
               link: { inDropdown: true },
@@ -458,15 +345,6 @@ class Main extends Component {
               image: {
                 uploadCallback: this.uploadImageCallBack.bind(this),
                 alt: { present: false, mandatory: false },
-<<<<<<< HEAD
-                previewImage: true,
-              },
-            }}
-          />
-          <style>{"@media print {.hidden-on-print{display: none;}}"}</style>
-        </div>
-      </>
-=======
                 previewImage: true
               }
             }}
@@ -597,7 +475,6 @@ class Main extends Component {
           </Dialog>
         </div>
       </div>
->>>>>>> master
     );
   }
 }
