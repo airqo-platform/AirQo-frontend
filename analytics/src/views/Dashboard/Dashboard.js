13a58cbf457fb152a7a9150e2edf849f5b9554ca
Priscilla-AL <priscilla@airqo.net>
2020-07-01 12:20:28 +0300

    resolving merge conflicts- update dashboard

--- analytics/src/views/Dashboard/Dashboard.js
+++ analytics/src/views/Dashboard/Dashboard.js
@@ -1,25 +1,10 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/styles';
-import {
-  Grid,
-  Card,
-  CardContent,
-  CardHeader,
-  Button,
-  Divider,
-  CardActions
-} from '@material-ui/core';
+import { Grid, Card, CardContent, CardHeader, Button, Divider, CardActions } from '@material-ui/core';
 import { Line, Bar } from 'react-chartjs-2';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
-import {
-  Pm25Levels,
-  Map,
-  CustomisableChart,
-  PollutantCategory,
-  ExceedancesChart,
-  TotalProfit
-} from './components';
+import { Pm25Levels, Map, CustomisableChart, PollutantCategory, ExceedancesChart, TotalProfit } from './components';
 import { useEffect, useState } from 'react';
 import 'chartjs-plugin-annotation';
 import palette from 'theme/palette';
@@ -27,14 +12,17 @@ import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
 //import Legend from './components/Map/Legend'
 import axios from 'axios';
+ 
 
 const useStyles = makeStyles(theme => ({
   root: {
     padding: theme.spacing(4)
   },
-  chartCard: {},
+  chartCard:{
+
+  },
   differenceIcon: {
-    color: theme.palette.text.secondary
+    color: theme.palette.text.secondary,
   },
   chartContainer: {
     height: 180,
@@ -45,123 +33,85 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+
 const Dashboard = props => {
   const classes = useStyles();
-  const {
-    className,
-    staticContext,
-    mappedAuth,
-    mappeduserState,
-    mappedErrors,
-    ...rest
-  } = props;
-  const { user } = mappedAuth;
+  const { className, staticContext, ...rest } = props;
 
-  function appendLeadingZeroes(n) {
-    if (n <= 9) {
+  function appendLeadingZeroes(n){
+    if(n <= 9){
       return '0' + n;
     }
-    return n;
+    return n
   }
 
-  let todaysDate = new Date();
-  const dateValue = appendLeadingZeroes(
-    todaysDate.getDate() +
-      '/' +
-      appendLeadingZeroes(todaysDate.getMonth() + 1) +
-      '/' +
-      todaysDate.getFullYear()
-  );
-
+  let todaysDate = new Date()
+  const dateValue = appendLeadingZeroes(todaysDate.getDate() +'/'+appendLeadingZeroes(todaysDate.getMonth()+1)+'/'+ todaysDate.getFullYear())
+ 
   const [backgroundColors, setBackgroundColors] = useState([]);
 
-  const [
-    pm25CategoriesLocationCount,
-    setPm25CategoriesLocationCount
-  ] = useState([]);
-
-  // useEffect(() => {
-  //   try{
-  //       props.fetchDefaults(user._id);
-  //   }
-  //   catch(e){
-  //   console.log(e);
-  //   }
-  //     }, []);
-
-  //   useEffect(() => {
-  // return ()=>{
-  //   props.fetchDefaults(user._id);
-  // }
-
-  //   }, []);
+  const [pm25CategoriesLocationCount, setPm25CategoriesLocationCount] = useState([]);
 
   useEffect(() => {
     // axios.get('https://analytcs-bknd-service-dot-airqo-250220.uc.r.appspot.com/api/v1/dashboard/locations/pm25categorycount?organisation_name=KCCA')
-    axios
-      .get(
-        'http://127.0.0.1:5000/api/v1/dashboard/locations/pm25categorycount?organisation_name=KCCA'
-      )
+    axios.get('http://127.0.0.1:5000/api/v1/dashboard/locations/pm25categorycount?organisation_name=KCCA')
       .then(res => res.data)
-      .then(data => {
-        setPm25CategoriesLocationCount(data);
-        console.log(data);
-        //console.log(data.pm25_categories)
+      .then((data) => {        
+        setPm25CategoriesLocationCount(data);        
+        console.log(data)  
+        //console.log(data.pm25_categories)           
       })
-      .catch(e => {
-        console.log(e);
-      });
+      .catch(console.log)
   }, []);
 
-  const [locations, setLocations] = useState([]);
+  const [locations,setLocations] = useState([]);
 
   useEffect(() => {
-    fetch(
-      'https://analytcs-bknd-service-dot-airqo-250220.uc.r.appspot.com/api/v1/dashboard/historical/daily/devices'
-    )
-      //fetch('http://127.0.0.1:5000/api/v1/dashboard/historical/daily/devices')
+    fetch('https://analytcs-bknd-service-dot-airqo-250220.uc.r.appspot.com/api/v1/dashboard/historical/daily/devices')
+    //fetch('http://127.0.0.1:5000/api/v1/dashboard/historical/daily/devices')
       .then(res => res.json())
-      .then(locationsData => {
-        setLocations(locationsData.results);
+      .then((locationsData) => {        
+        setLocations(locationsData.results);        
       })
-      .catch(e => {
-        console.log(e);
-      });
-  }, []);
+      .catch(console.log)
+  },[]);
 
+ 
   const locationsGraphData = {
     labels: locations.labels,
     datasets: [
       {
         label: 'PM2.5(µg/m3)',
         data: locations.average_pm25_values,
-        fill: false, // Don't fill area under the line
-        borderColor: palette.primary.main, // Line color
+        fill: false,          // Don't fill area under the line
+        borderColor: palette.primary.main , // Line color
         backgroundColor: locations.background_colors //palette.primary.main,
       }
     ]
-  };
+  }
+  
 
-  const options_main = {
+
+  
+  const options_main= {
     annotation: {
-      annotations: [
-        {
-          type: 'line',
-          mode: 'horizontal',
-          scaleID: 'y-axis-0',
-          value: 25,
-          borderColor: palette.text.secondary,
-          borderWidth: 2,
-          label: {
-            enabled: true,
-            content: 'WHO AQG',
-            //backgroundColor: palette.white,
-            titleFontColor: palette.text.primary,
-            bodyFontColor: palette.text.primary,
-            position: 'right'
-          }
-        }
-      ]
+      annotations: [{
+        type: 'line',
+        mode: 'horizontal',
+        scaleID: 'y-axis-0',
+        value: 25,
+        borderColor: palette.text.secondary,
+        borderWidth: 2,
+        label: {
+          enabled: true,
+          content: 'WHO AQG',
+          //backgroundColor: palette.white,
+          titleFontColor: palette.text.primary,
+          bodyFontColor: palette.text.primary,
+          position:'right'
+        },
+        
+      }]
     },
     responsive: true,
     maintainAspectRatio: false,
@@ -194,7 +144,7 @@ const Dashboard = props => {
           barPercentage: 0.5,
           categoryPercentage: 0.5,
           ticks: {
-            fontColor: palette.text.secondary
+            fontColor: palette.text.secondary,
             //fontSize:10
           },
           gridLines: {
@@ -205,6 +155,7 @@ const Dashboard = props => {
             display: true,
             labelString: 'Locations'
           }
+
         }
       ],
       yAxes: [
@@ -235,120 +186,173 @@ const Dashboard = props => {
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={4}>
-        <Grid item lg={2} sm={6} xl={2} xs={12}>
+      <Grid
+        container
+        spacing={4}
+      >
+        <Grid
+          item
+          lg={2}
+          sm={6}
+          xl={2}
+          xs={12}
+        >
           <PollutantCategory
             pm25level="Good"
-            pm25levelCount={
-              typeof pm25CategoriesLocationCount != 'undefined' &&
-              pm25CategoriesLocationCount != null &&
-              pm25CategoriesLocationCount.length > 0
-                ? pm25CategoriesLocationCount[0]['locations_with_category_good']
-                    .category_count
-                : ''
-            }
+            pm25levelCount={typeof pm25CategoriesLocationCount != 'undefined' && 
+            pm25CategoriesLocationCount !=null && pm25CategoriesLocationCount.length >0 ?pm25CategoriesLocationCount[0]['locations_with_category_good'].category_count:''}
             iconClass="pm25Good"
           />
-        </Grid>
-        <Grid item lg={2} sm={6} xl={2} xs={12}>
+        </Grid>       
+        <Grid
+          item
+          lg={2}
+          sm={6}
+          xl={2}
+          xs={12}
+          
+        >
           <PollutantCategory
             pm25level="Moderate"
-            pm25levelCount={
-              typeof pm25CategoriesLocationCount != 'undefined' &&
-              pm25CategoriesLocationCount != null &&
-              pm25CategoriesLocationCount.length > 0
-                ? pm25CategoriesLocationCount[1][
-                    'locations_with_category_moderate'
-                  ].category_count
-                : ''
-            }
+            pm25levelCount={typeof pm25CategoriesLocationCount != 'undefined' && 
+            pm25CategoriesLocationCount !=null && pm25CategoriesLocationCount.length >0 ? pm25CategoriesLocationCount[1]['locations_with_category_moderate'].category_count:''}
             iconClass="pm25Moderate"
           />
         </Grid>
-        <Grid item lg={2} sm={6} xl={2} xs={12}>
+        <Grid
+          item
+          lg={2}
+          sm={6}
+          xl={2}
+          xs={12}
+        >
           <PollutantCategory
             pm25level="UHFSG"
-            pm25levelCount={
-              typeof pm25CategoriesLocationCount != 'undefined' &&
-              pm25CategoriesLocationCount != null &&
-              pm25CategoriesLocationCount.length > 0
-                ? pm25CategoriesLocationCount[2].locations_with_category_UH4SG
-                    .category_count
-                : ''
-            }
+            pm25levelCount={typeof pm25CategoriesLocationCount != 'undefined' && 
+            pm25CategoriesLocationCount !=null && pm25CategoriesLocationCount.length >0 ?pm25CategoriesLocationCount[2].locations_with_category_UH4SG.category_count:''}
             iconClass="pm25UH4SG"
           />
         </Grid>
 
-        <Grid item lg={2} sm={6} xl={2} xs={12}>
+        <Grid
+          item
+          lg={2}
+          sm={6}
+          xl={2}
+          xs={12}
+        >
           <PollutantCategory
             pm25level="Unhealthy"
-            pm25levelCount={
-              typeof pm25CategoriesLocationCount != 'undefined' &&
-              pm25CategoriesLocationCount != null &&
-              pm25CategoriesLocationCount.length > 0
-                ? pm25CategoriesLocationCount[3]
-                    .locations_with_category_unhealth.category_count
-                : ''
-            }
+            pm25levelCount={typeof pm25CategoriesLocationCount != 'undefined' && 
+            pm25CategoriesLocationCount !=null && pm25CategoriesLocationCount.length >0 ?pm25CategoriesLocationCount[3].locations_with_category_unhealth.category_count:''}
             iconClass="pm25UnHealthy"
           />
         </Grid>
 
-        <Grid item lg={2} sm={6} xl={2} xs={12}>
+        <Grid
+          item
+          lg={2}
+          sm={6}
+          xl={2}
+          xs={12}
+        >
           <PollutantCategory
             pm25level="Very Unhealthy"
-            pm25levelCount={
-              typeof pm25CategoriesLocationCount != 'undefined' &&
-              pm25CategoriesLocationCount != null &&
-              pm25CategoriesLocationCount.length > 0
-                ? pm25CategoriesLocationCount[4]
-                    .locations_with_category_very_unhealthy.category_count
-                : ''
-            }
+            pm25levelCount={typeof pm25CategoriesLocationCount != 'undefined' && 
+            pm25CategoriesLocationCount !=null && pm25CategoriesLocationCount.length >0 ?pm25CategoriesLocationCount[4].locations_with_category_very_unhealthy.category_count:''}
             iconClass="pm25VeryUnHealthy"
           />
         </Grid>
-        <Grid item lg={2} sm={6} xl={2} xs={12}>
+        <Grid
+          item
+          lg={2}
+          sm={6}
+          xl={2}
+          xs={12}
+        >
           <PollutantCategory
             pm25level="Hazardous"
-            pm25levelCount={
-              typeof pm25CategoriesLocationCount != 'undefined' &&
-              pm25CategoriesLocationCount != null &&
-              pm25CategoriesLocationCount.length > 0
-                ? pm25CategoriesLocationCount[5]
-                    .locations_with_category_hazardous.category_count
-                : ''
-            }
+            pm25levelCount={typeof pm25CategoriesLocationCount != 'undefined' && 
+            pm25CategoriesLocationCount !=null && pm25CategoriesLocationCount.length >0 ?pm25CategoriesLocationCount[5].locations_with_category_hazardous.category_count:''}
             iconClass="pm25Harzadous"
           />
         </Grid>
-        <Grid item lg={6} md={6} sm={12} xl={6} xs={12} container spacing={2}>
-          <Grid item lg={12} md={12} sm={12} xl={12} xs={12}>
-            <Card {...rest} className={clsx(classes.chartCard)}>
-              <CardHeader
+        <Grid
+          item
+          lg={6}
+          md={6}
+          sm={12}
+          xl={6}
+          xs={12}
+          container
+          spacing={2}
+        >
+          <Grid
+            item
+            lg={12}
+            md={12}
+            sm={12}
+            xl={12}
+            xs={12}
+          >
+            <Card
+              {...rest}
+              className={clsx(classes.chartCard)}
+            >
+              <CardHeader              
                 title={`Mean Daily PM2.5 for Past 28 Days From ${dateValue}`}
               />
               <Divider />
               <CardContent>
+                
                 <div className={classes.chartContainer}>
-                  <Bar data={locationsGraphData} options={options_main} />
+                  <Bar
+                    data={locationsGraphData}
+                    options={options_main}
+                  />
                 </div>
               </CardContent>
-            </Card>
-          </Grid>
-          <Grid item lg={12} md={12} sm={12} xl={12} xs={12}>
-            <ExceedancesChart
-              className={clsx(classes.chartCard)}
+              
+            </Card>  
+          </Grid> 
+          <Grid
+            item
+            lg={12}
+            md={12}
+            sm={12}
+            xl={12}
+            xs={12}
+          >
+            <ExceedancesChart  
+              className={clsx(classes.chartCard)} 
               chartContainer={classes.chartContainer}
-            />
-          </Grid>
+            />  
+
+          </Grid>      
         </Grid>
 
-        <Grid item lg={6} md={6} sm={12} xl={6} xs={12}>
-          <Grid item lg={12} sm={12} xl={12} xs={12}>
-            <Map />
-          </Grid>
+        <Grid
+          item
+          lg={6}
+          md={6}
+          sm={12}
+          xl={6}
+          xs={12}
+          
+        >
+                 
+         
+          <Grid
+            item
+            lg={12}
+            sm={12}
+            xl={12}
+            xs={12}
+          >
+            <Map/>
+          </Grid>         
+               
+
 
           {/* <Grid
             container
@@ -356,7 +360,8 @@ const Dashboard = props => {
             className='MapCardContent'
           > */}
 
-          {/* <Grid
+          
+            {/* <Grid
               item
               lg={2}
               sm={4}
@@ -446,36 +451,75 @@ const Dashboard = props => {
                 
               />
             </Grid>  */}
-          {/* <p>
+             {/* <p>
               PM <sub>2.5</sub> - Particulate Matter 
             </p> */}
-
+                
           {/* </Grid>  */}
         </Grid>
+        
 
-        <Grid item lg={6} md={6} sm={12} xl={6} xs={12}>
-          <CustomisableChart className={clsx(classes.chartCard)} />
+        <Grid
+          item          
+          lg={6}
+          md={6}
+          sm={12}
+          xl={6}
+          xs={12}
+        >
+          
+          <CustomisableChart className={clsx(classes.chartCard)}/>            
+                                 
         </Grid>
-        <Grid item lg={6} md={6} sm={12} xl={6} xs={12}>
-          <CustomisableChart className={clsx(classes.chartCard)} />
+        <Grid
+          item
+          lg={6}
+          md={6}
+          sm={12}
+          xl={6}
+          xs={12}
+        >
+           <CustomisableChart className={clsx(classes.chartCard)}/>   
         </Grid>
 
-        <Grid item lg={6} md={6} sm={12} xl={6} xs={12}>
-          <CustomisableChart className={clsx(classes.chartCard)} />
-        </Grid>
+        
+    <Grid
+           item
+           lg={6}
+           md={6}
+           sm={12}
+           xl={6}
+           xs={12}
+        >
+          
+          <CustomisableChart className={clsx(classes.chartCard)}/>
+          
+    </Grid>
 
-        <Grid item lg={6} md={6} sm={12} xl={6} xs={12}>
-          <CustomisableChart className={clsx(classes.chartCard)} />
-        </Grid>
+    <Grid
+           item
+           lg={6}
+           md={6}
+           sm={12}
+           xl={6}
+           xs={12}
+        >
+          
+          <CustomisableChart className={clsx(classes.chartCard)}/>
+          
+    </Grid>
+   
+        
       </Grid>
     </div>
   );
 };
 
+
 Dashboard.propTypes = {
-  className: PropTypes.string,
-  mappedAuth: PropTypes.object.isRequired,
-  fetchDefaults: PropTypes.func.isRequired
+  className: PropTypes.string
 };
+
+
 
 export default Dashboard;
