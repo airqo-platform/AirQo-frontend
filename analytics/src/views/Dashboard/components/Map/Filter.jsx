88c12e1e9181666ed39d0e636db057fb53edb39b
Priscilla-AL <priscilla@airqo.net>
2020-07-03 10:51:07 +0300

    Add tooltips to map

--- analytics/src/views/Dashboard/components/Map/Filter.jsx
+++ analytics/src/views/Dashboard/components/Map/Filter.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import ReactTooltip from "react-tooltip";
 
 class Filter extends Component {
   state = {
@@ -52,8 +53,11 @@ class Filter extends Component {
                     checked={this.state.magnitudeFilter === 'Good' }
                     onChange={this.handleMagnitudeChange}
                   />
-                   <div class="control__indicator1">0 - 12</div>
-                   
+                   <div class="control__indicator1" data-tip data-for="good">0 - 12</div>
+                   <ReactTooltip id="good" place="right" effect="solid">
+                   <h3 style={{fontWeight:"normal" }}> GOOD</h3> 
+                   {/* <p>Air Quality is good for everyone</p> */}
+                   </ReactTooltip>                   
                 </label>
                 <label>
                   <input
@@ -63,7 +67,12 @@ class Filter extends Component {
                     checked={this.state.magnitudeFilter === 'Moderate' }
                     onChange={this.handleMagnitudeChange}
                   />
-                  <div class="control__indicator2">12.1 - 35.4</div>
+                  <div class="control__indicator2" data-tip data-for="moderate">12.1 - 35.4</div>
+                  <ReactTooltip id="moderate" place="right" effect="solid" multiline ="true">
+                   <h3 style={{fontWeight:"normal" }}>Moderate</h3> 
+                   {/* <p>Unusually sensitive people: Consider reducing prolonged or heavy exertion</p>
+                   <p>Everyone else: Air pollution poses little or no risk</p> */}
+                   </ReactTooltip> 
                   
                 </label>
                 <label>
@@ -74,8 +83,13 @@ class Filter extends Component {
                     checked={this.state.magnitudeFilter === 'UHFSG' }
                     onChange={this.handleMagnitudeChange}
                   />
-                  <div class="control__indicator3">35.5 - 55.4</div>
-                  
+                  <div class="control__indicator3" data-tip data-for="UHFSG">35.5 - 55.4</div>
+                  <ReactTooltip id="UHFSG" place="right" effect="solid">
+                   <h3 style={{fontWeight:"normal" }}> Unhealthy for sensitive groups</h3> 
+                   {/* <p>Sensitive groups: Reduce prolonged or heavy exertion. It's okay to be active outside, but take more breaks and do less intense activities</p>
+                   <p>People with asthma should follow their asthma action plans and keep quick relief medicine handy</p>
+                   <p>If you have heart disease: Symptoms such as palpitaions, shortness of breath, or unusual fatigue may indicate a serirous problem. If you have any of these, contact your health care provider</p> */}
+                   </ReactTooltip> 
                 </label>
                 <label>
                   <input
@@ -85,8 +99,12 @@ class Filter extends Component {
                     checked={this.state.magnitudeFilter === 'Unhealthy' }
                     onChange={this.handleMagnitudeChange}
                   />
-                  <div class="control__indicator4">55.5 - 150.4</div>
-                  
+                  <div class="control__indicator4" data-tip data-for="unhealthy">55.5 - 150.4</div>
+                  <ReactTooltip id="unhealthy" place="right" effect="solid">
+                   <h3 style={{fontWeight:"normal" }}>Unhealthy</h3> 
+                   {/* <p>Sensitive groups: Avoid prolonged or heavy exertion. Consider moving activities indoors or rescheduling.</p>
+                   <p>Everyone else: Reduce prolonged or heavy exertion. Take more breaks during all outdoor activities.</p> */}
+                   </ReactTooltip> 
                 </label>
                 <label>
                   <input
@@ -96,8 +114,12 @@ class Filter extends Component {
                     checked={this.state.magnitudeFilter === 'VeryUnhealthy' }
                     onChange={this.handleMagnitudeChange}
                   />
-                  <div class="control__indicator5">150.5 - 250.4</div>
-                  
+                  <div class="control__indicator5" data-tip data-for="veryunhealthy">150.5 - 250.4</div>
+                  <ReactTooltip id="veryunhealthy" place="right" effect="solid">
+                   <h3 style={{fontWeight:"normal" }}>Very Unhealthy</h3> 
+                   {/* <p>Sensitive groups: Avoid all physical activities outdoors. Move activities indoors or reschedule to a time when airquality is better</p>
+                   <p>Everyone else: Avoid prolonged or heavy exertion. Consider moving activities indoors or reschedule to a time when airquality is better</p> */}
+                   </ReactTooltip> 
                 </label>
                 <label>
                   <input
@@ -107,8 +129,12 @@ class Filter extends Component {
                     checked={this.state.magnitudeFilter === 'Harzadous'}
                     onChange={this.handleMagnitudeChange}
                   />
-                  <div class="control__indicator6">250.5 - 500</div>
-                  
+                  <div class="control__indicator6" data-tip data-for="harzadous">250.5 - 500</div>
+                  <ReactTooltip id="harzadous" place="right" effect="solid">
+                   <h3 style={{fontWeight:"normal" }}>Harzadous</h3> 
+                   {/* <p>Everyone: Avoid all physical activities outdoors</p>
+                   <p>Sesitive groups: Remain indoors and keeo activity levels low. Follow tips for keeping particle levels low indoors.</p> */}
+                   </ReactTooltip> 
                 </label>
               </form>
             </>
