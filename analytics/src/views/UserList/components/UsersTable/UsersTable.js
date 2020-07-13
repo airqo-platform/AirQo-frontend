8172b5568ddb4f5ad97563b4fa02038e3cb1605c
Martin <bbaalmart@gmail.com>
2020-07-13 14:52:42 +0300

    handling state management for notification alerts on user addition and deletion

--- analytics/src/views/UserList/components/UsersTable/UsersTable.js
+++ analytics/src/views/UserList/components/UsersTable/UsersTable.js
@@ -220,7 +220,6 @@ const UsersTable = props => {
                   <TableCell>Username</TableCell>
                   <TableCell>Role</TableCell>
                   <TableCell>Action</TableCell>
-                  
                 </TableRow>
               </TableHead>
               <TableBody>
@@ -393,7 +392,7 @@ const UsersTable = props => {
             </div>
           )}
           {mappeduserState.successMsg && !mappeduserState.isFetching && (
-            <Button onClick={hideConfirmDialog}>Close</Button>
+            <Button onClick={hideDeleteDialog}>Close</Button>
           )}
         </DialogActions>
       </Dialog>
