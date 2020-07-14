5199d0d5cdddd700f2f79e35c9581baba86c4ad7
danielogen <danielogen@gmail.com>
2020-07-14 11:11:11 +0300

    update the files after branching off from staging

--- analytics/src/config/constants.js
+++ analytics/src/config/constants.js
@@ -1,91 +1,96 @@
 /* eslint-disable */
 const devConfig = {
-  VERIFY_TOKEN_URI: 'http://localhost:3000/api/v1/users/reset/you',
-  UPDATE_PWD_URI: 'http://localhost:3000/api/v1/users/updatePasswordViaEmail',
-  UPDATE_PWD_IN_URI: 'http://localhost:3000/api/v1/users/updatePassword',
-  FORGOT_PWD_URI: 'http://localhost:3000/api/v1/users/forgotPassword',
-  LOGIN_USER_URI: 'http://localhost:3000/api/v1/users/loginUser',
-  REGISTER_USER_URI: 'http://localhost:3000/api/v1/users/registerUser',
+  VERIFY_TOKEN_URI: "http://localhost:3000/api/v1/users/reset/you",
+  UPDATE_PWD_URI: "http://localhost:3000/api/v1/users/updatePasswordViaEmail",
+  UPDATE_PWD_IN_URI: "http://localhost:3000/api/v1/users/updatePassword",
+  FORGOT_PWD_URI: "http://localhost:3000/api/v1/users/forgotPassword",
+  LOGIN_USER_URI: "http://localhost:3000/api/v1/users/loginUser",
+  REGISTER_USER_URI: "http://localhost:3000/api/v1/users/registerUser",
   REGISTER_CANDIDATE_URI:
-    'http://localhost:3000/api/v1/users/register/new/candidate',
-  REJECT_USER_URI: 'http://localhost:3000/api/v1/users/deny',
-  ACCEPT_USER_URI: 'http://localhost:3000/api/v1/users/accept',
-  GET_USERS_URI: 'http://localhost:3000/api/v1/users/',
-  GET_CANDIDATES_URI: 'http://localhost:3000/api/v1/users/candidates/fetch',
-  DEFAULTS_URI: 'http://localhost:3000/api/v1/users/defaults'
+    "http://localhost:3000/api/v1/users/register/new/candidate",
+  REJECT_USER_URI: "http://localhost:3000/api/v1/users/deny",
+  ACCEPT_USER_URI: "http://localhost:3000/api/v1/users/accept",
+  GET_USERS_URI: "http://localhost:3000/api/v1/users/",
+  GET_CANDIDATES_URI: "http://localhost:3000/api/v1/users/candidates/fetch",
+  DEFAULTS_URI: "http://localhost:3000/api/v1/users/defaults",
+  DOWNLOAD_DATA: "http://localhost:5000/api/v1/data/download?type=",
 };
 const testConfig = {
-  VERIFY_TOKEN_URI: 'http://localhost:3000/api/v1/users/reset/you',
-  UPDATE_PWD_URI: 'http://localhost:3000/api/v1/users/updatePasswordViaEmail',
-  UPDATE_PWD_IN_URI: 'http://localhost:3000/api/v1/users/updatePassword',
-  FORGOT_PWD_URI: 'http://localhost:3000/api/v1/users/forgotPassword',
-  LOGIN_USER_URI: 'http://localhost:3000/api/v1/users/loginUser',
-  REGISTER_USER_URI: 'http://localhost:3000/api/v1/users/registerUser',
+  VERIFY_TOKEN_URI: "http://localhost:3000/api/v1/users/reset/you",
+  UPDATE_PWD_URI: "http://localhost:3000/api/v1/users/updatePasswordViaEmail",
+  UPDATE_PWD_IN_URI: "http://localhost:3000/api/v1/users/updatePassword",
+  FORGOT_PWD_URI: "http://localhost:3000/api/v1/users/forgotPassword",
+  LOGIN_USER_URI: "http://localhost:3000/api/v1/users/loginUser",
+  REGISTER_USER_URI: "http://localhost:3000/api/v1/users/registerUser",
   REGISTER_CANDIDATE_URI:
-    'http://localhost:3000/api/v1/users/register/new/candidate',
-  REJECT_USER_URI: 'http://localhost:3000/api/v1/users/deny',
-  ACCEPT_USER_URI: 'http://localhost:3000/api/v1/users/accept',
-  GET_USERS_URI: 'http://localhost:3000/api/v1/users/',
-  GET_CANDIDATES_URI: 'http://localhost:3000/api/v1/users/candidates/fetch',
-  DEFAULTS_URI: 'http://localhost:3000/api/v1/users/defaults'
+    "http://localhost:3000/api/v1/users/register/new/candidate",
+  REJECT_USER_URI: "http://localhost:3000/api/v1/users/deny",
+  ACCEPT_USER_URI: "http://localhost:3000/api/v1/users/accept",
+  GET_USERS_URI: "http://localhost:3000/api/v1/users/",
+  GET_CANDIDATES_URI: "http://localhost:3000/api/v1/users/candidates/fetch",
+  DEFAULTS_URI: "http://localhost:3000/api/v1/users/defaults",
 };
 
 const stageConfig = {
   VERIFY_TOKEN_URI:
-    'https://airqo-250220.uc.r.appspot.com/api/v1/users/reset/you',
+    "https://airqo-250220.uc.r.appspot.com/api/v1/users/reset/you",
   UPDATE_PWD_URI:
-    'https://airqo-250220.uc.r.appspot.com/api/v1/users/updatePasswordViaEmail',
+    "https://airqo-250220.uc.r.appspot.com/api/v1/users/updatePasswordViaEmail",
   UPDATE_PWD_IN_URI:
-    'https://airqo-250220.uc.r.appspot.com/api/v1/users/updatePassword',
+    "https://airqo-250220.uc.r.appspot.com/api/v1/users/updatePassword",
   FORGOT_PWD_URI:
-    'https://airqo-250220.uc.r.appspot.com/api/v1/users/forgotPassword',
+    "https://airqo-250220.uc.r.appspot.com/api/v1/users/forgotPassword",
   LOGIN_USER_URI:
-    'https://airqo-250220.uc.r.appspot.com/api/v1/users/loginUser',
+    "https://airqo-250220.uc.r.appspot.com/api/v1/users/loginUser",
   REGISTER_USER_URI:
-    'https://airqo-250220.uc.r.appspot.com/api/v1/users/registerUser',
+    "https://airqo-250220.uc.r.appspot.com/api/v1/users/registerUser",
   REGISTER_CANDIDATE_URI:
-    'https://airqo-250220.uc.r.appspot.com/api/v1/users/register/new/candidate',
-  REJECT_USER_URI: 'https://airqo-250220.uc.r.appspot.com/api/v1/users/deny',
-  ACCEPT_USER_URI: 'https://airqo-250220.uc.r.appspot.com/api/v1/users/accept',
-  GET_USERS_URI: 'https://airqo-250220.uc.r.appspot.com/api/v1/users/',
+    "https://airqo-250220.uc.r.appspot.com/api/v1/users/register/new/candidate",
+  REJECT_USER_URI: "https://airqo-250220.uc.r.appspot.com/api/v1/users/deny",
+  ACCEPT_USER_URI: "https://airqo-250220.uc.r.appspot.com/api/v1/users/accept",
+  GET_USERS_URI: "https://airqo-250220.uc.r.appspot.com/api/v1/users/",
   GET_CANDIDATES_URI:
-    'https://airqo-250220.uc.r.appspot.com/api/v1/users/candidates/fetch',
-  DEFAULTS_URI: 'https://airqo-250220.uc.r.appspot.com/api/v1/users/defaults'
+    "https://airqo-250220.uc.r.appspot.com/api/v1/users/candidates/fetch",
+  DEFAULTS_URI: "https://airqo-250220.uc.r.appspot.com/api/v1/users/defaults",
+  DOWNLOAD_DATA:
+    "https://airqo-250220.uc.r.appspot.com/api/v1/data/download?type=",
 };
 const prodConfig = {
   VERIFY_TOKEN_URI:
-    'https://airqo-250220.uc.r.appspot.com/api/v1/users/reset/you',
+    "https://airqo-250220.uc.r.appspot.com/api/v1/users/reset/you",
   UPDATE_PWD_URI:
-    'https://airqo-250220.uc.r.appspot.com/api/v1/users/updatePasswordViaEmail',
+    "https://airqo-250220.uc.r.appspot.com/api/v1/users/updatePasswordViaEmail",
   UPDATE_PWD_IN_URI:
-    'https://airqo-250220.uc.r.appspot.com/api/v1/users/updatePassword',
+    "https://airqo-250220.uc.r.appspot.com/api/v1/users/updatePassword",
   FORGOT_PWD_URI:
-    'https://airqo-250220.uc.r.appspot.com/api/v1/users/forgotPassword',
+    "https://airqo-250220.uc.r.appspot.com/api/v1/users/forgotPassword",
   LOGIN_USER_URI:
-    'https://airqo-250220.uc.r.appspot.com/api/v1/users/loginUser',
+    "https://airqo-250220.uc.r.appspot.com/api/v1/users/loginUser",
   REGISTER_USER_URI:
-    'https://airqo-250220.uc.r.appspot.com/api/v1/users/registerUser',
+    "https://airqo-250220.uc.r.appspot.com/api/v1/users/registerUser",
   REGISTER_CANDIDATE_URI:
-    'https://airqo-250220.uc.r.appspot.com/api/v1/users/register/new/candidate',
-  REJECT_USER_URI: 'https://airqo-250220.uc.r.appspot.com/api/v1/users/deny',
-  ACCEPT_USER_URI: 'https://airqo-250220.uc.r.appspot.com/api/v1/users/accept',
-  GET_USERS_URI: 'https://airqo-250220.uc.r.appspot.com/api/v1/users/',
+    "https://airqo-250220.uc.r.appspot.com/api/v1/users/register/new/candidate",
+  REJECT_USER_URI: "https://airqo-250220.uc.r.appspot.com/api/v1/users/deny",
+  ACCEPT_USER_URI: "https://airqo-250220.uc.r.appspot.com/api/v1/users/accept",
+  GET_USERS_URI: "https://airqo-250220.uc.r.appspot.com/api/v1/users/",
   GET_CANDIDATES_URI:
-    'https://airqo-250220.uc.r.appspot.com/api/v1/users/candidates/fetch',
-  DEFAULTS_URI: 'https://airqo-250220.uc.r.appspot.com/api/v1/users/defaults'
+    "https://airqo-250220.uc.r.appspot.com/api/v1/users/candidates/fetch",
+  DEFAULTS_URI: "https://airqo-250220.uc.r.appspot.com/api/v1/users/defaults",
+  DOWNLOAD_DATA:
+    "https://airqo-250220.uc.r.appspot.com/api/v1/data/download?type=",
 };
 const defaultConfig = {
   PORT: process.env.PORT || 5000,
-  NODE_PATH: process.env.NODE_PATH || 'src'
+  NODE_PATH: process.env.NODE_PATH || "src",
 };
 
 function envConfig(env) {
   switch (env) {
-    case 'development':
+    case "development":
       return devConfig;
-    case 'test':
+    case "test":
       return stageConfig;
-    case 'stage':
+    case "stage":
       return stageConfig;
     default:
       return prodConfig;
@@ -94,5 +99,5 @@ function envConfig(env) {
 
 export default {
   ...defaultConfig,
-  ...envConfig(process.env.NODE_ENV)
+  ...envConfig(process.env.NODE_ENV),
 };
