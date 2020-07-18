92d3142c83133dabf7972721cee75fc6792c6026
Martin <bbaalmart@gmail.com>
2020-07-18 08:40:45 +0300

    changing all ports for Staging link to start with 31....

--- analytics/src/config/constants.js
+++ analytics/src/config/constants.js
@@ -56,46 +56,46 @@ const testConfig = {
 };
 
 const stageConfig = {
-  VERIFY_TOKEN_URI: 'http://34.78.78.202:30000/api/v1/users/reset/you',
+  VERIFY_TOKEN_URI: 'http://34.78.78.202:31000/api/v1/users/reset/you',
   UPDATE_PWD_URI:
-    'http://34.78.78.202:30000/api/v1/users/updatePasswordViaEmail',
-  UPDATE_PWD_IN_URI: 'http://34.78.78.202:30000/api/v1/users/updatePassword',
-  FORGOT_PWD_URI: 'http://34.78.78.202:30000/api/v1/users/forgotPassword',
-  LOGIN_USER_URI: 'http://34.78.78.202:30000/api/v1/users/loginUser',
-  REGISTER_USER_URI: 'http://34.78.78.202:30000/api/v1/users/registerUser',
+    'http://34.78.78.202:31000/api/v1/users/updatePasswordViaEmail',
+  UPDATE_PWD_IN_URI: 'http://34.78.78.202:31000/api/v1/users/updatePassword',
+  FORGOT_PWD_URI: 'http://34.78.78.202:31000/api/v1/users/forgotPassword',
+  LOGIN_USER_URI: 'http://34.78.78.202:31000/api/v1/users/loginUser',
+  REGISTER_USER_URI: 'http://34.78.78.202:31000/api/v1/users/registerUser',
   REGISTER_CANDIDATE_URI:
-    'http://34.78.78.202:30000/api/v1/users/register/new/candidate',
-  REJECT_USER_URI: 'http://34.78.78.202:30000/api/v1/users/deny',
-  ACCEPT_USER_URI: 'http://34.78.78.202:30000/api/v1/users/accept',
-  GET_USERS_URI: 'http://34.78.78.202:30000/api/v1/users/',
-  GET_CANDIDATES_URI: 'http://34.78.78.202:30000/api/v1/users/candidates/fetch',
-  DEFAULTS_URI: 'http://34.78.78.202:30000/api/v1/users/defaults',
+    'http://34.78.78.202:31000/api/v1/users/register/new/candidate',
+  REJECT_USER_URI: 'http://34.78.78.202:31000/api/v1/users/deny',
+  ACCEPT_USER_URI: 'http://34.78.78.202:31000/api/v1/users/accept',
+  GET_USERS_URI: 'http://34.78.78.202:31000/api/v1/users/',
+  GET_CANDIDATES_URI: 'http://34.78.78.202:31000/api/v1/users/candidates/fetch',
+  DEFAULTS_URI: 'http://34.78.78.202:31000/api/v1/users/defaults',
   GENERATE_CUSTOMISABLE_CHARTS_URI:
-    'http://34.78.78.202:30003/api/v1/dashboard/customisedchart',
+    'http://34.78.78.202:31003/api/v1/dashboard/customisedchart',
   GET_CUSTOMISABLE_CHART_INITIAL_DATA_URI:
-    'http://34.78.78.202:30003/api/v1/dashboard/customisedchart/random',
+    'http://34.78.78.202:31003/api/v1/dashboard/customisedchart/random',
   GET_MONITORING_SITES_LOCATIONS_URI:
-    'http://34.78.78.202:30003/api/v1/dashboard/monitoringsites/locations?organisation_name=KCCA',
+    'http://34.78.78.202:31003/api/v1/dashboard/monitoringsites/locations?organisation_name=KCCA',
   GET_PM25_CATEGORY_COUNT_URI:
-    'http://34.78.78.202:30003/api/v1/dashboard/locations/pm25categorycount?organisation_name=KCCA',
+    'http://34.78.78.202:31003/api/v1/dashboard/locations/pm25categorycount?organisation_name=KCCA',
   GET_HISTORICAL_DAILY_MEAN_AVERAGES_FOR_LAST_28_DAYS_URI:
-    'http://34.78.78.202:30003/api/v1/dashboard/historical/daily/devices',
-  GENERATE_DEVICE_GRAPH_URI: 'http://34.78.78.202:30003/api/v1/device/graph',
+    'http://34.78.78.202:31003/api/v1/dashboard/historical/daily/devices',
+  GENERATE_DEVICE_GRAPH_URI: 'http://34.78.78.202:31003/api/v1/device/graph',
   DOWNLOAD_CUSTOMISED_DATA_URI:
-    'http://34.78.78.202:30003/api/v1/data/download',
+    'http://34.78.78.202:31003/api/v1/data/download',
   GET_DEFAULT_REPORT_TEMPLATE_URI:
-    'http://34.78.78.202:30003/api/v1/report/get_default_report_template',
+    'http://34.78.78.202:31003/api/v1/report/get_default_report_template',
   SAVE_MONTHLY_REPORT_URI:
-    'http://34.78.78.202:30003/api/v1/report/save_monthly_report',
+    'http://34.78.78.202:31003/api/v1/report/save_monthly_report',
   GET_MONTHLY_REPORT_URI:
-    'http://34.78.78.202:30003/api/v1/report/get_monthly_report/',
+    'http://34.78.78.202:31003/api/v1/report/get_monthly_report/',
   DELETE_MONTHLY_REPORT_URI:
-    'http://34.78.78.202:30003/api/v1/report/delete_monthly_report/',
+    'http://34.78.78.202:31003/api/v1/report/delete_monthly_report/',
   UPDATE_MONTHLY_REPORT_URI:
-    'http://34.78.78.202:30003/api/v1/report/update_monthly_report/',
-  EXCEEDANCES_URI: 'http://34.78.78.202:30003/api/v1/dashboard/exceedances',
+    'http://34.78.78.202:31003/api/v1/report/update_monthly_report/',
+  EXCEEDANCES_URI: 'http://34.78.78.202:31003/api/v1/dashboard/exceedances',
   GET_MONITORING_SITES_URI:
-    'http://34.78.78.202:30003/api/v1/dashboard/monitoringsites?organisation_name=KCCA'
+    'http://34.78.78.202:31003/api/v1/dashboard/monitoringsites?organisation_name=KCCA'
 };
 const prodConfig = {
   VERIFY_TOKEN_URI: 'http://34.78.78.202:30000/api/v1/users/reset/you',
