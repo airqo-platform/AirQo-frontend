40fa04760ae3dfafe891002c0ee88eb64d732ba8
danielogen <danielogen@gmail.com>
2020-07-23 12:26:44 +0300

    clean up dashboard cards

--- analytics/src/views/Dashboard/Dashboard.js
+++ analytics/src/views/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/styles';
+import React from "react";
+import { makeStyles } from "@material-ui/styles";
 import {
   Grid,
   Card,
@@ -7,46 +7,46 @@ import {
   CardHeader,
   Button,
   Divider,
-  CardActions
-} from '@material-ui/core';
-import { Line, Bar } from 'react-chartjs-2';
-import clsx from 'clsx';
-import PropTypes from 'prop-types';
+  CardActions,
+} from "@material-ui/core";
+import { Line, Bar } from "react-chartjs-2";
+import clsx from "clsx";
+import PropTypes from "prop-types";
 import {
   Pm25Levels,
   Map,
   CustomisableChart,
   PollutantCategory,
   ExceedancesChart,
-  TotalProfit
-} from './components';
-import { useEffect, useState } from 'react';
-import 'chartjs-plugin-annotation';
-import palette from 'theme/palette';
-import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
-import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+  TotalProfit,
+} from "./components";
+import { useEffect, useState } from "react";
+import "chartjs-plugin-annotation";
+import palette from "theme/palette";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import ArrowRightIcon from "@material-ui/icons/ArrowRight";
 //import Legend from './components/Map/Legend'
-import axios from 'axios';
-import constants from '../../config/constants'
+import axios from "axios";
+import constants from "../../config/constants";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {
-    padding: theme.spacing(4)
+    padding: theme.spacing(4),
   },
   chartCard: {},
   differenceIcon: {
-    color: theme.palette.text.secondary
+    color: theme.palette.text.secondary,
   },
   chartContainer: {
     height: 200,
-    position: 'relative'
+    position: "relative",
   },
   actions: {
-    justifyContent: 'flex-end'
-  }
+    justifyContent: "flex-end",
+  },
 }));
 
-const Dashboard = props => {
+const Dashboard = (props) => {
   const classes = useStyles();
   const {
     className,
@@ -60,7 +60,7 @@ const Dashboard = props => {
 
   function appendLeadingZeroes(n) {
     if (n <= 9) {
-      return '0' + n;
+      return "0" + n;
     }
     return n;
   }
@@ -68,9 +68,9 @@ const Dashboard = props => {
   let todaysDate = new Date();
   const dateValue = appendLeadingZeroes(
     todaysDate.getDate() +
-      '/' +
+      "/" +
       appendLeadingZeroes(todaysDate.getMonth() + 1) +
-      '/' +
+      "/" +
       todaysDate.getFullYear()
   );
 
@@ -78,7 +78,7 @@ const Dashboard = props => {
 
   const [
     pm25CategoriesLocationCount,
-    setPm25CategoriesLocationCount
+    setPm25CategoriesLocationCount,
   ] = useState([]);
 
   // useEffect(() => {
@@ -102,24 +102,24 @@ const Dashboard = props => {
   useEffect(() => {
     // if (user._id != {}) {
     function jiraHelpdesk(callback) {
-      let jhdScript = document.createElement('script');
-      jhdScript.type = 'text/javascript';
-      jhdScript.setAttribute('data-jsd-embedded', null);
+      let jhdScript = document.createElement("script");
+      jhdScript.type = "text/javascript";
+      jhdScript.setAttribute("data-jsd-embedded", null);
       jhdScript.setAttribute(
-        'data-key',
-        'cf4a44fc-f333-4e48-8e6c-6b94f97cea15'
+        "data-key",
+        "cf4a44fc-f333-4e48-8e6c-6b94f97cea15"
       );
       jhdScript.setAttribute(
-        'data-base-url',
-        'https://jsd-widget.atlassian.com'
+        "data-base-url",
+        "https://jsd-widget.atlassian.com"
       );
-      jhdScript.src = 'https://jsd-widget.atlassian.com/assets/embed.js';
+      jhdScript.src = "https://jsd-widget.atlassian.com/assets/embed.js";
       if (jhdScript.readyState) {
         // old IE support
-        jhdScript.onreadystatechange = function() {
+        jhdScript.onreadystatechange = function () {
           if (
-            jhdScript.readyState === 'loaded' ||
-            jhdScript.readyState === 'complete'
+            jhdScript.readyState === "loaded" ||
+            jhdScript.readyState === "complete"
           ) {
             jhdScript.onreadystatechange = null;
             callback();
@@ -127,16 +127,16 @@ const Dashboard = props => {
         };
       } else {
         //modern browsers
-        jhdScript.onload = function() {
+        jhdScript.onload = function () {
           callback();
         };
       }
-      document.getElementsByTagName('head')[0].appendChild(jhdScript);
+      document.getElementsByTagName("head")[0].appendChild(jhdScript);
     }
 
-    jiraHelpdesk(function() {
-      let DOMContentLoaded_event = document.createEvent('Event');
-      DOMContentLoaded_event.initEvent('DOMContentLoaded', true, true);
+    jiraHelpdesk(function () {
+      let DOMContentLoaded_event = document.createEvent("Event");
+      DOMContentLoaded_event.initEvent("DOMContentLoaded", true, true);
       window.document.dispatchEvent(DOMContentLoaded_event);
     });
   }, []);
@@ -144,12 +144,12 @@ const Dashboard = props => {
   useEffect(() => {
     axios
       .get(constants.GET_PM25_CATEGORY_COUNT_URI)
-      .then(res => res.data)
-      .then(data => {
+      .then((res) => res.data)
+      .then((data) => {
         setPm25CategoriesLocationCount(data);
-        console.log(data);        
+        console.log(data);
       })
-      .catch(e => {
+      .catch((e) => {
         console.log(e);
       });
   }, []);
@@ -157,12 +157,12 @@ const Dashboard = props => {
   const [locations, setLocations] = useState([]);
 
   useEffect(() => {
-    fetch(constants.GET_HISTORICAL_DAILY_MEAN_AVERAGES_FOR_LAST_28_DAYS_URI)      
-      .then(res => res.json())
-      .then(locationsData => {
+    fetch(constants.GET_HISTORICAL_DAILY_MEAN_AVERAGES_FOR_LAST_28_DAYS_URI)
+      .then((res) => res.json())
+      .then((locationsData) => {
         setLocations(locationsData.results);
       })
-      .catch(e => {
+      .catch((e) => {
         console.log(e);
       });
   }, []);
@@ -171,35 +171,35 @@ const Dashboard = props => {
     labels: locations.labels,
     datasets: [
       {
-        label: 'PM2.5(µg/m3)',
+        label: "PM2.5(µg/m3)",
         data: locations.average_pm25_values,
         fill: false, // Don't fill area under the line
         borderColor: palette.primary.main, // Line color
-        backgroundColor: locations.background_colors //palette.primary.main,
-      }
-    ]
+        backgroundColor: locations.background_colors, //palette.primary.main,
+      },
+    ],
   };
 
   const options_main = {
     annotation: {
       annotations: [
         {
-          type: 'line',
-          mode: 'horizontal',
-          scaleID: 'y-axis-0',
+          type: "line",
+          mode: "horizontal",
+          scaleID: "y-axis-0",
           value: 25,
           borderColor: palette.text.secondary,
           borderWidth: 2,
           label: {
             enabled: true,
-            content: 'WHO AQG',
+            content: "WHO AQG",
             //backgroundColor: palette.white,
             titleFontColor: palette.text.primary,
             bodyFontColor: palette.text.primary,
-            position: 'right'
-          }
-        }
-      ]
+            position: "right",
+          },
+        },
+      ],
     },
     responsive: true,
     maintainAspectRatio: false,
@@ -208,7 +208,7 @@ const Dashboard = props => {
     cornerRadius: 0,
     tooltips: {
       enabled: true,
-      mode: 'index',
+      mode: "index",
       intersect: false,
       borderWidth: 1,
       borderColor: palette.divider,
@@ -221,7 +221,7 @@ const Dashboard = props => {
         //afterTitle: (items, data) =>
         //return data['labels'][tooltipItem[0]['index']]
         //label: (item, data) => data.datasets[item.datasetIndex].data[item.index]
-      }
+      },
     },
     layout: { padding: 0 },
     scales: {
@@ -232,25 +232,25 @@ const Dashboard = props => {
           barPercentage: 0.5,
           categoryPercentage: 0.5,
           ticks: {
-            fontColor: palette.text.secondary
+            fontColor: palette.text.secondary,
             //fontSize:10
           },
           gridLines: {
             display: false,
-            drawBorder: false
+            drawBorder: false,
           },
           scaleLabel: {
             display: true,
-            labelString: 'Locations'
-          }
-        }
+            labelString: "Locations",
+          },
+        },
       ],
       yAxes: [
         {
           ticks: {
             fontColor: palette.text.secondary,
             beginAtZero: true,
-            min: 0
+            min: 0,
             //suggestedMin:20
           },
           gridLines: {
@@ -260,15 +260,15 @@ const Dashboard = props => {
             drawBorder: false,
             zeroLineBorderDash: [2],
             zeroLineBorderDashOffset: [2],
-            zeroLineColor: palette.divider
+            zeroLineColor: palette.divider,
           },
           scaleLabel: {
             display: true,
-            labelString: 'PM2.5(µg/m3)'
-          }
-        }
-      ]
-    }
+            labelString: "PM2.5(µg/m3)",
+          },
+        },
+      ],
+    },
   };
 
   return (
@@ -278,12 +278,12 @@ const Dashboard = props => {
           <PollutantCategory
             pm25level="Good"
             pm25levelCount={
-              typeof pm25CategoriesLocationCount != 'undefined' &&
+              typeof pm25CategoriesLocationCount != "undefined" &&
               pm25CategoriesLocationCount != null &&
               pm25CategoriesLocationCount.length > 0
-                ? pm25CategoriesLocationCount[0]['locations_with_category_good']
+                ? pm25CategoriesLocationCount[0]["locations_with_category_good"]
                     .category_count
-                : ''
+                : ""
             }
             iconClass="pm25Good"
           />
@@ -292,13 +292,13 @@ const Dashboard = props => {
           <PollutantCategory
             pm25level="Moderate"
             pm25levelCount={
-              typeof pm25CategoriesLocationCount != 'undefined' &&
+              typeof pm25CategoriesLocationCount != "undefined" &&
               pm25CategoriesLocationCount != null &&
               pm25CategoriesLocationCount.length > 0
                 ? pm25CategoriesLocationCount[1][
-                    'locations_with_category_moderate'
+                    "locations_with_category_moderate"
                   ].category_count
-                : ''
+                : ""
             }
             iconClass="pm25Moderate"
           />
@@ -307,12 +307,12 @@ const Dashboard = props => {
           <PollutantCategory
             pm25level="UHFSG"
             pm25levelCount={
-              typeof pm25CategoriesLocationCount != 'undefined' &&
+              typeof pm25CategoriesLocationCount != "undefined" &&
               pm25CategoriesLocationCount != null &&
               pm25CategoriesLocationCount.length > 0
                 ? pm25CategoriesLocationCount[2].locations_with_category_UH4SG
                     .category_count
-                : ''
+                : ""
             }
             iconClass="pm25UH4SG"
           />
@@ -322,12 +322,12 @@ const Dashboard = props => {
           <PollutantCategory
             pm25level="Unhealthy"
             pm25levelCount={
-              typeof pm25CategoriesLocationCount != 'undefined' &&
+              typeof pm25CategoriesLocationCount != "undefined" &&
               pm25CategoriesLocationCount != null &&
               pm25CategoriesLocationCount.length > 0
                 ? pm25CategoriesLocationCount[3]
                     .locations_with_category_unhealth.category_count
-                : ''
+                : ""
             }
             iconClass="pm25UnHealthy"
           />
@@ -337,12 +337,12 @@ const Dashboard = props => {
           <PollutantCategory
             pm25level="Very Unhealthy"
             pm25levelCount={
-              typeof pm25CategoriesLocationCount != 'undefined' &&
+              typeof pm25CategoriesLocationCount != "undefined" &&
               pm25CategoriesLocationCount != null &&
               pm25CategoriesLocationCount.length > 0
                 ? pm25CategoriesLocationCount[4]
                     .locations_with_category_very_unhealthy.category_count
-                : ''
+                : ""
             }
             iconClass="pm25VeryUnHealthy"
           />
@@ -351,12 +351,12 @@ const Dashboard = props => {
           <PollutantCategory
             pm25level="Hazardous"
             pm25levelCount={
-              typeof pm25CategoriesLocationCount != 'undefined' &&
+              typeof pm25CategoriesLocationCount != "undefined" &&
               pm25CategoriesLocationCount != null &&
               pm25CategoriesLocationCount.length > 0
                 ? pm25CategoriesLocationCount[5]
                     .locations_with_category_hazardous.category_count
-                : ''
+                : ""
             }
             iconClass="pm25Harzadous"
           />
@@ -465,7 +465,7 @@ const Dashboard = props => {
 Dashboard.propTypes = {
   className: PropTypes.string,
   mappedAuth: PropTypes.object.isRequired,
-  fetchDefaults: PropTypes.func.isRequired
+  fetchDefaults: PropTypes.func.isRequired,
 };
 
 export default Dashboard;
