564dae422bc07cf4bb9b9549506eaca1644d8c69
danielogen <danielogen@gmail.com>
2020-07-23 10:18:16 +0300

    improve 404 page

--- analytics/src/layouts/Minimal/components/Topbar/Topbar.js
+++ analytics/src/layouts/Minimal/components/Topbar/Topbar.js
@@ -1,47 +1,57 @@
-import React from 'react';
-import { Link as RouterLink } from 'react-router-dom';
-import clsx from 'clsx';
-import PropTypes from 'prop-types';
-import { makeStyles } from '@material-ui/styles';
-import { AppBar, Toolbar } from '@material-ui/core';
+import React from "react";
+import { Link as RouterLink } from "react-router-dom";
+import clsx from "clsx";
+import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/styles";
+import { AppBar, Toolbar } from "@material-ui/core";
 
 const useStyles = makeStyles(() => ({
   root: {
-    boxShadow: 'none'
-  }
+    boxShadow: "none",
+    backgroundColor: "#3067e2",
+  },
 }));
 
 const logo_style = {
-    'height': '4em',
-    'width': '4em',
-    'borderRadius': '50%',
-    'paddingTop': '.2em'
-  }
+  height: "4em",
+  width: "4em",
+  borderRadius: "50%",
+  paddingTop: ".2em",
+};
 
-const Topbar = props => {
+const Topbar = (props) => {
   const { className, ...rest } = props;
 
   const classes = useStyles();
+  const kcca_logo_style = {
+    height: "3.5em",
+    width: "4em",
+    borderRadius: "15%",
+    paddingTop: ".2em",
+    marginRight: ".4em",
+  };
+  const mak_logo_style = {
+    height: "3.3em",
+    width: "4em",
+    borderRadius: "15%",
+    paddingTop: ".2em",
+    marginRight: ".4em",
+  };
   const airqo_logo_style = {
-    'height': '4em',
-    'width': '4em',
-    'paddingTop': '.2em',
-    'marginRight': '.4em'
-  }
+    height: "3.5em",
+    width: "5em",
+    paddingTop: ".2em",
+    marginRight: ".4em",
+  };
 
   return (
-    <AppBar
-      {...rest}
-      className={clsx(classes.root, className)}
-      color="primary"
-      position="fixed"
-    >
+    <AppBar {...rest} className={clsx(classes.root, className)}>
       <Toolbar>
         <RouterLink to="/">
           <img
             alt="Logo"
-            style = {logo_style}
-            src="images/logos/kcca_logo.jpg"
+            style={kcca_logo_style}
+            src="/images/logos/kcca_logo.jpg"
           />
         </RouterLink>
         <RouterLink to="/">
@@ -54,7 +64,7 @@ const Topbar = props => {
         <RouterLink to="/">
           <img
             alt="mak.ac.ug"
-            style={logo_style}
+            style={mak_logo_style}
             src="/images/logos/mak_logo.jpg"
           />
         </RouterLink>
@@ -64,7 +74,7 @@ const Topbar = props => {
 };
 
 Topbar.propTypes = {
-  className: PropTypes.string
+  className: PropTypes.string,
 };
 
 export default Topbar;
