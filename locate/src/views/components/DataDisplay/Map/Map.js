6047b6d513ba9c3b745f3b2d6ae8983ceff367c9
Priscilla-AL <priscilla@airqo.net>
2020-08-04 13:09:27 +0300

    Add layer

--- locate/src/views/components/DataDisplay/Map/Map.js
+++ locate/src/views/components/DataDisplay/Map/Map.js
@@ -2,7 +2,7 @@ import React from 'react';
 import clsx from 'clsx';
 import PropTypes from 'prop-types';
 import { makeStyles } from '@material-ui/styles';
-import { Map as LeafletMap, TileLayer, Popup, Marker} from 'react-leaflet';
+import { Map as LeafletMap, TileLayer, Popup, Marker, LayersControl, } from 'react-leaflet';
 import {Link } from 'react-router-dom';
 import {Card, CardContent, CardHeader, Divider} from '@material-ui/core';
 import { useEffect, useState } from 'react';
@@ -13,6 +13,8 @@ import Filter from './FilterPowerSource.jsx';
 import axios from "axios";
 import ReactDOM from 'react-dom';
 // import '../../../../assets/scss/index.scss';
+
+const {BaseLayer, Overlay} = LayersControl;
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -69,6 +71,7 @@ const Map = props => {
 
   return (
         <LeafletMap
+        
           animate
           attributionControl
           center={[0.3341424,32.5600613]}
@@ -81,7 +84,9 @@ const Map = props => {
         >
           <TileLayer
             url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
-          />           
+          />  
+          <LayersControl position='topright'>   
+          <Overlay checked name='Red: te reo'>   
           {contacts.map((contact) => (
             <Marker 
               position={[contact.latitude,contact.longitude]}
@@ -99,6 +104,8 @@ const Map = props => {
               </Popup>
             </Marker>   
           ))}  
+          </Overlay>  
+            </LayersControl>
           <FullscreenControl position="topleft" />
         </LeafletMap>
  
