d5cc0565de15fb88abda613ccb976490c1b8fcc6
Martin <bbaalmart@gmail.com>
2020-08-31 13:33:52 +0300

    react app run environments

--- netmanager/src/config/constants.js
+++ netmanager/src/config/constants.js
@@ -203,22 +203,6 @@ const devConfig = {
     "http://localhost:3000/api/v1/devices/update/components?device=",
 };
 
-const testConfig = {
-  VERIFY_TOKEN_URI: "http://localhost:3000/api/v1/users/reset/you",
-  UPDATE_PWD_URI: "http://localhost:3000/api/v1/users/updatePasswordViaEmail",
-  UPDATE_PWD_IN_URI: "http://localhost:3000/api/v1/users/updatePassword",
-  FORGOT_PWD_URI: "http://localhost:3000/api/v1/users/forgotPassword",
-  LOGIN_USER_URI: "http://localhost:3000/api/v1/users/loginUser",
-  REGISTER_USER_URI: "http://localhost:3000/api/v1/users/registerUser",
-  REGISTER_CANDIDATE_URI:
-    "http://localhost:3000/api/v1/users/register/new/candidate",
-  REJECT_USER_URI: "http://localhost:3000/api/v1/users/deny",
-  ACCEPT_USER_URI: "http://localhost:3000/api/v1/users/accept",
-  GET_USERS_URI: "http://localhost:3000/api/v1/users/",
-  GET_CANDIDATES_URI: "http://localhost:3000/api/v1/users/candidates/fetch",
-  DEFAULTS_URI: "http://localhost:3000/api/v1/users/defaults",
-};
-
 const stageConfig = {
   VERIFY_TOKEN_URI: "http://34.78.78.202:31000/api/v1/users/reset/you",
   UPDATE_PWD_URI:
@@ -264,17 +248,24 @@ const stageConfig = {
     "http://34.78.78.202:31003/api/v1/dashboard/monitoringsites?organisation_name=KCCA&pm25_category=",
   /** netmanager */
 };
+
+const defaultConfig = {
+  PORT: process.env.PORT || 5000,
+  NODE_PATH: process.env.NODE_PATH || "src",
+};
+
 function runConfig(env) {
+  console.log(`running in ${env} mode`);
   switch (env) {
     case "development":
       return devConfig;
-    case "test":
-      return testConfig;
     case "stage":
       return stageConfig;
+    case "production":
+      return prodConfig;
     default:
       return prodConfig;
   }
 }
 
-export default { ...runConfig(process.env.NODE_ENV) };
+export default { ...defaultConfig, ...runConfig(process.env.REACT_APP_ENV) };
