afb04271fc73d14a7528f14066c07535c2ffa80a
Martin <bbaalmart@gmail.com>
2020-08-31 14:13:54 +0300

    presentational components for Account, Candidates, Reports and Settings

---
+++ netmanager/src/views/pages/Account/components/AccountDetails/AccountDetails.js
@@ -0,0 +1,190 @@
+import React, { useState, useEffect } from 'react';
+import clsx from 'clsx';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/styles';
+import {
+    Card,
+    CardHeader,
+    CardContent,
+    CardActions,
+    Divider,
+    Grid,
+    Button,
+    TextField
+} from '@material-ui/core';
+
+const useStyles = makeStyles(theme => ({
+    container: {
+        display: 'flex',
+        flexWrap: 'wrap'
+    },
+    textField: {
+        marginLeft: theme.spacing.unit,
+        marginRight: theme.spacing.unit
+    },
+    dense: {
+        marginTop: 16
+    },
+    menu: {
+        width: 200
+    },
+    root: {}
+}));
+
+const AccountDetails = props => {
+    const { className, mappeduserState, mappedAuth, ...rest } = props;
+
+    const { user } = mappedAuth;
+    console.log('the user is here: ');
+    console.dir(user);
+
+    const classes = useStyles();
+
+    const initialState = {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        phoneNumber: ''
+    };
+
+    const [form, setState] = useState(initialState);
+
+    useEffect(() => {
+        var anchorElem = document.createElement('link');
+        anchorElem.setAttribute(
+            'href',
+            'https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css'
+        );
+        anchorElem.setAttribute('rel', 'stylesheet');
+        anchorElem.setAttribute('id', 'logincdn');
+    });
+
+    const handleChange = e => {
+        setState({
+            ...form,
+            [e.target.id]: e.target.value
+        });
+    };
+
+    const states = [{
+            value: 'Kyagwe',
+            label: 'Kyagwe'
+        },
+        {
+            value: 'Bukoto',
+            label: 'Bukoto'
+        },
+        {
+            value: 'Nansana',
+            label: 'Nansana'
+        }
+    ];
+
+    const clearState = () => {
+        setState({...initialState });
+    };
+
+    const onSubmit = e => {
+        e.preventDefault();
+        const userData = {
+            id: user._id,
+            firstName: form.firstName,
+            lastName: form.lastName,
+            email: form.email,
+            phoneNumber: form.phoneNumber
+        };
+        console.log('sending this guy here:');
+        console.log(userData);
+        props.mappedUpdateAuthenticatedUser(userData);
+        clearState();
+    };
+
+    return ( <
+        Card {...rest }
+        className = { clsx(classes.root, className) } >
+        <
+        form autoComplete = "off"
+        noValidate >
+        <
+        CardHeader subheader = "The information can be edited"
+        title = "Profile" / >
+        <
+        Divider / >
+        <
+        CardContent >
+        <
+        Grid container spacing = { 3 } >
+        <
+        Grid item md = { 6 }
+        xs = { 12 } >
+        <
+        TextField fullWidth helperText = "Please specify the first name"
+        label = "First name"
+        margin = "dense"
+        id = "firstName"
+        onChange = { handleChange }
+        required value = { form.firstName }
+        variant = "outlined" /
+        >
+        <
+        /Grid>{' '} <
+        Grid item md = { 6 }
+        xs = { 12 } >
+        <
+        TextField fullWidth label = "Last name"
+        margin = "dense"
+        id = "lastName"
+        onChange = { handleChange }
+        required value = { form.lastName }
+        variant = "outlined" /
+        >
+        <
+        /Grid>{' '} <
+        Grid item md = { 6 }
+        xs = { 12 } >
+        <
+        TextField fullWidth label = "Email Address"
+        margin = "dense"
+        id = "email"
+        onChange = { handleChange }
+        required value = { form.email }
+        variant = "outlined" /
+        >
+        <
+        /Grid>{' '} <
+        Grid item md = { 6 }
+        xs = { 12 } >
+        <
+        TextField fullWidth label = "Phone Number"
+        margin = "dense"
+        id = "phoneNumber"
+        onChange = { handleChange }
+        value = { form.phoneNumber }
+        variant = "outlined" /
+        >
+        <
+        /Grid>{' '} <
+        /Grid>{' '} <
+        /CardContent>{' '} <
+        Divider / >
+        <
+        CardActions >
+        <
+        Button color = "primary"
+        variant = "contained"
+        onClick = { onSubmit }
+        disabled >
+        Save details { ' ' } <
+        /Button>{' '} <
+        /CardActions>{' '} <
+        /form>{' '} <
+        /Card>
+    );
+};
+
+AccountDetails.propTypes = {
+    className: PropTypes.string,
+    mappedAuth: PropTypes.object.isRequired
+};
+
+export default AccountDetails;
---
+++ netmanager/src/views/pages/Settings/components/Password/Password.js
@@ -0,0 +1,106 @@
+/* eslint-disable */
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import clsx from 'clsx';
+import { makeStyles } from '@material-ui/styles';
+import { updatePassword } from '../../../../redux/Join/actions';
+import {
+  Card,
+  CardHeader,
+  CardContent,
+  CardActions,
+  Divider,
+  Button,
+  TextField
+} from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+  root: {}
+}));
+
+const Password = props => {
+  const { className, mappedAuth, mappeduserState, ...rest } = props;
+  const { user } = mappedAuth;
+  const userState = mappeduserState;
+
+  const classes = useStyles();
+
+  const initialState = {
+    password: '',
+    password2: ''
+  };
+
+  const [values, setValues] = useState(initialState);
+
+  const clearState = () => {
+    setValues({ ...initialState });
+  };
+
+  const handleChange = event => {
+    setValues({
+      ...values,
+      [event.target.id]: event.target.value
+    });
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    const userData = {
+      id: user._id,
+      password: values.password,
+      password2: values.password2
+    };
+    console.log("sending them through...");
+    console.log(userData);
+    props.mappedUpdatePassword(userData);
+    clearState();
+  };
+
+  return (
+    <Card {...rest} className={clsx(classes.root, className)}>
+      <CardHeader subheader="Update password" title="Password" />
+      <Divider />
+      <CardContent>
+        <TextField
+          fullWidth
+          label="Password"
+          id="password"
+          onChange={handleChange}
+          type="password"
+          value={values.password}
+          variant="outlined"
+        />
+        <TextField
+          fullWidth
+          label="Confirm password"
+          id="password2"
+          onChange={handleChange}
+          style={{ marginTop: '1rem' }}
+          type="password"
+          value={values.password2}
+          variant="outlined"
+        />
+      </CardContent>
+      <Divider />
+      <CardActions>
+        <Button color="primary" variant="outlined" onClick={onSubmit} disabled>
+          Update
+        </Button>
+      </CardActions>
+    </Card>
+  );
+};
+
+Password.propTypes = {
+  className: PropTypes.string,
+  mappedAuth: PropTypes.object.isRequired,
+  mappeduserState: PropTypes.object.isRequired,
+  updatePassword: PropTypes.func.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  auth: state.auth,
+  userState: state.userState
+});
+
+export default Password;
