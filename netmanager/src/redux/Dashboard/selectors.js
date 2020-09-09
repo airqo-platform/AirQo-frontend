39e5e71fc2355515fb02cd46e1da4c4b016e3d4d
ja-odur <odurjoseph8@gmail.com>
2020-09-09 18:06:58 +0300

    ch: add filterLocationData selector

--- netmanager/src/redux/Dashboard/selectors.js
+++ netmanager/src/redux/Dashboard/selectors.js
@@ -1,1 +1,6 @@
 // take a slice of the application state and return some data based on that
+import { useSelector } from "react-redux";
+
+export const useFilterLocationData = () => {
+  return useSelector((state) => state.dashboard.filterLocationData);
+};
