c6ceff2400490d2d32337a34830ee3fed35043db
ja-odur <odurjoseph8@gmail.com>
2020-09-09 18:06:20 +0300

    ch: add filterLocationData actions and thunk

--- netmanager/src/redux/Dashboard/operations.js
+++ netmanager/src/redux/Dashboard/operations.js
@@ -1,1 +1,25 @@
-//for representing chained operations using redux-thunk
+// for representing chained operations using redux-thunk
+import {
+  REFRESH_FILTER_LOCATION_DATA_SUCCESS,
+  REFRESH_FILTER_LOCATION_DATA_ERROR,
+} from "./actions";
+import constants from "../../config/constants";
+
+export const refreshFilterLocationData = () => {
+  return async (dispatch) => {
+    return await fetch(constants.GET_MONITORING_SITES_LOCATIONS_URI)
+      .then((res) => res.json())
+      .then((filterLocationsData) => {
+        dispatch({
+          type: REFRESH_FILTER_LOCATION_DATA_SUCCESS,
+          payload: filterLocationsData.airquality_monitoring_sites,
+        });
+      })
+      .catch((err) => {
+        dispatch({
+          type: REFRESH_FILTER_LOCATION_DATA_ERROR,
+          payload: err,
+        });
+      });
+  };
+};
