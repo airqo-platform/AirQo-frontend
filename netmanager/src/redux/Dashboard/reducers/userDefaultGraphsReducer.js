19cafee7311c11083ec8d616d62175a6f0aa0342
ja-odur <odurjoseph8@gmail.com>
2020-09-11 10:22:59 +0300

    ch: add userDefaultsGraphs reducer

---
+++ netmanager/src/redux/Dashboard/reducers/userDefaultGraphsReducer.js
@@ -0,0 +1,47 @@
+import { LOAD_USER_DEFAULT_GRAPHS_SUCCESS } from "../actions";
+
+const initialUserDefaultGraphsState = [
+  {
+    pollutant: "PM 2.5",
+    frequency: "daily",
+    chartType: "line",
+    chartTitle: "Mean Daily PM 2.5 for Kawempe division",
+    locations: [
+      "Bwaise II",
+      "Kyebando",
+      "Komamboga",
+      "Kazo Angola",
+      "Wandegeya",
+    ],
+  },
+  {
+    pollutant: "PM 2.5",
+    frequency: "daily",
+    chartType: "line",
+    chartTitle: "Mean Daily PM 2.5 for Lubaga division",
+    locations: ["Namirembe", "Kawala", "Lubya", "Nakulabye", "Mutundwe"],
+  },
+  {
+    pollutant: "PM 2.5",
+    frequency: "daily",
+    chartType: "line",
+    chartTitle: "Mean Daily PM 2.5 for Makindye division",
+    locations: ["Kibuye I", "Nsambya Central", "Kisugu", "Ggaba"],
+  },
+  {
+    pollutant: "PM 2.5",
+    frequency: "daily",
+    chartType: "line",
+    chartTitle: "Mean Daily PM 2.5 for Nakawa division",
+    locations: ["Nakawa", "Kiswa", "Luzira", "Naguru I", "Kyanja"],
+  },
+];
+
+export default function (state = initialUserDefaultGraphsState, action) {
+  switch (action.type) {
+    case LOAD_USER_DEFAULT_GRAPHS_SUCCESS:
+      return action.payload;
+    default:
+      return state;
+  }
+}
