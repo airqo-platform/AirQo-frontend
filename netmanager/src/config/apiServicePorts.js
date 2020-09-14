b9b2b1db460add4678c1ffa0a13529f237e7809b
ja-odur <odurjoseph8@gmail.com>
2020-09-14 18:20:55 +0300

    ch: add service ports for various api services

---
+++ netmanager/src/config/apiServicePorts.js
@@ -0,0 +1,41 @@
+export const PROD_SERVICE_MAPPER = {
+  authService: 30000,
+  dataManagement: 30001,
+  deviceRegistry: 30002,
+  analytics: 30003,
+  locate: 30004,
+  locationRegistry: 30005,
+  deviceMonitoring: 30006,
+  deviceManagement: 30007,
+  incentives: 30008,
+  predictionService: 30009,
+  sensorLocation: 30010,
+};
+
+export const STAGING_SERVICE_MAPPER = {
+  authService: 31000,
+  dataManagement: 31001,
+  deviceRegistry: 31002,
+  analytics: 31003,
+  locate: 31004,
+  locationRegistry: 31005,
+  deviceMonitoring: 31006,
+  deviceManagement: 31007,
+  incentives: 31008,
+  predictionService: 31009,
+  sensorLocation: 31010,
+};
+
+export const LOCAL_SERVICE_MAPPER = {
+  authService: 3000,
+  dataManagement: 3001,
+  deviceRegistry: 3002,
+  analytics: 3003,
+  locate: 3004,
+  locationRegistry: 3005,
+  deviceMonitoring: 3006,
+  deviceManagement: 3007,
+  incentives: 3008,
+  predictionService: 3009,
+  sensorLocation: 3010,
+};
