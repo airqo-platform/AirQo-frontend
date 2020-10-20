216551640db2ef7c88e3cfbbedbb4653d5187e6e
ja-odur <odurjoseph8@gmail.com>
2020-10-20 22:43:51 +0300

    refactor transform array

--- netmanager/src/redux/DeviceRegistry/operations.js
+++ netmanager/src/redux/DeviceRegistry/operations.js
@@ -3,15 +3,9 @@ import {
     LOAD_ALL_DEVICES_SUCCESS,
     LOAD_ALL_DEVICES_FAILURE,
 } from "./actions";
+import { transformArray } from "../utils";
 import { getAllDevicesApi } from "../../views/apis/deviceRegistry";
 
-const transformDeviceArray = (devices) => {
-    const devicesState = {}
-    devices.map((device) => {
-        devicesState[device.id] = device
-    })
-    return devicesState
-}
 
 export const loadDevicesData = () => {
   return async (dispatch) => {
@@ -20,7 +14,7 @@ export const loadDevicesData = () => {
           if(responseData.devices){
               dispatch({
                   type: LOAD_ALL_DEVICES_SUCCESS,
-                  payload: transformDeviceArray(responseData.devices),
+                  payload: transformArray(responseData.devices, 'id'),
               });
           }
 
