96a4035b63b82764f5eb6c07e75dc1b1fcf1f15a
ja-odur <odurjoseph8@gmail.com>
2020-10-23 22:28:14 +0300

    add maintenace logs reducer

--- netmanager/src/redux/DeviceRegistry/reducers/index.js
+++ netmanager/src/redux/DeviceRegistry/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import devices from "./device";
+import maintenanceLogs from "./maintenanceLogs";
 
 
 export default combineReducers({
-    devices
+    devices,
+    maintenanceLogs,
 });
