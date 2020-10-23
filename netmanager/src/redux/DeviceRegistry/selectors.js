dfb0bb87a2c66cc4f06bdeee0269c85ef5bc544c
ja-odur <odurjoseph8@gmail.com>
2020-10-23 23:03:49 +0300

    add device logs selectors

--- netmanager/src/redux/DeviceRegistry/selectors.js
+++ netmanager/src/redux/DeviceRegistry/selectors.js
@@ -4,3 +4,11 @@ import { useSelector } from "react-redux";
 export const useDevicesData = () => {
   return useSelector((state) => state.deviceRegistry.devices);
 };
+
+export const useMaintenanceLogsData = () => {
+  return useSelector((state) => state.deviceRegistry.maintenanceLogs);
+};
+
+export const useDeviceLogsData = (deviceName) => {
+  return useSelector((state) => state.deviceRegistry.maintenanceLogs[deviceName] || []);
+};
