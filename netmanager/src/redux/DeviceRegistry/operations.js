8218c28346eeed2726e36eca74d8fb7c83f92566
ja-odur <odurjoseph8@gmail.com>
2020-10-23 22:28:44 +0300

    add load device logs thunk

--- netmanager/src/redux/DeviceRegistry/operations.js
+++ netmanager/src/redux/DeviceRegistry/operations.js
@@ -2,9 +2,11 @@
 import {
     LOAD_ALL_DEVICES_SUCCESS,
     LOAD_ALL_DEVICES_FAILURE,
+    LOAD_MAINTENANCE_LOGS_SUCCESS,
+    LOAD_MAINTENANCE_LOGS_FAILURE,
 } from "./actions";
 import { transformArray } from "../utils";
-import { getAllDevicesApi } from "../../views/apis/deviceRegistry";
+import { getAllDevicesApi, getDeviceMaintenanceLogsApi } from "../../views/apis/deviceRegistry";
 
 
 export const loadDevicesData = () => {
@@ -27,3 +29,21 @@ export const loadDevicesData = () => {
       });
   };
 };
+
+export const loadDeviceMaintenanceLogs = (deviceName) => {
+    return async (dispatch) => {
+        return await getDeviceMaintenanceLogsApi(deviceName)
+            .then((responseData) => {
+                dispatch({
+                    type: LOAD_MAINTENANCE_LOGS_SUCCESS,
+                    payload: { [deviceName]: responseData }
+                })
+            })
+            .catch((err) => {
+                dispatch({
+                  type: LOAD_MAINTENANCE_LOGS_FAILURE,
+                  payload: err,
+                });
+            })
+    }
+}
