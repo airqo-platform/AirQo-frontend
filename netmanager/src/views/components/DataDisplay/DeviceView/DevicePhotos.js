9c198769f2127afd22372e9d48cba1a7677575e1
ja-odur <odurjoseph8@gmail.com>
2020-10-23 14:05:31 +0300

    update photos page

--- netmanager/src/views/components/DataDisplay/DeviceView/DevicePhotos.js
+++ netmanager/src/views/components/DataDisplay/DeviceView/DevicePhotos.js
@@ -1,8 +1,24 @@
 import React from 'react';
 
+const styles = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    minHeight: "79vh",
+}
+
+const spanStyles = {
+    color: "grey",
+    fontSize: "50px",
+    textTransform: "capitalize",
+    padding: "40px",
+    border: "1px dotted grey"
+}
 
 export const DevicePhotos = () => {
     return (
-        <div>photos</div>
+        <div style={styles}>
+            <span style={spanStyles}>coming soon...</span>
+        </div>
     )
 };
