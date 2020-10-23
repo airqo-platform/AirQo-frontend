f5634361b67d014bdac48fb93f12d325434df88d
ja-odur <odurjoseph8@gmail.com>
2020-10-23 14:05:03 +0300

    add icons to tabs

--- netmanager/src/views/components/DataDisplay/DeviceView/DeviceToolBar.js
+++ netmanager/src/views/components/DataDisplay/DeviceView/DeviceToolBar.js
@@ -5,7 +5,16 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab'
+import Tab from '@material-ui/core/Tab';
+import {
+  Update,
+  AddOutlined,
+  EditOutlined,
+  CloudUploadOutlined,
+  UndoOutlined,
+  PageviewOutlined,
+  EventBusy, PhotoOutlined
+} from "@material-ui/icons";
 
 
 const a11yProps = (index) => {
@@ -19,6 +28,9 @@ const useStyles = makeStyles(theme => ({
     root: {
         flexGrow: 1,
         backgroundColor: theme.palette.background.paper,
+    },
+    wrapper: {
+        flexDirection: "row",
     },
     margin: {
         width: '98%',
@@ -40,12 +52,19 @@ const useStyles = makeStyles(theme => ({
     }
 }))
 
+const iconStyles = {
+    marginRight: "2px",
+}
+
 const LinkTab = (props) => {
     const classes = useStyles();
 
     return (
         <Tab
-            className={classes.tab}
+            classes={{
+                root: classes.tab,
+                wrapper: classes.wrapper,
+            }}
             component='a'
             wrapped
             onClick={event => event.preventDefault()}
@@ -59,7 +78,7 @@ export const DeviceToolBar = ({ deviceName }) => {
     const classes = useStyles();
     const match = useRouteMatch();
     const history = useHistory()
-    const [value, setValue] = React.useState(`${match.url}/overview`);
+    const [value, setValue] = React.useState(history.location.pathname);
 
     const handleChange = (event, newValue) => {
         setValue(newValue);
@@ -81,12 +100,42 @@ export const DeviceToolBar = ({ deviceName }) => {
                   textColor="primary"
                   aria-label="nav tabs example"
                 >
-                  <LinkTab label="Overview" value={`${match.url}/overview`}/>
-                  <LinkTab label="Edit" value={`${match.url}/edit`} />
-                  <LinkTab label="Maintenance logs" value={`${match.url}/maintenance-logs`} />
-                  <LinkTab label="Deploy status" value={`${match.url}/deploy-status`} />
-                  <LinkTab label="Components" value={`${match.url}/components`} />
-                  <LinkTab label="Photos" value={`${match.url}/photos`} />
+                  <LinkTab
+                      label="Overview"
+                      icon={<PageviewOutlined style={iconStyles} />}
+                      value={`${match.url}/overview`}
+                      { ...a11yProps(0) }
+                  />
+                  <LinkTab
+                      label="Edit"
+                      icon={<EditOutlined style={iconStyles} />}
+                      value={`${match.url}/edit`}
+                      { ...a11yProps(1) }
+                  />
+                  <LinkTab
+                      label="Maintenance logs"
+                      icon={<Update style={iconStyles} />}
+                      value={`${match.url}/maintenance-logs`}
+                      { ...a11yProps(2) }
+                  />
+                  <LinkTab
+                      label="Deploy status"
+                      icon={<CloudUploadOutlined style={iconStyles} />}
+                      value={`${match.url}/deploy-status`}
+                      { ...a11yProps(3) }
+                  />
+                  <LinkTab
+                      label="Components"
+                      icon={<AddOutlined style={iconStyles} />}
+                      value={`${match.url}/components`}
+                      { ...a11yProps(4) }
+                  />
+                  <LinkTab
+                      label="Photos"
+                      icon={<PhotoOutlined style={iconStyles} />}
+                      value={`${match.url}/photos`}
+                      { ...a11yProps(5) }
+                  />
                 </Tabs>
               </Toolbar>
             </AppBar>
