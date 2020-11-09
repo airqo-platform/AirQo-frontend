e8b67e2a036f4261cdac0539055c027990b72bce
ja-odur <odurjoseph8@gmail.com>
2020-11-09 20:50:44 +0300

    add image upload components

--- netmanager/src/views/components/DataDisplay/DeviceView/DevicePhotos.js
+++ netmanager/src/views/components/DataDisplay/DeviceView/DevicePhotos.js
@@ -1,24 +1,109 @@
-import React from 'react';
+import React, { useState } from "react";
+import ImageUploading from "react-images-uploading";
+import { Button } from "@material-ui/core";
 
-const styles = {
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    minHeight: "79vh",
-}
 
-const spanStyles = {
-    color: "grey",
-    fontSize: "50px",
-    textTransform: "capitalize",
-    padding: "40px",
-    border: "1px dotted grey"
-}
+const galleryContainerStyles = {
+  display: "flex",
+  flexWrap: "wrap",
+  alignItems: "center",
+  justifyContent: "center",
+};
+
+const galleryItemStyles = {
+  margin: "10px",
+  height: "400px",
+};
+
+const Gallery = ({
+  imageList,
+  onImageUpload,
+  onImageRemoveAll,
+  onImageUpdate,
+  onImageRemove,
+  isDragging,
+  dragProps,
+}) => {
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "flex-end",
+          margin: "10px 0",
+        }}
+      >
+        <Button
+          variant="contained"
+          color="primary"
+          style={isDragging ? { color: "red" } : undefined}
+          onClick={onImageUpload}
+          {...dragProps}
+        >
+          {" "}
+          Add Photo(s)
+        </Button>
+      </div>
+      <div style={galleryContainerStyles}>
+        {imageList.map((image, index) => (
+          <div key={index} style={galleryItemStyles}>
+            <img
+              src={image["data_url"]}
+              alt=""
+              width="auto"
+              height="100%"
+            />
+            {/*<div className="image-item__btn-wrapper">*/}
+            {/*  <button onClick={() => onImageUpdate(index)}>Update new</button>*/}
+            {/*  <button onClick={() => onImageRemove(index)}>Remove new</button>*/}
+            {/*</div>*/}
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default function DevicePhotos() {
-    return (
-        <div style={styles}>
-            <span style={spanStyles}>coming soon...</span>
-        </div>
-    )
-};
+  const [images, setImages] = useState([]);
+  const maxNumber = 69;
+
+  const onChange = (imageList, addUpdateIndex) => {
+    // data for submit
+    console.log(imageList, addUpdateIndex);
+    setImages(imageList);
+  };
+
+  return (
+    <div className="App">
+      <ImageUploading
+        multiple
+        value={images}
+        onChange={onChange}
+        maxNumber={maxNumber}
+        dataURLKey="data_url"
+      >
+        {({
+          imageList,
+          onImageUpload,
+          onImageRemoveAll,
+          onImageUpdate,
+          onImageRemove,
+          isDragging,
+          dragProps,
+        }) => (
+          <Gallery
+            imageList={imageList}
+            onImageUpload={onImageUpload}
+            onImageRemoveAll={onImageRemoveAll}
+            onImageUpdate={onImageUpdate}
+            onImageRemove={onImageRemove}
+            isDragging={isDragging}
+            dragProps={dragProps}
+          />
+        )}
+      </ImageUploading>
+    </div>
+  );
+}
+
