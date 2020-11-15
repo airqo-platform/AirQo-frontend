65c9ad730e93b6d21b3760e772cc9c72e9575589
ja-odur <odurjoseph8@gmail.com>
2020-11-16 01:28:31 +0300

    add crop component

--- netmanager/src/views/pages/Account/components/AccountProfile/AccountProfile.js
+++ netmanager/src/views/pages/Account/components/AccountProfile/AccountProfile.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import PropTypes from "prop-types";
+import ReactCrop from "react-image-crop";
 import clsx from "clsx";
 import moment from "moment";
 import { makeStyles } from "@material-ui/styles";
@@ -13,6 +14,10 @@ import {
   Button,
   LinearProgress,
 } from "@material-ui/core";
+
+// css
+import "react-image-crop/dist/ReactCrop.css";
+import "assets/css/account-profile.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {},
@@ -45,45 +50,117 @@ const AccountProfile = (props) => {
 
   const { user } = mappedAuth;
 
+  const [crop, setCrop] = useState({ aspect: 1 / 1, width: 30, unit: "%" });
+  const [uploadedFile, setUploadedFile] = useState("");
+  const [showCrop, setShowCrop] = useState(false);
+  const hiddenFileInput = useRef(null);
+
+  const cancelCrop = (event) => {
+    event.preventDefault();
+    setShowCrop(false);
+  };
+  const onPictureUploadClick = (event) => {
+    event.preventDefault();
+    hiddenFileInput.current.click();
+  };
+
+  const handleInputChange = (event) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const reader = new FileReader();
+      reader.addEventListener("load", () => {
+        setUploadedFile(reader.result);
+        setShowCrop(true);
+      });
+      reader.readAsDataURL(event.target.files[0]);
+    }
+  };
+
   return (
-    <Card {...rest} className={clsx(classes.root, className)}>
-      <CardContent>
-        <div className={classes.details}>
-          <div>
-            <Typography gutterBottom variant="h2">
-              {`${user.firstName} ${user.lastName}`}
-            </Typography>
-            <Typography
-              className={classes.locationText}
-              color="textSecondary"
-              variant="body1"
-            >
-              {(user.city && user.country && `${user.city}, ${user.country}`) ||
-                "Kampala, Uganda"}
-            </Typography>
-            <Typography
-              className={classes.dateText}
-              color="textSecondary"
-              variant="body1"
-            >
-              {moment().format("hh:mm A")} ({"GMT+3"})
-            </Typography>
+    <>
+      <Card {...rest} className={clsx(classes.root, className)}>
+        <CardContent>
+          <div className={classes.details}>
+            <div>
+              <Typography gutterBottom variant="h2">
+                {`${user.firstName} ${user.lastName}`}
+              </Typography>
+              <Typography
+                className={classes.locationText}
+                color="textSecondary"
+                variant="body1"
+              >
+                {(user.city &&
+                  user.country &&
+                  `${user.city}, ${user.country}`) ||
+                  "Kampala, Uganda"}
+              </Typography>
+              <Typography
+                className={classes.dateText}
+                color="textSecondary"
+                variant="body1"
+              >
+                {moment().format("hh:mm A")} ({"GMT+3"})
+              </Typography>
+            </div>
+            <Avatar className={classes.avatar} src={user.avatar} />
           </div>
-          <Avatar className={classes.avatar} src={user.avatar} />
+          <div className={classes.progress}>
+            <Typography variant="body1">Profile Completeness: 70%</Typography>
+            <LinearProgress value={70} variant="determinate" />
+          </div>
+        </CardContent>
+        <Divider />
+        <CardActions className={classes.buttonContainer}>
+          <Button
+            className={classes.uploadButton}
+            color="primary"
+            variant="text"
+            onClick={onPictureUploadClick}
+          >
+            Upload picture
+          </Button>
+          <input
+            type="file"
+            accept="image/*"
+            ref={hiddenFileInput}
+            onChange={handleInputChange}
+            style={{ display: "none" }}
+          />
+          <Button variant="text">Remove picture</Button>
+        </CardActions>
+      </Card>
+      <div className={`img-crop-preview ${showCrop ? "" : "hidden"}`}>
+        <div className={"crop-container"}>
+          <div>crop photo</div>
+          <ReactCrop
+            src={uploadedFile}
+            crop={crop}
+            className={"react-crop"}
+            style={{ maxHeight: "80vh", overflow: "scroll" }}
+            imageStyle={{ width: "100%", height: "auto" }}
+            onChange={(newCrop) => setCrop(newCrop)}
+          />
+          <div>
+            <Button
+              className={classes.uploadButton}
+              color="primary"
+              variant="outlined"
+              onClick={cancelCrop}
+            >
+              Cancel
+            </Button>
+            <Button
+              className={classes.uploadButton}
+              color="primary"
+              variant="contained"
+              onClick={onPictureUploadClick}
+            >
+              Save
+            </Button>
+          </div>
         </div>
-        <div className={classes.progress}>
-          <Typography variant="body1">Profile Completeness: 70%</Typography>
-          <LinearProgress value={70} variant="determinate" />
-        </div>
-      </CardContent>
-      <Divider />
-      <CardActions className={classes.buttonContainer}>
-        <Button className={classes.uploadButton} color="primary" variant="text">
-          Upload picture
-        </Button>
-        <Button variant="text">Remove picture</Button>
-      </CardActions>
-    </Card>
+      </div>
+    </>
   );
 };
 
