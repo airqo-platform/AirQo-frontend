9d4a35752264819804641943caaed55488d5a28b
ja-odur <odurjoseph8@gmail.com>
2020-11-15 22:32:54 +0300

    fix profile alignment

--- netmanager/src/views/pages/Account/components/AccountProfile/AccountProfile.js
+++ netmanager/src/views/pages/Account/components/AccountProfile/AccountProfile.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import moment from 'moment';
-import { makeStyles } from '@material-ui/styles';
+import React from "react";
+import PropTypes from "prop-types";
+import clsx from "clsx";
+import moment from "moment";
+import { makeStyles } from "@material-ui/styles";
 import {
   Card,
   CardActions,
@@ -11,97 +11,74 @@ import {
   Typography,
   Divider,
   Button,
-  LinearProgress
-} from '@material-ui/core';
+  LinearProgress,
+} from "@material-ui/core";
 
-const useStyles = makeStyles(theme => ({
+const useStyles = makeStyles((theme) => ({
   root: {},
   details: {
-    display: 'flex'
+    display: "flex",
   },
   avatar: {
-    marginLeft: 'auto',
-    height: 110,
-    width: 100,
+    marginLeft: "30%",
+    height: "202px",
+    width: "170px",
     flexShrink: 0,
-    flexGrow: 0
+    flexGrow: 0,
   },
   progress: {
-    marginTop: theme.spacing(2)
+    marginTop: theme.spacing(2),
   },
   uploadButton: {
-    marginRight: theme.spacing(2)
-  }
+    marginRight: theme.spacing(2),
+  },
+  buttonContainer: {
+    display: "flex",
+    justifyContent: "flex-end",
+  },
 }));
 
-const AccountProfile = props => {
-  const { className, mappedAuth,mappeduserState, ...rest } = props;
+const AccountProfile = (props) => {
+  const { className, mappedAuth, ...rest } = props;
 
   const classes = useStyles();
 
   const { user } = mappedAuth;
 
-  const userState = mappeduserState;
-
-  // const user = {
-  //   name: 'Shen Zhi',
-  //   city: 'Los Angeles',
-  //   country: 'USA',
-  //   timezone: 'GTM-7',
-  //   avatar: '/images/avatars/avatar_11.png'
-  // };
-
   return (
-    <Card
-      {...rest}
-      className={clsx(classes.root, className)}
-    >
+    <Card {...rest} className={clsx(classes.root, className)}>
       <CardContent>
         <div className={classes.details}>
           <div>
-            <Typography
-              gutterBottom
-              variant="h2"
-            >
-              {user.firstName}{" "}{user.lastName}
+            <Typography gutterBottom variant="h2">
+              {`${user.firstName} ${user.lastName}`}
             </Typography>
             <Typography
               className={classes.locationText}
               color="textSecondary"
               variant="body1"
             >
-              {/* {user.city}, {user.country} */}
-              {"Kampala"},{"Uganda"}
+              {(user.city && user.country && `${user.city}, ${user.country}`) ||
+                "Kampala, Uganda"}
             </Typography>
             <Typography
               className={classes.dateText}
               color="textSecondary"
               variant="body1"
             >
-              {/* {moment().format('hh:mm A')} ({user.timezone})  */}
-              {moment().format('hh:mm A')} ({"GMT+3"})
+              {moment().format("hh:mm A")} ({"GMT+3"})
             </Typography>
           </div>
-          <Avatar
-            className={classes.avatar}
-            src={user.avatar}
-          />
+          <Avatar className={classes.avatar} src={user.avatar} />
         </div>
         <div className={classes.progress}>
           <Typography variant="body1">Profile Completeness: 70%</Typography>
-          <LinearProgress
-            value={70}
-            variant="determinate"
-          />
+          <LinearProgress value={70} variant="determinate" />
         </div>
       </CardContent>
       <Divider />
-      <CardActions>
-        <Button
-          className={classes.uploadButton}
-          color="primary"
-          variant="text"
-        >
+      <CardActions className={classes.buttonContainer}>
+        <Button className={classes.uploadButton} color="primary" variant="text">
           Upload picture
         </Button>
         <Button variant="text">Remove picture</Button>
