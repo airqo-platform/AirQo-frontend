be4c5b484f8a507c27699293b6a1458adeecccb2
ja-odur <odurjoseph8@gmail.com>
2020-11-18 22:50:08 +0300

    add monitoring site data on map

--- netmanager/src/views/pages/Map/OverlayMap.js
+++ netmanager/src/views/pages/Map/OverlayMap.js
@@ -1,15 +1,43 @@
 import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import { heatmapPredictApi } from "views/apis/predict";
+import { getMonitoringSitesInfoApi } from "views/apis/analytics";
 import { heatMapPaint } from "./paints";
 import { transformDataToGeoJson } from "./utils";
 
 // css
 import "assets/css/map.css";
 
+const markerClasses = {
+  0: "marker-good",
+  12.1: "marker-moderate",
+  35.5: "marker-uhfsg",
+  55.5: "marker-unhealthy",
+  150.5: "marker-v-unhealthy",
+  250.5: "marker-hazardous",
+};
+
+const getMarkerClass = (markerValue) => {
+  let keys = Object.keys(markerClasses);
+  // in-place reverse sorting
+  keys.sort((key1, key2) => -(key1 - key2));
+
+  for (let i = 0; i < keys.length; i++) {
+    if (markerValue >= keys[i]) {
+      return markerClasses[keys[i]];
+    }
+  }
+  return "";
+};
+
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
 
-export const OverlayMap = ({ center, zoom, heatMapData }) => {
+export const OverlayMap = ({
+  center,
+  zoom,
+  heatMapData,
+  monitoringSiteData,
+}) => {
   const mapContainerRef = useRef(null);
   const [map, setMap] = useState(null);
 
@@ -61,14 +89,31 @@ export const OverlayMap = ({ center, zoom, heatMapData }) => {
     }
   });
 
-  return <div className="overlay-map-container" ref={mapContainerRef} />;
+  return (
+    <div className="overlay-map-container" ref={mapContainerRef}>
+      {map &&
+        monitoringSiteData.features.forEach((feature) => {
+          const el = document.createElement("div");
+          el.className = `marker ${getMarkerClass(
+            feature.properties.Last_Hour_PM25_Value
+          )}`;
+          el.innerText = feature.properties.Last_Hour_PM25_Value;
+          new mapboxgl.Marker(el)
+            .setLngLat(feature.geometry.coordinates)
+            .addTo(map);
+        })}
+      :
+    </div>
+  );
 };
 
 const MapContainer = () => {
   const [heatMapData, setHeatMapData] = useState(
     transformDataToGeoJson([], { longitude: "long", latitude: "lat" })
   );
-  const [monitoringSiteData, setMonitoringSiteMapData] = useState([]);
+  const [monitoringSiteData, setMonitoringSiteMapData] = useState(
+    transformDataToGeoJson([], { longitude: "Longitude", latitude: "Latitude" })
+  );
 
   useEffect(() => {
     const locationData = {
@@ -77,9 +122,16 @@ const MapContainer = () => {
       min_lat: 0.1,
       max_lat: 0.5,
     };
-    // getMonitoringSitesInfoApi('').then((responseData) => {
-    //   setMonitoringSiteMapData(responseData.airquality_monitoring_sites)
-    // }).catch((err) => {})
+    getMonitoringSitesInfoApi("")
+      .then((responseData) => {
+        setMonitoringSiteMapData(
+          transformDataToGeoJson(responseData.airquality_monitoring_sites, {
+            longitude: "Longitude",
+            latitude: "Latitude",
+          })
+        );
+      })
+      .catch((err) => {});
     heatmapPredictApi(locationData)
       .then((responseData) => {
         setHeatMapData(
@@ -98,6 +150,7 @@ const MapContainer = () => {
         center={[32.5600613, 0.3341424]}
         zoom={9}
         heatMapData={heatMapData}
+        monitoringSiteData={monitoringSiteData}
       />
     </div>
   );
