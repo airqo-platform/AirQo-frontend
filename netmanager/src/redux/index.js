5ba6d3ebbedcf5c36a21383d1ce12035bd81097d
ja-odur <odurjoseph8@gmail.com>
2020-11-26 13:58:46 +0300

    add mapdata reducers

--- netmanager/src/redux/index.js
+++ netmanager/src/redux/index.js
@@ -7,7 +7,8 @@ import dashboardReducer from "./Dashboard/reducers/dashboardReducer";
 import orgReducer from "./Join/reducers/orgReducer";
 import deviceRegistryReducer from "./DeviceRegistry/reducers";
 import locationRegistryReducer from "./LocationRegistry/reducers";
-import mainAlertReducer from "./MainAlert/reducers"
+import mainAlertReducer from "./MainAlert/reducers";
+import mapDataReducer from "./MapData/reducers";
 
 export default combineReducers({
   auth: authReducer,
@@ -19,4 +20,5 @@ export default combineReducers({
   deviceRegistry: deviceRegistryReducer,
   locationRegistry: locationRegistryReducer,
   mainAlert: mainAlertReducer,
+  mapData: mapDataReducer,
 });
