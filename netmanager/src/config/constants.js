1d781159e7d4ca6b644dc7ce06dbf1bc13437d34
ja-odur <odurjoseph8@gmail.com>
2020-12-18 01:51:09 +0300

    add device status uri

--- netmanager/src/config/constants.js
+++ netmanager/src/config/constants.js
@@ -53,7 +53,8 @@ const prodConfig = {
     "http://34.78.78.202:30006/api/v1/monitor/device/maintenance_logs/",
   ADD_MAINTENANCE_URI:
     "http://34.78.78.202:30001/api/v1/data/channels/maintenance/add",
-  DEVICE_RECENT_FEEDS: "http://34.78.78.202:30001/api/v1/data/feeds/transform/recent",
+  DEVICE_RECENT_FEEDS:
+    "http://34.78.78.202:30001/api/v1/data/feeds/transform/recent",
   REGISTER_DEVICE_URI: "http://34.78.78.202:30002/api/v1/devices/ts",
   ALL_DEVICES_URI: "http://34.78.78.202:30002/api/v1/devices",
   EDIT_DEVICE_URI: "http://34.78.78.202:30002/api/v1/devices/ts/update?device=",
@@ -67,6 +68,8 @@ const prodConfig = {
     "http://34.78.78.202:30006/api/v1/monitor/network/devices/bestperforming",
   GET_NETWORK_WORST_PERFORMING_DEVICES:
     "http://34.78.78.202:30006/api/v1/monitor/network/devices/worstperforming",
+  ALL_DEVICES_STATUS:
+    "http://34.78.78.202:30006/api/v1/monitor/network/devices/status",
   ADD_COMPONENT_URI:
     "http://34.78.78.202:30002/api/v1/devices/add/components?device=",
   GET_ONLINE_OFFLINE_MAINTENANCE_STATUS:
@@ -173,6 +176,8 @@ const devConfig = {
     "http://localhost:4001/api/v1/monitor/devices/offline",
   GET_NETWORK_UPTIME: "http://localhost:4001/api/v1/monitor/network/uptime",
   GET_DEVICE_UPTIME: "http://localhost:4001/api/v1/monitor/device/uptime",
+  ALL_DEVICES_STATUS:
+    "http://localhost:4001/api/v1/monitor/network/devices/status",
   GET_DEVICE_BATTERY_VOLTAGE:
     "http://127.0.0.1:4001/api/v1/monitor/device/batteryvoltage",
   GET_DEVICE_SENSOR_CORRELATION:
@@ -193,7 +198,8 @@ const devConfig = {
   UPDATE_LOCATION_URI: "http://127.0.0.1:4000/api/v1/location_registry/update",
   ADD_MAINTENANCE_URI:
     "http://localhost:3000/api/v1/data/channels/maintenance/add",
-  DEVICE_RECENT_FEEDS: "http://localhost:3000/api/v1/data/feeds/transform/recent",
+  DEVICE_RECENT_FEEDS:
+    "http://localhost:3000/api/v1/data/feeds/transform/recent",
   REGISTER_DEVICE_URI: "http://localhost:3000/api/v1/devices/ts",
   ALL_DEVICES_URI: "http://localhost:3000/api/v1/devices",
   EDIT_DEVICE_URI: "http://localhost:3000/api/v1/devices/ts/update?device=",
@@ -257,6 +263,8 @@ const stageConfig = {
     "http://34.78.78.202:31006/api/v1/monitor/device/maintenance_logs/",
   GET_DEVICE_POWER_TYPE:
     "http://34.78.78.202:31006/api/v1/monitor/device/power_type",
+  ALL_DEVICES_STATUS:
+    "http://34.78.78.202:31006/api/v1/monitor/network/devices/status",
   GET_DEVICE_STATUS_FOR_PIECHART_DISPLAY:
     "http://34.78.78.202:31006/api/v1/monitor/device/status/latest",
   GET_LATEST_OFFLINE_DEVICES:
@@ -271,7 +279,8 @@ const stageConfig = {
     "http://34.78.78.202:31006/api/v1/monitor/device/maintenance_logs/",
   ADD_MAINTENANCE_URI:
     "http://34.78.78.202:31001/api/v1/data/channels/maintenance/add",
-  DEVICE_RECENT_FEEDS: "http://34.78.78.202:31001/api/v1/data/feeds/transform/recent",
+  DEVICE_RECENT_FEEDS:
+    "http://34.78.78.202:31001/api/v1/data/feeds/transform/recent",
   REGISTER_DEVICE_URI: "http://34.78.78.202:31002/api/v1/devices/ts",
   ALL_DEVICES_URI: "http://34.78.78.202:31002/api/v1/devices",
   EDIT_DEVICE_URI: "http://34.78.78.202:31002/api/v1/devices/ts/update?device=",
