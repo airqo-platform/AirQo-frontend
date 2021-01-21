b7462dbf01e38be5b5d390194d03443091bec408
ja-odur <odurjoseph8@gmail.com>
2021-01-21 22:22:18 +0300

    add devices uptime actions and thunks

--- netmanager/src/redux/DeviceManagement/operations.js
+++ netmanager/src/redux/DeviceManagement/operations.js
@@ -2,10 +2,16 @@
 import {
   LOAD_DEVICES_STATUS_SUCCESS,
   LOAD_DEVICES_STATUS_FAILURE,
-    LOAD_NETWORK_UPTIME_SUCCESS,
-    LOAD_NETWORK_UPTIME_FAILURE
+  LOAD_NETWORK_UPTIME_SUCCESS,
+  LOAD_NETWORK_UPTIME_FAILURE,
+  LOAD_ALL_DEVICES_UPTIME_SUCCESS,
+  LOAD_ALL_DEVICES_UPTIME_FAILURE,
 } from "./actions";
-import { getDevicesStatusApi, getNetworkUptimeApi } from "views/apis/deviceMonitoring";
+import {
+  getDevicesStatusApi,
+  getNetworkUptimeApi,
+  getAllDevicesUptimeApi,
+} from "views/apis/deviceMonitoring";
 
 export const loadDevicesStatusData = () => async (dispatch) => {
   return await getDevicesStatusApi()
@@ -24,7 +30,7 @@ export const loadDevicesStatusData = () => async (dispatch) => {
 };
 
 export const loadNetworkUptimeData = (days) => async (dispatch) => {
-    return await getNetworkUptimeApi({ days })
+  return await getNetworkUptimeApi({ days })
     .then((responseData) => {
       dispatch({
         type: LOAD_NETWORK_UPTIME_SUCCESS,
@@ -37,4 +43,20 @@ export const loadNetworkUptimeData = (days) => async (dispatch) => {
         payload: err,
       });
     });
-}
+};
+
+export const loadAllDevicesUptimeData = (days) => async (dispatch) => {
+  return await getAllDevicesUptimeApi({ days })
+    .then((responseData) => {
+      dispatch({
+        type: LOAD_ALL_DEVICES_UPTIME_SUCCESS,
+        payload: responseData.data,
+      });
+    })
+    .catch((err) => {
+      dispatch({
+        type: LOAD_ALL_DEVICES_UPTIME_FAILURE,
+        payload: err,
+      });
+    });
+};
