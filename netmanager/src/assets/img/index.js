9d95d63fff69227cbc4ece994be1803424852867
ja-odur <odurjoseph8@gmail.com>
2021-01-24 23:42:39 +0300

    add pie-chart icon

--- netmanager/src/assets/img/index.js
+++ netmanager/src/assets/img/index.js
@@ -1,2 +1,4 @@
 export { SortAscendingIcon } from "./SortAscendingIcon";
 export { SortDescendingIcon } from "./SortDescendingIcon";
+export { PieChartIcon } from "./PieChartIcon";
+export { BarChartIcon } from "./BarChartIcon";
