1144b3a3a046c89dc41c6614f44513de03a021c2
Martin <bbaalmart@gmail.com>
2021-02-08 06:45:51 +0300

    add form field validation error messages and disable reset  button accordingly

--- netmanager/src/views/pages/ResetPassword/ResetPassword.js
+++ netmanager/src/views/pages/ResetPassword/ResetPassword.js
@@ -4,51 +4,76 @@ import axios from "axios";
 import Alert from "@material-ui/lab/Alert";
 import Collapse from "@material-ui/core/Collapse";
 import PropTypes from "prop-types";
+import { isEqual, isEmpty } from "underscore";
 
 import { Link, useLocation, BrowserRouter as Router } from "react-router-dom";
 
 import classnames from "classnames";
 import constants from "../../../config/constants";
 
+const validPasswordRegex = RegExp(/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/);
+
 export default function ResetPassword() {
-  const [state, setState] = useState({
-    password: "",
+  const passwordInitialState = {
     confirmPassword: "",
-  });
-
-  const [errors, setErrors] = useState({
     password: "",
-    confirmPassword: "",
-  });
+  };
 
-  const [alert, setAlert] = useState({
+  const alertInitialState = {
     open: false,
     message: "",
     severity: "",
-  });
+  };
+
+  const [newPassword, setNewPassword] = useState(passwordInitialState);
+
+  const [errors, setErrors] = useState(passwordInitialState);
+
+  const [alert, setAlert] = useState(alertInitialState);
 
   const [updated, setUpdated] = useState(false);
 
   const query = new URLSearchParams(useLocation().search);
-  console.log("query: ", query);
+
+  const isEnabled =
+    isEmpty(errors.confirmPassword) &&
+    isEmpty(errors.password) &&
+    !isEmpty(newPassword.password) &&
+    !isEmpty(newPassword.confirmPassword) &&
+    isEqual(newPassword.password, newPassword.confirmPassword)
+      ? true
+      : false;
 
   const onChange = (e) => {
-    setState({
-      ...state,
-      [e.target.id]: e.target.value,
+    const id = e.target.id;
+    const value = e.target.value;
+    const passwordErrors = {};
+
+    if (id === "password") {
+      passwordErrors.password = validPasswordRegex.test(value)
+        ? ""
+        : "Minimum six characters, at least one uppercase letter, one lowercase letter and one number!";
+    }
+    if (id === "confirmPassword") {
+      passwordErrors.confirmPassword =
+        value !== newPassword.password ? "passwords don't match" : "";
+    }
+
+    setErrors(passwordErrors);
+
+    setNewPassword({
+      ...newPassword,
+      [id]: value,
     });
   };
 
   const onSubmit = (e) => {
     e.preventDefault();
 
-    const { password } = state;
+    const { password } = newPassword;
 
     const token = query.get("token");
     const tenant = query.get("tenant");
-
-    console.log("the token: ", token);
-    console.log("the tenant: ", tenant);
 
     return axios
       .put(
@@ -65,7 +90,7 @@ export default function ResetPassword() {
       )
       .then((response) => {
         console.log("response: ", response.data);
-        setState({
+        setNewPassword({
           password: "",
           confirmPassword: "",
         });
@@ -96,60 +121,63 @@ export default function ResetPassword() {
             </h4>
           </div>
 
-          <form noValidate onSubmit={onSubmit}>
-            <div className="input-field col s12">
-              <Collapse in={alert.open}>
-                <Alert
-                  severity={alert.severity}
-                  onClose={() => setAlert({ ...alert, open: false })}
+          {!updated && (
+            <form noValidate onSubmit={onSubmit}>
+              <div className="input-field col s12">
+                <Collapse in={alert.open}>
+                  <Alert
+                    severity={alert.severity}
+                    onClose={() => setAlert({ ...alert, open: false })}
+                  >
+                    {alert.message}
+                  </Alert>
+                </Collapse>
+              </div>
+              <div className="input-field col s12">
+                <input
+                  onChange={onChange}
+                  value={newPassword.password}
+                  error={errors.password}
+                  id="password"
+                  type="password"
+                  className={classnames("", {
+                    invalid: errors.password,
+                  })}
+                />
+                <label htmlFor="password">Password</label>
+                <span className="red-text">{errors.password}</span>
+              </div>
+              <div className="input-field col s12">
+                <input
+                  onChange={onChange}
+                  value={newPassword.confirmPassword}
+                  error={errors.confirmPassword}
+                  id="confirmPassword"
+                  type="password"
+                  className={classnames("", {
+                    invalid: errors.confirmPassword,
+                  })}
+                />
+                <label htmlFor="confirmPassword">Confirm Password</label>
+                <span className="red-text">{errors.confirmPassword}</span>
+              </div>
+              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+                <button
+                  style={{
+                    width: "150px",
+                    borderRadius: "3px",
+                    letterSpacing: "1.5px",
+                    marginTop: "1rem",
+                  }}
+                  type="submit"
+                  disabled={!isEnabled}
+                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
                 >
-                  {alert.message}
-                </Alert>
-              </Collapse>
-            </div>
-            <div className="input-field col s12">
-              <input
-                onChange={onChange}
-                value={state.password}
-                error={errors.password}
-                id="password"
-                type="password"
-                className={classnames("", {
-                  invalid: errors.password,
-                })}
-              />
-              <label htmlFor="password">Password</label>
-              <span className="red-text">{errors.password}</span>
-            </div>
-            <div className="input-field col s12">
-              <input
-                onChange={onChange}
-                value={state.confirmPassword}
-                error={errors.confirmPassword}
-                id="confirmPassword"
-                type="password"
-                className={classnames("", {
-                  invalid: errors.confirmPassword,
-                })}
-              />
-              <label htmlFor="confirmPassword">Confirm Password</label>
-              <span className="red-text">{errors.confirmPassword}</span>
-            </div>
-            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-              <button
-                style={{
-                  width: "150px",
-                  borderRadius: "3px",
-                  letterSpacing: "1.5px",
-                  marginTop: "1rem",
-                }}
-                type="submit"
-                className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-              >
-                Reset
-              </button>
-            </div>
-          </form>
+                  Reset
+                </button>
+              </div>
+            </form>
+          )}
           {updated && (
             <div>
               <Link to="/" className="btn-flat waves-effect">
@@ -157,11 +185,11 @@ export default function ResetPassword() {
                 to home
               </Link>
               <p>
-                Your password has been successfully reset, please try logging in
+                Your password has been successfully reset, please try to login
                 again.
               </p>
               <p className="grey-text text-darken-1">
-                Already have an account? <Link to="/login">Log in</Link>
+                Navigate to the Login Page <Link to="/login">Log in</Link>
               </p>
             </div>
           )}
@@ -180,6 +208,6 @@ ResetPassword.propTypes = {
   }),
 };
 
-const mapSateToProps = (state) => ({
-  errors: state.errors,
+const mapSateToProps = (newPassword) => ({
+  errors: newPassword.errors,
 });
