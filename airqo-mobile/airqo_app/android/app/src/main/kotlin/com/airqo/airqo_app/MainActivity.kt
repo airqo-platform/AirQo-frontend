b24d29f30aa7af3adfa99b12dcf9863a0e212010
Noah Nsimbe <nsimbenoah@gmail.com>
2021-04-14 21:20:16 +0300

    inital setup

---
+++ airqo-mobile/airqo_app/android/app/src/main/kotlin/com/airqo/airqo_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.airqo.airqo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ airqo-mobile/airqo_app/lib/main.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Airqo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Airqo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'Hello there, welcome to the Airqo App',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
