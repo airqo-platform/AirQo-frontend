0533d2a0993c05ffdd363ea183d89983555e8280
ja-odur <odurjoseph8@gmail.com>
2021-04-16 17:07:33 +0300

    [PLAT-483] add sidebar widget

--- netmanager/src/views/layouts/common/Sidebar/components/index.js
+++ netmanager/src/views/layouts/common/Sidebar/components/index.js
@@ -1,2 +1,2 @@
-export { default as Profile } from './Profile';
-export { default as SidebarNav } from './SidebarNav';
+export { default as Profile } from "./Profile";
+export { default as SidebarNav, SidebarWidgets } from "./SidebarNav";
