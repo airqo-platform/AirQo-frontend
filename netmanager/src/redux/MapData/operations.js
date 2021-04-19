48937338e15ffa26b59ddd8a3cde37cbaecd8ac8
ja-odur <odurjoseph8@gmail.com>
2021-04-19 23:35:06 +0300

    [PLAT-485] add get events actions and operations

--- netmanager/src/redux/MapData/operations.js
+++ netmanager/src/redux/MapData/operations.js
@@ -4,10 +4,13 @@ import {
   LOAD_PM25_HEATMAP_DATA_FAILURE,
   LOAD_PM25_SENSOR_DATA_SUCCESS,
   LOAD_PM25_SENSOR_DATA_FAILURE,
+  LOAD_MAP_EVENTS_SUCCESS,
+  LOAD_MAP_EVENTS_FAILURE,
 } from "./actions";
 import { heatmapPredictApi } from "views/apis/predict";
 import { getMonitoringSitesInfoApi } from "views/apis/analytics";
 import { transformDataToGeoJson } from "views/pages/Map/utils";
+import { getEventsApi } from "views/apis/deviceRegistry";
 
 export const loadPM25HeatMapData = () => async (dispatch) => {
   return await heatmapPredictApi()
@@ -46,6 +49,33 @@ export const loadPM25SensorData = () => async (dispatch) => {
     .catch(() => {
       dispatch({
         type: LOAD_PM25_SENSOR_DATA_FAILURE,
+      });
+    });
+};
+
+export const loadMapEventsData = (params) => async (dispatch) => {
+  return await getEventsApi(params)
+    .then((responseData) => {
+      const payload = transformDataToGeoJson(
+        responseData.measurements,
+        {
+          longitude: "Longitude",
+          latitude: "Latitude",
+        },
+        (feature) => [
+          feature.location.longitude.value,
+          feature.location.latitude.value,
+        ]
+      );
+
+      dispatch({
+        type: LOAD_MAP_EVENTS_SUCCESS,
+        payload,
+      });
+    })
+    .catch(() => {
+      dispatch({
+        type: LOAD_MAP_EVENTS_FAILURE,
       });
     });
 };
