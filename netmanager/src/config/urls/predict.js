2a0e3bedf1e7b8e1619c3546499a1247c20dd8b8
ja-odur <odurjoseph8@gmail.com>
2021-04-27 21:48:07 +0300

    PLAT-117: add predict urls

---
+++ netmanager/src/config/urls/predict.js
@@ -0,0 +1,5 @@
+import { stripTrailingSlash } from "../utils";
+
+const BASE_PREDICT_URL = stripTrailingSlash(process.env.REACT_APP_BASE_URL);
+
+export const GET_HEATMAP_DATA = `${BASE_PREDICT_URL}/predict/heatmap`;
