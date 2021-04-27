5526e300ed3e002a27b2e2001681ae71789640c7
ja-odur <odurjoseph8@gmail.com>
2021-04-27 21:29:36 +0300

    PLAT-117: add locationRegistry urls

---
+++ netmanager/src/config/urls/locationRegistry.js
@@ -0,0 +1,17 @@
+import { stripTrailingSlash } from "../utils";
+
+const BASE_LOCATION_REGISTRY_URL = stripTrailingSlash(
+  process.env.REACT_APP_BASE_URL
+);
+
+export const ALL_LOCATIONS_URI = `${BASE_LOCATION_REGISTRY_URL}/location_registry/locations`;
+
+export const CREATE_ID_URI = `${BASE_LOCATION_REGISTRY_URL}/location_registry/create_id`;
+
+export const REGISTER_LOCATION_URI = `${BASE_LOCATION_REGISTRY_URL}/location_registry/register`;
+
+export const VIEW_LOCATION_URI = `${BASE_LOCATION_REGISTRY_URL}/location_registry/location?loc_ref=`;
+
+export const EDIT_LOCATION_DETAILS_URI = `${BASE_LOCATION_REGISTRY_URL}/location_registry/edit?loc_ref=`;
+
+export const UPDATE_LOCATION_URI = `${BASE_LOCATION_REGISTRY_URL}/location_registry/update`;
