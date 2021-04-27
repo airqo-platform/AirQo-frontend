52fdaa60b97e5938d7a26b1448b09a061f261fe6
ja-odur <odurjoseph8@gmail.com>
2021-04-27 21:24:39 +0300

    PLAT-117: add locate urls

---
+++ netmanager/src/config/urls/locate.js
@@ -0,0 +1,13 @@
+import { stripTrailingSlash } from "../utils";
+
+const BASE_LOCATE_URL = stripTrailingSlash(process.env.REACT_APP_BASE_URL);
+
+export const RUN_LOCATE_MODEL = `${BASE_LOCATE_URL}/map/parishes`;
+
+export const SAVE_LOCATE_MAP = `${BASE_LOCATE_URL}/map/savelocatemap`;
+
+export const GET_LOCATE_MAP = `${BASE_LOCATE_URL}/map/getlocatemap/`;
+
+export const UPDATE_LOCATE_MAP = `${BASE_LOCATE_URL}/map/updatelocatemap/`;
+
+export const DELETE_LOCATE_MAP = `${BASE_LOCATE_URL}/map/deletelocatemap/`;
