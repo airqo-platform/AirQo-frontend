fefd5a40ace88e6eeed5c6a51bc1e5c00458d2ba
ja-odur <odurjoseph8@gmail.com>
2021-04-27 23:45:53 +0300

    PLAT-117: remove un-used functions

--- netmanager/src/config/utils.js
+++ netmanager/src/config/utils.js
@@ -8,79 +8,8 @@ const URL_PATTERN = new RegExp(
   "i"
 );
 
-const validateUrl = (url) => URL_PATTERN.test(url);
+export const validateUrl = (url) => URL_PATTERN.test(url);
 
 export const stripTrailingSlash = (url) => {
   return url.replace(/\/$/, "");
-};
-
-export const joinUrl = (baseUrl, absoluteUrl, options) => {
-  const { skipStrip } = options || {};
-  if (skipStrip) {
-    return baseUrl.concat(absoluteUrl);
-  }
-  return stripTrailingSlash(baseUrl).concat(absoluteUrl);
-};
-
-export const attachPort = (baseUrl, port) => {
-  return stripTrailingSlash(baseUrl).concat(":").concat(port);
-};
-
-export const createAbsoluteUrls = (
-  baseUrl,
-  servicePortMapper,
-  apiVersion,
-  relativeUrlsObject
-) => {
-  return Object.keys(relativeUrlsObject).reduce((joinedUrls, next) => {
-    const [service, relativeUrl] = relativeUrlsObject[next];
-    const port = servicePortMapper[service];
-
-    if (!port) {
-      throw Error(`Unknown service ${service}`);
-    }
-    const absoluteUrl = joinUrl(
-      joinUrl(attachPort(baseUrl, port), apiVersion),
-      relativeUrl
-    );
-
-    if (!validateUrl(absoluteUrl)) {
-      throw Error(`${absoluteUrl} generated url is invalid`);
-    }
-    joinedUrls[next] = absoluteUrl;
-    return joinedUrls;
-  }, {});
-};
-
-export const createUrlFactory = (baseUrl, apiVersion, protocol) => {
-  const allowedProtocols = ["http", "https"];
-  return (relativeUrl, options) => {
-    let { overwrittenBaseUrl, overwrittenApiVersion, overwrittenProtocol } =
-      options || {};
-
-    if (overwrittenProtocol || protocol) {
-      if (!allowedProtocols.includes(overwrittenProtocol || protocol)) {
-        throw new Error("Unknown protocol");
-      }
-    } else {
-      overwrittenProtocol = "";
-    }
-    const protocolLessUrl = joinUrl(
-      overwrittenBaseUrl || baseUrl,
-      joinUrl(overwrittenApiVersion || apiVersion, relativeUrl)
-    );
-
-    const url = joinUrl(
-      overwrittenProtocol || protocol
-        ? `${overwrittenProtocol || protocol}://`
-        : "",
-      protocolLessUrl,
-      { skipStrip: true }
-    );
-
-    if (!validateUrl(url)) {
-      throw new Error(`${url} generated url is invalid`);
-    }
-    return url;
-  };
 };
