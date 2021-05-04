d5762cc50e7405ea1c59374f58ee47f84ecf450c
ja-odur <odurjoseph8@gmail.com>
2021-05-04 13:23:49 +0300

    [PLAT-528]: fix linting

--- netmanager/src/redux/MainAlert/operations.js
+++ netmanager/src/redux/MainAlert/operations.js
@@ -1,29 +1,28 @@
 // for representing chained operations using redux-thunk
 import {
-    UPDATE_ALERT_SUCCESS,
-    HIDE_ALERT_SUCCESS,
-    RESET_ALERT_SUCCESS,
+  UPDATE_ALERT_SUCCESS,
+  HIDE_ALERT_SUCCESS,
+  RESET_ALERT_SUCCESS,
 } from "./actions";
-
 
 export const updateMainAlert = (newAlertData) => {
   return (dispatch) => {
     dispatch({
-        type: UPDATE_ALERT_SUCCESS,
-        payload: newAlertData,
+      type: UPDATE_ALERT_SUCCESS,
+      payload: newAlertData,
     });
   };
 };
 
 export const hideMainAlert = () => {
-    return (dispatch) => {
-        dispatch({
-            type: HIDE_ALERT_SUCCESS,
-            payload: { show: false, message: "", severity: "success" },
-        })
-    }
+  return (dispatch) => {
+    dispatch({
+      type: HIDE_ALERT_SUCCESS,
+      payload: { show: false, message: "", severity: "success" },
+    });
+  };
 };
 
-export const resetAlertState = () => dispatch => {
-    dispatch({ type: RESET_ALERT_SUCCESS });
+export const resetAlertState = () => (dispatch) => {
+  dispatch({ type: RESET_ALERT_SUCCESS });
 };
