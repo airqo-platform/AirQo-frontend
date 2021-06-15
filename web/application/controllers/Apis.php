4028b36a4bffe1085101792f82cbf443c9317269
danielogen <danielogen@gmail.com>
2021-06-15 15:52:44 +0300

    moved from assets_job folder

--- web/application/controllers/Apis.php
+++ web/application/controllers/Apis.php
@@ -1202,6 +1202,76 @@ public function airqoPlace24Hours()
         }
     }
 
+    // moved from assets/job folder
+    /*
+    * Description: update node details incase a node has been rename, removed, etc
+    * Frequency: daily, every midnight
+    */
+    public function airqoJobUpdateNodeDetails(){
+        $devices = array();
+        $response = array();
+        $this->ApisModel->init();
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_URL, DEVICES_DATA);
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        $json = json_decode($result);
+
+        if ($json) {
+            
+            $success = $json->success;
+            
+            if($success == true) {
+                $devices = $json->devices;
+                
+                $sql_delete = "TRUNCATE tbl_app_nodes";
+                    
+                if($this->db->query($sql_delete)) {
+                    $total = 0;
+                    foreach($devices as $device) {
+                
+                        $device_id          = $device->channelID;
+                        $device_name        = $device->siteName;
+                        $device_location    = $device->locationName;
+                        $device_lat         = number_format((float)$device->latitude, 8, '.', '');
+                        $device_lng         = number_format((float)$device->longitude, 8, '.', '');
+                        $device_type        = 'Commercial area';
+                        $delete_status      = '0';
+                        
+                        if($device->isActive == true){
+                            $active_status = '1';
+                            
+                            $sql_insert = "INSERT INTO tbl_app_nodes (an_id, an_channel_id, an_name, an_lat, an_lng, an_map_address, an_type, an_active, an_deleted) VALUES (NULL, '$device_id', '$device_name', '$device_lat', '$device_lng', '$device_location', '$device_type', '$active_status', '$delete_status')";
+                            $update_node_details = $this->db->query($sql_insert);
+
+                            $total =  $total + 1;
+                        }
+                    }
+
+                    if($total > 0){
+                        $state      = $this->ApisModel->stateOk();
+                        $state_name = "success";
+                        $state_code = 100;
+                        $message    = "Sucessful (".$total." places updated)";
+                        $debug      = "API Config OK";
+                        echo  $this->ApisModel->api_response($response, $state, $state_name, $state_code, $message, $debug);  
+                    }else{
+                        $state      = $this->ApisModel->stateOk();
+                        $state_name = "success";
+                        $state_code = 109;
+                        $message    = "No places to update";
+                        $debug      = "API Config OK";
+                        echo  $this->ApisModel->api_response($response, $state, $state_name, $state_code, $message, $debug);
+                    }
+                }
+            }
+        }
+    }
+
     // public function placeForecast()
     // {   
     //     $response = array();
