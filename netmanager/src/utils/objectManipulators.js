7c5ff47ec00e693c0aaa2d51a8918bc7b3990aa0
ja-odur <odurjoseph8@gmail.com>
2021-08-03 23:17:41 +0300

    [PLAT-692] create object manipulators

---
+++ netmanager/src/utils/objectManipulators.js
@@ -0,0 +1,25 @@
+import { clone as _clone, each as _each } from "underscore";
+
+export const dropEmpty = (obj) => {
+  let clone = _clone(obj);
+  _each(clone, function (v, k) {
+    if (!v) {
+      delete clone[k];
+    }
+  });
+  return clone;
+};
+
+export const mergeErrorObjects = (errorsArr, { errorKey, errorMsgKey }) => {
+  const mergedError = {};
+  errorsArr.map((error) => {
+    mergedError[error[errorKey]] = error[errorMsgKey];
+  });
+  return mergedError;
+};
+
+export const createAlertBarExtraContent = (arr, callback) => {
+  const extra = [];
+  arr.map((value, key) => extra.push(callback(value, key)));
+  return extra;
+};
