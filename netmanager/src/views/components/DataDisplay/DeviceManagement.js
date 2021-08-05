5aea743873d30de872d964e078a7b32b04d1a632
ja-odur <odurjoseph8@gmail.com>
2021-08-05 12:55:19 +0300

    sort device leaderboard by uptime and name

--- netmanager/src/views/components/DataDisplay/DeviceManagement.js
+++ netmanager/src/views/components/DataDisplay/DeviceManagement.js
@@ -201,11 +201,20 @@ export default function DeviceManagement() {
         uptime: uptimeSum / deviceUptime.length,
       });
     });
+
+    const sortByName = (device1, device2) => {
+      if (device1.deviceName.toLowerCase() > device2.deviceName.toLowerCase())
+        return 1;
+      if (device1.deviceName.toLowerCase() < device2.deviceName.toLowerCase())
+        return -1;
+      return 0;
+    };
+
     // reverse sorting
     averageUptime.sort((device1, device2) => {
       if (device1.uptime < device2.uptime) return 1;
       if (device1.uptime > device2.uptime) return -1;
-      return 0;
+      return sortByName(device1, device2);
     });
     return averageUptime;
   };
