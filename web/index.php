4ef109b81236961c80fde79e695d6e0a82cba923
danielogen <danielogen@gmail.com>
2021-08-09 08:58:46 +0300

    turn on error reporting in dev envirnoment

--- web/index.php
+++ web/index.php
@@ -69,8 +69,8 @@ switch (ENVIRONMENT)
 	case 'development':
 // 		error_reporting(-1);
 // 		ini_set('display_errors', 1);
-        error_reporting(E_ALL);
-		ini_set('display_errors', 0);
+        error_reporting(E_ERROR);
+		ini_set('display_errors', 1);
 	break;
 
 	case 'staging':
