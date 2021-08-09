66462768e2a11fd8f0cbb00f380f6a0abfeb926e
ja-odur <odurjoseph8@gmail.com>
2021-08-09 14:00:05 +0300

    [PLAT-698] add filter site options util

--- netmanager/src/utils/sites.js
+++ netmanager/src/utils/sites.js
@@ -1,10 +1,23 @@
+import { isEmpty } from "underscore";
+
+const siteLabel = (name, description, generated_name) =>
+  `${name || description || generated_name} (${generated_name})`;
+
 export const createSiteOptions = (sites) => {
   const siteOptions = [];
   sites.map(({ name, description, generated_name, ...rest }) => {
     siteOptions.push({
-      label: `${name || description || generated_name} (${generated_name})`,
+      label: siteLabel(name, description, generated_name),
       value: rest._id,
     });
   });
   return siteOptions;
+};
+
+export const filterSite = (sites, site_id) => {
+  const currentSite = sites.filter((site) => site.value === site_id);
+  if (isEmpty(currentSite)) {
+    return { label: "", value: null };
+  }
+  return currentSite[0];
 };
