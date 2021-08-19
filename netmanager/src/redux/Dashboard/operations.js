574614a72675efc62bbd3e91e240d57cfc127057
ja-odur <odurjoseph8@gmail.com>
2021-08-19 22:31:33 +0300

    [PLAt-701] disable filtering of defaults

--- netmanager/src/redux/Dashboard/operations.js
+++ netmanager/src/redux/Dashboard/operations.js
@@ -13,9 +13,8 @@ import {
   LOAD_DASHBOARD_SITES_FAILURE,
 } from "./actions";
 import { DEFAULTS_URI } from "config/urls/authService";
-import { KCCAInitialUserDefaultGraphsState } from "./constants";
-import { filterDefaults } from "./utils";
 import { getMonitoringSitesLocationsApi } from "views/apis/location";
+import { getUserChartDefaultsApi } from "views/apis/authService";
 import { getSitesApi } from "views/apis/analytics";
 
 export const loadSites = () => async (dispatch) => {
@@ -59,10 +58,9 @@ export const loadUserDefaultGraphData = () => {
       .get(DEFAULTS_URI, { params: { user } })
       .then((res) => res.data)
       .then((userDefaultsData) => {
-        const { defaults } = userDefaultsData;
         dispatch({
           type: LOAD_USER_DEFAULT_GRAPHS_SUCCESS,
-          payload: filterDefaults(defaults, KCCAInitialUserDefaultGraphsState),
+          payload: userDefaultsData.defaults || [],
         });
       })
       .catch((err) => {
@@ -73,6 +71,30 @@ export const loadUserDefaultGraphData = () => {
       });
   };
 };
+
+// export const setUserDefaultGraphDataNew = (filter) => {
+//   return async (dispatch, getState) => {
+//     const user = getState().auth.user._id;
+//     const { chartTitle } = filter;
+//     return await axios
+//       .put(DEFAULTS_URI, filter, {
+//         params: { user, chartTitle },
+//       })
+//       .then((res) => res.data)
+//       .then((responseData) => {
+//         dispatch({
+//           type: SET_USER_DEFAULTS_GRAPHS_SUCCESS,
+//           payload: responseData,
+//         });
+//       })
+//       .catch((err) => {
+//         dispatch({
+//           type: SET_USER_DEFAULTS_GRAPHS_ERROR,
+//           payload: err,
+//         });
+//       });
+//   };
+// };
 
 export const setUserDefaultGraphData = (filter) => {
   return async (dispatch, getState) => {
