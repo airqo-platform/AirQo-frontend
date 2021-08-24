7a5652f3db9c59ff5f9941a5311beb9eed77bf3d
ja-odur <odurjoseph8@gmail.com>
2021-08-25 00:13:43 +0300

    [PLAT-701] remove commented code

--- netmanager/src/redux/Dashboard/operations.js
+++ netmanager/src/redux/Dashboard/operations.js
@@ -72,30 +72,6 @@ export const loadUserDefaultGraphData = () => {
   };
 };
 
-// export const setUserDefaultGraphDataNew = (filter) => {
-//   return async (dispatch, getState) => {
-//     const user = getState().auth.user._id;
-//     const { chartTitle } = filter;
-//     return await axios
-//       .put(DEFAULTS_URI, filter, {
-//         params: { user, chartTitle },
-//       })
-//       .then((res) => res.data)
-//       .then((responseData) => {
-//         dispatch({
-//           type: SET_USER_DEFAULTS_GRAPHS_SUCCESS,
-//           payload: responseData,
-//         });
-//       })
-//       .catch((err) => {
-//         dispatch({
-//           type: SET_USER_DEFAULTS_GRAPHS_ERROR,
-//           payload: err,
-//         });
-//       });
-//   };
-// };
-
 export const setUserDefaultGraphData = (filter) => {
   return async (dispatch, getState) => {
     const user = getState().auth.user._id;
