27e50da4b7fd7e3a5e74e8808d6047c08bc9db87
ja-odur <odurjoseph8@gmail.com>
2021-09-06 11:31:06 +0300

    [PLAT-736] configure entrypoint

---
+++ website/frontend/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import './index.css'
+
+const App = () => {
+    return (
+        <div>
+            <h1>AirQo</h1>
+            <h2>React App Set Successfully</h2>
+            <h3>Date : {new Date().toDateString()}</h3>
+        </div>
+    )
+}
+
+const root = document.getElementById('app');
+
+// Bootstrap the main app
+render(<App />, root);
