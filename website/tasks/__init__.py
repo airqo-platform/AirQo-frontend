cdbd25e24f63cd342cce512158ab313970cb0f75
ja-odur <odurjoseph8@gmail.com>
2021-09-07 17:18:12 +0300

    [PLAT-736] remove set-node-path. we will be using direnv instead

--- website/tasks/__init__.py
+++ website/tasks/__init__.py
@@ -4,7 +4,6 @@ from .testing import lint_js, lint_py, test_python
 from .dev import (
     prettier_js,
     prettier_py,
-    set_node_path,
     build_prod,
     webpack_dev_server,
     python_server
@@ -17,7 +16,6 @@ task_col.add_task(prettier_py)
 task_col.add_task(lint_js)
 task_col.add_task(lint_py)
 task_col.add_task(test_python)
-task_col.add_task(set_node_path)
 task_col.add_task(build_prod)
 task_col.add_task(webpack_dev_server)
 task_col.add_task(python_server)
--- website/tasks/dev.py
+++ website/tasks/dev.py
@@ -12,12 +12,6 @@ def prettier_py(context):
     run('autopep8 --in-place --recursive .', echo=True)
 
 
-@task(name='node-path')
-def set_node_path(context):
-    """set node path"""
-    run('bin/functions/set_node_path.sh', echo=True)
-
-
 @task(name='build')
 def build_prod(context):
     """run webpack build"""
