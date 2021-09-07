5815f28f6d759f4c3b8c628b3e1e32fc7bce49c9
ja-odur <odurjoseph8@gmail.com>
2021-09-07 19:09:18 +0300

    [PLAT-736] move secret_key to .env

--- website/backend/settings.py
+++ website/backend/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '@8#@vl151uz-v02!$(83^_+^9rlj^67u%)_qeo^9oldx%*wvkc'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
