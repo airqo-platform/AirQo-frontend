38d6e83291c497c945fcaa45d0d7e5ef142f4aaa
ja-odur <odurjoseph8@gmail.com>
2021-09-14 20:05:10 +0300

    [PLAT-750] add faq model

--- website/backend/FAQ/models.py
+++ website/backend/FAQ/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class FAQ(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    question = models.CharField(max_length=150)
+    answer = models.TextField()
