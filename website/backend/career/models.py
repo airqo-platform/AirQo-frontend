0e0434e938c2039dc707a9ffe7264e3c3ff13999
ja-odur <odurjoseph8@gmail.com>
2021-09-14 20:35:08 +0300

    [PLAT-750] add career model

--- website/backend/career/models.py
+++ website/backend/career/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Career(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    expiry = models.DateTimeField()
