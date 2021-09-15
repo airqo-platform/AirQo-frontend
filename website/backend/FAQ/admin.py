dd7dee9f6fa0199723e96e4367eeff780533b504
ja-odur <odurjoseph8@gmail.com>
2021-09-16 00:18:26 +0300

    [PLAT-750] add faq to admin site

--- website/backend/FAQ/admin.py
+++ website/backend/FAQ/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import FAQ
+
 
 # Register your models here.
+@admin.register(FAQ)
+class FAQAdmin(admin.ModelAdmin):
+    list_display = ('question', 'answer', 'created_at')
+    readonly_fields = ('created_at', 'updated_at')
+    list_per_page = 10
+    search_fields = ('question', 'answer')
