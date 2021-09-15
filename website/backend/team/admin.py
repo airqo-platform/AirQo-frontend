f449a0379e0f2b5789a33cb0c23089efac901f6a
ja-odur <odurjoseph8@gmail.com>
2021-09-15 22:07:58 +0300

    [PLAT-750] add Member model to admin site

--- website/backend/team/admin.py
+++ website/backend/team/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Member
+
 
 # Register your models here.
+@admin.register(Member)
+class MemberAdmin(admin.ModelAdmin):
+    list_display = ('name', 'title', 'picture')
+    readonly_fields = ('created_at', 'updated_at')
+    list_per_page = 10
+    search_fields = ('name', 'title')
