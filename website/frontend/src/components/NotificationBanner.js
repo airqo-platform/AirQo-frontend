70cb8f3bf294fafdd37f0d8a8a01a576e44f6115
ja-odur <odurjoseph8@gmail.com>
2021-10-13 15:14:51 +0300

    [PLAT-817] add cancel svg icon

---
+++ website/frontend/src/components/NotificationBanner.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Cancel from '../icons/nav/Cancel';
+
+const NotificationBanner = () => (
+        <div className="NotificationBanner">
+            <div className="wrapper">
+                <span className="first">Air Quality monitoring in your home. coming soon!</span>
+                <span className="second">Join the waiting list</span>
+            </div>
+            <Cancel />
+        </div>
+);
+
+export default NotificationBanner;
