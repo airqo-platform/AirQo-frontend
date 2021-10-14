04376e610196c766cf9a546765345907b61e932f
ja-odur <odurjoseph8@gmail.com>
2021-10-14 10:43:17 +0300

    [PLAT-817] add navBar to main app

--- website/frontend/App.js
+++ website/frontend/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import NotificationBanner from './src/components/NotificationBanner';
+import { TopBar } from './src/components/nav';
 
 const App = () => (
     <div>
         <NotificationBanner />
+        <TopBar />
     </div>
 );
 
