00001e0f8c93090dbc9efb32750049be1ce24206
ja-odur <odurjoseph8@gmail.com>
2021-10-17 18:24:22 +0300

    [PLAT-817] add footer content section

--- website/frontend/src/components/Footer.js
+++ website/frontend/src/components/Footer.js
@@ -1,11 +1,49 @@
 import React from 'react';
-import MakText from "../icons/nav/MakText";
+import MakText from 'icons/nav/MakText';
+import AirQo from 'icons/footer/AirQo';
+import Instagram from 'icons/footer/Instagram';
+import Facebook from 'icons/footer/Facebook';
+import Youtube from 'icons/footer/Youtube';
+import Twitter from 'icons/footer/Twitter';
 
-const Footer = () => {
-    return (
+const Footer = () => (
         <footer className="Footer">
             <div className="body-section">
-
+                <div className="logo">
+                    <AirQo />
+                    <div className="social-links">
+                        <Instagram />
+                        <Facebook />
+                        <Youtube />
+                        <Twitter />
+                    </div>
+                </div>
+                <div className="content">
+                    <div className="section">
+                        <div>Products</div>
+                        <div>AirQo Monitors</div>
+                        <div>AirQo Analytics</div>
+                        <div>AirQo App</div>
+                        <div>AirQo API</div>
+                        <div>Research</div>
+                    </div>
+                    <div className="section">
+                        <div>Company</div>
+                        <div>About us</div>
+                        <div>Contact us</div>
+                        <div>Careers</div>
+                        <div>Sustainability</div>
+                        <div>COVID19</div>
+                    </div>
+                    <div className="section">
+                        <div>Resources</div>
+                        <div>Help Center</div>
+                        <div>Developer</div>
+                        <div>Partners</div>
+                        <div>Press</div>
+                        <div>Access to Data</div>
+                    </div>
+                </div>
             </div>
             <div className="copyright-section">
                 <div className="copyright-container">
@@ -21,7 +59,6 @@ const Footer = () => {
                 </div>
             </div>
         </footer>
-    )
-}
+);
 
 export default Footer;
