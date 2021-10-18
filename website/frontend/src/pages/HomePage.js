ad20124eeee1e7214dae7f51c0223474e459573c
ja-odur <odurjoseph8@gmail.com>
2021-10-18 21:16:38 +0300

    [PLAT-817] add subscribe html

--- website/frontend/src/pages/HomePage.js
+++ website/frontend/src/pages/HomePage.js
@@ -4,16 +4,26 @@ import NavTab from '../components/nav/NavTab';
 
 const HomePage = () => (
     <div className="HomePage">
-        <div className="container">
-            <div className="text-container">
-                <div className="text-primary">Clean Air for all African Cities </div>
-                <div className="text-secondary">We empower communities with accurate, hyperlocal and timely air quality data to drive air pollution mitigation actions.</div>
-                <div className="btn-container">
-                    <NavTab text="Get the App — It’s free" width={236} hideArrow filled style={{ marginRight: '17px' }} />
-                    <NavTab text="Get involved" width={160} hideArrow colored />
+        <div className="intro-container">
+            <div className="container">
+                <div className="text-container">
+                    <div className="text-primary">Clean Air for all African Cities </div>
+                    <div className="text-secondary">We empower communities with accurate, hyperlocal and timely air quality data to drive air pollution mitigation actions.</div>
+                    <div className="btn-container">
+                        <NavTab text="Get the App — It’s free" width={236} hideArrow filled style={{ marginRight: '17px' }} />
+                        <NavTab text="Get involved" width={160} hideArrow colored />
+                    </div>
                 </div>
+                <PlaceHolder />
             </div>
-            <PlaceHolder />
+        </div>
+        <div className="subscribe-container">
+            <div className="text-primary">Get air quality updates!</div>
+            <div className="text-secondary">Subscribe to our newsletter and learn about the quality of the air you are breathing</div>
+            <div className="btn-container">
+                <div className="input"><div className="text">Enter email</div></div>
+                <div className="button">Subscribe</div>
+            </div>
         </div>
     </div>
 );
