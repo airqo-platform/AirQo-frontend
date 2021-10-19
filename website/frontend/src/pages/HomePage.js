2cc0c45b467a71b550672e60b092fe0a09eaccbe
ja-odur <odurjoseph8@gmail.com>
2021-10-19 15:55:35 +0300

    [PLAT-817] add partners markup

--- website/frontend/src/pages/HomePage.js
+++ website/frontend/src/pages/HomePage.js
@@ -1,6 +1,18 @@
 import React from 'react';
 import PlaceHolder from 'icons/homepage/PlaceHolder';
+import Google from 'icons/homepage/partners/google.svg';
+import WorldBankGroup from 'icons/homepage/partners/worldbankgroup.svg';
+import BirminghamUniversity from 'icons/homepage/partners/birmingham-university.svg';
+import Zindi from 'icons/homepage/partners/zindi.svg';
+import ESPRC from 'icons/homepage/partners/EPSRC.svg';
+import NRF from 'icons/homepage/partners/NRF.svg';
+import ColumbiaUniversity from 'icons/homepage/partners/columbia-university.svg';
+import ASAP from 'icons/homepage/partners/ASAP.svg';
 import NavTab from '../components/nav/NavTab';
+
+const Icon = ({ icon }) => (
+        <div className="icon">{icon}</div>
+);
 
 const HomePage = () => (
     <div className="HomePage">
@@ -15,6 +27,21 @@ const HomePage = () => (
                     </div>
                 </div>
                 <PlaceHolder />
+            </div>
+        </div>
+        <div className="partner-section">
+            <div>
+                <div className="text">Our Partners</div>
+                <div className="icon-wrapper">
+                    <Icon icon={<Google height={76} width={164} />} />
+                    <Icon icon={<WorldBankGroup width={124} height={124} />} />
+                    <Icon icon={<BirminghamUniversity width={220} height={102} />} />
+                    <Icon icon={<Zindi width={220} height={102} />} />
+                    <Icon icon={<ESPRC width={192} height={90} />} />
+                    <Icon icon={<NRF width={234} height={108} />} />
+                    <Icon icon={<ColumbiaUniversity width={220} height={102} />} />
+                    <Icon icon={<ASAP width={220} height={102} />} />
+                </div>
             </div>
         </div>
         <div className="subscribe-container">
