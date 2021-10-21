988cd2916803207bcba08cd9342f7989ca99d71b
ja-odur <odurjoseph8@gmail.com>
2021-10-21 18:00:43 +0300

    [PLAT-842] reimplement manager map

---
+++ netmanager/src/views/components/DataDisplay/Map/MapBoxMap.js
@@ -0,0 +1,112 @@
+import React, { useState } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import { useHistory } from "react-router-dom";
+
+import "assets/css/manager-map.css";
+
+const onlineClassGenerator = (online) =>
+  (online && "manager-online") || "manager-offline";
+
+const maintenanceClassGenerator = (maintenanceStatus) => {
+  if (maintenanceStatus === "overdue") return "manager-overdue";
+  if (maintenanceStatus === "due") return "manager-due";
+  if (maintenanceStatus === -1) return "manager-unknown";
+  return "manager-good";
+};
+
+const maintenanceLabelColorGenerator = (maintenanceStatus) => {
+  if (maintenanceStatus === "overdue") return "popup-danger";
+  if (maintenanceStatus === "due") return "popup-warning";
+  if (maintenanceStatus === -1) return "popup-grey";
+  return "popup-success";
+};
+
+const MapBoxMap = ({ devices }) => {
+  const history = useHistory();
+  const [viewport, setViewport] = useState({
+    latitude: 0.3341424,
+    longitude: 32.5600613,
+    zoom: 7,
+  });
+
+  const [selectedDevice, setSelectedDevice] = useState(null);
+
+  const handleDetailsClick = (device) => (event) => {
+    event.preventDefault();
+    history.push(`/device/${device.name}/overview`);
+  };
+
+  return (
+    <div className="manager-map-container">
+      <ReactMapGL
+        {...viewport}
+        width="100%"
+        height="100%"
+        onViewportChange={(viewport) => setViewport(viewport)}
+        mapStyle={"mapbox://styles/mapbox/streets-v11"}
+        mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+      >
+        {devices.map(
+          (device) =>
+            device.latitude &&
+            device.longitude && (
+              <Marker
+                key={device.name}
+                longitude={device.longitude}
+                latitude={device.latitude}
+              >
+                <div
+                  onClick={() => setSelectedDevice(device)}
+                  className={`manager-marker ${onlineClassGenerator(
+                    device.isOnline
+                  )} ${maintenanceClassGenerator(device.maintenance_status)}`}
+                />
+              </Marker>
+            )
+        )}
+        {selectedDevice && (
+          <Popup
+            longitude={selectedDevice.longitude}
+            latitude={selectedDevice.latitude}
+            offsetLeft={17}
+            offsetTop={5}
+            onClose={() => setSelectedDevice(null)}
+          >
+            <div className={"popup-container"}>
+              <span>
+                <b>Device Name</b>: {selectedDevice.name}
+              </span>
+              <span>
+                <b>Status</b>:{" "}
+                {selectedDevice.isOnline ? (
+                  <span className={"popup-success"}>online</span>
+                ) : (
+                  <span className={"popup-danger"}>offline</span>
+                )}
+              </span>
+              <span>
+                <b>Maintenance Status</b>:{" "}
+                <span
+                  className={maintenanceLabelColorGenerator(
+                    selectedDevice.maintenance_status
+                  )}
+                >
+                  {(selectedDevice.maintenance_status === -1 && "not set") ||
+                    selectedDevice.maintenance_status}
+                </span>
+              </span>
+              <a
+                className={"popup-more-details"}
+                onClick={handleDetailsClick(selectedDevice)}
+              >
+                Device details
+              </a>
+            </div>
+          </Popup>
+        )}
+      </ReactMapGL>
+    </div>
+  );
+};
+
+export default MapBoxMap;
