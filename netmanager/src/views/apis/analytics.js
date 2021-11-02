e4db6374176c82c8a5735d86c8c005d36302dfb2
ja-odur <odurjoseph8@gmail.com>
2021-11-02 23:10:36 +0300

    [PLAT-849] add reports api endpoints

--- netmanager/src/views/apis/analytics.js
+++ netmanager/src/views/apis/analytics.js
@@ -3,6 +3,8 @@ import {
   GET_DATA_MAP,
   GET_SITES,
   DOWNLOAD_CUSTOMISED_DATA_URI,
+  REPORTS_URL,
+  REPORTS_ATTRIBUTE_DATA,
 } from "config/urls/analytics";
 
 export const getMonitoringSitesInfoApi = async (pm25Category) => {
@@ -16,7 +18,39 @@ export const getSitesApi = async () => {
 };
 
 export const downloadDataApi = async (downloadType, data) => {
-  return axios
+  return await axios
     .post(DOWNLOAD_CUSTOMISED_DATA_URI, data, { params: { downloadType } })
+    .then((response) => response.data);
+};
+
+export const createReportApi = async (data) => {
+  return await axios.post(REPORTS_URL, data).then((response) => response.data);
+};
+
+export const getAllReportsApi = async () => {
+  return await axios.get(REPORTS_URL).then((response) => response.data);
+};
+
+export const getSingleReportApi = async (reportId) => {
+  return await axios
+    .get(`${REPORTS_URL}/${reportId}`)
+    .then((response) => response.data);
+};
+
+export const updateReportApi = async (reportId, data) => {
+  return await axios
+    .patch(`${REPORTS_URL}/${reportId}`, data)
+    .then((response) => response.data);
+};
+
+export const deleteReportApi = async (reportId) => {
+  return await axios
+    .delete(`${REPORTS_URL}/${reportId}`)
+    .then((response) => response.data);
+};
+
+export const getReportAttributeDataApi = async (params, data) => {
+  return await axios
+    .get(REPORTS_ATTRIBUTE_DATA, { params, data })
     .then((response) => response.data);
 };
