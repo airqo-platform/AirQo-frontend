d7da06d122c54e7f61eaa192e057d4f08322f430
ja-odur <odurjoseph8@gmail.com>
2021-11-04 13:51:48 +0300

    [PLAT-850] add airqloud selection capability

--- netmanager/src/views/containers/AirQloudDropDown.js
+++ netmanager/src/views/containers/AirQloudDropDown.js
@@ -1,34 +1,49 @@
 import React from "react";
+import { useDispatch } from "react-redux";
 import { useAirQloudsData } from "utils/customHooks/AirQloudsHooks";
+import { useCurrentAirQloudData } from "redux/AirQloud/selectors";
+import { setCurrentAirQloudData } from "redux/AirQloud/operations";
 
 // styles
 import "assets/css/dropdown.css";
 
 const AirQloudDropDown = () => {
+  const currentAirqQloud = useCurrentAirQloudData();
+  const dispatch = useDispatch();
+
   const airqlouds = Object.values(useAirQloudsData());
+
   airqlouds.sort((a, b) => {
     if (a.long_name < b.long_name) return -1;
     if (a.long_name > b.long_name) return 1;
     return 0;
   });
 
-  console.log(airqlouds);
+  const handleAirQloudChange = (airqloud) => () =>
+    dispatch(setCurrentAirQloudData(airqloud));
+
   return (
     <label className="dropdown">
-      <div className="dd-button">Dropdown</div>
+      <div className="dd-button">{currentAirqQloud.long_name}</div>
 
       <input type="checkbox" className="dd-input" id="test" />
 
       <ul className="dd-menu">
-        <li>
-          <a href="http://rane.io">Link to Rane.io</a>
+        <li className="selected">
+          {currentAirqQloud.long_name} AirQloud{" "}
+          <span>
+            {currentAirqQloud.sites && currentAirqQloud.sites.length} sites
+          </span>
         </li>
         <li className="divider" />
-        {airqlouds.map((airqloud, key) => (
-          <li key={key}>
-            {airqloud.long_name} <span>{airqloud.sites.length} sites</span>
-          </li>
-        ))}
+        {airqlouds.map(
+          (airqloud, key) =>
+            currentAirqQloud._id !== airqloud._id && (
+              <li key={key} onClick={handleAirQloudChange(airqloud)}>
+                {airqloud.long_name} <span>{airqloud.sites.length} sites</span>
+              </li>
+            )
+        )}
       </ul>
     </label>
   );
