1cd49aa4d6356cfac61e0b886018a396d67884b5
ja-odur <odurjoseph8@gmail.com>
2021-12-03 11:19:14 +0300

    [PLAT-891] convert category pollutant to to a dropdown

--- netmanager/src/views/pages/Dashboard/components/PollutantCategory/PollutantCategory.js
+++ netmanager/src/views/pages/Dashboard/components/PollutantCategory/PollutantCategory.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import clsx from "clsx";
 import PropTypes from "prop-types";
 import { makeStyles } from "@material-ui/styles";
 import { Card, CardContent, Grid, Typography, Avatar } from "@material-ui/core";
-import ArrowUpwardIcon from "@material-ui/icons/ArrowUpward";
-import PeopleIcon from "@material-ui/icons/PeopleOutlined";
+
+// styles
+import "assets/css/pollutant-category.css";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -40,34 +41,61 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const TotalUsers = (props) => {
+const PollutantCategory = (props) => {
+  const ref = useRef();
   const { className, pm25level, pm25levelCount, iconClass, ...rest } = props;
+  const [show, setShow] = useState(false);
 
   const classes = useStyles();
 
+  const toggleShow = () => {
+    setShow(!show);
+  };
+
+  useEffect(() => {
+    const checkIfClickedOutside = (e) => {
+      // If the menu is open and the clicked target is not within the menu,
+      // then close the menu
+      if (show && ref.current && !ref.current.contains(e.target))
+        setShow(false);
+    };
+
+    document.addEventListener("mousedown", checkIfClickedOutside);
+
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener("mousedown", checkIfClickedOutside);
+    };
+  }, [show]);
+
   return (
-    <Card {...rest} className={clsx(classes.root, className)}>
-      <CardContent>
-        <Grid container justify="space-between">
-          <Grid item style={{ maxWidth: "50%" }}>
-            <Typography
-              className={classes.title}
-              color="textSecondary"
-              gutterBottom
-              variant="body2"
-            >
-              {pm25level}
-            </Typography>
-            {/* <Typography variant="h3">{pm25levelCount}</Typography> */}
+    <label className="pc-dropdown" ref={ref} onClick={toggleShow}>
+      <Card
+        {...rest}
+        className={clsx(classes.root, className)}
+        onClick={toggleShow}
+      >
+        <CardContent>
+          <Grid container justify="space-between">
+            <Grid item style={{ maxWidth: "50%" }}>
+              <Typography
+                className={classes.title}
+                color="textSecondary"
+                gutterBottom
+                variant="body2"
+              >
+                {pm25level}
+              </Typography>
+              {/* <Typography variant="h3">{pm25levelCount}</Typography> */}
+            </Grid>
+            <Grid item>
+              <Avatar className={classes.avatar + " " + iconClass}>
+                {/* <PeopleIcon className={classes.icon} /> */}
+                {pm25levelCount}
+              </Avatar>
+            </Grid>
           </Grid>
-          <Grid item>
-            <Avatar className={classes.avatar + " " + iconClass}>
-              {/* <PeopleIcon className={classes.icon} /> */}
-              {pm25levelCount}
-            </Avatar>
-          </Grid>
-        </Grid>
-        {/* <div className={classes.difference}>
+          {/* <div className={classes.difference}>
           <ArrowUpwardIcon className={classes.differenceIcon} />
           <Typography
             className={classes.differenceValue}
@@ -77,13 +105,17 @@ const TotalUsers = (props) => {
             Since last hour
           </Typography>
         </div> */}
-      </CardContent>
-    </Card>
+        </CardContent>
+      </Card>
+      <ul className={`pc-dd-menu ${(!show && "dd-input") || ""}`}>
+        <li className="pc-empty">no sites</li>
+      </ul>
+    </label>
   );
 };
 
-TotalUsers.propTypes = {
+PollutantCategory.propTypes = {
   className: PropTypes.string,
 };
 
-export default TotalUsers;
+export default PollutantCategory;
