a2fdc9c0c72d436ae2b5e412f98191be3d3e9dc0
ja-odur <odurjoseph8@gmail.com>
2021-12-10 20:57:41 +0300

    [PLAT-919] merge d3 modules

---
+++ netmanager/src/d3.js
@@ -0,0 +1,19 @@
+import * as d3array from "d3-array";
+import * as d3axis from "d3-axis";
+import * as d3collection from "d3-collection";
+import * as d3selection from "d3-selection";
+import * as d3scale from "d3-scale";
+import * as d3scaleChromatic from "d3-scale-chromatic";
+import * as d3shape from "d3-shape";
+import * as d3transition from "d3-transition";
+
+export default {
+  ...d3array,
+  ...d3axis,
+  ...d3collection,
+  ...d3selection,
+  ...d3scale,
+  ...d3scaleChromatic,
+  ...d3shape,
+  ...d3transition,
+};
