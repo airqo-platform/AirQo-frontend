77e6570669193bfc3a8912b86a8ebbc716522ca8
ja-odur <odurjoseph8@gmail.com>
2021-12-17 00:13:15 +0300

    [PLAT-919] add chart action to chart container

--- netmanager/src/views/components/d3/ChartContainer.js
+++ netmanager/src/views/components/d3/ChartContainer.js
@@ -1,18 +1,24 @@
-import React from "react";
+import React, { useRef } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCog } from "@fortawesome/free-solid-svg-icons";
 
 // css styles
 import "assets/css/d3/chart-container.scss";
 
-const ChartContainer = ({ title, children }) => {
+const ChartContainer = ({ title, children, action, open, onClick, id }) => {
+  const anchorEl = useRef();
   return (
-    <div className="panel panel-default">
+    <div className="panel panel-default" id={id}>
       <div className="panel-heading">
         <div className="panel-title">
           {title || "Chart Area"}
-          <ul className="rad-panel-action">
-            <FontAwesomeIcon icon={faCog} />
+          <ul className="rad-panel-action" ref={anchorEl}>
+            <FontAwesomeIcon
+              icon={faCog}
+              onClick={onClick}
+              style={{ cursor: "pointer" }}
+            />
+            {action(open, anchorEl.current)}
           </ul>
         </div>
       </div>
