b755273b5f1e7540a6598774e7ac4dfa93b29722
ja-odur <odurjoseph8@gmail.com>
2022-01-27 02:08:03 +0300

    [PLAT-951] add pie chart legend

--- netmanager/src/views/components/d3/MultiLevelPieChart.js
+++ netmanager/src/views/components/d3/MultiLevelPieChart.js
@@ -24,6 +24,16 @@ const createDomain = (freq, domain) => {
   return timeFunc.range(...modifiedDomain);
 };
 
+const PM_COLOR_CATEGORY = [
+  { label: "Good", color: "#45e50d" },
+  { label: "Moderate", color: "#f8fe28" },
+  { label: "UHFSG", color: "#ee8310" },
+  { label: "Unhealthy", color: "#fe0000" },
+  { label: "VeryUnhealthy", color: "#8639c0" },
+  { label: "Hazardous", color: "#81202e" },
+  { label: "Others", color: "#808080" },
+];
+
 const BrushChart = ({
   data,
   selection,
@@ -221,11 +231,13 @@ const MultiLevelPieChart = ({
 }) => {
   const ref = useRef();
   const focusRef = useRef();
-  const margin = { top: 20, right: 20, bottom: 20, left: 35 };
+  const margin = { top: 10, right: 20, bottom: 10, left: 20 };
   const winWidth = 650;
   const winHeight = 370;
   const width = winWidth - margin.left - margin.right;
   const height = winHeight - margin.top - margin.bottom;
+
+  const legendWidth = 120;
 
   const maxRadius = Math.min(width, height) / 2;
 
@@ -244,7 +256,7 @@ const MultiLevelPieChart = ({
       .attr("class", "pie")
       .attr("width", width)
       .attr("height", height)
-      .attr("transform", `translate(${width / 2}, ${height / 2})`);
+      .attr("transform", `translate(${width / 2.5}, ${height / 2})`);
 
     const paths = pieFocus.selectAll("path").data(angles);
 
@@ -271,6 +283,27 @@ const MultiLevelPieChart = ({
 
     // Clear chart
     focus.html("");
+
+    const legendSpace = height / 2 / PM_COLOR_CATEGORY.length;
+
+    PM_COLOR_CATEGORY.map((d, i) => {
+      focus
+        .append("rect")
+        .attr("x", width - legendWidth)
+        .attr("y", legendSpace / 2 + i * legendSpace - 10)
+        .attr("width", 10)
+        .attr("height", 10)
+        .style("fill", () => d.color)
+        .text(d.label);
+
+      focus
+        .append("text")
+        .attr("x", width - legendWidth + 12) // space legend
+        .attr("y", legendSpace / 2 + i * legendSpace)
+        .attr("class", "legend-pie") // style the legend
+        .style("fill", () => "#808080")
+        .text(d.label);
+    });
 
     data.map((d, index) => {
       drawPieChart(index, focus, d);
