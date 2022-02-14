26f1f3538c245edc917c028f1371326fc17a41a1
Mike Mwanje <mwanjemike767@gmail.com>
2022-02-15 00:45:04 +0300

    Use allowed hosts set in .env file

--- website/backend/settings.py
+++ website/backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = config('DJANGO_ALLOWED_HOSTS').split(' ')
 
 
 # Application definition
