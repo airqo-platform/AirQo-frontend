aa07ed4ee6f1eec32cc053b441b00c9633677e3f
Mike Mwanje <mwanjemike767@gmail.com>
2022-03-04 22:36:19 +0300

    remove redundant step

--- website/docker/wsgi-entrypoint.sh
+++ website/docker/wsgi-entrypoint.sh
@@ -2,7 +2,6 @@
 
 ./manage.py migrate
 ./manage.py collectstatic --noinput
-cp /app/frontend/static/frontend/index.html /app/static/
 
 # number of workers = 4 * CPU cores
 gunicorn backend.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
