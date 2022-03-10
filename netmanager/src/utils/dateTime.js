674ab8bcdd9fbeeaacfe9b6090bed3fe3eb52abc
Mike Mwanje <mwanjemike767@gmail.com>
2022-03-10 11:31:21 +0300

    add error handler to date format

--- netmanager/src/utils/dateTime.js
+++ netmanager/src/utils/dateTime.js
@@ -68,15 +68,17 @@ export const humanReadableDate = (dateString, options) => {
 };
 
 export const formatDate = (date) => {
-  let d = new Date(date),
-    month = "" + (d.getMonth() + 1),
-    day = "" + d.getDate(),
-    year = d.getFullYear();
+    if(!date) return '-';
 
-  if (month.length < 2) month = "0" + month;
-  if (day.length < 2) day = "0" + day;
+    date = new Date(date)
+    let day = date.getDate();
+    let month = date.getMonth()+1;
+    const year = date.getFullYear();
 
-  return [year, month, day].join("-");
+    if (day < 10) day = `0${day}`
+    if (month < 10) month = `0${month}`
+
+  return [year, month, day].join('-');
 };
 
 export const roundToEndOfDay = (dateISOString) => {
