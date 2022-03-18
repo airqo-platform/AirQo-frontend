efa9d3157d005559d8014f046f035affa274ab2f
FaithDaka <faithbusigu8@gmail.com>
2022-03-18 10:31:52 +0300

    reset App.js

--- website/frontend/App.js
+++ website/frontend/App.js
@@ -3,7 +3,6 @@ import NotificationBanner from './src/components/NotificationBanner';
 import { TopBar } from './src/components/nav';
 import Footer from './src/components/Footer';
 import HomePage from "./src/pages/HomePage";
-import MapSection from './src/components/MapSection/MapSection';
 
 const App = () => (
     <div>
