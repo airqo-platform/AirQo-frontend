0858b60a95cd32c3aa5a147e316dfa27ab7a88b1
ja-odur-mfs <odurjoseph8@gmail.com>
2022-03-20 17:53:36 +0300

    [WEB-118] Add hero section

--- website/frontend/src/components/Hero.js
+++ website/frontend/src/components/Hero.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import HeroCity from 'icons/homepage/hero-city.png';
+import useWindowSize from 'utils/customHooks';
+import HeroCityImg from 'icons/homepage/hero-city.png';
+import HeroCityLargeImg from 'icons/homepage/hero-city-full.png';
+
+const breakPoint = 580;
 
 const Button = ({ className, label }) => <button className={className || 'button-hero'}>{label}</button>;
 
-const Hero = () => (
+const Hero = () => {
+  const size = useWindowSize();
+
+  return (
         <div className="Hero">
-            <img src={HeroCity} alt="Hero image" />
-            <p className="hero-title">Clean air for all African Cities </p>
-            <p className="hero-sub">We empower communities with accurate, hyperlocal and timely air quality data to drive air pollution mitigation actions.</p>
-            <div>
-                <Button label="Request a demo" />
-                <Button className="button-signin" label="Sign in" />
+            <img src={size.width <= breakPoint ? HeroCityImg : HeroCityLargeImg} alt="Hero image" />
+            <div className="hero-content">
+                <div>
+                <p className="hero-title">Clean air for <br />all African Cities </p>
+                <p className="hero-sub">We empower communities with accurate, hyperlocal and timely air quality data to
+                    drive air pollution mitigation actions.
+                </p>
+                <div className="hero-buttons">
+                    <Button label="Request a demo" />
+                    <Button className="button-signin" label="Sign in" />
+                </div>
+                </div>
             </div>
         </div>
-);
+  );
+};
 
 export default Hero;
