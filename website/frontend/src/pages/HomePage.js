e5ba59f2e996809c6318fe6c159b6e5bacd37d57
Codebmk <belindamarion0@gmail.com>
2022-03-22 15:40:32 +0300

    changes

--- website/frontend/src/pages/HomePage.js
+++ website/frontend/src/pages/HomePage.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Page from './Page';
 import Hero from '../components/Hero';
 import Monitor from '../components/Monitor';
+import TopBar from '../components/nav/TopBar';
 
 const HomePage = () => (
     <Page>
         <div className="HomePage">
+            <TopBar />
             <Hero />
             <Monitor />
         </div>
