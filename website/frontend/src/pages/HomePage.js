4ce2a10481b2321452f3af943ec909b4909963a7
FaithDaka <faithbusigu8@gmail.com>
2022-03-23 15:48:31 +0300

    Added Partner section to homepage

--- website/frontend/src/pages/HomePage.js
+++ website/frontend/src/pages/HomePage.js
@@ -4,11 +4,13 @@ import Hero from '../components/Hero';
 import Monitor from '../components/Monitor';
 import GetApp from '../components/get-app/GetApp';
 import MapSection from '../components/MapSection/MapSection';
+import Partners from '../components/Partners';
 
 const HomePage = () => (
     <Page>
         <div className="HomePage">
             <Hero />
+            <Partners />
             <Monitor />
             <MapSection />
             <GetApp />
