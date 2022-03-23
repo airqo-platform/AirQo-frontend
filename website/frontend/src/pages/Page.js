4ca5a3b6445ff6b93084c209bf4418fa6e020d84
Mike Mwanje <mwanjemike767@gmail.com>
2022-03-23 13:58:36 +0300

    backed by section integration

--- website/frontend/src/pages/Page.js
+++ website/frontend/src/pages/Page.js
@@ -1,13 +1,20 @@
 import React from 'react';
-import NotificationBanner from 'src/components/NotificationBanner';
-import Footer from 'src/components/Footer';
+import PropTypes from 'prop-types';
+import NotificationBanner from '../components/NotificationBanner';
+import Footer from '../components/Footer';
+import BackedBy from '../components/backedBy';
 
 const Page = ({ children }) => (
         <div className="Page">
             <NotificationBanner />
             {children}
+            <BackedBy />
             <Footer />
         </div>
 );
 
 export default Page;
+
+Page.propTypes = {
+  children: PropTypes.element.isRequired,
+};
