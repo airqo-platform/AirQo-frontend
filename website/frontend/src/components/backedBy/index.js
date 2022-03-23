6a5f66b1ddbe9c06f29b6f42d3b5f9a7d19dca96
Mike Mwanje <mwanjemike767@gmail.com>
2022-03-23 15:55:04 +0300

    reduce number of visible cards

--- website/frontend/src/components/backedBy/index.js
+++ website/frontend/src/components/backedBy/index.js
@@ -9,12 +9,12 @@ import OrganisationCard from './OrganizationCard';
 
 const responsive = {
   superLargeDesktop: { breakpoint: { max: 4000, min: 3000 }, items: 5 },
-  desktop: { breakpoint: { max: 3000, min: 1024 }, items: 2 },
+  desktop: { breakpoint: { max: 3000, min: 1024 }, items: 1.8 },
   tablet: { breakpoint: { max: 1024, min: 464 }, items: 2 },
   mobile: { breakpoint: { max: 464, min: 0 }, items: 1 },
 };
 
-const cards = Array(6).fill(
+const cards = Array(5).fill(
     <OrganisationCard
       imageUrl={false}
       logo={false}
