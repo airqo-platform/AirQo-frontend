4cd8553da2a17a35d584da88205f0737473708cc
ja-odur-mfs <odurjoseph8@gmail.com>
2022-03-24 09:24:40 +0300

    [WEB-134] Install Mui

---
+++ calibrate/src/configs/views/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TopBar from "./components/TopBar";
+
+const HomePage = () => {
+  return (
+    <div>
+      <TopBar />
+    </div>
+  );
+};
+
+export default HomePage;
