907004a8a5c6d06515af25631e7542f33da16605
ja-odur-mfs <odurjoseph8@gmail.com>
2022-04-03 00:36:02 +0300

    [WEB-142] Move topbar to global page section

--- website/frontend/src/pages/Page.js
+++ website/frontend/src/pages/Page.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NotificationBanner from 'src/components/NotificationBanner';
 import Footer from 'src/components/Footer';
+import TopBar from 'src/components/nav/TopBar';
 
 const Page = ({ children }) => (
         <div className="Page">
             <NotificationBanner />
+            <TopBar />
             {children}
             <Footer />
         </div>
