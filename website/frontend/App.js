af91a6b58c5249c4667629b5b6c622d739417bd6
Codebmk <belindamarion0@gmail.com>
2022-04-07 08:29:44 +0300

    Resolve conflicts

--- website/frontend/App.js
+++ website/frontend/App.js
@@ -4,12 +4,14 @@ import HomePage from 'src/pages/HomePage';
 import ResearchPage from 'src/pages/ResearchPage';
 import Press from 'src/pages/Press/Press';
 import Terms from "./src/pages/Legal/Terms";
+import CommunityPage from './src/pages/Solutions/CommunityPage';
 
 const App = () => (
     <Router>
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/solutions/research" element={<ResearchPage />} />
+            <Route path="/solutions/communities" element={<CommunityPage />} />
             <Route path="/press" element={<Press />} />
             <Route path="/terms" element={<Terms />} />
         </Routes>
