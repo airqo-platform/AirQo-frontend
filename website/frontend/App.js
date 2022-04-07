93cf4b4ddd221b44427a63f276374daf5da64fd2
Codebmk <belindamarion0@gmail.com>
2022-04-07 08:29:44 +0300

    Resolve conflicts

--- website/frontend/App.js
+++ website/frontend/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from 'src/pages/HomePage';
 import ResearchPage from './src/pages/ResearchPage';
+import CommunityPage from './src/pages/Solutions/CommunityPage';
 
 const App = () => (
     <Router>
         <Routes>
             <Route path="/" element={<HomePage />} />
             <Route path="/solutions/research" element={<ResearchPage />} />
+            <Route path="/solutions/communities" element={<CommunityPage />} />
         </Routes>
 
     </Router>
--- website/frontend/src/pages/Solutions/CommunityPage.js
+++ website/frontend/src/pages/Solutions/CommunityPage.js
@@ -3,7 +3,7 @@ import Page from '../Page';
 import TopBar from '../../components/nav/TopBar';
 import { NewsletterSection } from '../../components/NewsletterSection';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
-import AirQommunityBlob from "assets/img/community/AirQo_blob_full.svg";
+import AirQommunities from "assets/img/community/AirQommunities.png";
 import Blob from "assets/img/community/AirQo_blob_fill.svg";
 import AssetImg1 from "assets/img/community/Rectangle 406.png";
 import AssetImg2 from "assets/img/community/Rectangle 408-1.png";
@@ -15,7 +15,6 @@ import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 const CommunityPage = () => (
     <Page>
         <div className="CommunityPage">
-            <TopBar />
             
             <div className="hero">
                 <div className="header-1">
@@ -30,7 +29,7 @@ const CommunityPage = () => (
 
             <div className="wrapper">
                 <div className="blob">
-                    <AirQommunityBlob className="blob-img" />
+                    <img src={AirQommunities} className="blob-img" />
                 </div>
                 
                 <div className="section-1">
@@ -42,23 +41,22 @@ const CommunityPage = () => (
                         </div>
                         <div className="img-stack">
                             <div className="stack-1">
-                                <img src={AssetImg1} alt="Image Stock 1" style={{marginBottom: "24px"}}/>
+                                <img src={AssetImg1} alt="Image Stock 1"/>
                                 <img src={AssetImg2} alt="Image Stock 2" />
                             </div>
                             <img src={AssetImg3} className="stack-2" alt="Image Stock 3" />
+                            <div className="layer-2">
+                                <Blob className="blob" style={{height:"360px"}} />
+                            </div>
                         </div>
                     </div>
-                    <div className="layer-2">
-                        <Blob className="blob" style={{height:"360px"}} />
-                    </div>
-                    
                 </div>
 
                 <div className="strip">
                     <div className="left-strip">
                         <span>
                             <h3>0113</h3>
-                            <img src={AirQoArrowLeft} />
+                            <AirQoArrowLeft className="left-strip-arrow" />
                         </span>
                         <h3>AirQo Hosts</h3>
                     </div>
@@ -67,22 +65,24 @@ const CommunityPage = () => (
                     </div>
                 </div>
 
-                <div className="section-2">
+                <div className="section-1">
                     <div className="layer-1">
                         <div className="text">
-                            <h1>AirQommunity champions and AirQo hosts</h1>
-                            <p>Recruiting and creating community groups we call AirQommunity. Through these groups, we encourage individuals to share pictures of air pollution practices around their neighbourhoods — information we leverage in building air pollution case studies.</p>
-                            <p>Collaboration with locals, locals volunteer to host the installation of air quality monitoring devices at either their places of work or homes. This goes a long way in enforcing the participatory approach in collecting air quality data — involving communities at every step of the journey.</p>
+                            <h1>Community trainings and awareness workshops </h1>
+                            <p>Focusing on high risk areas, we use the exiciting air quality data as evidence to raise awareness. Collaborating with industrial businesses, and vehicles repair business on how to improve the quality of air they breath.</p>
+                            <p>Training local leaders and students. The AirQo dashboard is a great tool to track air pollution across Africa. We conduct How-to training and workshops to equip local leaders with skils on how to read and interpret air quality analytics.</p>
                         </div>
                         <div className="img-stack">
                             <div className="stack-1">
-                                <img src={AssetImg1} alt="Image Stock 1" />
+                                <img src={AssetImg1} alt="Image Stock 1"/>
                                 <img src={AssetImg2} alt="Image Stock 2" />
                             </div>
                             <img src={AssetImg3} className="stack-2" alt="Image Stock 3" />
+                            <div className="layer-2">
+                                <Blob className="blob" style={{height:"360px"}} />
+                            </div>
                         </div>
                     </div>
-                    <img src={AirQommunityBlob} className="layer-2" />
                 </div>
 
                 <div className="lower-banner">
@@ -91,10 +91,10 @@ const CommunityPage = () => (
                     <h3>Mercy Nasejje</h3>
                     <h6>Health, Safety and Environmental Lead at MOGAS</h6>
 
-                    <button><PlayArrowIcon/>Watch video</button>
+                    <button><PlayArrowIcon style={{marginRight: "10px"}}/>Watch video</button>
                 </div>
             </div>
-            <NewsletterSection />
+            
         </div>
     </Page>
 );
