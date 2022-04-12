d9c9fe1806a9addde2f5e7e8f79d9a293730536b
FaithDaka <faithbusigu8@gmail.com>
2022-04-12 10:16:10 +0300

    tweaks

--- website/frontend/src/components/Partners.js
+++ website/frontend/src/components/Partners.js
@@ -1,12 +1,16 @@
 import React from 'react'
-import Google from 'icons/homepage/partners/google.svg';
-import WorldBankGroup from 'icons/homepage/partners/worldbankgroup.svg';
-import BirminghamUniversity from 'icons/homepage/partners/birmingham-university.svg';
-import Zindi from 'icons/homepage/partners/zindi.svg';
-import ESPRC from 'icons/homepage/partners/EPSRC.svg';
+import GOOGLE from 'icons/homepage/partners/google.svg';
+import WBG from 'icons/homepage/partners/worldbankgroup.svg';
+import BU from 'icons/homepage/partners/birmingham-university.svg';
+import ZINDI from 'icons/homepage/partners/zindi.svg';
+import EPSRC from 'icons/homepage/partners/EPSRC.svg';
 import NRF from 'icons/homepage/partners/NRF.svg';
-import ColumbiaUniversity from 'icons/homepage/partners/columbia-university.svg';
+import CU from 'icons/homepage/partners/columbia-university.svg';
 import ASAP from 'icons/homepage/partners/ASAP.svg';
+import Epsrc from 'icons/homepage/partners-sm/epsrc.svg';
+import Google from 'icons/homepage/partners-sm/google.svg';
+import BirminghamUniversity from 'icons/homepage/partners-sm/birmingham.svg';
+import Zindi from 'icons/homepage/partners-sm/zindi.svg';
 
 const Partners = () => {
     return (
@@ -17,27 +21,27 @@ const Partners = () => {
                 </div>
                 <div className='logos-web'>
                     <div className='logo'>
-                        <div><ESPRC /></div>
-                        <div><Google /></div>
-                        <div><WorldBankGroup /></div>
-                        <div><BirminghamUniversity /></div>
-                        <div><Zindi /></div>
+                        <div><EPSRC /></div>
+                        <div><GOOGLE /></div>
+                        <div><WBG /></div>
+                        <div><BU /></div>
+                        <div><ZINDI /></div>
                         <div><NRF /></div>
-                        <div><ColumbiaUniversity /></div>
+                        <div><CU /></div>
                         <div><ASAP /></div>
                     </div>
                 </div>
                 <div className='logos-mobile'>
                     <div className='mobile-row' id="row1">
-                        <div><ESPRC /></div>
+                        <div><Epsrc /></div>
                         <div><Google /></div>
-                        <div><WorldBankGroup /></div>
-                        <div><BirminghamUniversity /></div>
+                        <div><WBG /></div>
+                        <div><ZINDI /></div>
                     </div>
                     <div className='mobile-row' id="row2">
+                        <div><BirminghamUniversity /></div>
                         <div><Zindi /></div>
-                        <div><NRF /></div>
-                        <div><ColumbiaUniversity /></div>
+                        <div><CU /></div>
                         <div><ASAP /></div>
                     </div>
                 </div>
