38ab46689e4a6cf1c01297b20a5d9ffc2f3d165b
ja-odur-mfs <odurjoseph8@gmail.com>
2022-04-18 12:56:25 +0300

    Add top air-quality reading

--- website/frontend/src/components/Hero.js
+++ website/frontend/src/components/Hero.js
@@ -2,6 +2,8 @@ import React from 'react';
 import useWindowSize from 'utils/customHooks';
 import HeroCityImg from 'icons/homepage/hero-city.png';
 import HeroCityLargeImg from 'icons/homepage/hero-city-full.png';
+import Location from 'icons/homepage/hero/location.svg';
+import Reload from 'icons/homepage/hero/reload.svg';
 
 const breakPoint = 580;
 
@@ -12,10 +14,33 @@ const Hero = () => {
 
   return (
         <div className="Hero">
-            <img src={size.width <= breakPoint ? HeroCityImg : HeroCityLargeImg} alt="Hero image" />
+            <span>
+                <img src={size.width <= breakPoint ? HeroCityImg : HeroCityLargeImg} alt="Hero image" />
+                <div className="air-quality-reading">
+                    <div className="top-reading">
+                        <span className="name-wrapper">
+                            <Location />
+                            <span className="location-name">Nakawa, Kampala</span>
+                        </span>
+                        <span className="time-wrapper">
+                            <span className="location-time">15:21:01(EAT)</span>
+                            <Reload />
+                        </span>
+                    </div>
+                    {/*<div className="bottom-reading">*/}
+                    {/*    <span>*/}
+                    {/*        <span>Good</span>*/}
+                    {/*    </span>*/}
+                    {/*    <span>*/}
+                    {/*        <span></span>*/}
+                    {/*        <span></span>*/}
+                    {/*    </span>*/}
+                    {/*</div>*/}
+                </div>
+            </span>
             <div className="hero-content">
                 <div>
-                <p className="hero-title">Clean air for <br />all African Cities </p>
+                <p className="hero-title">Clean air for <br />all African cities </p>
                 <p className="hero-sub">We empower communities with accurate, hyperlocal and timely air quality data to
                     drive air pollution mitigation actions.
                 </p>
