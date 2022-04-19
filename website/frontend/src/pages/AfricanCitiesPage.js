7bca8cc867d2ecdeac8fd87f888b1ba50c305823
ja-odur-mfs <odurjoseph8@gmail.com>
2022-04-20 01:36:34 +0300

    Add Kampala and publication content

--- website/frontend/src/pages/AfricanCitiesPage.js
+++ website/frontend/src/pages/AfricanCitiesPage.js
@@ -8,6 +8,11 @@ import Burundi from 'icons/africanCities/countries/burundi.svg';
 import Senegal from 'icons/africanCities/countries/senegal.svg';
 import Mozambique from 'icons/africanCities/countries/mozambique.svg';
 import { useInitScrollTop } from "../../utils/customHooks";
+import ConsultImg from "../icons/research/consult.png";
+import ConsultLongImg from "../icons/research/consult-long.png";
+import ConsultImg2 from "../icons/research/consult-2.png";
+import BackgroundShape from "../icons/africanCities/background-shape.svg";
+import ArrowRight from "../icons/research/arrow-right.svg";
 
 const CityHeroSection = () => {
     return (
@@ -45,6 +50,95 @@ const CityTabs = () => {
     )
 }
 
+const ContentKampala = () => {
+    return (
+        <div className="cities-content">
+            <div className="ug-container">
+                <div className="consult-text">
+                    <div>
+                        <p>Collaboration with KCCA</p>
+                        <p>AirQo partnered with the Kampala Capital City Authority (KCCA) in 2018, to facilitate air quality monitoring and data analytics tools across Kampala. and other cities around the country.</p>
+                        <span className="number-list">
+                            <span>1</span>
+                            <span>Developed a physical network of low-cost air quality sensors in Kampala and the metropolitan areas comprising over 70 installations.</span>
+                        </span>
+                        <span className="number-list">
+                            <span>2</span>
+                            <span>Developed a dedicated data analytics dashboard and supporting capacity development on air quality monitoring</span>
+                        </span>
+                        <span className="number-list">
+                            <span>3</span>
+                            <span>Improved air quality monitoring resolution. Kampala city is now one of the cities in Africa with the highest concentration of air quality monitors.</span>
+                        </span>
+                    </div>
+                </div>
+                <div className="consult-images">
+                    <img className="img-small" src={ConsultImg} alt="consult image" />
+                    <img className="img-long" src={ConsultLongImg} alt="consult long image" />
+                    <img className="img-small" src={ConsultImg2} alt="consult image 2" />
+                    <BackgroundShape className="background-shape" />
+                </div>
+            </div>
+
+            <div className="content-intro"><p>We continue to conduct joint awareness campaigns during the annual air quality awareness week.</p></div>
+
+            <div className="ug-container">
+                <div className="consult-text">
+                    <div>
+                        <p>Collaboration with NEMA</p>
+                        <p>NEMA is the lead government agency mandated to coordinate, supervise, and regulate environmental management in Uganda.</p>
+                        <span className="number-list">
+                            <span>1</span>
+                            <span>AirQo contributes to the air quality chapter in the national state of the environment report, a biennal statutory requirement</span>
+                        </span>
+                        <span className="number-list">
+                            <span>2</span>
+                            <span>Officially contributed to the development of the national air quality regulations and standards for Uganda</span>
+                        </span>
+                        <span className="number-list">
+                            <span>3</span>
+                            <span>Raise the profile of air quality as a national priority. Air quality is now featured as an important priority in the coutnry’s National Development Plan III.</span>
+                        </span>
+                    </div>
+                </div>
+                <div className="consult-images">
+                    <img className="img-small" src={ConsultImg} alt="consult image" />
+                    <img className="img-long-right" src={ConsultLongImg} alt="consult long image" />
+                    <img className="img-small" src={ConsultImg2} alt="consult image 2" />
+                    <BackgroundShape className="background-shape" />
+                </div>
+            </div>
+
+            <div className="cities-divider" />
+        </div>
+    )
+}
+
+const AdvocateSection = () => {
+    return (
+        <div className="advocate-wrapper">
+            <div className="advocate">
+                <p>We believe in advocating for policy reforms </p>
+                <p>Our model is to work directly with policymakers alongside stakeholder groups to support the policy development process, ultimately pushing air quality management mainstream.</p>
+            </div>
+        </div>
+    )
+}
+
+const PublicationsSection = () => {
+    return (
+        <div className="publications-section">
+            <div className="title">Publications</div>
+            <div className="main-text">Developed the first ever clean air action plan for Kampala Capital City, a blue print for other cities in Uganda</div>
+            <div className="author">Author</div>
+            <div className="team">AirQo Team</div>
+            <div>
+                <div className="link"><span>Read full whitepaper <ArrowRight /></span></div>
+            </div>
+        </div>
+    )
+}
+
 const AfricanCitiesPage = () => {
     useInitScrollTop();
     return (
@@ -53,6 +147,9 @@ const AfricanCitiesPage = () => {
                 <CityHeroSection />
                 <CityBanner />
                 <CityTabs />
+                <ContentKampala />
+                <AdvocateSection />
+                <PublicationsSection />
             </div>
         </Page>
     )
