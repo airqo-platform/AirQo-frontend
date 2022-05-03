58b2920bbdb087217490c761909c4e20e3f04c33
Codebmk <belindamarion0@gmail.com>
2022-05-03 13:03:44 +0300

    Responsive Mega Menu

--- website/frontend/src/components/nav/TopBar.js
+++ website/frontend/src/components/nav/TopBar.js
@@ -1,27 +1,118 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AirQo from 'icons/nav/AirQo';
 import NavTab from './NavTab';
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from 'assets/svg/Menu.svg';
+import CloseIcon from 'assets/svg/Close.svg';
 import { Link } from 'react-router-dom';
 
 const TopBar = () => {
+
+    const toggleDropdown = (dropdownId) => () => {
+        var dropdownElems = document.getElementsByClassName("dropdown");
+
+        for(let i=0; i<dropdownElems.length; i++){
+            if(dropdownElems[i] != document.getElementById(dropdownId)){
+                dropdownElems[i].classList.remove("toggle_dropdown");
+            }
+        }
+
+        document.getElementById(dropdownId).classList.toggle("toggle_dropdown");
+    }
+
+    const toggleMenu = () => {
+        document.getElementById("menu").classList.toggle("toggle_menu_btn");
+        document.getElementById("close-menu").classList.toggle("toggle_close_menu_btn");
+        document.getElementById("nav-center").classList.toggle("toggle_nav_center");
+        document.getElementById("nav-right").classList.toggle("toggle_nav_right");    
+    }
+
+    const toggleCloseMenu = () => {
+        document.getElementById("close-menu").classList.remove("toggle_close_menu_btn");
+        document.getElementById("menu").classList.remove("toggle_menu_btn");
+        document.getElementById("nav-center").classList.remove("toggle_nav_center");
+        document.getElementById("nav-right").classList.remove("toggle_nav_right"); 
+    }
+
+    // useEffect(() => {
+    //     function handleWindowClick(e) {
+    //         if(!e.target.matches(".nav-dropdown-item")) {
+    //             // var dropdownElems = document.getElementsByClassName("dropdown");
+
+    //             // for(let i=0; i<dropdownElems.length; i++){
+    //             //     if(dropdownElems[i].classList.contains("toggle_dropdown")){
+    //             //         dropdownElems[i].classList.remove("toggle_dropdown");
+    //             //     }
+    //             // }
+    //         }
+    //     }
+    //     window.addEventListener('click', handleWindowClick);
+        
+    //     return () => window.removeEventListener('click', handleWindowClick);
+    // },[]);
+
+    
+
     return(
         <div className="TopBar">
             <div className="wrapper">
-                <Link to="/"><AirQo /></Link>
-                <div className="nav-center">
-                    <NavTab className="NavTab__menuItem" text="Solutions" />
-                    <NavTab className="NavTab__menuItem" text="Our work" />
-                    <NavTab className="NavTab__menuItem" text="About" path="/about-us" hideArrow />
-                    <NavTab className="NavTab__menuItem" text="Get involved" path="/get-involved" hideArrow />
+                <div className="logo">
+                    <Link to="/"><AirQo /></Link>
                 </div>
-                <div className="nav-right">
-                    <NavTab className="NavTab__menuItem" text="Sign In"  hideArrow colored />
-                    <NavTab className="NavTab__menuItem" text="Request a demo"  hideArrow filled />
+                <div className="nav-center" id="nav-center">
+                    <div className="nav-dropdown-item">
+                        <NavTab  
+                            text="Solutions" 
+                            onClick={toggleDropdown("african-cities-dropdown")} />
+                        <div className="dropdown" id="african-cities-dropdown">
+                            <h3 className="title">Solutions</h3>
+                            <div className="dropdown-list">
+                                <div className="dropdown-list-item">
+                                    <h3>For African Cities</h3>
+                                    <h4>Air quality analytics for city councils</h4>
+                                </div>
+                                <div className="dropdown-list-item">
+                                    <h3>For Communities</h3>
+                                    <h4>Recruiting locals to drive awareness</h4>
+                                </div>
+                                <div className="dropdown-list-item">
+                                    <h3>For Research</h3>
+                                    <h4>Free access to air quality analytics</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="nav-dropdown-item">
+                        <NavTab  
+                            text="Our work" 
+                            onClick={toggleDropdown("our-work-dropdown")} />
+                        <div className="dropdown" id="our-work-dropdown">
+                            <h3 className="title">Our Work</h3>
+                            <div className="dropdown-list">
+                                <div className="dropdown-list-item">
+                                    <h3>Air Quality Monitors</h3>
+                                    <h4>Low-cost monitoring devices</h4>
+                                </div>
+                                <div className="dropdown-list-item">
+                                    <h3>Air Quality Dashboard</h3>
+                                    <h4>Historical and forecast data</h4>
+                                </div>
+                                <div className="dropdown-list-item">
+                                    <h3>Air Quality Mobile</h3>
+                                    <h4>Track the air around you</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <NavTab text="About" path="/about-us" hideArrow />
+                    <NavTab text="Get involved" path="/get-involved" hideArrow />
                 </div>
-                
-                <MenuIcon className="menu-btn" />
+                <div className="nav-right" id="nav-right">
+                    <NavTab text="Request a demo"  hideArrow filled />
+                    <NavTab text="Sign In"  hideArrow colored />
+                </div>
             </div>
+            <MenuIcon className="menu-btn" id="menu" onClick={toggleMenu} />
+            <CloseIcon className="close-menu-btn" id="close-menu" onClick={toggleCloseMenu} />
         </div>
     )
 };
