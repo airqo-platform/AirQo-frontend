46abedf2a792ca14b482838286d924e1ff7fa5ae
ja-odur <odurjoseph8@gmail.com>
2022-05-06 01:37:47 +0300

    [PLAT-1115] CLean CSV headers

--- calibrate/src/views/components/Calibrate.js
+++ calibrate/src/views/components/Calibrate.js
@@ -19,7 +19,8 @@ function csvHeaders(str, delimiter = ",") {
   // slice from start of text to the first \n index
   // use split to create an array from string by delimiter
   const headers = str.slice(0, str.indexOf("\n")).split(delimiter);
-  return headers;
+
+  return headers.map((header) => header.replace(/\r?\n|\r/g, ""));
   // slice from \n index + 1 to the end of the text
   // use split to create an array of each csv value row
   const rows = str.slice(str.indexOf("\n") + 1).split("\n");
