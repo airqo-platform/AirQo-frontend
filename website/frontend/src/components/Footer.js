93066a9ed4ac5b591fe0099cb6bda752bbd6e798
ja-odur <odurjoseph8@gmail.com>
2022-05-09 09:24:43 +0300

    connect the data

--- website/frontend/src/components/Footer.js
+++ website/frontend/src/components/Footer.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Modal, Box } from '@mui/material';
 import MakText from 'icons/nav/MakText';
@@ -20,6 +20,8 @@ import Burundi from 'icons/africanCities/countries/burundi.svg';
 import Senegal from 'icons/africanCities/countries/senegal.svg';
 import Mozambique from 'icons/africanCities/countries/mozambique.svg';
 
+import { useAirqloudSummaryData, useCurrentAirqloudData } from 'reduxStore/AirQlouds/selectors';
+
 const style = {
   position: 'absolute',
   top: '50%',
@@ -38,7 +40,22 @@ const CountryTab = ({ className, flag, name }) => (
         <div className={className}>{flag} <span>{name}</span></div>
 );
 
-const Footer = () => (
+const Footer = () => {
+  const [open, setOpen] = useState(false);
+
+  const airqloudSummaries = useAirqloudSummaryData();
+  const currentAirqloud = useCurrentAirqloudData();
+
+  const currentAirqloudData = airqloudSummaries[currentAirqloud] || { numberOfSites: 0 };
+
+  console.log('data', currentAirqloudData);
+  const explodeSummaryCount = (numberOfSites) => {
+    const paddedCount = numberOfSites.toString().padStart(4, '0');
+    return paddedCount.split('');
+  };
+
+  const toggleOpen = () => setOpen(!open);
+  return (
         <footer className="footer-wrapper">
             <div className="Footer">
             <div className="body-section">
@@ -61,15 +78,15 @@ const Footer = () => (
                             <span><Link to="/solutions/research">For Research</Link></span>
                         </div>
                     </span>
-                    {/* <span className="content-tabs middle-tab"> */}
-                    {/*    <span>Our Works</span> */}
-                    {/*    <div> */}
-                    {/*        <span>Air Quality Monitors</span> */}
-                    {/*        <span>Air Quality Analytics</span> */}
-                    {/*        <span>Air Quality Mobile App</span> */}
-                    {/*    </div> */}
-                    {/* </span> */}
-                    <span className="content-tabs">
+                        {/* <span className="content-tabs middle-tab"> */}
+                        {/*    <span>Our Works</span> */}
+                        {/*    <div> */}
+                        {/*        <span>Air Quality Monitors</span> */}
+                        {/*        <span>Air Quality Analytics</span> */}
+                        {/*        <span>Air Quality Mobile App</span> */}
+                        {/*    </div> */}
+                        {/* </span> */}
+                        <span className="content-tabs">
                         <span>About</span>
                         <div>
                             <span><Link to="/about-us">About</Link></span>
@@ -77,70 +94,74 @@ const Footer = () => (
                             <span><Link to="/press">Press</Link></span>
                             <span><a target="_blank" href="https://medium.com/@airqo">Blog</a></span>
                         </div>
-                    </span>
-                </div>
-                <div className="airqlouds-summary">
-                    <div className="airqloud-dropdown">
-                        <Location />
-                        <span>Uganda</span>
-                        <ArrowDown />
+                        </span>
                     </div>
-                    <div className="airqloud-count">
+                    <div className="airqlouds-summary" onClick={toggleOpen}>
+                        <div className="airqloud-dropdown">
+                            <Location />
+                            <span>{currentAirqloud}</span>
+                            <ArrowDown />
+                        </div>
+                        <div className="airqloud-count">
                         <span className="count-value">
-                            <span>0</span>
-                            <span>1</span>
-                            <span>1</span>
-                            <span>3</span>
+                            {
+                                explodeSummaryCount(currentAirqloudData.numberOfSites)
+                                  .map((value, key) => <span key={key}>{value}</span>)
+                            }
                         </span> {' '}
-                        <span className="count-text">Monitors in Uganda</span>
+                            <span className="count-text">Monitors in {currentAirqloud}</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="copyright-section">
+                    <div className="copyright-container">
+                        <div className="text-copyright">© {new Date().getFullYear()} AirQo</div>
+                        <div className="terms-section">
+                            <span className="text-terms mr-24"><Link to="/terms">Terms of service</Link></span>
+                            {/* <span className="text-terms mr-24">Privacy policy</span> */}
+                            {/* <span className="text-terms mr-24">Sustainability</span> */}
+                        </div>
+                    </div>
+                    <div className="project-container mb-24">
+                        <div className="project">
+                            <div className="project-text">A project by</div>
+                            <MakText />
+                        </div>
                     </div>
                 </div>
             </div>
-            <div className="copyright-section">
-                <div className="copyright-container">
-                    <div className="text-copyright">© {new Date().getFullYear()} AirQo</div>
-                    <div className="terms-section">
-                        <span className="text-terms mr-24"><Link to="/terms">Terms of service</Link></span>
-                        {/* <span className="text-terms mr-24">Privacy policy</span> */}
-                        {/* <span className="text-terms mr-24">Sustainability</span> */}
-                    </div>
-                </div>
-                <div className="project-container mb-24">
-                    <div className="project">
-                        <div className="project-text">A project by</div>
-                        <MakText />
-                    </div>
-                </div>
-            </div>
-            </div>
-            <Modal open>
+            <Modal open={open} onClose={toggleOpen}>
                 <Box sx={style}>
-                <div className="modal">
-                    <div className="modal-title"><span>Country AirQloud</span> <CancelIcon /></div>
-                    <div className="divider" />
-                    <div className="title">Our growing network in Africa</div>
-                    <div className="sub-title">View AirQo developments in your country</div>
-                    <div className="category-label">Selected country</div>
-                    <CountryTab className="tab tab-selected tab-margin" flag={<Uganda />} name={"Uganda"} />
-                    <div className="category-label">Select country</div>
-                    <div className="countries">
-                        <CountryTab className="tab tab-selected tab-margin-sm" flag={<Uganda />} name={"Uganda"} />
-                        <CountryTab className="tab tab-margin-sm" flag={<Kenya />} name={"Kenya"} />
-                        <CountryTab className="tab tab-margin-sm" flag={<Nigeria />} name={"Nigeria"} />
-                        <CountryTab className="tab tab-margin-sm" flag={<Ghana />} name={"Ghana"} />
-                        <CountryTab className="tab tab-margin-sm" flag={<Burundi />} name={"Burundi"} />
-                        <CountryTab className="tab tab-margin-sm" flag={<Senegal />} name={"Senegal"} />
-                        <CountryTab className="tab tab-margin-sm tab-mb" flag={<Mozambique />} name={"Mozambique"} />
+                    <div className="modal">
+                        <div className="modal-title">
+                            <span>Country AirQloud</span>
+                            <CancelIcon className="modal-cancel" onClick={toggleOpen} />
+                        </div>
+                        <div className="divider" />
+                        <div className="title">Our growing network in Africa</div>
+                        <div className="sub-title">View AirQo developments in your country</div>
+                        <div className="category-label">Selected country</div>
+                        <CountryTab className="tab tab-selected tab-margin" flag={<Uganda />} name="Uganda" />
+                        <div className="category-label">Select country</div>
+                        <div className="countries">
+                            <CountryTab className="tab tab-selected tab-margin-sm" flag={<Uganda />} name="Uganda" />
+                            <CountryTab className="tab tab-margin-sm" flag={<Kenya />} name="Kenya" />
+                            <CountryTab className="tab tab-margin-sm" flag={<Nigeria />} name="Nigeria" />
+                            <CountryTab className="tab tab-margin-sm" flag={<Ghana />} name="Ghana" />
+                            <CountryTab className="tab tab-margin-sm" flag={<Burundi />} name="Burundi" />
+                            <CountryTab className="tab tab-margin-sm" flag={<Senegal />} name="Senegal" />
+                            <CountryTab className="tab tab-margin-sm tab-mb" flag={<Mozambique />} name="Mozambique" />
+                        </div>
+                        <div className="divider" />
+                        <div className="btns">
+                            <div className="cancel-btn">cancel</div>
+                            <div className="save-btn">save</div>
+                        </div>
                     </div>
-                    <div className="divider" />
-                    <div className="btns">
-                        <div className="cancel-btn">cancel</div>
-                        <div className="save-btn">save</div>
-                    </div>
-                </div>
                 </Box>
             </Modal>
         </footer>
-);
+  );
+};
 
 export default Footer;
