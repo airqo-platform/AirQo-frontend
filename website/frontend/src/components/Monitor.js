32b91b83b8313fb9d435f3783034743c2ee9cdd1
Codebmk <belindamarion0@gmail.com>
2022-05-16 09:09:26 +0300

    small changes

--- website/frontend/src/components/Monitor.js
+++ website/frontend/src/components/Monitor.js
@@ -8,7 +8,7 @@ const Monitor = () => (
                 <div className="label"><span>Air Quality Monitors</span></div>
                 <p className="text-main">Low-cost air quality monitoring device</p>
                 <p className="text-sub">Our signature low-cost air quality monitor is locally built and uniquely designed to withstand physical environmental conditions.</p>
-                {/* <p className="learn-more"><span>Lean more</span><ArrowRight style={{ marginLeft: '5px' }} /></p> */}
+                {/* <p className="learn-more"><span>Learn more</span><ArrowRight style={{ marginLeft: '5px' }} /></p> */}
             </div>
             <img src={MonitorImg} alt="Monitor image" />
         </div>
