65c0d10f8f7102b391caa0029a3622f6078b43fd
danielogen <danielogen@gmail.com>
2022-05-19 15:20:41 +0300

    correct the api route

--- calibrate/src/configs/urls.js
+++ calibrate/src/configs/urls.js
@@ -4,6 +4,6 @@ const BASE_CALIBRATE_URL = stripTrailingSlash(
   process.env.REACT_APP_BASE_CALIBRATE_URL || process.env.REACT_APP_BASE_URL
 );
 
-export const CALIBRATE_TOOL_URL = `${BASE_CALIBRATE_URL}/tool`;
+export const CALIBRATE_TOOL_URL = `${BASE_CALIBRATE_URL}/calibrate/tool`;
 
-export const TRAIN_CALIBRATE_TOOL_URL = `${BASE_CALIBRATE_URL}/tool/train`;
+export const TRAIN_CALIBRATE_TOOL_URL = `${BASE_CALIBRATE_URL}/calibrate/tool/train`;
