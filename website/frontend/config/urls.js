25fab3f04e55393578451f7ce70fe21032a376e5
FaithDaka <faithbusigu8@gmail.com>
2022-06-07 07:48:46 +0300

    Reverted the hotfix

--- website/frontend/config/urls.js
+++ website/frontend/config/urls.js
@@ -1,13 +1,13 @@
 import { stripTrailingSlash } from './utils';
 
 const BASE_AIRQLOUDS_URL = stripTrailingSlash(
-  process.env.REACT_APP_BASE_AIRQLOUDS_URL || process.env.REACT_NETMANAGER_BASE_URL,
+    process.env.REACT_APP_BASE_AIRQLOUDS_URL || process.env.REACT_NETMANAGER_BASE_URL,
 );
 
 export const AIRQLOUD_SUMMARY = `${BASE_AIRQLOUDS_URL}/devices/airqlouds/summary?tenant=airqo`;
 
 const BASE_NEWSLETTER_URL = stripTrailingSlash(
-  process.env.REACT_APP_BASE_NEWSLETTER_URL || process.env.REACT_NETMANAGER_BASE_URL,
+    process.env.REACT_APP_BASE_NEWSLETTER_URL || process.env.REACT_NETMANAGER_BASE_URL,
 );
 
 export const NEWSLETTER_SUBSCRIPTION = `${BASE_NEWSLETTER_URL}/users/newsletter/subscribe?tenant=airqo`;
