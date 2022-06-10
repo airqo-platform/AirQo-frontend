e02550ad1f1ea65e5253a4243e842aa79e799a94
Codebmk <belindamarion0@gmail.com>
2022-06-10 10:29:19 +0300

    remove unwanted style

--- website/frontend/src/components/nav/NavTab.js
+++ website/frontend/src/components/nav/NavTab.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 const NavTab = ({ text, width, hideArrow, colored, filled, style, path, onClick, externalLink }) => {
     return(
         <>
-        {hideArrow ? (externalLink ? (<a href={path} className={classNames("NavTab", { colored, filled })} style={{ textDecoration:"none", color:"#fff", width: width - 32, ...(style || {}) }} onClick={onClick}>{text}</a>) : (<Link to={path || "/"} style={{textDecoration:"none", color:"#000"}}>
+        {hideArrow ? (externalLink ? (<a href={path} className={classNames("NavTab", { filled })} style={{ textDecoration:"none", color:"#fff", width: width - 32, ...(style || {}) }} onClick={onClick}>{text}</a>) : (<Link to={path || "/"} style={{textDecoration:"none", color:"#000"}}>
             <div className={classNames("NavTab", { colored, filled })} style={{ width: width - 32, ...(style || {}) }} onClick={onClick}>
                 <span>{text}</span>
             </div>
