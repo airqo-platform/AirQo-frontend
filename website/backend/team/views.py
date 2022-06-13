2a0130e9e8672352132d9d8b3d11a5e6ee2f59d6
ja-odur <odurjoseph8@gmail.com>
2022-06-13 09:34:12 +0300

    Enable ordering in view. Set permission to any

--- website/backend/team/views.py
+++ website/backend/team/views.py
@@ -1,8 +1,11 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 from .models import Member
 from .serializers import TeamMemberSerializer
 
 
 class TeamViewSet(viewsets.ReadOnlyModelViewSet):
+    permission_classes = (AllowAny,)
+    ordering_fields = ('order', 'name')
     queryset = Member.objects.all()
     serializer_class = TeamMemberSerializer
