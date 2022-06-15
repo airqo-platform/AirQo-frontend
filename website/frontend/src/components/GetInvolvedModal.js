b20d7194e9ef571fac397d980629bc802cdce6b5
ja-odur <odurjoseph8@gmail.com>
2022-06-15 09:19:10 +0300

    Add email form

--- website/frontend/src/components/GetInvolvedModal.js
+++ website/frontend/src/components/GetInvolvedModal.js
@@ -4,6 +4,7 @@ import { Modal, Box } from '@mui/material';
 import CloseIcon from 'assets/svg/Close-thin.svg';
 import ArrowBackIcon from 'assets/svg/arrowback-thin.svg';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { Link } from 'react-router-dom';
 
 const BoxWrapper = ({ children }) => (
         <div className="GetInvolvedModalWrapper">
@@ -17,6 +18,49 @@ const GetInvolvedTab = ({ category, infoText }) => (
             <div className="text-holder">
                 I’m a <strong>{category}</strong>. <br />
                 {infoText}
+            </div>
+        </div>
+);
+
+const GetInvolvedLanding = () => (
+        <div>
+            <GetInvolvedTab category="Partner" infoText="Interested in supporting AirQo’s vision" />
+            <GetInvolvedTab category="Policymaker" infoText="Interested in air quality information" />
+            <GetInvolvedTab category="Community Champion" infoText="Interested in raising awareness about air pollution." />
+            <GetInvolvedTab category="Researcher" infoText="Interested in Air Quality data and analytics" />
+            <GetInvolvedTab category="Developer" infoText="Interested in air quality data API" />
+        </div>
+);
+
+const GetInvolvedEmail = () => (
+        <div className="form-section">
+            <div className="wrapper">
+                <form className="register-form">
+                    <div className="form-field">
+                        <label>First name</label>
+                        <input type="text" id="fname" required />
+                    </div>
+                    <div className="form-field">
+                        <label>Last name</label>
+                        <input type="text" id="lname" required />
+                    </div>
+                    <div className="form-field">
+                        <label>Email address</label>
+                        <input type="email" id="email" required />
+                    </div>
+                    <div className="input-field">
+                        <input type="checkbox" required />
+                        <label>
+                            I agree to the <u>Terms of Service</u> and <u>Privacy Policy</u>
+                        </label>
+                    </div>
+                </form>
+                <div className="section-button-row">
+                    <Link to="/get-involved/check-mail" style={{ textDecoration: 'none' }}>
+                        <a className="register-btn btn-disabled btn-active" type="button">Create account</a>
+                    </Link>
+                </div>
+
             </div>
         </div>
 );
@@ -38,13 +82,8 @@ const GetInvolvedModal = ({ open, toggleOpen }) => (
                     </div>
                     <div className="content">
                         <span><ArrowBackIcon /><CloseIcon /></span>
-                        <div>
-                            <GetInvolvedTab category="Partner" infoText="Interested in supporting AirQo’s vision" />
-                            <GetInvolvedTab category="Policymaker" infoText="Interested in air quality information" />
-                            <GetInvolvedTab category="Community Champion" infoText="Interested in raising awareness about air pollution." />
-                            <GetInvolvedTab category="Researcher" infoText="Interested in Air Quality data and analytics" />
-                            <GetInvolvedTab category="Developer" infoText="Interested in air quality data API" />
-                        </div>
+                        {/*<GetInvolvedLanding />*/}
+                        <GetInvolvedEmail />
                     </div>
                 </Box>
             </BoxWrapper>
