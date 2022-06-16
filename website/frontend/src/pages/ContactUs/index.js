d7a6c23bdb0c9458def0de8e193b8b4224086b58
Busigu Faith Daka <53012890+FaithDaka@users.noreply.github.com>
2022-06-16 23:53:25 +0300

    [Website] - Ft contact us (#671)
    * new layout
    
    * change card layout and content
    
    * submit interactions and link to feedback page
    
    * Feedback page
    
    * Route to feedback page
    
    * contact us styling
    
    * spelling
    
    * global state for contact us
    
    * url configuration
    
    * endpoint

--- website/frontend/src/pages/ContactUs/index.js
+++ website/frontend/src/pages/ContactUs/index.js
@@ -1,28 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PageMini from '../PageMini';
 
-const ContactUs = ({ children }) => {
+const ContactUs = ({ children, arrow }) => {
     return (
         <PageMini>
             <div className='contact-us'>
                 <div className='left-section'>
                     <div className='section-container'>
                         <div className='content'>
-                            <h2>Get in Touch</h2>
-                            <p>We're here to help, we check our email frequently.</p>
+                            <h2>Get in touch</h2>
                             <span id='_1'>
-                                <p><b>Uganda, East Africa</b><br />Makerere University</p>
+                                <p>Makerere University</p>
                             </span>
                             <span id='_2'>
-                                <p>Software Systems Centre, Block B,<br />Level 3, College of Computing and<br /> Information Sciences, Plot 56 University<br /> Pool Road</p>
+                                <p>Software Systems Centre, Block B, Level 3, College of Computing and Information Sciences, Plot 56 University Pool Road. Kampala, Uganda</p>
                             </span>
                             <span id='_3'>
-                                <p>+256 703734167 <br />info@airqo.net</p>
+                                <p>T: <a href='tel:+256703734167'>+256 703734167</a> <br />E: <a href='mailto:info@airqo.net?subject=Mail from AirQo Website'>info@airqo.net</a></p>
                             </span>
                         </div>
                     </div>
                 </div>
                 <div className='right-section'>
+                    <Link to='/contact' className='icon'>{arrow}</Link>
                     <div className='section-container'>
                         <div className='content'>
                             {children}
