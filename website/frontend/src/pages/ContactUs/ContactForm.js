216b93595b9157247bfcfc3cdddfaf8d71b04791
FaithDaka <faithbusigu8@gmail.com>
2022-06-16 12:35:28 +0300

    submit interactions and link to feedback page

--- website/frontend/src/pages/ContactUs/ContactForm.js
+++ website/frontend/src/pages/ContactUs/ContactForm.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import ContactUs from '.';
 import { useInitScrollTop } from 'utils/customHooks';
+import { Link, useNavigate } from 'react-router-dom';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 
 const ContactForm = () => {
     useInitScrollTop();
+    const history = useNavigate();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        history('/contact/sent');
+    }
     return (
-        <ContactUs>
-            <form>
+        <ContactUs arrow={<ArrowBackIcon />}>
+            <form onSubmit={handleSubmit}>
                 <div>
-                    <label>Your full name*</label><br />
+                    <label>Full name</label><br />
                     <input type="text" required />
                 </div>
                 <div>
-                    <label>Email address*</label><br />
+                    <label>Email address</label><br />
                     <input type="email" required />
                 </div>
                 <div>
                     <label>Your message*</label><br />
-                    <textarea type="text" rows="8" cols="30" required/>
+                    <textarea type="text" rows="8" cols="30" required />
                 </div>
-                <input type="submit" value="Submit" className='submit-button' />
+                <div className='checkpoint'>
+                    <input type='checkbox' required />
+                    <label>I agree to the <Link to='/terms'>Terms of Service</Link> and <Link to='/terms'>Privacy Policy</Link></label>
+                </div>
+                <input type="submit" value="Send" className='submit-button' />
             </form>
         </ContactUs>
     )
