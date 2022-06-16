9ea1b3a181d5d09f812357f2e2bec243caecd149
FaithDaka <faithbusigu8@gmail.com>
2022-06-16 12:34:35 +0300

    change card layout and content

--- website/frontend/src/pages/ContactUs/cards.js
+++ website/frontend/src/pages/ContactUs/cards.js
@@ -4,10 +4,10 @@ import ContactCard from './card';
 const Cards = () => {
     return (
         <div className='cards'>
-            <ContactCard preamble={"I have a question about"} title={"Air Quality Monitors"} page_link={"/contact/form"}></ContactCard>
+            <ContactCard preamble={"I have a question about"} title={"Air Quality Tools"} page_link={"/contact/form"}></ContactCard>
             <ContactCard preamble={"I have a question about"} title={"Air Quality Data"} page_link={"/contact/form"}></ContactCard>
-            <ContactCard preamble={"I have"} title={"some feedback"} page_link={"/contact/form"}></ContactCard>
-            <ContactCard preamble={"It's a"} title={"general inquiry"} page_link={"/contact/form"}></ContactCard>
+            <ContactCard preamble={"I have some"} title={"feedback"} page_link={"/contact/form"}></ContactCard>
+            <ContactCard preamble={"I have a"} title={"general inquiry"} page_link={"/contact/form"}></ContactCard>
         </div>
     )
 }
