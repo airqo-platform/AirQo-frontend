8934df11c639072a6b6d477b1b70c8ec020e33f2
Noah Nsimbe <37845280+NoahNsimbe@users.noreply.github.com>
2022-06-16 21:01:46 +0300

    Fix - share buttons don't work (#669)
    * Created custom heart icon
    
    * Created custom analytics footer
    
    * Added custom loading indicator to share feature
    
    * Update native_api.dart
    
    * typo
    
    * Set inkwell tapping
    
    * Set click handlers for analytics cards
    
    * cleanup
    
    * Update native_api.dart
    
    temporarily removed share test due to Whatsapp limitations
    
    * Update analytics_widgets.dart
    
    * Added mount checks

--- mobile/lib/widgets/custom_widgets.dart
+++ mobile/lib/widgets/custom_widgets.dart
@@ -7,11 +7,17 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:lottie/lottie.dart';
+import 'package:provider/provider.dart';
 
 import '../models/enum_constants.dart';
+import '../models/place_details.dart';
+import '../services/app_service.dart';
+import '../services/native_api.dart';
 import '../themes/app_theme.dart';
 import '../themes/colors.dart';
 import 'buttons.dart';
+import 'custom_shimmer.dart';
 
 class AppRefreshIndicator extends StatelessWidget {
   const AppRefreshIndicator({
@@ -244,6 +250,134 @@ class MiniAnalyticsAvatar extends StatelessWidget {
           const Spacer(),
         ],
       ),
+    );
+  }
+}
+
+class HeartIcon extends StatelessWidget {
+  const HeartIcon({
+    Key? key,
+    required this.showAnimation,
+    required this.placeDetails,
+  }) : super(key: key);
+
+  final bool showAnimation;
+  final PlaceDetails placeDetails;
+
+  @override
+  Widget build(BuildContext context) {
+    if (showAnimation) {
+      return SizedBox(
+        height: 16.67,
+        width: 16.67,
+        child: Lottie.asset(
+          'assets/lottie/animated_heart.json',
+          repeat: false,
+          reverse: false,
+          animate: true,
+          fit: BoxFit.cover,
+        ),
+      );
+    }
+
+    return Consumer<PlaceDetailsModel>(
+      builder: (context, placeDetailsModel, child) {
+        return SvgPicture.asset(
+          PlaceDetails.isFavouritePlace(
+            placeDetailsModel.favouritePlaces,
+            placeDetails,
+          )
+              ? 'assets/icon/heart.svg'
+              : 'assets/icon/heart_dislike.svg',
+          semanticsLabel: 'Favorite',
+          height: 16.67,
+          width: 16.67,
+        );
+      },
+    );
+  }
+}
+
+class AnalyticsCardFooter extends StatefulWidget {
+  const AnalyticsCardFooter({
+    Key? key,
+    required this.placeDetails,
+    required this.measurement,
+    required this.shareKey,
+    this.loadingRadius,
+  }) : super(key: key);
+  final PlaceDetails placeDetails;
+  final Measurement measurement;
+  final GlobalKey shareKey;
+  final double? loadingRadius;
+
+  @override
+  State<AnalyticsCardFooter> createState() => _AnalyticsCardFooterState();
+}
+
+class _AnalyticsCardFooterState extends State<AnalyticsCardFooter> {
+  bool _showHeartAnimation = false;
+  bool _shareLoading = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: _shareLoading
+              ? LoadingIcon(
+                  radius: widget.loadingRadius ?? 14,
+                )
+              : InkWell(
+                  onTap: () async => _share(),
+                  child: IconTextButton(
+                    iconWidget: SvgPicture.asset(
+                      'assets/icon/share_icon.svg',
+                      color: CustomColors.greyColor,
+                      semanticsLabel: 'Share',
+                    ),
+                    text: 'Share',
+                  ),
+                ),
+        ),
+        Expanded(
+          child: InkWell(
+            onTap: () async => _updateFavPlace(),
+            child: IconTextButton(
+              iconWidget: HeartIcon(
+                showAnimation: _showHeartAnimation,
+                placeDetails: widget.placeDetails,
+              ),
+              text: 'Favorite',
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  Future<void> _share() async {
+    setState(() => _shareLoading = true);
+    final shareMeasurement = widget.measurement;
+    shareMeasurement.site.name = widget.placeDetails.name;
+    final complete = await ShareService.shareCard(
+      context,
+      widget.shareKey,
+      shareMeasurement,
+    );
+    if (complete && mounted) {
+      setState(() => _shareLoading = false);
+    }
+  }
+
+  Future<void> _updateFavPlace() async {
+    setState(() => _showHeartAnimation = true);
+    Future.delayed(const Duration(seconds: 2), () {
+      setState(() => _showHeartAnimation = false);
+    });
+    await AppService().updateFavouritePlace(
+      widget.placeDetails,
+      context,
     );
   }
 }
