b7a969e86f4fca4261c995040ca6285c199c79da
FaithDaka <faithbusigu8@gmail.com>
2022-06-16 12:36:04 +0300

    Feedback page

---
+++ website/frontend/src/pages/ContactUs/Feedback.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import FeedbackImg from 'assets/img/Feedback.png'
+import { Link } from 'react-router-dom'
+import { useInitScrollTop } from 'utils/customHooks';
+
+const Feedback = () => {
+    useInitScrollTop();
+    return (
+        <div className='contactus-feedback'>
+            <div>
+                <img src={FeedbackImg} alt='Thank you for your feedback' />
+            </div>
+            <h2>Thank for reaching out. <br />We’ll get back to you!</h2>
+            <button>
+                <Link to='/'>Back Home</Link>
+            </button>
+        </div>
+    )
+}
+
+export default Feedback
