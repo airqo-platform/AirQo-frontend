4cfa550d2b4834e3b3b88e09d6bac3a6521e4af2
FaithDaka <faithbusigu8@gmail.com>
2022-06-23 11:33:44 +0300

    left column

--- website/frontend/src/components/HighlightsSection/index.js
+++ website/frontend/src/components/HighlightsSection/index.js
@@ -11,31 +11,33 @@ const HighlightsSection = () => {
 
   return (
     <div className='highlights-section'>
-      <div className='content'>
-        <Post
-          postImg={TeamImg}
-          tag1={'features'}
-          tag2={'event'}
-          title={'AirQo at the Sustainability Research & Innovation (SRI) Congress 2022'}
-          article_title={'Read more'}
-          article_link={'https://sri2022.org/about-sri2022/'}
-        />
-        {
-          <Post 
-          postImg={FeatureImg}
-          tag1={'features'}
-          tag2={'event'}
-          title={'Helping communities combat air pollution through digital technologies'}
-          article_title={'Read article'}
+      <div className='highlights-container'>
+        <div className='content'>
+          <Post
+            postImg={TeamImg}
+            tag1={'features'}
+            tag2={'event'}
+            title={'AirQo at the Sustainability Research & Innovation (SRI) Congress 2022'}
+            article_title={'Read more'}
+            article_link={'https://sri2022.org/about-sri2022/'}
           />
-        }
-      </div>
-      <div className='pagination'>
-        <Pagination
-          paginate={paginate}
-          totalPosts={totalPosts}
-          postsPerHighlight={totalPosts}
-        />
+          {
+            <Post
+              postImg={FeatureImg}
+              tag1={'features'}
+              tag2={'event'}
+              title={'Helping communities combat air pollution through digital technologies'}
+              article_title={'Read article'}
+            />
+          }
+        </div>
+        <div className='pagination'>
+          <Pagination
+            paginate={paginate}
+            totalPosts={totalPosts}
+            postsPerHighlight={totalPosts}
+          />
+        </div>
       </div>
     </div>
   )
