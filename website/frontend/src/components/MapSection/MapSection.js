3b10826ed98d82d71acb4eb35a7b9b2c9121814f
FaithDaka <faithbusigu8@gmail.com>
2022-07-05 13:17:55 +0300

    homepage consistency

--- website/frontend/src/components/MapSection/MapSection.js
+++ website/frontend/src/components/MapSection/MapSection.js
@@ -8,19 +8,19 @@ const MapSection = () => {
   const largeScreen = 1440;
   return (
     <div className="map-section">
-        <div className='backdrop'>
-            <div className='map-content'>
-                <span id='first-pill'><p>Air Quality Map</p></span>
-                  <h3 className='content-h'>Live air quality insights across Africa</h3>
-                <span className='content-p'>
-                      <p>Visualize hourly air quality information with a single click, over our growing network across African cities.</p>
-                </span>
-                {/* <span id='second-pill'><p>View map</p> <i> <ArrowRight/> </i></span> */}
-            </div>
-            <div className='map-image'>
-                <img className = 'map-img' src={windowSize.width <= largeScreen ? HomePageMap : MapWrapper}/>
-            </div>
+      <div className='backdrop'>
+        <div className='map-content'>
+          <span id='first-pill'><p>Air Quality Map</p></span>
+          <h3 className='content-h'>Live air quality insights across Africa</h3>
+          <span className='content-p'>
+            <p>Visualize hourly air quality information with a single click, over our growing network across African cities.</p>
+          </span>
+          {/* <span id='second-pill'><p>View map</p> <i> <ArrowRight/> </i></span> */}
         </div>
+        <div className='map-image'>
+          <img className='map-img' src={windowSize.width <= largeScreen ? HomePageMap : MapWrapper} />
+        </div>
+      </div>
     </div>
   )
 }
