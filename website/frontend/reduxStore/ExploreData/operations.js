b7ba2f3df15735303fb2c0ce6b3eab921da0ae75
ja-odur <odurjoseph8@gmail.com>
2022-07-05 23:46:34 +0300

    CLean up redux

--- website/frontend/reduxStore/ExploreData/operations.js
+++ website/frontend/reduxStore/ExploreData/operations.js
@@ -1,24 +1,27 @@
-import { requestDataAccessApi } from "apis";
-import { EXPLORE_DATA_REQUEST_SUCCESS, EXPLORE_STATE_DATA_SUCCESS, EXPLORE_DATA_REQUEST_FAILURE } from "./actions";
+import { requestDataAccessApi } from 'apis';
+import {
+  UPDATE_EXPLORE_DATA_SUCCESS,
+  UPDATE_EXPLORE_DATA_FAILURE,
+  EXPLORE_DATA_REQUEST_SUCCESS,
+  EXPLORE_DATA_REQUEST_FAILURE,
+} from './actions';
 
-export const addExploreDataRequest = (data) => async (dispatch) => {
-    return await requestDataAccessApi({data})
-    .then(res=>{
-        // console.log('User data: ', data);
-        dispatch({
-            type: EXPLORE_DATA_REQUEST_SUCCESS,
-            payload: {...data, success: true}
-        });
-    })
-    .catch(err => {
-        dispatch({
-            type: EXPLORE_DATA_REQUEST_FAILURE,
-        })
-    });}
-
-export const postStateData = (data) => (dispatch) =>  {
-    return dispatch({
-        type: EXPLORE_STATE_DATA_SUCCESS,
-        payload: {...data, success: true}
+export const postExploreDataRequest = (data) => async (dispatch) => await requestDataAccessApi({ data })
+  .then(() => {
+    // console.log('User data: ', data);
+    dispatch({
+      type: EXPLORE_DATA_REQUEST_SUCCESS,
+      payload: { success: true },
     });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+export const updateExploreData = (data) => (dispatch) => {
+  console.log("new data", data)
+  dispatch({
+    type: UPDATE_EXPLORE_DATA_SUCCESS,
+    payload: data,
+  });
 }
--- website/frontend/reduxStore/ExploreData/reducers.js
+++ website/frontend/reduxStore/ExploreData/reducers.js
@@ -1,22 +1,26 @@
-import { EXPLORE_DATA_REQUEST_SUCCESS, EXPLORE_STATE_DATA_SUCCESS } from "./actions";
+import { EXPLORE_DATA_REQUEST_SUCCESS, UPDATE_EXPLORE_DATA_SUCCESS } from './actions';
 
 const initialState = {
-    category: null,
-    firstName: null,
-    lastName: null,
-    long_organization: null,
-    jobTitle: null,
-    email: null,
-    success: false
-}
+  userType: {
+    individual: false,
+    organization: false,
+  },
+  category: "",
+  firstName: null,
+  lastName: null,
+  long_organization: null,
+  jobTitle: null,
+  email: null,
+  success: false,
+};
 
 export default function (state = initialState, action) {
-    switch (action.type) {
-        case EXPLORE_DATA_REQUEST_SUCCESS:
-            return {...state, ...action.payload};
-        case EXPLORE_STATE_DATA_SUCCESS:
-            return {...state, ...action.payload};
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case EXPLORE_DATA_REQUEST_SUCCESS:
+      return { ...state, ...action.payload };
+    case UPDATE_EXPLORE_DATA_SUCCESS:
+      return { ...state, ...action.payload };
+    default:
+      return state;
+  }
 }
