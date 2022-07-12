ad4b2f5c39a8b93efad8fe64ba294f34c04f22ca
FaithDaka <faithbusigu8@gmail.com>
2022-07-12 13:03:08 +0300

    Loadspinner

--- website/frontend/App.js
+++ website/frontend/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense, useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import HomePage from 'src/pages/HomePage';
@@ -16,32 +16,46 @@ import Register from './src/pages/GetInvolved/Register';
 import CheckMail from './src/pages/GetInvolved/CheckMail';
 import store from './store';
 import Feedback from './src/pages/ContactUs/Feedback';
+import Loadspinner from './src/components/LoadSpinner';
 
 store.dispatch(loadAirQloudSummaryData());
 
-const App = () => (
-    <Provider store={store}>
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomePage />} />
-                <Route path="/solutions/research" element={<ResearchPage />} />
-                <Route path="/solutions/communities" element={<CommunityPage />} />
-                <Route path="/solutions/african-cities" element={<AfricanCitiesPage />}>
-                    <Route path="uganda" element={<ContentUganda />} />
-                    <Route path="kenya" element={<ContentKenya />} />
-                </Route>
-                <Route path="/about-us" element={<AboutUsPage />} />
-                <Route path="/press" element={<Press />} />
-                <Route path="/terms" element={<Terms />} />
-                <Route path="/contact" element={<ContactUsPage />} />
-                <Route path="/contact/form" element={<ContactForm />} />
-                <Route path="/contact/sent" element={<Feedback />} />
-                <Route path="/get-involved" element={<GetInvolved />} />
-                <Route path="/get-involved/register" element={<Register />} />
-                <Route path="/get-involved/check-mail" element={<CheckMail />} />
-            </Routes>
-        </Router>
-    </Provider>
-);
+const App = () => {
+    const [isLoading, setLoading] = useState(true);
+
+    useEffect(() => {
+        setTimeout(()=>setLoading(false), 2500);
+    }, []);
+
+    return (
+        isLoading ? <Loadspinner /> :
+            <Provider store={store}>
+                <Suspense
+                    fallback={<Loadspinner />}>
+                    <Router>
+                        <Routes>
+                            <Route path="/" element={<HomePage />} />
+                            <Route path="/solutions/research" element={<ResearchPage />} />
+                            <Route path="/solutions/communities" element={<CommunityPage />} />
+                            <Route path="/solutions/african-cities" element={<AfricanCitiesPage />}>
+                                <Route path="uganda" element={<ContentUganda />} />
+                                <Route path="kenya" element={<ContentKenya />} />
+                            </Route>
+                            <Route path="/about-us" element={<AboutUsPage />} />
+                            <Route path="/press" element={<Press />} />
+                            <Route path="/terms" element={<Terms />} />
+                            <Route path="/contact" element={<ContactUsPage />} />
+                            <Route path="/contact/form" element={<ContactForm />} />
+                            <Route path="/contact/sent" element={<Feedback />} />
+                            <Route path="/get-involved" element={<GetInvolved />} />
+                            <Route path="/get-involved/register" element={<Register />} />
+                            <Route path="/get-involved/check-mail" element={<CheckMail />} />
+                            <Route path="/loader" element={<Loadspinner />} />
+                        </Routes>
+                    </Router>
+                </Suspense>
+            </Provider>
+    )
+};
 
 export default App;
