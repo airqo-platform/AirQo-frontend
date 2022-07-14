1d92dfc8fd011b951f6a04767642f79911801b71
ja-odur <odurjoseph8@gmail.com>
2022-07-14 22:02:25 +0300

    Add loader to detailed page detailed page

--- website/frontend/src/pages/CareerDetailPage.js
+++ website/frontend/src/pages/CareerDetailPage.js
@@ -3,8 +3,9 @@ import { useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { useInitScrollTop } from 'utils/customHooks';
 import { loadCareersListingData } from 'reduxStore/Careers/operations';
-import { useCareerListingData } from 'reduxStore/Careers/selectors';
+import { useCareerListingData, useCareerLoadingData } from 'reduxStore/Careers/selectors';
 import { isEmpty } from 'underscore';
+import Loadspinner from 'src/components/LoadSpinner';
 import PageMini from './PageMini';
 
 const BulletList = ({ name, bullets }) => (
@@ -19,7 +20,7 @@ const CareerDetailPage = () => {
   const { uniqueTitle } = useParams();
   const dispatch = useDispatch();
   const careerListing = useCareerListingData();
-
+  const loading = useCareerLoadingData();
   const listing = careerListing[uniqueTitle] || {};
 
   useEffect(() => {
@@ -28,21 +29,40 @@ const CareerDetailPage = () => {
 
   return (
         <PageMini>
-            <div className="CareersDetailPage">
-                <div className="content">
-                    <header className="title">
-                        <span>{listing.title}</span>
-                        <span>{listing.department && listing.department.name}  /  {listing.type}</span>
-                    </header>
-                    {(listing.descriptions || []).map((value, key) => (
-                            <p className="description" key={key}>{value.description}</p>
-                    ))}
-                    {(listing.bullets || []).map((value, key) => <BulletList key={key} name={value.name} bullets={value.bullet_points} />)}
+            { loading && <Loadspinner /> }
+            { isEmpty(listing) && <div className="careers-detail--no-listing">Job listing not found or closed.</div>}
+            {
+                !loading && !isEmpty(listing) && (
+                    <div className="CareersDetailPage">
+                        <div className="content">
+                            <header className="title">
+                                <span>{listing.title}</span>
+                                <span>{listing.department && listing.department.name} / {listing.type}</span>
+                            </header>
+                            {(listing.descriptions || []).map((value, key) => (
+                                <p className="description" key={key}>{value.description}</p>
+                            ))}
+                            {(listing.bullets || []).map((value, key) => (
+                                <BulletList
+                                  key={key}
+                                  name={value.name}
+                                  bullets={value.bullet_points}
+                                />
+                            ))}
 
-                    {/* eslint-disable-next-line react/button-has-type */}
-                    <a target="_blank" href={listing.apply_url} rel="noreferrer"><button className="button-hero">Apply</button></a>
-                </div>
-            </div>
+                            {/* eslint-disable-next-line react/button-has-type */}
+                            {
+                                listing.apply_url
+                                && (
+                                    <a target="_blank" href={listing.apply_url} rel="noreferrer">
+                                        <button className="button-hero">Apply</button>
+                                    </a>
+                                )
+                            }
+                        </div>
+                    </div>
+                )
+            }
         </PageMini>
   );
 };
