0bbb63fe31ff3285a677ab5411107dd1b623b795
Mike Mwanje <mwanjemike767@gmail.com>
2022-08-29 18:58:27 +0300

    intergrate with cypress dashboard

--- platform/cypress.config.js
+++ platform/cypress.config.js
@@ -6,11 +6,11 @@ module.exports = defineConfig({
       // implement node event listeners here
     },
   },
-
   component: {
     devServer: {
       framework: "next",
       bundler: "webpack",
     },
   },
+  projectId: "ap5jjk"
 });
