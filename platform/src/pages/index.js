9478caf5e279a88b2867f388b19961e6b5711507
Mike Mwanje <mwanjemike767@gmail.com>
2022-08-29 16:02:21 +0300

    remove space

--- platform/src/pages/index.js
+++ platform/src/pages/index.js
@@ -3,7 +3,7 @@ function HomePage() {
     <>
       <h1>Airqo platform (beta)</h1>
       <div>
-        Find the original airqo platform{" "}
+        Find the original airqo platform
         <a href={process.env.NEXT_PUBLIC_ORIGINAL_PLATFORM}>here</a>
       </div>
     </>
