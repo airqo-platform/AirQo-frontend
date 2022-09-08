7d8dc5f879dcd99e49f8abb66d6f22cd208f7288
Benjamin Ssempala <benssempal1445@gmail.com>
2022-09-08 19:02:22 +0300

    dart metrics

--- mobile/lib/main_common.dart
+++ mobile/lib/main_common.dart
@@ -20,6 +20,7 @@ class AirQoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var config = AppConfig.of(context);
+
     return MultiProvider(
       providers: [
         ChangeNotifierProvider(
