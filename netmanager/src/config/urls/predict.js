25d3c27a7efdb17903e53da37c9a821c652018cc
FaithDaka <faithbusigu8@gmail.com>
2022-09-15 23:07:56 +0300

    feat: kampala airqloud heatmap

--- netmanager/src/config/urls/predict.js
+++ netmanager/src/config/urls/predict.js
@@ -4,5 +4,4 @@ const BASE_PREDICT_URL = stripTrailingSlash(
   process.env.REACT_APP_BASE_PREDICT_URL || process.env.REACT_APP_BASE_URL
 );
 
-export const GET_HEATMAP_DATA = `${BASE_PREDICT_URL}/predict/heatmap?airqloud=kawempe`;
-console.log('Heatmap URL:', GET_HEATMAP_DATA)
+export const GET_HEATMAP_DATA = `${BASE_PREDICT_URL}/predict/heatmap?airqloud=kampala`;
