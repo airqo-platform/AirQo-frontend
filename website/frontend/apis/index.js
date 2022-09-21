9f1cf456cae3b92e724d36cc0f272a98f9823c0f
Belinda Marion Kobusingye <belindamarion0@gmail.com>
2022-09-21 16:48:36 +0300

    website api auth config

--- website/frontend/apis/index.js
+++ website/frontend/apis/index.js
@@ -10,36 +10,33 @@ import {
 } from 'config/urls';
 import { HIGHLIGHTS_URL, TAGS_URL } from '../config/urls';
 
-const headers = {
+const header = {
     Authorization: process.env.REACT_APP_AUTHORIZATION_TOKEN,
 };
 
 export const getAirQloudSummaryApi = async () =>
-    await axios.get(AIRQLOUD_SUMMARY, { headers }).then((response) => response.data);
+    await axios.get(AIRQLOUD_SUMMARY, { headers: { header } }).then((response) => response.data);
 
 export const newsletterSubscriptionApi = async (data) =>
-    await axios.post(NEWSLETTER_SUBSCRIPTION, data, { headers }).then((response) => response.data);
+    await axios.post(NEWSLETTER_SUBSCRIPTION, data, { headers: { header } }).then((response) => response.data);
 
 export const contactUsApi = async (data) =>
-    await axios.post(INQUIRY_URL, data, { headers }).then((response) => response.data);
+    await axios.post(INQUIRY_URL, data, { headers: { header } }).then((response) => response.data);
 
 export const sendInquiryApi = async (data) =>
-    await axios.post(INQUIRY_URL, data, { headers }).then((response) => response.data);
+    await axios.post(INQUIRY_URL, data, { headers: { header } }).then((response) => response.data);
 
 export const requestDataAccessApi = async (data) =>
-    await axios.post(EXPLORE_DATA_URL, data, { headers }).then((response) => response.data);
+    await axios.post(EXPLORE_DATA_URL, data, { headers: { header } }).then((response) => response.data);
 
 // Careers endpoints
-export const getAllCareersApi = async () => await axios.get(CAREERS_URL, { headers }).then((response) => response.data);
+export const getAllCareersApi = async () => await axios.get(CAREERS_URL).then((response) => response.data);
 
-export const getAllDepartmentsApi = async () =>
-    await axios.get(DEPARTMENTS_URL, { headers }).then((response) => response.data);
+export const getAllDepartmentsApi = async () => await axios.get(DEPARTMENTS_URL).then((response) => response.data);
 
 // Teams endpoints
-export const getAllTeamMembersApi = async () =>
-    await axios.get(TEAMS_URL, { headers }).then((response) => response.data);
+export const getAllTeamMembersApi = async () => await axios.get(TEAMS_URL).then((response) => response.data);
 
 // Highlights endpoints
-export const getAllHighlightsApi = async () =>
-    await axios.get(HIGHLIGHTS_URL, { headers }).then((response) => response.data);
-export const getAllTagsApi = async () => await axios.get(TAGS_URL, { headers }).then((response) => response.data);
+export const getAllHighlightsApi = async () => await axios.get(HIGHLIGHTS_URL).then((response) => response.data);
+export const getAllTagsApi = async () => await axios.get(TAGS_URL).then((response) => response.data);
