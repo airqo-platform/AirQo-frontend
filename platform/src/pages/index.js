1c8db00e6bf93d241f74c4b8d1eee554f1666188
Belinda Marion Kobusingye <belindamarion0@gmail.com>
2022-09-22 15:59:59 +0300

    tailwind css setup

--- platform/src/pages/index.js
+++ platform/src/pages/index.js
@@ -6,6 +6,7 @@ function HomePage() {
         Find the original airqo platform
         <a href={process.env.NEXT_PUBLIC_ORIGINAL_PLATFORM}>here</a>
       </div>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
     </>
   );
 }
