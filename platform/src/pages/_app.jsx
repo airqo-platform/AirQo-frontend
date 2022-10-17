3089584118b255fe40d42bf172255b95ecadec50
patrikbodnar <100933923+patrikbodnar@users.noreply.github.com>
2022-10-17 21:19:14 +0200

    Moving favicon link to _document.jxs

--- platform/src/pages/_app.jsx
+++ platform/src/pages/_app.jsx
@@ -1,13 +1,5 @@
 import '@/styles/global.scss';
-import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link rel="shortcut icon" href="icons/favicon.ico" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
