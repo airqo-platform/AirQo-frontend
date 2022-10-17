ea84c026747b973867239781f1cc9ea481cc8d21
patrikbodnar <100933923+patrikbodnar@users.noreply.github.com>
2022-10-17 20:52:23 +0200

    added favicon to the analyrics platform
    Succesfully added  favicon to the analyrics platform. :+1:

--- platform/src/pages/_app.jsx
+++ platform/src/pages/_app.jsx
@@ -1,5 +1,13 @@
 import '@/styles/global.scss';
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="shortcut icon" href="icons/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
