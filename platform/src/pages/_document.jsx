099e78b176eeed2e1aba709b3c29c9ca07257357
bhanuloshali1995 <bhanuloshali11@gmail.com>
2022-10-18 00:15:44 +0530

    fix: added favicon

--- platform/src/pages/_document.jsx
+++ platform/src/pages/_document.jsx
@@ -12,6 +12,10 @@ class MyDocument extends Document {
         <Head>
           <link rel='preconnect' href='https://fonts.googleapis.com' />
           <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin />
+          <link rel="icon" type="image/x-icon" href="favicon.png" />
+          <link rel="apple-touch-icon" href="favicon.png" />
+          <link rel="shortcut icon" href="favicon.png" />
+          <link rel="apple-touch-icon" sizes="76x76" href="favicon.png" />
           <link
             href='https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap'
             rel='stylesheet'
