c3094103e9265d09f75fde08ba2cf4aa257a3326
Belinda Marion Kobusingye <belindamarion0@gmail.com>
2022-11-16 16:13:29 +0300

    delete polluntant value from localstorage on logout

--- netmanager/src/utils/setAuthToken.js
+++ netmanager/src/utils/setAuthToken.js
@@ -1,25 +1,25 @@
 //we will use this to delete the Authorization header for our axios requests
 //depending on whether the user is logged in or not
 
-import axios from "axios";
-import jwt_decode from "jwt-decode";
+import axios from 'axios';
+import jwt_decode from 'jwt-decode';
 
 const setAuthToken = (token) => {
   if (token) {
     const decoded_token = jwt_decode(token);
     const tenant = decoded_token.organization;
 
-    if (typeof tenant === "undefined") {
+    if (typeof tenant === 'undefined') {
       // remove stored tokens to enable user to re-login
-      localStorage.removeItem("jwtToken");
-      localStorage.removeItem("currentUser");
+      localStorage.removeItem('jwtToken');
+      localStorage.removeItem('currentUser');
       throw new Error(
         "This user's organization could not be determined, Please refresh to login again"
       );
     }
 
     // Apply authorization token to every request if logged in
-    axios.defaults.headers.common["Authorization"] = token;
+    axios.defaults.headers.common['Authorization'] = token;
     axios.interceptors.request.use((config) => {
       const params = config.params || {};
 
@@ -27,9 +27,10 @@ const setAuthToken = (token) => {
     });
   } else {
     // Delete auth header
-    delete axios.defaults.headers.common["Authorization"];
+    delete axios.defaults.headers.common['Authorization'];
     localStorage.removeItem('mapStyle');
     localStorage.removeItem('mapMode');
+    localStorage.removeItem('pollutant');
   }
 };
 export default setAuthToken;
