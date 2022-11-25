393170930ae8b89047e0cb1df151e37f63a0c98c
HassanBahati <mukisabahati@gmail.com>
2022-11-25 11:20:58 +0300

    add index test

---
+++ docs/cypress/e2e/tests/index.cy.js
@@ -0,0 +1,8 @@
+/// <reference types="cypress" />
+
+describe("hello world test", () => {
+    //block of tests
+    it("test home", ()=>{
+        cy.visit("https://staging-docs.airqo.net/#/")
+    })
+});
