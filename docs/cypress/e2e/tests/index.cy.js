cfde9b808cedd348ef10887b02ca8b34e9821ebb
HassanBahati <mukisabahati@gmail.com>
2022-11-27 08:12:23 +0300

    feat: check for presence of search input in the result items

--- docs/cypress/e2e/tests/index.cy.js
+++ docs/cypress/e2e/tests/index.cy.js
@@ -1,12 +1,15 @@
 /// <reference types="cypress" />
 
 describe("hello world test", () => {
-    //block of tests
-    it("test home", ()=>{
-        cy.visit("https://staging-docs.airqo.net/#/")
-    })
+  //block of tests
+  it("test home", () => {
+    cy.visit("https://staging-docs.airqo.net/#/");
+  });
 
-    it("Verify ability to Search", ()=>{
-        cy.get(".input-wrap  > input").type("AirQo")
-    })
+  it("Verify ability to Search", () => {
+    cy.get(".input-wrap  > input").type("AirQo");
+    cy.get(".results-panel").should(($lis) => {
+      expect($lis.eq(0), "first item").to.contain("AirQo");
+    });
+  });
 });
