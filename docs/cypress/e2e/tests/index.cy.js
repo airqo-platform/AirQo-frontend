8d85d90eec7e5874409f5570cd86fb66698907d5
HassanBahati <mukisabahati@gmail.com>
2022-11-27 07:30:32 +0300

    feat: add tests to verify side menu button is functional

--- docs/cypress/e2e/tests/index.cy.js
+++ docs/cypress/e2e/tests/index.cy.js
@@ -1,8 +1,11 @@
 /// <reference types="cypress" />
 
 describe("Verify AirQo Documentation Home Page", () => {
+  it("test home", () => {
+    cy.visit("https://staging-docs.airqo.net/");
+  });
 
-    it("test home", ()=>{
-        cy.visit("https://staging-docs.airqo.net/")
-    })
+  it("Verify Side Menu Button is functional", () => {
+    cy.get(".sidebar-toggle").click();
+  });
 });
