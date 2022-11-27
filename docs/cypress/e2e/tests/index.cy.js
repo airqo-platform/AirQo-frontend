62ec030917de0c5c6db0ca7743dbe015a44168b4
HassanBahati <mukisabahati@gmail.com>
2022-11-27 10:26:35 +0300

    feat: verify edit on gitub button is clickable

--- docs/cypress/e2e/tests/index.cy.js
+++ docs/cypress/e2e/tests/index.cy.js
@@ -1,8 +1,13 @@
 /// <reference types="cypress" />
+import { ReplaceUrl } from "../../../utils/index";
 
 describe("hello world test", () => {
-    //block of tests
-    it("test home", ()=>{
-        cy.visit("https://staging-docs.airqo.net/#/")
-    })
+  //block of tests
+  it("test home", () => {
+    cy.visit("https://staging-docs.airqo.net/#/");
+  });
+
+  it("Verify Edit On GitHub Button is clickable", () => {
+    cy.contains("Edit On GitHub").click();
+  });
 });
