8c4df2d8bbec094ff35528e2aa93cedfeda54c76
HassanBahati <mukisabahati@gmail.com>
2022-11-27 11:46:46 +0300

    refactor: format file

--- docs/cypress/e2e/tests/index.cy.js
+++ docs/cypress/e2e/tests/index.cy.js
@@ -1,8 +1,8 @@
 /// <reference types="cypress" />
 
 describe("hello world test", () => {
-    //block of tests
-    it("test home", ()=>{
-        cy.visit("https://staging-docs.airqo.net/#/")
-    })
+  //block of tests
+  it("test home", () => {
+    cy.visit("https://staging-docs.airqo.net/#/");
+  });
 });
