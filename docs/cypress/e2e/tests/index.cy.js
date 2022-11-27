a0f3c34ef0b44dcef0c279540bd1397a595f75a0
HassanBahati <mukisabahati@gmail.com>
2022-11-27 10:50:56 +0300

    feat: verify destination of edit on github button

--- docs/cypress/e2e/tests/index.cy.js
+++ docs/cypress/e2e/tests/index.cy.js
@@ -9,4 +9,13 @@ describe("hello world test", () => {
   it("Verify Edit On GitHub Button is clickable", () => {
     cy.contains("Edit On GitHub").click();
   });
+
+  it("Verify Destination of Edit On Github button", () => {
+    cy.contains("Edit On GitHub").click();
+    cy.on("url:changed", (newUrl) => {
+      expect(newUrl).to.contain(
+        "https://github.com/airqo-platform/AirQo-frontend/tree/staging/docs"
+      );
+    });
+  });
 });
