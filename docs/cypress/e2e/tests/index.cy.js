f692208319955d68ddd174614f83a023d56dcf59
HassanBahati <mukisabahati@gmail.com>
2022-11-27 12:45:33 +0300

    format file

--- docs/cypress/e2e/tests/index.cy.js
+++ docs/cypress/e2e/tests/index.cy.js
@@ -16,9 +16,15 @@ describe("hello world test", () => {
 
   it("Verify Side Bar menu sections exist", () => {
     cy.get(".sidebar > .search").should("be.exist");
-
     cy.get(".sidebar > .app-name").should("be.exist");
-
     cy.get(".sidebar > .sidebar-nav").should("be.exist");
+  });
+
+  it("Verify Side Bar Navigation menu Links are present", () => {
+    cy.get("a[title='API']").first().should("be.exist");
+    cy.get("a[title='Calibration']").first().should("be.exist");
+    cy.get("a[title='Hardware']").first().should("be.exist");
+    cy.get("a[title='Mobile App']").first().should("be.exist");
+    cy.get("a[title='Platform']").first().should("be.exist");
   });
 });
