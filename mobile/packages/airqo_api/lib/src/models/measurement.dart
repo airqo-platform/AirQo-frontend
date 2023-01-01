b767d3fcba5ecc4e0a7a4c084c0886436a5e8425
Noah Nsimbe <37845280+NoahNsimbe@users.noreply.github.com>
2023-01-01 19:14:31 +0300

    MOB-151 json serialization

--- mobile/packages/airqo_api/lib/src/models/measurement.dart
+++ mobile/packages/airqo_api/lib/src/models/measurement.dart
@@ -1,6 +1,4 @@
 import 'package:json_annotation/json_annotation.dart';
-
-import 'measurement_value.dart';
 import 'site.dart';
 
 part 'measurement.g.dart';
@@ -19,37 +17,56 @@ class Measurement {
     required this.site,
   });
 
-  @JsonKey(name: 'time')
+  @JsonKey(
+    name: 'time',
+    fromJson: _timeFromJson,
+  )
   DateTime dateTime;
   final MeasurementValue pm2_5;
   final MeasurementValue pm10;
   @JsonKey(name: 'siteDetails')
   final Site site;
+
+  static DateTime _timeFromJson(String json) {
+    DateTime dateTime = DateTime.parse(json);
+    final int offSet = DateTime.now().timeZoneOffset.inHours;
+    return dateTime.add(Duration(hours: offSet));
+  }
 }
 
-List<Measurement> parseMeasurements(dynamic jsonBody) {
+@JsonSerializable()
+class MeasurementValue {
+  factory MeasurementValue.fromJson(Map<String, dynamic> json) =>
+      _$MeasurementValueFromJson(json);
+
+  const MeasurementValue({
+    required this.value,
+    required this.calibratedValue,
+  });
+
+  @JsonKey(required: false, name: 'calibratedValue', fromJson: _valueFromJson)
+  final double? calibratedValue;
+
+  @JsonKey(required: false, name: 'value', fromJson: _valueFromJson)
+  final double value;
+
+  static double _valueFromJson(double json) {
+    return double.parse(json.toStringAsFixed(2));
+  }
+}
+
+List<Measurement> parseMeasurements(Map<String, dynamic> jsonBody) {
   final measurements = <Measurement>[];
 
   final jsonArray = jsonBody['measurements'];
-  final offSet = DateTime.now().timeZoneOffset.inHours;
+
   for (final jsonElement in jsonArray) {
     try {
-      final measurement = Measurement.fromJson(jsonElement);
-      measurement.dateTime = measurement.dateTime.add(Duration(hours: offSet));
-      measurements.add(measurement);
+      measurements.add(Measurement.fromJson(jsonElement));
     } catch (exception, _) {
-      // TODO create utils package
-      // await logException(
-      //   exception,
-      //   stackTrace,
-      // );
+      print(exception);
     }
   }
-  measurements.sort(
-    (siteA, siteB) => siteA.site.name.toLowerCase().compareTo(
-          siteB.site.name.toLowerCase(),
-        ),
-  );
 
   return measurements;
 }
