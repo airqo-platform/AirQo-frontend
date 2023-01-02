bae0c04c0f0d57b5842858e2983b8fee4b07d4fd
FaithDaka <faithbusigu8@gmail.com>
2023-01-02 19:15:12 +0300

    feat: tweets on homepage

--- website/frontend/src/components/Tweets.js
+++ website/frontend/src/components/Tweets.js
@@ -6,30 +6,31 @@ const TweetsComponent = () => {
     script.src = 'https://platform.twitter.com/widgets.js';
     document.getElementsByClassName('twitter-embed')[0].appendChild(script);
   }, []);
-//   const iframes = document.getElementsByClassName('css-1dbjc4n r-140t1nj r-qklmqi');
-  const iframe = document.getElementById("twitter-widget-0");
-//   console.log(iframes.length)
-
-//   element = iframe.contentWindow.document.getElementsByClassName(
-//     'css-1dbjc4n r-140t1nj r-qklmqi'
-//   );
-  console.log(iframe)
+  
   return (
-    <section className="twitterContainer" style={{ padding: '90px 60px 0 60px' }}>
-      <div className="twitter-embed">
-        <a
-          className="twitter-timeline"
-          data-theme="light"
-          data-tweet-limit="3"
-          data-chrome="noheader nofooter"
-          width="400"
-          border-color="#eee"
-          href="https://twitter.com/AirQoProject"
-          style={{ fontWeight: '500', fontSize: '22px', fontFamily: 'Inter', opacity:'0.7' }}>
-          Tweets by @AirQoProject
-        </a>
-      </div>
-    </section>
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width:'100%'
+      }}>
+      <section className="twitterContainer" style={{ padding: '90px 60px 0 60px' }}>
+        <div className="twitter-embed" style={{ maxWidth: '1088px', minWidth: '380px', width:'95vw'}}>
+          <a
+            className="twitter-timeline"
+            data-theme="light"
+            data-tweet-limit="5"
+            data-chrome="noheader nofooter transparent"
+            data-height="560"
+            border-color="#eee"
+            href="https://twitter.com/AirQoProject"
+            style={{ fontWeight: '500', fontSize: '22px', fontFamily: 'Inter', opacity: '0.7' }}>
+            Tweets by @AirQoProject
+          </a>
+        </div>
+      </section>
+    </div>
   );
 };
 
