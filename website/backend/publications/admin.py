af1d8c67d14bd53756ef5626c720dd52efb4b798
FaithDaka <faithbusigu8@gmail.com>
2023-01-05 18:45:54 +0300

    publication backend

---
+++ website/backend/publications/admin.py
@@ -0,0 +1,29 @@
+from django.contrib import admin
+
+from .models import Publication
+
+@admin.register(Publication)
+class PublicationAdmin(admin.ModelAdmin):
+    list_display = ("title", "authors", "category", "link", "link_title", "created")
+    list_filter = ("category", "created")
+    list_per_page = 10
+    search_fields = ("title", "category", "authors")
+    readonly_fields = (
+        "id",
+        "created",
+        "author",
+        "updated_by",
+        "modified"
+    )
+    fields = (
+        "title",
+        "authors",
+        "category",
+        "link",
+        "link_title",
+        "order",
+        "created",
+        "author",
+        "modified",
+        "updated_by"
+    )
