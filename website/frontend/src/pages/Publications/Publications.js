0932f388dbd401fda6f8ec70ccbd491e22655865
FaithDaka <faithbusigu8@gmail.com>
2023-01-05 22:09:37 +0300

    publication page

--- website/frontend/src/pages/Publications/Publications.js
+++ website/frontend/src/pages/Publications/Publications.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { useInitScrollTop } from 'utils/customHooks';
 import SEO from 'utils/seo';
+import { loadPublicationsData } from '../../../reduxStore/Publications/operations';
+import { usePublicationsData } from '../../../reduxStore/Publications/selectors';
 import Page from '../Page';
 import CardComponent from './CardComponent';
+import ReportComponent from './ReportComponent';
 
 const PublicationsPage = () => {
+  useInitScrollTop();
+  const [selectedTab, setSelectedTab] = useState('All');
+  const onClickTabItem = (tab) => setSelectedTab(tab);
+
+  const dispatch = useDispatch();
+  const publicationsData = usePublicationsData();
+
+  useEffect(() => {
+    dispatch(loadPublicationsData());
+  }, [publicationsData.length]);
+
   return (
     <Page>
       <div className="list-page">
@@ -24,11 +40,77 @@ const PublicationsPage = () => {
         </div>
         <div className="page-body">
           <div className="content">
-            <div className="list-cards">
-              <div className="card">
-                <CardComponent/>
-              </div>
+            <div className="nav">
+              <span id="tab1">
+                <button
+                  className={selectedTab === 'All' ? 'selected' : 'unselected'}
+                  onClick={() => onClickTabItem('All')}>
+                  All
+                </button>
+              </span>
+              <span id="tab2">
+                <button
+                  className={selectedTab === 'Research' ? 'selected' : 'unselected'}
+                  onClick={() => onClickTabItem('Research')}>
+                  Research Publications
+                </button>
+              </span>
+              <span id="tab3">
+                <button
+                  className={selectedTab === 'Reports' ? 'selected' : 'unselected'}
+                  onClick={() => onClickTabItem('Reports')}>
+                  Technical reports and Policy documents
+                </button>
+              </span>
             </div>
+            {selectedTab === 'All' ? (
+              publicationsData.map((publication) => (
+                <div className="press-cards-lg">
+                  <div className="card-lg">
+                    <CardComponent
+                      title={publication.title}
+                      authors={publication.authors}
+                      link={publication.link}
+                      linkTitle={publication.link_title}
+                    />
+                  </div>
+                </div>
+              ))
+            ) : (
+              <div />
+            )}
+            {selectedTab === 'Research' ? (
+              publicationsData
+                .filter((publication) => publication.category === 'research')
+                .map((publication) => (
+                  <div className="press-cards-lg">
+                    <div className="card-lg">
+                      <CardComponent
+                        title={publication.title}
+                        authors={publication.authors}
+                        link={publication.link}
+                        linkTitle={publication.link_title}
+                      />
+                    </div>
+                  </div>
+                ))
+            ) : (
+              <div />
+            )}
+            {selectedTab === 'Reports' ? (
+              publicationsData
+                .filter((publication) => publication.category === 'technical' || publication.category === 'policy')
+                .map((publication) => (
+                  <ReportComponent
+                    title={publication.title}
+                    authors={publication.authors}
+                    link={publication.link}
+                    linkTitle={publication.link_title}
+                  />
+                ))
+            ) : (
+              <div />
+            )}
           </div>
         </div>
       </div>
