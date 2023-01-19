aed90a7be7f777fc73eca08b8ac06047e20b7256
FaithDaka <faithbusigu8@gmail.com>
2023-01-19 14:36:00 +0300

    collocation overview

---
+++ platform/src/pages/collocation/overview.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import HeaderNav from '../../common/components/Collocation/header'
+import Layout from '../../common/components/Layout'
+import CollocationNone from '@/icons/Collocation/overview.svg';
+
+const CollocationOverview = () => {
+  return (
+    <Layout>
+      <HeaderNav component={'Overview'} />
+      <div className='mx-6 border-2 rounded-lg border-slate-100 flex justify-center items-center'>
+        <div className='flex justify-center items-center flex-col py-28'>
+          <CollocationNone />
+          <div className='flex flex-col justify-center text-center mt-10'>
+            <h4 className='text-xl font-normal mb-6'>
+              You have no devices under collocation
+            </h4>
+            <div>
+              <p className='text-grey-300 text-sm font-light w-96'>
+                This is where you’ll find quick highlights of your collocated
+                monitors
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export default CollocationOverview
