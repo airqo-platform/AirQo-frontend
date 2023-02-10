8ea3dd4944ee3dac548eb8a9b475636d4091cf70
Belinda Marion Kobusingye <belindamarion0@gmail.com>
2023-02-10 13:54:14 +0300

    device registry env url

--- platform/src/core/urls/deviceRegistry.js
+++ platform/src/core/urls/deviceRegistry.js
@@ -1,7 +1,5 @@
 import { stripTrailingSlash } from '../utils/strings';
 
-const BASE_DEVICE_REGISTRY_URL = stripTrailingSlash(
-  process.env.NEXT_PUBLIC_BASE_AIRQLOUDS_URL || process.env.NEXT_PUBLIC_API_BASE_URL,
-);
+const BASE_DEVICE_REGISTRY_URL = stripTrailingSlash(process.env.NEXT_PUBLIC_API_BASE_URL);
 
 export const DEVICES = `${BASE_DEVICE_REGISTRY_URL}/devices`;
