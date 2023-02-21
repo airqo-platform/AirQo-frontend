0e24df637cfbd61e1a996e5c1437346d7a2a8207
Belinda Marion Kobusingye <belindamarion0@gmail.com>
2023-02-21 11:30:56 +0300

    sort table functionality

--- platform/src/common/components/AddMonitor/Table/index.jsx
+++ platform/src/common/components/AddMonitor/Table/index.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import FilterIcon from '@/icons/Actions/filter_alt.svg';
 import SortByAlphaIcon from '@/icons/Actions/sort_by_alpha.svg';
 import ArrowDropDownIcon from '@/icons/arrow_drop_down';
@@ -10,6 +10,7 @@ import DataTable from './DataTable';
 const Table = ({ collocationDevices }) => {
   const [currentPage, setCurrentPage] = useState(1);
   const [searchTerm, setSearchTerm] = useState('');
+  const [filteredData, setFilteredData] = useState([]);
 
   const handlePrevClick = () => {
     setCurrentPage(currentPage - 1);
@@ -23,14 +24,12 @@ const Table = ({ collocationDevices }) => {
   let startIndex = (currentPage - 1) * pageSize;
   let endIndex = startIndex + pageSize;
 
-  const handleSearch = (searchTerm) => {
-    setSearchTerm(searchTerm);
-    setCurrentPage(1);
-  };
-
-  let filteredData = collocationDevices.filter((row) =>
-    Object.values(row).join('').toLowerCase().includes(searchTerm.toLowerCase()),
-  );
+  useEffect(() => {
+    const filterList = collocationDevices.filter((row) =>
+      Object.values(row).join('').toLowerCase().includes(searchTerm.toLowerCase()),
+    );
+    setFilteredData(filterList);
+  }, [searchTerm, collocationDevices]);
 
   // Check if last page is empty and adjust pagination if necessary
   if (filteredData.length > 0 && endIndex > filteredData.length) {
@@ -39,6 +38,53 @@ const Table = ({ collocationDevices }) => {
   }
 
   const paginatedData = filteredData.slice(startIndex, endIndex);
+
+  const handleSearch = (searchTerm) => {
+    setSearchTerm(searchTerm);
+    setCurrentPage(1);
+  };
+
+  const handleSort = (sortOption) => {
+    let sortedData = [];
+
+    switch (sortOption) {
+      case 'newest':
+        sortedData = sortByDate(filteredData, 'desc');
+        break;
+      case 'oldest':
+        sortedData = sortByDate(filteredData, 'asc');
+        break;
+      case 'ascending':
+        sortedData = sortByDeviceName(filteredData, 'asc');
+        break;
+      case 'descending':
+        sortedData = sortByDeviceName(filteredData, 'desc');
+        break;
+      default:
+        setFilteredData(sortedData);
+    }
+
+    setFilteredData(sortedData);
+  };
+
+  const sortByDate = (data, order) => {
+    const sortedData = [...data].sort((a, b) =>
+      order === 'asc'
+        ? new Date(a.createdAt) - new Date(b.createdAt)
+        : new Date(b.createdAt) - new Date(a.createdAt),
+    );
+
+    return sortedData;
+  };
+
+  const sortByDeviceName = (data, order) => {
+    const sortedData = [...data].sort((a, b) =>
+      order === 'asc'
+        ? a.long_name.localeCompare(b.long_name)
+        : b.long_name.localeCompare(a.long_name),
+    );
+    return sortedData;
+  };
 
   return (
     <div className='w-full'>
@@ -58,15 +104,53 @@ const Table = ({ collocationDevices }) => {
               <ArrowDropDownIcon />
             </div>
           </Button>
-          <Button className={'h-9 w-auto bg-[#0000000A] rounded-[4px] text-black font-medium'}>
-            <div className='mr-1'>
-              <SortByAlphaIcon />
-            </div>
-            Sort by
-            <div className='ml-1'>
-              <ArrowDropDownIcon />
-            </div>
-          </Button>
+          <div className='dropdown'>
+            <Button
+              tabIndex={0}
+              className={'h-9 w-auto bg-[#0000000A] rounded-[4px] text-black font-medium mb-1'}
+            >
+              <div className='mr-1'>
+                <SortByAlphaIcon />
+              </div>
+              Sort by
+              <div className='ml-1'>
+                <ArrowDropDownIcon />
+              </div>
+            </Button>
+            <ul
+              tabIndex={0}
+              className='dropdown-content menu p-2 shadow bg-base-100 rounded-box w-44'
+            >
+              <li
+                role='button'
+                onClick={() => handleSort('newest')}
+                className='text-sm text-grey leading-[21px]'
+              >
+                <a>Newest date first</a>
+              </li>
+              <li
+                role='button'
+                onClick={() => handleSort('oldest')}
+                className='text-sm text-grey leading-[21px]'
+              >
+                <a>Oldest date first</a>
+              </li>
+              <li
+                role='button'
+                onClick={() => handleSort('ascending')}
+                className='text-sm text-grey leading-[21px]'
+              >
+                <a>Name A {'-->'} Z</a>
+              </li>
+              <li
+                role='button'
+                onClick={() => handleSort('descending')}
+                className='text-sm text-grey leading-[21px]'
+              >
+                <a>Name Z {'-->'} A</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
       <div className='overflow-x-scroll md:overflow-x-hidden'>
--- platform/src/pages/collocation/add_monitor.jsx
+++ platform/src/pages/collocation/add_monitor.jsx
@@ -27,14 +27,14 @@ const AddMonitor = () => {
     data: data,
     isLoading,
     // isSuccess,
-    // isError,
+    isError,
     // error,
   } = useGetCollocationDevicesQuery();
 
   let collocationDevices = !isLoading && data.devices;
   return (
     <>
-      {isLoading ? (
+      {isLoading && !isError ? (
         <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center'>
           <progress className='progress w-56'></progress>
         </div>
@@ -66,6 +66,31 @@ const AddMonitor = () => {
               </h4>
             </div>
             <Table collocationDevices={!isLoading && collocationDevices} />
+          </div>
+        </Layout>
+      )}
+
+      {isError && (
+        <Layout>
+          <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center'>
+            <div className='alert alert-error shadow-lg'>
+              <div>
+                <svg
+                  xmlns='http://www.w3.org/2000/svg'
+                  className='stroke-current flex-shrink-0 h-6 w-6'
+                  fill='none'
+                  viewBox='0 0 24 24'
+                >
+                  <path
+                    strokeLinecap='round'
+                    strokeLinejoin='round'
+                    strokeWidth='2'
+                    d='M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z'
+                  />
+                </svg>
+                <span>Unable to return data</span>
+              </div>
+            </div>
           </div>
         </Layout>
       )}
