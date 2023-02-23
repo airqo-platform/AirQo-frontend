9361f4b71d6c14a070930d41520be1cd04c40273
Mutabazi Noble <mutabazinoble@gmail.com>
2023-02-23 11:11:39 +0300

    configure UI

--- mobile/android/app/src/main/kotlin/com/airqo/app/AirQoHomeScreenWidget.kt
+++ mobile/android/app/src/main/kotlin/com/airqo/app/AirQoHomeScreenWidget.kt
@@ -1,7 +1,6 @@
 package com.airqo.app
 
 import android.appwidget.AppWidgetManager
-import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.SharedPreferences
 import android.widget.RemoteViews
@@ -9,57 +8,51 @@ import es.antonborri.home_widget.HomeWidgetBackgroundIntent
 import es.antonborri.home_widget.HomeWidgetLaunchIntent
 import es.antonborri.home_widget.HomeWidgetProvider
 
-/**
- * Implementation of App Widget functionality.
- * App Widget Configuration implemented in [AirQoHomeScreenWidgetConfigureActivity]
- */
-class AirQoHomeScreenWidget : AppWidgetProvider() {
 
-    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray, widgetData: SharedPreferences) {
+class AirQoHomeScreenWidget : HomeWidgetProvider() {
+    override fun onUpdate(
+        context: Context,
+        appWidgetManager: AppWidgetManager,
+        appWidgetIds: IntArray,
+        widgetData: SharedPreferences
+    ) {
         appWidgetIds.forEach { widgetId ->
             val views = RemoteViews(context.packageName, R.layout.air_qo_home_screen_widget).apply {
                 // Open App on Widget Click
                 val pendingIntent = HomeWidgetLaunchIntent.getActivity(
                     context,
-                    MainActivity::class.java)
+                    MainActivity::class.java
+                )
                 setOnClickPendingIntent(R.id.widget_bg, pendingIntent)
 
                 // Swap Title Text by calling Dart Code in the Background
-                setTextViewText(R.id.widget_title, widgetData.getString("title", null)
-                    ?: "No Title Set")
-                val backgroundIntent = HomeWidgetBackgroundIntent.getBroadcast(
-                    context,
-                    Uri.parse("homeWidgetExample://titleClicked")
+                setTextViewText(
+                    R.id.location, widgetData.getString("location", null)
+                        ?: "None"
                 )
-                setOnClickPendingIntent(R.id.widget_title, backgroundIntent)
+//                val backgroundIntent = HomeWidgetBackgroundIntent.getBroadcast(
+//                    context,
+//                    Uri.parse("homeWidgetExample://titleClicked")
+//                )
+//                setOnClickPendingIntent(R.id.widget_title, backgroundIntent)
 
                 val message = widgetData.getString("message", null)
-                setTextViewText(R.id.widget_message, message
-                    ?: "No Message Set")
+                setTextViewText(
+                    R.id.pm, message
+                        ?: "None"
+                )
                 // Detect App opened via Click inside Flutter
-                val pendingIntentWithData = HomeWidgetLaunchIntent.getActivity(
-                    context,
-                    MainActivity::class.java,
-                    Uri.parse("homeWidgetExample://message?message=$message"))
-                setOnClickPendingIntent(R.id.widget_message, pendingIntentWithData)
+//                val pendingIntentWithData = HomeWidgetLaunchIntent.getActivity(
+//                    context,
+//                    MainActivity::class.java,
+//                    Uri.parse("homeWidgetExample://message?message=$message")
+//                )
+//                setOnClickPendingIntent(R.id.widget_message, pendingIntentWithData)
             }
 
             appWidgetManager.updateAppWidget(widgetId, views)
         }
     }
-//    override fun onDeleted(context: Context, appWidgetIds: IntArray) {
-//        // When the user deletes the widget, delete the preference associated with it.
-//        for (appWidgetId in appWidgetIds) {
-//            deleteTitlePref(context, appWidgetId)
-//        }
-//    }
-//
-//    override fun onEnabled(context: Context) {
-//        // Enter relevant functionality for when the first widget is created
-//    }
-//
-//    override fun onDisabled(context: Context) {
-//        // Enter relevant functionality for when the last widget is disabled
-//    }
 }
+
 
