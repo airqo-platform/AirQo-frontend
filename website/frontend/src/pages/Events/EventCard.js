38792b1c84ddc872c1e3a9157f5655eea2599fea
FaithDaka <faithbusigu8@gmail.com>
2023-03-16 13:14:06 +0300

    route to details and event badge

--- website/frontend/src/pages/Events/EventCard.js
+++ website/frontend/src/pages/Events/EventCard.js
@@ -1,10 +1,16 @@
-import { CalendarTodayOutlined } from '@mui/icons-material'
-import React from 'react'
+import { CalendarTodayOutlined } from '@mui/icons-material';
+import React from 'react';
 import { format } from 'date-fns';
+import { useNavigate } from 'react-router-dom';
 
-const EventCard = ({image, title, subText, startDate, link}) => {
+const EventCard = ({ image, title, subText, startDate, endDate, link, key }) => {
+  const navigate = useNavigate();
+  const routeToDetails = (link) => (event) => {
+    event.preventDefault();
+    navigate(`/events/${link}/`);
+  };
   return (
-    <div className="card">
+    <div className="card" key={key}>
       <img src={image} alt={title} height={250} width={300} />
       <div>
         <h1>{title}</h1>
@@ -13,14 +19,20 @@ const EventCard = ({image, title, subText, startDate, link}) => {
           <CalendarTodayOutlined />
           <span>{format(new Date(startDate), 'do MMMM, yyyy')}</span>
         </div>
+        <div className="badge">
+          {new Date(startDate).getTime() === new Date().getTime() ||
+            (new Date(endDate).getTime() === new Date().getTime() && (
+              <div className="today">Happening today</div>
+            ))}
+        </div>
         <div className="cta">
-          <a className="link" href={link}>
+          <button className="link" onClick={routeToDetails(link)}>
             Read More {'-->'}
-          </a>
+          </button>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default EventCard
+export default EventCard;
