0fd6bcbaa5e61a882f82c9013c68d661ff3baba3
Belinda Marion Kobusingye <46527380+Codebmk@users.noreply.github.com>
2023-03-17 08:54:14 +0300

    collocate header responsiveness

--- platform/src/common/components/Collocation/header.jsx
+++ platform/src/common/components/Collocation/header.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const HeaderNav = ({ component, children }) => {
   return (
-    <div className='flex justify-between items-center px-6 py-8'>
-      <div>
+    <div className='md:flex md:justify-between md:items-center px-6 py-8'>
+      <div className='mr-3 mb-3 lg:mr-0 lg:mb-0'>
         <span className='text-xl opacity-50 mr-4'>Collocation</span>
         <span className='font-mono opacity-10 text-xl mr-4'>{'>'}</span>
         <span className='text-xl font-semibold'>{component}</span>
