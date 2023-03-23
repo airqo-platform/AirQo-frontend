47dd9a35cdc835ffee1acb7f6b8b4e6f953b2080
Belinda Marion Kobusingye <46527380+Codebmk@users.noreply.github.com>
2023-03-23 12:15:45 +0300

    align error to top right corner

--- platform/src/common/components/Toast/index.jsx
+++ platform/src/common/components/Toast/index.jsx
@@ -1,3 +1,5 @@
+import WarningCircleIcon from '@/icons/Common/warning_circle';
+
 const Toast = ({ message, type }) => {
   const colors = {
     success: 'bg-green-500',
@@ -6,11 +8,12 @@ const Toast = ({ message, type }) => {
 
   const textColor = type === 'success' ? 'text-black-600' : 'text-white';
 
-  const containerStyles = `flex fixed bottom-5 right-6 z-50 p-4 w-80 text-sm ${colors[type]} ${textColor} rounded-md shadow-lg transition-opacity`;
+  const containerStyles = `flex fixed top-5 right-6 z-50 p-4 w-80 text-sm ${colors[type]} ${textColor} rounded-md shadow-lg transition-opacity`;
 
   return (
     <div className={containerStyles}>
-      <p>{message}</p>
+      {type === 'error' && <WarningCircleIcon fillColor='#FFF' />}
+      <p className='ml-2'>{message}</p>
     </div>
   );
 };
