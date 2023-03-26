30d7829565403c004e92e813fd38f0ae11a65d01
Belinda Marion Kobusingye <46527380+Codebmk@users.noreply.github.com>
2023-03-26 10:07:17 +0300

    update baseUrl to match docker config

--- platform/cypress.config.js
+++ platform/cypress.config.js
@@ -5,7 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:4040',
+    baseUrl: 'http://localhost:3000',
   },
   component: {
     devServer: {
