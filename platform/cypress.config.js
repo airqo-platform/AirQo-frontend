01b045995f95722f536f1447e3f3daf29cedac49
Belinda Marion Kobusingye <46527380+Codebmk@users.noreply.github.com>
2023-03-26 10:27:29 +0300

    Update cypress.config.js

--- platform/cypress.config.js
+++ platform/cypress.config.js
@@ -2,10 +2,10 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'http://localhost:3000',
   },
   component: {
     devServer: {
