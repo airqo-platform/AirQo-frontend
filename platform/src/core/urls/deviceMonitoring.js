a05bd1cee35869e062f4f53c2826cb440ec369f1
Belinda Marion Kobusingye <46527380+Codebmk@users.noreply.github.com>
2023-03-28 14:45:28 +0300

    add device monitoring env variable

--- platform/src/core/urls/deviceMonitoring.js
+++ platform/src/core/urls/deviceMonitoring.js
@@ -1,5 +1,7 @@
 import { stripTrailingSlash } from '../utils/strings';
 
-const BASE_DEVICE_MONITORING_URL = stripTrailingSlash(process.env.NEXT_PUBLIC_API_BASE_URL);
+const BASE_DEVICE_MONITORING_URL = stripTrailingSlash(
+  process.env.NEXT_PUBLIC_BASE_DEVICE_MONITORING_URL,
+);
 
-export const COLLOCATION = `http://104.155.76.226:5001/api/v1/monitor/devices/collocation`;
+export const COLLOCATION = `${BASE_DEVICE_MONITORING_URL}/api/v1/monitor/devices/collocation`;
