17cc66eb564e96eaa8b50d48a3b45bd36cf07a09
FaithDaka <faithbusigu8@gmail.com>
2023-05-03 15:09:45 +0300

    initial verification logic

--- platform/src/pages/account/creation/step2.jsx
+++ platform/src/pages/account/creation/step2.jsx
@@ -1,9 +1,37 @@
 import React, { useState } from 'react';
 import AccountPageLayout from '../../../common/components/Account/Layout';
 import Link from 'next/link';
+import { useDispatch, useSelector } from 'react-redux';
+import { createUser } from '@/lib/store/services/account/CreationSlice';
+import {
+  setUserFirstName,
+  setUserLastName,
+  setUserPassword,
+} from '../../../lib/store/services/account/CreationSlice';
+import { useRouter } from 'next/router';
 
 const AccountCreationPage2 = () => {
-  const [isChecked, setIsChecked] = useState(false);
+  const [firstName, setFirstName] = useState();
+  const [lastName, setLastName] = useState();
+  const [password, setPassword] = useState();
+
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const userEmail = useSelector((state) => state.account.userData.email);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    dispatch(setUserFirstName(firstName));
+    dispatch(setUserLastName(lastName));
+    dispatch(setUserPassword(password));
+    try {
+      await dispatch(createUser({email:userEmail, firstName, lastName}));
+      router.push('/account/creation/step3');
+    } catch (err) {
+      return err;
+    }
+  };
+
   return (
     <AccountPageLayout>
       <div className='w-full'>
@@ -11,81 +39,101 @@ const AccountCreationPage2 = () => {
         <p className='text-xl text-black-700 font-normal mt-3'>
           Get access to air quality analytics across Africa
         </p>
-        <div className='mt-6'>
-          <div className='flex flex-row justify-between'>
-            <div className='w-full'>
-              <div className='text-xs'>First name*</div>
-              <div className='mt-2 w-11/12'>
-                <input
-                  type='text'
-                  placeholder='e.g. Greta'
-                  className='input w-full rounded-none bg-form-input focus:outline-form-input focus:outline-none focus:outline-offset-0'
-                  required
-                />
+        <form onSubmit={handleSubmit}>
+          <div className='mt-6'>
+            <div className='flex flex-row justify-between'>
+              <div className='w-full'>
+                <div className='text-xs'>First name*</div>
+                <div className='mt-2 w-11/12'>
+                  <input
+                    onChange={(e) => {
+                      setFirstName(e.target.value);
+                    }}
+                    type='text'
+                    placeholder='e.g. Greta'
+                    className='input w-full rounded-none bg-form-input focus:outline-form-input focus:outline-none focus:outline-offset-0'
+                    required
+                  />
+                </div>
+              </div>
+              <div className='w-full'>
+                <div className='text-xs'>Last name*</div>
+                <div className='mt-2 w-full'>
+                  <input
+                    onChange={(e) => {
+                      setLastName(e.target.value);
+                    }}
+                    type='text'
+                    placeholder='e.g. Nagawa'
+                    className='input w-full rounded-none bg-form-input focus:outline-form-input focus:outline-none focus:outline-offset-0'
+                    required
+                  />
+                </div>
               </div>
             </div>
+          </div>
+          <div className='mt-6'>
             <div className='w-full'>
-              <div className='text-xs'>Last name*</div>
+              <div className='text-xs'>Password*</div>
               <div className='mt-2 w-full'>
                 <input
-                  type='text'
-                  placeholder='e.g. Nagawa'
+                  onChange={(e) => {
+                    setPassword(e.target.value);
+                  }}
+                  pattern='^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&]).{8,}$'
+                  title='Password should include an uppercase letter, lowercase letter, a number and special character(#?!@$^&)'
+                  minLength={'8'}
+                  maxLength={'20'}
+                  type='password'
                   className='input w-full rounded-none bg-form-input focus:outline-form-input focus:outline-none focus:outline-offset-0'
                   required
                 />
               </div>
             </div>
           </div>
-        </div>
-        <div className='mt-6'>
-          <div className='w-full'>
-            <div className='text-xs'>Password*</div>
-            <div className='mt-2 w-full'>
-              <input
-                type='password'
-                className='input w-full rounded-none bg-form-input focus:outline-form-input focus:outline-none focus:outline-offset-0'
-                required
-              />
+          <div className='mt-6'>
+            <div className='flex flex-row items-center'>
+              <div>
+                <input
+                  type='checkbox'
+                  className='checkbox rounded border-2 border-check-box'
+                  required
+                />
+              </div>
+              <div className='ml-4'>
+                <p className='text-xs'>
+                  I agree to the{' '}
+                  <a href='#' className='underline'>
+                    Terms of Service
+                  </a>{' '}
+                  and{' '}
+                  <a href='#' className='underline'>
+                    Privacy Policy
+                  </a>
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        <div className='mt-6'>
-          <div className='flex flex-row items-center'>
-            <div>
-              <input type='checkbox' className='checkbox rounded border-2 border-check-box' />
-            </div>
-            <div className='ml-4'>
-              <p className='text-xs'>
-                I agree to the{' '}
-                <a href='#' className='underline'>
-                  Terms of Service
-                </a>{' '}
-                and{' '}
-                <a href='#' className='underline'>
-                  Privacy Policy
-                </a>
-              </p>
-            </div>
-          </div>
-        </div>
-        <div className='mt-14'>
-          <div className='flex flex-col md:flex-row items-center justify-start md:justify-between'>
-            <div className='w-full md:w-1/3'>
-              <Link href='/account/creation'>
-                <button className='w-full text-sm btn bg-white rounded-none outline-none border-2 border-check-box hover:bg-gray-100 hover:border-inherit focus:border-inherit'>
-                  <span style={{ color: '#353E52' }}>Back</span>
-                </button>
-              </Link>
-            </div>
-            <div className='w-full md:w-1/3 mt-6 md:mt-0'>
-              <Link href='/account/creation/step3'>
-                <button className='w-full btn bg-blue-900 rounded-none text-sm outline-none border-none hover:bg-blue-950'>
+          <div className='mt-14'>
+            <div className='flex flex-col-reverse md:flex-row items-center justify-start md:justify-between'>
+              <div className='w-full md:w-1/3 mt-6 lg:mt-0'>
+                <Link href='/account/creation'>
+                  <button className='w-full text-sm btn bg-white rounded-none outline-none border-2 border-check-box hover:bg-gray-100 hover:border-inherit focus:border-inherit'>
+                    <span style={{ color: '#353E52' }}>Back</span>
+                  </button>
+                </Link>
+              </div>
+              <div className='w-full md:w-1/3'>
+                <button
+                  type='submit'
+                  onClick={handleSubmit}
+                  className='w-full btn bg-blue-900 rounded-none text-sm outline-none border-none hover:bg-blue-950'>
                   Continue
                 </button>
-              </Link>
+              </div>
             </div>
           </div>
-        </div>
+        </form>
       </div>
     </AccountPageLayout>
   );
--- platform/src/pages/account/creation/step3.jsx
+++ platform/src/pages/account/creation/step3.jsx
@@ -1,24 +1,41 @@
-import React from 'react'
-import AccountPageLayout from '../../../common/components/Account/Layout'
+import React, { useState } from 'react';
+import AccountPageLayout from '../../../common/components/Account/Layout';
+import { useDispatch, useSelector } from 'react-redux';
+import { createUser } from '../../../lib/store/services/account/CreationSlice';
 
 const AccountCreationPage3 = () => {
+  const userData = useSelector((state) => state.account.userData);
+  const errors = useSelector((state) => state.account.errors);
+  const dispatch = useDispatch();
+
+  const handleSubmit = async () => {
+    try {
+      const res = await dispatch(createUser(userData));
+      return res;
+    } catch (err) {
+      console.log('An error occurred:', err);
+    }
+  };
+
   return (
     <AccountPageLayout>
       <div className='w-full'>
         <h2 className='text-3xl text-black-700 font-medium'>Verify your details</h2>
         <p className='text-xl text-black-700 font-normal mt-3'>
-          We sent a verification email to <b>greta.nagawa@gmail.com</b>
+          We sent a verification email to <b>{userData.email}</b>
         </p>
         <div className='mt-6'>
           <span className='text-sm text-grey-300'>Not seeing the email?</span>
-          <span className='text-sm text-blue-900 font-medium'>
+          <span
+            className='text-sm text-blue-900 font-medium hover:cursor-pointer'
+            onClick={() => handleSubmit()}>
             {' '}
-            <a href='#'>Resend</a>
+            Resend
           </span>
         </div>
       </div>
     </AccountPageLayout>
   );
-}
+};
 
-export default AccountCreationPage3
+export default AccountCreationPage3;
