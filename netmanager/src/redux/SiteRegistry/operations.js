25b40322ee9124242622c21a240e5fd9fbde9b78
Belinda Marion Kobusingye <46527380+Codebmk@users.noreply.github.com>
2023-05-10 17:53:45 +0300

    added sites summary api

--- netmanager/src/redux/SiteRegistry/operations.js
+++ netmanager/src/redux/SiteRegistry/operations.js
@@ -2,10 +2,12 @@ import {
   LOAD_SITES_SUCCESS,
   LOAD_SITES_FAILURE,
   LOAD_SITE_OPTIONS_SUCCESS,
-} from "./actions";
-import { getSitesApi } from "views/apis/deviceRegistry";
-import { transformArray } from "../utils";
-import { createSiteOptions } from "utils/sites";
+  LOAD_SITES_SUMMARY_SUCCESS,
+  LOAD_SITES_SUMMARY_FAILURE
+} from './actions';
+import { getSitesApi, getSitesSummaryApi } from 'views/apis/deviceRegistry';
+import { transformArray } from '../utils';
+import { createSiteOptions } from 'utils/sites';
 
 export const loadSitesData = () => {
   return async (dispatch) => {
@@ -13,17 +15,39 @@ export const loadSitesData = () => {
       .then((responseData) => {
         dispatch({
           type: LOAD_SITES_SUCCESS,
-          payload: transformArray(responseData.sites || [], "_id"),
+          payload: transformArray(responseData.sites || [], '_id')
         });
         dispatch({
           type: LOAD_SITE_OPTIONS_SUCCESS,
-          payload: createSiteOptions(responseData.sites || []),
+          payload: createSiteOptions(responseData.sites || [])
         });
       })
       .catch((err) => {
         dispatch({
           type: LOAD_SITES_FAILURE,
-          payload: err,
+          payload: err
+        });
+      });
+  };
+};
+
+export const loadSitesSummary = () => {
+  return async (dispatch) => {
+    return await getSitesSummaryApi()
+      .then((responseData) => {
+        dispatch({
+          type: LOAD_SITES_SUMMARY_SUCCESS,
+          payload: transformArray(responseData.sites || [], '_id')
+        });
+        dispatch({
+          type: LOAD_SITE_OPTIONS_SUCCESS,
+          payload: createSiteOptions(responseData.sites || [])
+        });
+      })
+      .catch((err) => {
+        dispatch({
+          type: LOAD_SITES_SUMMARY_FAILURE,
+          payload: err
         });
       });
   };
--- netmanager/src/views/apis/deviceRegistry.js
+++ netmanager/src/views/apis/deviceRegistry.js
@@ -144,6 +144,10 @@ export const getSitesApi = async () => {
   return await axios.get(SITES).then((response) => response.data);
 };
 
+export const getSitesSummaryApi = async () => {
+  return await axios.get(SITES).then((response) => response.data);
+};
+
 export const updateSiteApi = async (site_id, siteData) => {
   return await axios
     .put(SITES, siteData, { params: { id: site_id } })
