d38ed1ac24ce973d3f0d3012f1bbc4de84f94963
Belinda Marion Kobusingye <46527380+Codebmk@users.noreply.github.com>
2023-05-10 17:54:40 +0300

    Update index.js

--- netmanager/src/redux/SiteRegistry/reducers/index.js
+++ netmanager/src/redux/SiteRegistry/reducers/index.js
@@ -1,8 +1,10 @@
-import { combineReducers } from "redux";
-import sites from "./sites";
-import siteOptions from "./siteOptions";
+import { combineReducers } from 'redux';
+import sites from './sites';
+import siteOptions from './siteOptions';
+import sitesSummary from './sitesSummary';
 
 export default combineReducers({
   sites,
   siteOptions,
+  sitesSummary
 });
