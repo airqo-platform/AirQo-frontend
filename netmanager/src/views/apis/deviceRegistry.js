a00674da61efd35ffe3ee4c5038b16da7d8e63e5
Belinda Marion Kobusingye <46527380+Codebmk@users.noreply.github.com>
2023-05-10 18:09:16 +0300

    Modified site registry table to display summary api data

--- netmanager/src/views/apis/deviceRegistry.js
+++ netmanager/src/views/apis/deviceRegistry.js
@@ -145,7 +145,7 @@ export const getSitesApi = async () => {
 };
 
 export const getSitesSummaryApi = async () => {
-  return await axios.get(SITES).then((response) => response.data);
+  return await axios.get(`${SITES}/summary`).then((response) => response.data);
 };
 
 export const updateSiteApi = async (site_id, siteData) => {
--- netmanager/src/views/components/Sites/SitesTable.js
+++ netmanager/src/views/components/Sites/SitesTable.js
@@ -1,45 +1,43 @@
-import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { useHistory } from "react-router-dom";
-import LoadingOverlay from "react-loading-overlay";
-import { isEmpty } from "underscore";
-import Tooltip from "@material-ui/core/Tooltip";
-import DeleteIcon from "@material-ui/icons/DeleteOutlineOutlined";
-import { Parser } from "json2csv";
-import { loadSitesData } from "redux/SiteRegistry/operations";
-import { useSitesArrayData } from "redux/SiteRegistry/selectors";
-import CustomMaterialTable from "../Table/CustomMaterialTable";
-import ConfirmDialog from "../../containers/ConfirmDialog";
-import { deleteSiteApi } from "views/apis/deviceRegistry";
-import { updateMainAlert } from "redux/MainAlert/operations";
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
+import LoadingOverlay from 'react-loading-overlay';
+import { isEmpty } from 'underscore';
+import Tooltip from '@material-ui/core/Tooltip';
+import DeleteIcon from '@material-ui/icons/DeleteOutlineOutlined';
+import { Parser } from 'json2csv';
+import { loadSitesData, loadSitesSummary } from 'redux/SiteRegistry/operations';
+import { useSitesSummaryData } from 'redux/SiteRegistry/selectors';
+import CustomMaterialTable from '../Table/CustomMaterialTable';
+import ConfirmDialog from '../../containers/ConfirmDialog';
+import { deleteSiteApi } from 'views/apis/deviceRegistry';
+import { updateMainAlert } from 'redux/MainAlert/operations';
 
 // css
-import "assets/css/location-registry.css";
+import 'assets/css/location-registry.css';
 
-const BLANK_SPACE_HOLDER = "-";
-const renderCell = (field) => (rowData) => (
-  <span>{rowData[field] || BLANK_SPACE_HOLDER}</span>
-);
+const BLANK_SPACE_HOLDER = '-';
+const renderCell = (field) => (rowData) => <span>{rowData[field] || BLANK_SPACE_HOLDER}</span>;
 
 const SitesTable = () => {
   const history = useHistory();
   const dispatch = useDispatch();
-  const sites = useSitesArrayData();
+  const sites = useSitesSummaryData();
 
   const [isLoading, setIsLoading] = useState(false);
-  const [delState, setDelState] = useState({ open: false, name: "", id: "" });
+  const [delState, setDelState] = useState({ open: false, name: '', id: '' });
 
   useEffect(() => {
     //code to retrieve all locations data
     if (isEmpty(sites)) {
       setIsLoading(true);
-      dispatch(loadSitesData());
+      dispatch(loadSitesSummary());
       setIsLoading(false);
     }
   }, []);
 
   const handleDeleteSite = (e) => {
-    setDelState({ open: false, name: "", id: "" });
+    setDelState({ open: false, name: '', id: '' });
     deleteSiteApi(delState.id)
       .then((resData) => {
         dispatch(loadSitesData());
@@ -47,19 +45,16 @@ const SitesTable = () => {
           updateMainAlert({
             message: resData.message,
             show: true,
-            severity: "success",
+            severity: 'success'
           })
         );
       })
       .catch((error) => {
         dispatch(
           updateMainAlert({
-            message:
-              error.response &&
-              error.response.data &&
-              error.response.data.message,
+            message: error.response && error.response.data && error.response.data.message,
             show: true,
-            severity: "error",
+            severity: 'error'
           })
         );
       });
@@ -70,55 +65,55 @@ const SitesTable = () => {
       <LoadingOverlay active={isLoading} spinner text="Loading Locations...">
         <CustomMaterialTable
           pointerCursor
-          userPreferencePaginationKey={"sites"}
+          userPreferencePaginationKey={'sites'}
           title="Site Registry"
           columns={[
             {
-              title: "Name",
-              field: "name",
-              render: renderCell("name"),
+              title: 'Name',
+              field: 'name',
+              render: renderCell('name')
             },
             {
-              title: "Site ID",
-              field: "generated_name",
-              render: renderCell("generated_name"),
-              cellStyle: { fontFamily: "Open Sans" },
+              title: 'Site ID',
+              field: 'generated_name',
+              render: renderCell('generated_name'),
+              cellStyle: { fontFamily: 'Open Sans' }
             },
             {
-              title: "Description",
-              field: "description",
-              render: renderCell("description"),
-              cellStyle: { fontFamily: "Open Sans" },
+              title: 'Description',
+              field: 'description',
+              render: renderCell('description'),
+              cellStyle: { fontFamily: 'Open Sans' }
             },
             {
-              title: "Country",
-              field: "country",
-              render: renderCell("country"),
-              cellStyle: { fontFamily: "Open Sans" },
+              title: 'Country',
+              field: 'country',
+              render: renderCell('country'),
+              cellStyle: { fontFamily: 'Open Sans' }
             },
             {
-              title: "District",
-              field: "district",
-              render: renderCell("district"),
-              cellStyle: { fontFamily: "Open Sans" },
+              title: 'District',
+              field: 'district',
+              render: renderCell('district'),
+              cellStyle: { fontFamily: 'Open Sans' }
             },
             {
-              title: "Region",
-              field: "region",
-              render: renderCell("region"),
-              cellStyle: { fontFamily: "Open Sans" },
+              title: 'Region',
+              field: 'region',
+              render: renderCell('region'),
+              cellStyle: { fontFamily: 'Open Sans' }
             },
             {
-              title: "Actions",
+              title: 'Actions',
               render: (rowData) => (
                 <div>
                   <Tooltip title="Delete">
                     <DeleteIcon
                       // className={"hover-red"}
                       style={{
-                        margin: "0 5px",
-                        cursor: "not-allowed",
-                        color: "grey",
+                        margin: '0 5px',
+                        cursor: 'not-allowed',
+                        color: 'grey'
                       }}
                       // disable deletion for now
                       // onClick={(event) => {
@@ -132,8 +127,8 @@ const SitesTable = () => {
                     />
                   </Tooltip>
                 </div>
-              ),
-            },
+              )
+            }
           ]}
           onRowClick={(event, data) => {
             event.preventDefault();
@@ -143,69 +138,68 @@ const SitesTable = () => {
           options={{
             search: true,
             exportButton: true,
-            searchFieldAlignment: "left",
+            searchFieldAlignment: 'left',
             showTitle: false,
             searchFieldStyle: {
-              fontFamily: "Open Sans",
+              fontFamily: 'Open Sans'
             },
             headerStyle: {
-              fontFamily: "Open Sans",
+              fontFamily: 'Open Sans',
               fontSize: 16,
-              fontWeight: 600,
+              fontWeight: 600
             },
             exportCsv: (columns, data) => {
               const fields = [
-                "name",
-                "description",
-                "generated_name",
-                "latitude",
-                "longitude",
-                "country",
-                "region",
-                "district",
-                "city",
-                "county",
-                "sub_county",
-                "parish",
-                "street",
-                "formatted_name",
-                "altitude",
-                "greenness",
-                "landform_90",
-                "landform_270",
-                "aspect",
-                "distance_to_nearest_road",
-                "distance_to_nearest_primary_road",
-                "distance_to_nearest_tertiary_road",
-                "distance_to_nearest_unclassified_road",
-                "distance_to_nearest_residential_road",
-                "distance_to_nearest_secondary_road",
-                "distance_to_kampala_center",
-                "bearing_to_kampala_center",
+                'name',
+                'description',
+                'generated_name',
+                'latitude',
+                'longitude',
+                'country',
+                'region',
+                'district',
+                'city',
+                'county',
+                'sub_county',
+                'parish',
+                'street',
+                'formatted_name',
+                'altitude',
+                'greenness',
+                'landform_90',
+                'landform_270',
+                'aspect',
+                'distance_to_nearest_road',
+                'distance_to_nearest_primary_road',
+                'distance_to_nearest_tertiary_road',
+                'distance_to_nearest_unclassified_road',
+                'distance_to_nearest_residential_road',
+                'distance_to_nearest_secondary_road',
+                'distance_to_kampala_center',
+                'bearing_to_kampala_center'
               ];
               const json2csvParser = new Parser({ fields });
               const csv = json2csvParser.parse(data);
               let filename = `site-registry.csv`;
-              const link = document.createElement("a");
+              const link = document.createElement('a');
               link.setAttribute(
-                "href",
-                "data:text/csv;charset=utf-8,%EF%BB%BF" +
-                  encodeURIComponent(csv)
+                'href',
+                'data:text/csv;charset=utf-8,%EF%BB%BF' + encodeURIComponent(csv)
               );
-              link.setAttribute("download", filename);
-              link.style.visibility = "hidden";
+              link.setAttribute('download', filename);
+              link.style.visibility = 'hidden';
               document.body.appendChild(link);
               link.click();
               document.body.removeChild(link);
-            },
+            }
           }}
         />
       </LoadingOverlay>
       <ConfirmDialog
         open={delState.open}
-        title={"Delete a site?"}
+        title={'Delete a site?'}
         message={`Are you sure you want to delete this ${delState.name} site`}
-        close={() => setDelState({ open: false, name: "", id: "" })}
+        close={() => setDelState({ open: false, name: '', id: '' })}
         confirm={handleDeleteSite}
         error
       />
