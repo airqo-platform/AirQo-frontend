9fe5b3e97f3fcf1f05149f6656c4c9ee9ff3b559
Mutabazi Noble <60974514+Mnoble-19@users.noreply.github.com>
2023-05-24 17:19:22 +0300

    Add favourite places as backup

--- mobile/lib/services/widget_service.dart
+++ mobile/lib/services/widget_service.dart
@@ -1,20 +1,54 @@
+import 'package:app/models/air_quality_reading.dart';
+import 'package:app/models/favourite_place.dart';
 import 'package:app/services/location_service.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:home_widget/home_widget.dart';
 import 'package:collection/collection.dart';
+import 'package:path/path.dart';
+import '../blocs/favourite_place/favourite_place_bloc.dart';
 import '../models/widget_data.dart';
 
 class WidgetService {
   static Future<void> sendData() async {
     final currentLocation = await LocationService.getCurrentLocation();
-    final airQualityReading = await LocationService.getNearestSite(
-          currentLocation!.latitude,
-          currentLocation.longitude,
-        ) ??
-        (await LocationService.getSurroundingSites(
-          latitude: currentLocation.latitude,
-          longitude: currentLocation.longitude,
-        )).firstOrNull;
-
+    final airQualityReading = currentLocation != null
+        ? await LocationService.getNearestSite(
+              currentLocation.latitude,
+              currentLocation.longitude,
+            ) ??
+            (await LocationService.getSurroundingSites(
+              latitude: currentLocation.latitude,
+              longitude: currentLocation.longitude,
+            ))
+                .firstOrNull
+        : (BlocProvider.of<FavouritePlaceBloc>(context as BuildContext)
+                .state
+                .isNotEmpty
+            ? await LocationService.getNearestSite(
+                  BlocProvider.of<FavouritePlaceBloc>(context as BuildContext)
+                      .state
+                      .first
+                      .latitude,
+                  BlocProvider.of<FavouritePlaceBloc>(context as BuildContext)
+                      .state
+                      .first
+                      .longitude,
+                ) ??
+                (await LocationService.getSurroundingSites(
+                  latitude: BlocProvider.of<FavouritePlaceBloc>(
+                          context as BuildContext)
+                      .state
+                      .first
+                      .latitude,
+                  longitude: BlocProvider.of<FavouritePlaceBloc>(
+                          context as BuildContext)
+                      .state
+                      .first
+                      .longitude,
+                ))
+                    .firstOrNull
+            : null);
 
     if (airQualityReading != null) {
       final widgetData =
