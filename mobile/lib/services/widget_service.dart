c1a9ee87913161c3c72fe0fe46cda4a797f39c0d
Mutabazi Noble <60974514+Mnoble-19@users.noreply.github.com>
2023-05-24 03:11:07 +0300

    fix location logic

--- mobile/lib/services/widget_service.dart
+++ mobile/lib/services/widget_service.dart
@@ -1,47 +1,40 @@
-import 'dart:math';
-
-import 'package:app/models/current_location.dart';
 import 'package:app/services/location_service.dart';
-import 'package:geolocator/geolocator.dart';
 import 'package:home_widget/home_widget.dart';
-
-//import location_service.dart';
-import '../blocs/nearby_location/nearby_location_bloc.dart';
-import '../blocs/search/search_bloc.dart';
-import '../models/air_quality_reading.dart';
+import 'package:collection/collection.dart';
 import '../models/widget_data.dart';
-import 'hive_service.dart';
 
 class WidgetService {
   static Future<void> sendData() async {
-    CurrentLocation? currentLocation =
-        await LocationService.getCurrentLocation();
+    final currentLocation = await LocationService.getCurrentLocation();
+    final airQualityReading = await LocationService.getNearestSite(
+          currentLocation!.latitude,
+          currentLocation.longitude,
+        ) ??
+        (await LocationService.getSurroundingSites(
+          latitude: currentLocation.latitude,
+          longitude: currentLocation.longitude,
+        )).firstOrNull;
 
-    Future<AirQualityReading?> airQualityReading =
-        LocationService.getNearestSite(
-            currentLocation!.latitude, currentLocation!.longitude);
 
-    if (currentLocation == null) {
-      return;
+    if (airQualityReading != null) {
+      final widgetData =
+          WidgetData.initializeFromAirQualityReading(airQualityReading);
+      await Future.wait(widgetData.idMapping().entries.map(
+            (entry) =>
+                HomeWidget.saveWidgetData<String>(entry.key, entry.value),
+          ));
     }
-
-    WidgetData widgetData = WidgetData.initializeFromAirQualityReading(
-        airQualityReading as AirQualityReading);
-    widgetData.idMapping().forEach((key, value) async {
-      await HomeWidget.saveWidgetData<String>(key, value);
-    });
-
-    return;
   }
 
   static Future<void> updateWidget() async {
-    //TODO: Disabled for now
-    // var rectangleWidgetUpdate = HomeWidget.updateWidget(
-    //   name: 'AirQoHomeScreenWidget',
-    //   iOSName: 'AirQoHomeScreenWidget',
-    //   qualifiedAndroidName: 'com.airqo.app.AirQoHomeScreenWidget',
-    // );
-    // return Future.wait([rectangleWidgetUpdate, circularWidgetUpdate]);
+//TODO: Disabled for now
+// var rectangleWidgetUpdate = HomeWidget.updateWidget(
+//   name: 'AirQoHomeScreenWidget',
+//   iOSName: 'AirQoHomeScreenWidget',
+//   qualifiedAndroidName: 'com.airqo.app.AirQoHomeScreenWidget',
+// );
+// return Future.wait([rectangleWidgetUpdate, circularWidgetUpdate]);
+
     await HomeWidget.updateWidget(
       name: 'AirQoCircularWidget',
       androidName: 'AirQoCircularWidget',
