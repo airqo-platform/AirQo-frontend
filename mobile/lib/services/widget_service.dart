84c4142ff85db32c99e6eeb3923cfc354b9c75d7
Mutabazi Noble <60974514+Mnoble-19@users.noreply.github.com>
2023-05-26 10:27:28 +0300

    Update widget_service.dart

--- mobile/lib/services/widget_service.dart
+++ mobile/lib/services/widget_service.dart
@@ -1,5 +1,4 @@
 import 'package:app/models/air_quality_reading.dart';
-import 'package:app/models/favourite_place.dart';
 import 'package:app/services/location_service.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -7,48 +6,53 @@ import 'package:home_widget/home_widget.dart';
 import 'package:collection/collection.dart';
 import 'package:path/path.dart';
 import '../blocs/favourite_place/favourite_place_bloc.dart';
-import '../models/widget_data.dart';
+import '../models/models.dart';
+import 'services.dart';
 
 class WidgetService {
   static Future<void> sendData() async {
-    final currentLocation = await LocationService.getCurrentLocation();
-    final airQualityReading = currentLocation != null
-        ? await LocationService.getNearestSite(
-              currentLocation.latitude,
-              currentLocation.longitude,
-            ) ??
-            (await LocationService.getSurroundingSites(
-              latitude: currentLocation.latitude,
-              longitude: currentLocation.longitude,
-            ))
-                .firstOrNull
-        : (BlocProvider.of<FavouritePlaceBloc>(context as BuildContext)
-                .state
-                .isNotEmpty
-            ? await LocationService.getNearestSite(
-                  BlocProvider.of<FavouritePlaceBloc>(context as BuildContext)
-                      .state
-                      .first
-                      .latitude,
-                  BlocProvider.of<FavouritePlaceBloc>(context as BuildContext)
-                      .state
-                      .first
-                      .longitude,
-                ) ??
-                (await LocationService.getSurroundingSites(
-                  latitude: BlocProvider.of<FavouritePlaceBloc>(
-                          context as BuildContext)
-                      .state
-                      .first
-                      .latitude,
-                  longitude: BlocProvider.of<FavouritePlaceBloc>(
-                          context as BuildContext)
-                      .state
-                      .first
-                      .longitude,
-                ))
-                    .firstOrNull
-            : null);
+    CurrentLocation? currentLocation =
+        await LocationService.getCurrentLocation();
+    // final favouritePlaces =
+    //     BlocProvider.of<FavouritePlaceBloc>(context as BuildContext).state;
+    // final firstFavouritePlace =
+    //     favouritePlaces.isNotEmpty ? favouritePlaces.first : null;
+
+    Future<AirQualityReading?> getAirQualityReading(
+        double latitude, double longitude) async {
+      return await LocationService.getNearestSite(latitude, longitude) ??
+          (await LocationService.getSurroundingSites(
+                  latitude: latitude, longitude: longitude))
+              .firstOrNull;
+    }
+
+    final hiveAirQualityReading =
+        HiveService().getNearbyAirQualityReadings().isNotEmpty
+            ? (HiveService().getNearbyAirQualityReadings()..shuffle()).first
+            : null;
+
+    // AirQualityReading? airQualityReading = currentLocation != null
+    //     ? await getAirQualityReading(
+    //         currentLocation.latitude, currentLocation.longitude)
+    //     : firstFavouritePlace != null
+    //         ? await getAirQualityReading(
+    //             firstFavouritePlace.latitude, firstFavouritePlace.longitude)
+    //         : hiveAirQualityReading;
+
+    AirQualityReading? airQualityReading = currentLocation != null
+        ? await getAirQualityReading(
+            currentLocation.latitude, currentLocation.longitude)
+        : hiveAirQualityReading;
+
+    // final airQualityReading = await LocationService.getNearestSite(
+    //       currentLocation!.latitude,
+    //       currentLocation.longitude,
+    //     ) ??
+    //     (await LocationService.getSurroundingSites(
+    //       latitude: currentLocation.latitude,
+    //       longitude: currentLocation.longitude,
+    //     ))
+    //         .firstOrNull;
 
     if (airQualityReading != null) {
       final widgetData =
