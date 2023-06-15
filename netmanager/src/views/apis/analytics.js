347903cc3d10165a998b012eb27fa29bd7b9614e
Belinda Marion Kobusingye <46527380+Codebmk@users.noreply.github.com>
2023-06-15 13:00:56 +0300

    schedule export api config

--- netmanager/src/views/apis/analytics.js
+++ netmanager/src/views/apis/analytics.js
@@ -3,7 +3,8 @@ import {
   GET_DATA_MAP,
   GET_SITES,
   DOWNLOAD_CUSTOMISED_DATA_URI,
-  D3_CHART_DATA_URI
+  D3_CHART_DATA_URI,
+  SCHEDULE_EXPORT_DATA
 } from 'config/urls/analytics';
 
 let token = localStorage.jwtToken;
@@ -33,4 +34,20 @@ export const downloadDataApi = async (data) => {
 
 export const loadD3ChartDataApi = async (data) => {
   return await axios.post(D3_CHART_DATA_URI, data).then((response) => response.data);
+};
+
+export const scheduleExportDataApi = async (data) => {
+  return await axios.post(SCHEDULE_EXPORT_DATA, data).then((response) => response.data);
+};
+
+export const refreshScheduleExportDataApi = async (requestId) => {
+  return await axios
+    .patch(SCHEDULE_EXPORT_DATA, { params: { requestId } })
+    .then((response) => response.data);
+};
+
+export const getScheduleExportDataApi = async (userId) => {
+  return await axios
+    .get(SCHEDULE_EXPORT_DATA, { params: { userId } })
+    .then((response) => response.data);
 };
