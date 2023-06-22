483788964b48a0e71554e1a6bb92ed4d28e96206
Ochieng Paul <ochiengpaul442@gmail.com>
2023-06-22 18:17:15 +0300

    update

--- website/frontend/src/pages/Events/index.js
+++ website/frontend/src/pages/Events/index.js
@@ -37,11 +37,11 @@ const EventsPage = () => {
   const loading = useSelector((state) => state.eventsData.loading);
 
   // hook to handle see more/less button
-  const [numEventsToShow, setNumEventsToShow] = useState(3);
+  const [numEventsToShow, setNumEventsToShow] = useState(9);
 
   // for handling see less button
   const handleSeeLess = () => {
-    setNumEventsToShow(3);
+    setNumEventsToShow(9);
     window.scrollTo({ top: 0, behavior: 'smooth' });
   };
 
@@ -113,28 +113,26 @@ const EventsPage = () => {
                           link={event.unique_title}
                         />
                       ))}
-                  {upcomingEvents.length === 0 && selectedNavTab === 'upcoming events' ? (
-                    <div className="no-events">
-                      <span>There are currently no events</span>
+                </div>
+                {upcomingEvents.length === 0 && selectedNavTab === 'upcoming events' ? (
+                  <div className="no-events">
+                    <span>There are currently no events</span>
+                  </div>
+                ) : null}
+                <div className="see-more-container">
+                  {(upcomingEvents.length > numEventsToShow &&
+                    selectedNavTab === 'upcoming events') ||
+                  (pastEvents.length > numEventsToShow && selectedNavTab === 'past events') ? (
+                    <div className="see-more">
+                      <button onClick={() => setNumEventsToShow(numEventsToShow + 6)}>More</button>
                     </div>
                   ) : null}
-                  <div className="see-more-container">
-                    {(upcomingEvents.length > numEventsToShow &&
-                      selectedNavTab === 'upcoming events') ||
-                    (pastEvents.length > numEventsToShow && selectedNavTab === 'past events') ? (
-                      <div className="see-more">
-                        <button onClick={() => setNumEventsToShow(numEventsToShow + 6)}>
-                          More
-                        </button>
-                      </div>
-                    ) : null}
 
-                    {numEventsToShow > 9 && (
-                      <div className="see-less">
-                        <button onClick={() => handleSeeLess()}>Less</button>
-                      </div>
-                    )}
-                  </div>
+                  {numEventsToShow > 9 && (
+                    <div className="see-less">
+                      <button onClick={() => handleSeeLess()}>Less</button>
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
