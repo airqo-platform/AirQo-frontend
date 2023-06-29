085784d2090718ecc6ff8256ffd02a5b57ef1c3b
FaithDaka <faithbusigu8@gmail.com>
2023-06-29 12:57:13 +0300

    configure url

--- platform/src/core/apis/Account.js
+++ platform/src/core/apis/Account.js
@@ -5,5 +5,5 @@ export const postUserCreationDetails = async (data) =>
   await axios.post(AUTH_URL, data).then((response) => response.data);
 
 export const getGoogleAuthDetails = async () => {
-  await axios.get(GOOGLE_AUTH_URL).then((response) => response.data);
+  await axios.get(GOOGLE_AUTH_URL);
 };
