2a8afbe187e7db675c0713052efa1eac54a3e2e0
Mutabazi Noble <60974514+Mnoble-19@users.noreply.github.com>
2023-07-03 21:27:24 +0300

    fix github code check issues

--- mobile/lib/blocs/dashboard/dashboard_bloc.dart
+++ mobile/lib/blocs/dashboard/dashboard_bloc.dart
@@ -7,7 +7,6 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 part 'dashboard_event.dart';
-
 part 'dashboard_state.dart';
 
 class DashboardBloc extends Bloc<DashboardEvent, DashboardState> {
--- mobile/lib/models/models.dart
+++ mobile/lib/models/models.dart
@@ -16,6 +16,5 @@ export 'widget_data.dart';
 export 'insight.dart';
 export 'health_tip.dart';
 export 'phone_auth_model.dart';
-export 'insight.dart';
 export 'current_location.dart';
 export 'address.dart';
