3bf3f8a5800ac187f5c671d8e4dbc10ef38674d6
Mutabazi Noble <60974514+Mnoble-19@users.noreply.github.com>
2023-07-03 21:19:17 +0300

    Code cleanup

--- mobile/lib/services/widget_service.dart
+++ mobile/lib/services/widget_service.dart
@@ -2,6 +2,7 @@ import 'package:app/models/air_quality_reading.dart';
 import 'package:app/services/location_service.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive/hive.dart';
 import 'package:home_widget/home_widget.dart';
 import 'package:collection/collection.dart';
 import 'package:path/path.dart';
@@ -13,55 +14,24 @@ class WidgetService {
   static Future<void> sendData() async {
     CurrentLocation? currentLocation =
         await LocationService.getCurrentLocation();
-    final favouritePlaces =
-        BlocProvider.of<FavouritePlaceBloc>(context as BuildContext).state;
-    final firstFavouritePlace =
-        favouritePlaces.isNotEmpty ? favouritePlaces.first : null;
-    //
-    Future<AirQualityReading?> getAirQualityReading(
-        double latitude, double longitude) async {
-      return await LocationService.getNearestSite(latitude, longitude) ??
-          (await LocationService.getSurroundingSites(
-                  latitude: latitude, longitude: longitude))
-              .firstOrNull;
-    }
+    var airQualityReading = await LocationService.getNearestSite(
+          currentLocation!.latitude,
+          currentLocation.longitude,
+        ) ??
+        (await LocationService.getSurroundingSites(
+          latitude: currentLocation.latitude,
+          longitude: currentLocation.longitude,
+        ))
+            .firstOrNull;
 
-    final hiveAirQualityReading =
-        HiveService().getNearbyAirQualityReadings().isNotEmpty
-            ? (HiveService().getNearbyAirQualityReadings()..shuffle()).first
-            : null;
-
-    AirQualityReading? airQualityReading = currentLocation != null
-        ? await getAirQualityReading(
-            currentLocation.latitude, currentLocation.longitude)
-        : firstFavouritePlace != null
-            ? await getAirQualityReading(
-                firstFavouritePlace.latitude, firstFavouritePlace.longitude)
-            : hiveAirQualityReading;
-    //
-    // AirQualityReading? airQualityReading = currentLocation != null
-    //     ? await getAirQualityReading(
-    //         currentLocation.latitude, currentLocation.longitude)
-    //     : hiveAirQualityReading;
-    //
-    // // final airQualityReading = await LocationService.getNearestSite(
-    // //       currentLocation!.latitude,
-    // //       currentLocation.longitude,
-    // //     ) ??
-    // //     (await LocationService.getSurroundingSites(
-    // //       latitude: currentLocation.latitude,
-    // //       longitude: currentLocation.longitude,
-    // //     ))
-    // //         .firstOrNull;
-
-    if (airQualityReading != null) {
-      final widgetData =
-          WidgetData.initializeFromAirQualityReading(airQualityReading);
-      await Future.wait(widgetData.idMapping().entries.map(
-            (entry) =>
-                HomeWidget.saveWidgetData<String>(entry.key, entry.value),
-          ));
-    }
+    airQualityReading ??= HiveService().getNearbyAirQualityReadings().isNotEmpty
+        ? (HiveService().getNearbyAirQualityReadings()..shuffle()).first
+        : null;
+    final widgetData =
+        WidgetData.initializeFromAirQualityReading(airQualityReading!);
+    await Future.wait(widgetData.idMapping().entries.map(
+          (entry) => HomeWidget.saveWidgetData<String>(entry.key, entry.value),
+        ));
   }
 
   static Future<void> updateWidget() async {
