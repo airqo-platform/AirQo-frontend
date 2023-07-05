d9a61334141f082b1bc70356e757bcf8e5753539
danielmarv <103020028+danielmarv@users.noreply.github.com>
2023-07-05 14:52:58 +0300

    TopBar Item

--- platform/src/common/components/TopBar/index.jsx
+++ platform/src/common/components/TopBar/index.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import ProfileIcon from '@/icons/SideBar/Profile.svg';
 import SearchMdIcon from '@/icons/SideBar/search-md.svg';
 import NotificationsIcon from '@/icons/SideBar/Notification.svg';
+import TopBarItem from './TopBarItem';
 
 const TopBar = () => {
   return (
@@ -10,19 +11,9 @@ const TopBar = () => {
        <div className="font-bold">Home</div>
         <div className='flex  justify-end md:justify-between items-center'>
          <div className='flex w-auto'>
-           <div className='border border-grey-750 w-15 h-10 rounded-lg flex items-center justify-between ml-3 p-[10px]'>
-             {/* <SearchMdIcon  /> */}
-             A
-           </div>
-           <div className='border border-grey-750 w-full h-10 rounded-lg ml-3 flex items-center justify-between py-[10px] px-3'>
-             {/* <NotificationsIcon /> */}
-             A
-           </div>
-           <div className='border border-grey-750 w-full h-10 p-2 box-border rounded-lg flex items-center justify-between ml-3'>
-            
-             {/* <ProfileIcon /> */}
-             A
-           </div>
+           <TopBarItem Icon={SearchMdIcon}/>
+           <TopBarItem Icon={NotificationsIcon}/>
+           <TopBarItem Icon={ProfileIcon}/>
          </div>
        </div>
      </div>
