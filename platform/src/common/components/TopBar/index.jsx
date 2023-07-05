2bdc026a1e4b5c36912307dcd788b685364637cd
danielmarv <103020028+danielmarv@users.noreply.github.com>
2023-07-05 14:24:24 +0300

    Update index.jsx

--- platform/src/common/components/TopBar/index.jsx
+++ platform/src/common/components/TopBar/index.jsx
@@ -7,7 +7,7 @@ const TopBar = () => {
   return (
     <div>
       <div className="w-full flex border-b-[1px] sm:px-4 py-3 px-4 lg:px-6 justify-between items-center shadow-sm">
-       <div className="font-bold">{currentPage}</div>
+       <div className="font-bold">Home</div>
         <div className='flex  justify-end md:justify-between items-center'>
          <div className='flex w-auto'>
            <div className='border border-grey-750 w-15 h-10 rounded-lg flex items-center justify-between ml-3 p-[10px]'>
