c078871346c028252544cf3f3b4c4722a9f95baa
danielmarv <103020028+danielmarv@users.noreply.github.com>
2023-07-05 09:24:30 +0300

    Redesigning the sidebar ant the top bar

---
+++ platform/src/common/components/SideBar/AnnouncementCard.jsx
@@ -0,0 +1,11 @@
+import Announcement from "@/icons/SideBar/announcement-card.svg"
+
+const AnnouncementCard = () => {
+  return (
+    <div className="self-stretch rounded-lg border border-gray-100 bg-white p-1 ">
+      <Announcement />
+    </div>
+  )
+}
+
+export default AnnouncementCard;
--- platform/src/common/components/TopBar/index.jsx
+++ platform/src/common/components/TopBar/index.jsx
@@ -1,50 +1,38 @@
-import AirqoLogo from '@/icons/airqo_logo.svg';
-import HelpCenterIcon from '@/icons/help_center.svg';
-import NotificationsIcon from '@/icons/notifications.svg';
+import ProfileIcon from '@/icons/SideBar/Profile.svg';
+import SearchMdIcon from '@/icons/SideBar/search-md.svg';
+import NotificationsIcon from '@/icons/SideBar/Notification.svg';
 import ArrowDropDownIcon from '@/icons/arrow_drop_down';
 import SearchIcon from '@/icons/Actions/search.svg';
 import { useState } from 'react';
 
 const TopBar = () => {
   const [searchInput, setSearchInput] = useState();
+  const [currentPage, setCurrentPage] = useState('Home'); // Set the initial page name here
+
+  // Function to update the current page
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
   return (
-    <nav className='fixed top-0 w-full z-10 px-4 py-3 h-16 box-border border-b-[0.5px] border-b-grey-750 bg-white'>
-      <div className='flex justify-end md:justify-between items-center'>
-        <AirqoLogo className='invisible lg:visible w-[46.56px] h-8' />
-
-        <form className='hidden relative md:block md:max-w-[280px] lg:max-w-[448px] h-10 w-full'>
-          <div className='absolute my-[10px] mx-3'>
-            <SearchIcon />
-          </div>
-          <input
-            type='search'
-            id='search'
-            placeholder='Search'
-            value={searchInput}
-            onChange={(e) => setSearchInput(e.target.value)}
-            className='h-10 w-full bg-grey-250 flex justify-center pl-10 rounded-md text-sm border-0 focus:outline-none focus:ring focus:ring-violet-300'
-          />
-        </form>
-
+    <div className="w-full flex border-b-[1px] sm:px-4 py-3 px-4 lg:px-6 justify-between items-center shadow-sm">
+      <div className="font-bold">{currentPage}</div>
+       <div className='flex  justify-end md:justify-between items-center'>
         <div className='flex w-auto'>
-          <div className='border border-grey-750 w-10 h-10 rounded-lg ml-3 p-[10px]'>
-            <HelpCenterIcon className='w-5 h-5 stroke-grey-200 opacity-30' />
+          <div className='border border-grey-750 w-15 h-10 rounded-lg flex items-center justify-between ml-3 p-[10px]'>
+            <SearchMdIcon  />
           </div>
-          <div className='border border-grey-750 w-10 h-10 rounded-lg ml-3 py-[10px] px-3'>
-            <NotificationsIcon className='w-4 h-5 stroke-grey-200 opacity-30' />
+          <div className='border border-grey-750 w-full h-10 rounded-lg ml-3 flex items-center justify-between py-[10px] px-3'>
+            <NotificationsIcon />
           </div>
           <div className='border border-grey-750 w-full h-10 p-2 box-border rounded-lg flex items-center justify-between ml-3'>
-            <div className='flex items-center mr-[22.5px]'>
-              <div className='bg-grey-700 w-6 h-6 p-2 rounded-full mr-3 flex justify-center items-center'>
-                <h3 className='font-normal text-xs'>DO</h3>
-              </div>
-              <h3 className='hidden md:block text-xs font-normal'>Deo Okedi</h3>
-            </div>
-            <ArrowDropDownIcon />
+            
+            <ProfileIcon />
           </div>
         </div>
       </div>
-    </nav>
+    </div>
+
+
   );
 };
 
