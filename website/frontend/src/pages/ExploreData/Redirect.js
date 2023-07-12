43ccf9608a2a1f0e43a848814780e409bf411bad
FaithDaka <faithbusigu8@gmail.com>
2023-07-12 13:38:24 +0300

    redirect logic

--- website/frontend/src/pages/ExploreData/Redirect.js
+++ website/frontend/src/pages/ExploreData/Redirect.js
@@ -3,10 +3,8 @@ import { isAndroid, isIOS } from 'react-device-detect';
 
 const QRCodeRedirect = () => {
     const redirectUser = isAndroid ? 'https://play.google.com/store/apps/details?id=com.airqo.app' : isIOS ? 'https://apps.apple.com/ug/app/airqo-monitoring-air-quality/id1337573091' : 'https://www.airqo.net/explore-data/download-apps'
-    console.log('Redirect', redirectUser)
-    return (
-        <a href={redirectUser} rel="noopener noreferrer"></a>
-    )
+    window.location.href = redirectUser
+    return null;
 }
 
 export default QRCodeRedirect;
