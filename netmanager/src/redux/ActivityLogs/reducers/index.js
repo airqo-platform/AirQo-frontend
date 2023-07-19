e404db15f853c77a31f48353da4412bde942a3f9
danielmarv <103020028+danielmarv@users.noreply.github.com>
2023-07-19 14:04:41 +0300

    Update index.js

--- netmanager/src/redux/ActivityLogs/reducers/index.js
+++ netmanager/src/redux/ActivityLogs/reducers/index.js
@@ -2,11 +2,9 @@ import { combineReducers } from 'redux';
 import activities from './activities';
 import activityOptions from './activityOptions';
 import activitySummary from './activitySummary';
-import activityDetails from './activityDetails';
 
 export default combineReducers({
   activities,
   activityOptions,
-  activitySummary,
-  activityDetails
+  activitySummary
 });
