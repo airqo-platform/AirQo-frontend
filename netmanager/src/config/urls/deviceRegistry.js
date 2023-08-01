575a1775e7ecc182d82890193dc65bad94dc28c7
Belinda Marion Kobusingye <46527380+Codebmk@users.noreply.github.com>
2023-08-01 13:37:41 +0300

    migrate device registry apis

--- netmanager/src/config/urls/deviceRegistry.js
+++ netmanager/src/config/urls/deviceRegistry.js
@@ -1,59 +1,55 @@
 import { stripTrailingSlash } from '../utils';
 
-const BASE_DEVICE_REGISTRY_URL = stripTrailingSlash(
-  process.env.REACT_APP_BASE_DEVICE_REGISTRY_URL || process.env.REACT_APP_BASE_URL
-);
-
 const BASE_DEVICE_REGISTRY_URL_V2 = stripTrailingSlash(process.env.REACT_APP_BASE_URL_V2);
 
-export const ACTIVITY_URI = `${BASE_DEVICE_REGISTRY_URL}/devices/activities`;
+export const ACTIVITY_URI = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/activities`;
 
-export const REGISTER_DEVICE_URI = `${BASE_DEVICE_REGISTRY_URL}/devices`;
+export const REGISTER_DEVICE_URI = `${BASE_DEVICE_REGISTRY_URL_V2}/devices`;
 
-export const ALL_DEVICES_URI = `${BASE_DEVICE_REGISTRY_URL}/devices`;
+export const ALL_DEVICES_URI = `${BASE_DEVICE_REGISTRY_URL_V2}/devices`;
 
-export const EDIT_DEVICE_URI = `${BASE_DEVICE_REGISTRY_URL}/devices`;
+export const EDIT_DEVICE_URI = `${BASE_DEVICE_REGISTRY_URL_V2}/devices`;
 
-export const SOFT_EDIT_DEVICE_URI = `${BASE_DEVICE_REGISTRY_URL}/devices/soft`;
+export const SOFT_EDIT_DEVICE_URI = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/soft`;
 
-export const DEVICES_IN_LOCATION_URI = `${BASE_DEVICE_REGISTRY_URL}/devices/by/location?loc=`;
+export const DEVICES_IN_LOCATION_URI = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/by/location?loc=`;
 
-export const DEPLOY_DEVICE_URI = `${BASE_DEVICE_REGISTRY_URL}/devices/activities/deploy`;
+export const DEPLOY_DEVICE_URI = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/activities/deploy`;
 
-export const ADD_MAINTENANCE_LOGS_URI = `${BASE_DEVICE_REGISTRY_URL}/devices/activities/maintain`;
+export const ADD_MAINTENANCE_LOGS_URI = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/activities/maintain`;
 
-export const DELETE_DEVICE_URI = `${BASE_DEVICE_REGISTRY_URL}/devices/ts/delete`;
+export const DELETE_DEVICE_URI = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/ts/delete`;
 
-export const ADD_COMPONENT_URI = `${BASE_DEVICE_REGISTRY_URL}/devices/add/components?device=`;
+export const ADD_COMPONENT_URI = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/add/components?device=`;
 
-export const GET_COMPONENTS_URI = `${BASE_DEVICE_REGISTRY_URL}/devices/list/components`;
+export const GET_COMPONENTS_URI = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/list/components`;
 
-export const DELETE_COMPONENT_URI = `${BASE_DEVICE_REGISTRY_URL}/devices/delete/components?comp=`;
+export const DELETE_COMPONENT_URI = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/delete/components?comp=`;
 
-export const UPDATE_COMPONENT_URI = `${BASE_DEVICE_REGISTRY_URL}/devices/update/components?device=`;
+export const UPDATE_COMPONENT_URI = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/update/components?device=`;
 
-export const UPDATE_COMPONENT = `${BASE_DEVICE_REGISTRY_URL}/devices/update/components`;
+export const UPDATE_COMPONENT = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/update/components`;
 
-export const DELETE_COMPONENT = `${BASE_DEVICE_REGISTRY_URL}/devices/delete/components`;
+export const DELETE_COMPONENT = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/delete/components`;
 
-export const DELETE_DEVICE_PHOTO = `${BASE_DEVICE_REGISTRY_URL}/devices/photos`;
+export const DELETE_DEVICE_PHOTO = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/photos`;
 
-export const RECALL_DEVICE_URI = `${BASE_DEVICE_REGISTRY_URL}/devices/activities/recall`;
+export const RECALL_DEVICE_URI = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/activities/recall`;
 
 export const EVENTS = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/events?tenant=airqo`;
 
-export const SITES = `${BASE_DEVICE_REGISTRY_URL}/devices/sites`;
+export const SITES = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/sites`;
 
-export const AIRQLOUDS = `${BASE_DEVICE_REGISTRY_URL}/devices/airqlouds?tenant=airqo`;
+export const AIRQLOUDS = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/airqlouds?tenant=airqo`;
 
-export const REFRESH_AIRQLOUD = `${BASE_DEVICE_REGISTRY_URL}/devices/airqlouds/refresh`;
+export const REFRESH_AIRQLOUD = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/airqlouds/refresh`;
 
-export const DASHBOARD_AIRQLOUDS = `${BASE_DEVICE_REGISTRY_URL}/devices/airqlouds/dashboard`;
+export const DASHBOARD_AIRQLOUDS = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/airqlouds/dashboard`;
 
-export const DECRYPT = `${BASE_DEVICE_REGISTRY_URL}/devices/decrypt`;
+export const DECRYPT = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/decrypt`;
 
-export const QRCODE = `${BASE_DEVICE_REGISTRY_URL}/devices/qrcode`;
+export const QRCODE = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/qrcode`;
 
-export const SOFT_EDIT_DEVICE_IMAGE = `${BASE_DEVICE_REGISTRY_URL}/devices/photos/soft?tenant=airqo`;
+export const SOFT_EDIT_DEVICE_IMAGE = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/photos/soft?tenant=airqo`;
 
-export const GET_DEVICE_IMAGES = `${BASE_DEVICE_REGISTRY_URL}/devices/photos?tenant=airqo`;
+export const GET_DEVICE_IMAGES = `${BASE_DEVICE_REGISTRY_URL_V2}/devices/photos?tenant=airqo`;
