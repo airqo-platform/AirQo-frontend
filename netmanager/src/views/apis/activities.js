658140f2670a1e99c8636fb87de353ccdf70633f
Daniel Ntege <danientege785@gmail.com>
2023-08-08 14:02:09 +0300

    Update activities.js

--- netmanager/src/views/apis/activities.js
+++ netmanager/src/views/apis/activities.js
@@ -1,17 +1,18 @@
 import axios from "axios";
 import { CURRENT_AIRQLOUD_KEY } from "../../config/localStorageKeys";
-import { GET_ACCESS_TOKEN } from "../../config/urls/authService";
+import { 
+  recallDeviceApi, 
+  deployDeviceApi,
+  addMaintenanceLogApi
+} from "./deviceRegistry";
 
-export const postData = async () => {
-  const url = "https://staging-platform.airqo.net/api/v2/devices/activities/maintain"; // Replace with your actual endpoint
-  
+export const recallPostData = async () => {
   try {
     const storedData = localStorage.getItem(CURRENT_AIRQLOUD_KEY);
     if (!storedData) {
       console.error("Error: No data found in local storage");
       return;
     }
-
 
     const parsedData = JSON.parse(storedData);
 
@@ -22,7 +23,55 @@ export const postData = async () => {
       lastName: parsedData.lastName,
     };
 
-    const response = await axios.post(url, formData);
+    const response = await axios.post(recallDeviceApi(parsedData.deviceName), formData); 
+    console.log("Response:", response.data);
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+};
+
+export const maintainPostData = async () => {
+  try {
+    const storedData = localStorage.getItem(CURRENT_AIRQLOUD_KEY);
+    if (!storedData) {
+      console.error("Error: No data found in local storage");
+      return;
+    }
+
+    const parsedData = JSON.parse(storedData);
+
+    const formData = {
+      userName: parsedData.userName,
+      email: parsedData.email,
+      firstName: parsedData.firstName,
+      lastName: parsedData.lastName,
+    };
+
+    const response = await axios.post(addMaintenanceLogApi(parsedData.deviceName), formData); 
+    console.log("Response:", response.data);
+  } catch (error) {
+    console.error("Error:", error.message);
+  }
+};
+
+export const deployPostData = async () => {
+  try {
+    const storedData = localStorage.getItem(CURRENT_AIRQLOUD_KEY);
+    if (!storedData) {
+      console.error("Error: No data found in local storage");
+      return;
+    }
+
+    const parsedData = JSON.parse(storedData);
+
+    const formData = {
+      userName: parsedData.userName,
+      email: parsedData.email,
+      firstName: parsedData.firstName,
+      lastName: parsedData.lastName,
+    };
+
+    const response = await axios.post(deployDeviceApi(parsedData.deviceName), formData); 
     console.log("Response:", response.data);
   } catch (error) {
     console.error("Error:", error.message);
