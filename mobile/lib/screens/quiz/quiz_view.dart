970a11acd4d9de4bf23af75ecba67ae0046b859d
Katende Jericho <katendejericho5@gmail.com>
2023-08-08 16:52:45 +0300

    quiz initial

--- mobile/lib/screens/quiz/quiz_view.dart
+++ mobile/lib/screens/quiz/quiz_view.dart
@@ -111,7 +111,9 @@ class QuizCardWidget extends StatelessWidget {
         backgroundColor: Colors.white,
         padding: const EdgeInsets.fromLTRB(16.0, 8.0, 8.0, 8.0),
       ),
-      onPressed: () async {},
+      onPressed: () async {
+        _displayBottomSheet(context);
+      },
       child: Row(
         crossAxisAlignment: CrossAxisAlignment.center,
         children: [
@@ -157,6 +159,221 @@ class QuizCardWidget extends StatelessWidget {
     );
   }
 }
+
+Future<dynamic> _displayBottomSheet(BuildContext context) {
+  return showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return SizedBox(
+          height: 200,
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.3,
+            height: MediaQuery.of(context).size.height * 0.87,
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 23),
+            decoration: const ShapeDecoration(
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(32),
+                  topRight: Radius.circular(32),
+                ),
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.fromLTRB(31, 10, 31, 31),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  SizedBox(
+                    width: 120,
+                    height: 120,
+                    child: Container(
+                      width: 100,
+                      height: 100,
+                      decoration: ShapeDecoration(
+                        color: const Color(0xFFD1FADF),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                      ),
+                      child: const Icon(
+                        Icons.check_circle,
+                        color: Color.fromARGB(188, 7, 77, 50),
+                        size: 80,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 32),
+                  const Column(
+                    mainAxisSize: MainAxisSize.min,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      SizedBox(
+                        child: AutoSizeText(
+                          'You have completed the quiz!',
+                          maxLines: 2,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                            color: Color.fromARGB(255, 31, 35, 45),
+                            fontSize: 24,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w700,
+                            height: 1.70,
+                            letterSpacing: -0.90,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 24),
+                      SizedBox(
+                        //width: 307,
+                        //height: 89,
+                        child: AutoSizeText(
+                          'Way to go🎊. You have unlocked personalised air quality recommendations to empower you on your clean air journey.',
+                          textAlign: TextAlign.center,
+                          maxLines: 3,
+                          style: TextStyle(
+                            color: Color(0xFF6F87A1),
+                            fontSize: 20,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w500,
+                            height: 1.50,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      });
+}
+
+// class QuizPage extends StatelessWidget {
+//   const QuizPage({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Stack(
+//       children: [
+//         const Positioned.fill(
+//           child: Column(
+//             children: [
+//               Expanded(
+//                 child: SizedBox(
+//                   width: double.infinity,
+//                   child: Image(
+//                     image: NetworkImage(
+//                       "https://images.unsplash.com/photo-1547721064-da6cfb341d50",
+//                     ),
+//                     fit: BoxFit.cover,
+//                   ),
+//                 ),
+//               ),
+//             ],
+//           ),
+//         ),
+//         Positioned(
+//           bottom: 0,
+//           left: 0,
+//           right: 0,
+//           child: Container(
+//               width: MediaQuery.of(context).size.width * 0.3,
+//               height: MediaQuery.of(context).size.height * 0.87,
+//               padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 23),
+//               decoration: const ShapeDecoration(
+//                 color: Colors.white,
+//                 shape: RoundedRectangleBorder(
+//                   borderRadius: BorderRadius.only(
+//                     topLeft: Radius.circular(32),
+//                     topRight: Radius.circular(32),
+//                   ),
+//                 ),
+//               ),
+//               child: Padding(
+//                 padding: const EdgeInsets.fromLTRB(31, 10, 31, 31),
+//                 child: Column(
+//                   mainAxisSize: MainAxisSize.min,
+//                   mainAxisAlignment: MainAxisAlignment.start,
+//                   crossAxisAlignment: CrossAxisAlignment.center,
+//                   children: [
+//                     const SizedBox(
+//                       height: 40,
+//                     ),
+//                     SizedBox(
+//                       width: 120,
+//                       height: 120,
+//                       child: Container(
+//                         width: 100,
+//                         height: 100,
+//                         decoration: ShapeDecoration(
+//                           color: const Color(0xFFD1FADF),
+//                           shape: RoundedRectangleBorder(
+//                             borderRadius: BorderRadius.circular(15),
+//                           ),
+//                         ),
+//                         child: const Icon(
+//                           Icons.check_circle,
+//                           color: Color.fromARGB(188, 7, 77, 50),
+//                           size: 80,
+//                         ),
+//                       ),
+//                     ),
+//                     const SizedBox(height: 32),
+//                     const Column(
+//                       mainAxisSize: MainAxisSize.min,
+//                       mainAxisAlignment: MainAxisAlignment.start,
+//                       crossAxisAlignment: CrossAxisAlignment.center,
+//                       children: [
+//                         SizedBox(
+//                           child: AutoSizeText(
+//                             'You have completed the quiz!',
+//                             maxLines: 2,
+//                             textAlign: TextAlign.center,
+//                             style: TextStyle(
+//                               color: Color.fromARGB(255, 31, 35, 45),
+//                               fontSize: 24,
+//                               fontFamily: 'Inter',
+//                               fontWeight: FontWeight.w700,
+//                               height: 1.70,
+//                               letterSpacing: -0.90,
+//                             ),
+//                           ),
+//                         ),
+//                         SizedBox(height: 24),
+//                         SizedBox(
+//                           //width: 307,
+//                           //height: 89,
+//                           child: AutoSizeText(
+//                             'Way to go🎊. You have unlocked personalised air quality recommendations to empower you on your clean air journey.',
+//                             textAlign: TextAlign.center,
+//                             maxLines: 3,
+//                             style: TextStyle(
+//                               color: Color(0xFF6F87A1),
+//                               fontSize: 20,
+//                               fontFamily: 'Inter',
+//                               fontWeight: FontWeight.w500,
+//                               height: 1.50,
+//                             ),
+//                           ),
+//                         ),
+//                       ],
+//                     ),
+//                   ],
+//                 ),
+//               )),
+//         ),
+//       ],
+//     );
+//   }
+// }
 
 // class QuizProgressBar extends StatelessWidget {
 //   const QuizProgressBar({super.key});
