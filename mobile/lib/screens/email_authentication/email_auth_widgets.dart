bb9dd25f6471c918b3d1656550d0f067b45dce74
Katende Jericho <katendejericho5@gmail.com>
2023-08-09 15:07:28 +0300

    text fix

--- mobile/lib/screens/email_authentication/email_auth_widgets.dart
+++ mobile/lib/screens/email_authentication/email_auth_widgets.dart
@@ -228,7 +228,7 @@ class EmailVerificationSubTitle extends StatelessWidget {
             subtitle = 'Sure you read it correctly? Pro Tip: Copy & Paste';
             break;
           case AuthenticationStatus.success:
-            subtitle = 'Pheww, almost done, hold in there.';
+            subtitle = 'Pheww, almost done, hang in there.';
             break;
         }
 
