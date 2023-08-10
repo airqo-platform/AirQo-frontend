6332635f257101e655b80481e914b97818c6e013
Ochieng Paul <ochiengpaul442@gmail.com>
2023-08-10 16:56:12 +0300

    update to geolocate component

--- website/frontend/src/components/Geolocate.js
+++ website/frontend/src/components/Geolocate.js
@@ -1,7 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { setCurrentAirQloudData } from '../../reduxStore/AirQlouds/operations';
+// import { getUserCountryApi } from '../../apis';
 
-function Geolocate() {
-  return <div>Geolocate</div>;
-}
+const Geolocate = ({ countries }) => {
+  const dispatch = useDispatch();
+  const [storedCountry, setStoredCountry] = useState(localStorage.getItem('userCountry'));
+
+  useEffect(() => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        async (position) => {
+          const { latitude, longitude } = position.coords;
+          await updateUserCountry(latitude, longitude);
+        },
+        (error) => {
+          console.error('Error getting user geolocation:', error);
+        }
+      );
+    }
+  }, []);
+
+  const updateUserCountry = async (latitude, longitude) => {
+    try {
+      //   const response = await getUserCountryApi(latitude, longitude);
+      const response = { countryName: 'Uganda' };
+      const { countryName } = response;
+      const selectedCountry = countries.some((country) => countryName === country.name)
+        ? countryName
+        : 'Uganda';
+
+      if (selectedCountry !== storedCountry) {
+        dispatch(setCurrentAirQloudData(selectedCountry));
+        setStoredCountry(selectedCountry);
+        localStorage.setItem('userCountry', selectedCountry);
+      }
+    } catch (error) {
+      console.error('Error fetching user country:', error);
+    }
+  };
+
+  return null;
+};
 
 export default Geolocate;
