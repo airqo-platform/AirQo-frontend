ab58f0f6679da600dbf26c7271856d6e3fd1d7c6
FaithDaka <faithbusigu8@gmail.com>
2023-08-15 11:50:11 +0300

    Set permissions

--- website/backend/impact/serializers.py
+++ website/backend/impact/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers
 from .models import ImpactNumber
 
-
 class ImpactSerializer(serializers.ModelSerializer):
     class Meta:
         model = ImpactNumber
--- website/backend/impact/views.py
+++ website/backend/impact/views.py
@@ -1,8 +1,9 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 from .models import ImpactNumber
 from .serializers import ImpactSerializer
 
-
 class ImpactViewSet(viewsets.ReadOnlyModelViewSet):
+    permission_classes = (AllowAny,)
     queryset = ImpactNumber.objects.all()
     serializer_class = ImpactSerializer
