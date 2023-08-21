698ba0894e38c07bc5b7a153c51fbbb7b12c831d
Katende Jericho <katendejericho5@gmail.com>
2023-08-21 16:19:41 +0300

    major

--- mobile/lib/screens/offline_banner.dart
+++ mobile/lib/screens/offline_banner.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-import 'package:app/screens/home_page.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
