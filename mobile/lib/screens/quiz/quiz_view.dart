40f34636180d2435b058a29c3158162fbb98fb48
Katende Jericho <katendejericho5@gmail.com>
2023-08-24 16:57:14 +0300

    minor

--- mobile/lib/screens/quiz/quiz_view.dart
+++ mobile/lib/screens/quiz/quiz_view.dart
@@ -506,7 +506,7 @@ class QuizAnswerWidget extends StatelessWidget {
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         SizedBox(
-                          height: MediaQuery.of(context).size.height * 0.05,
+                          height: MediaQuery.of(context).size.height * 0.1,
                           width: MediaQuery.of(context).size.width * 0.3,
                           child: NextButton(
                             showIcon: false,
