2ea278a10d5b53d18e76ce16873bcfc957931ce9
Benjamin Ssempala <86492979+BenjaminSsempala@users.noreply.github.com>
2023-09-04 11:01:26 +0300

    Correcting github website changes

--- website/frontend/templates/utils/groupBy.js
+++
@@ -1,7 +0,0 @@
-const groupBy = (arr, getKey) => arr.reduce((r, v) => {
-  r[getKey(v)] = r[getKey(v)] || [];
-  r[getKey(v)].push(v);
-  return r;
-}, Object.create(null));
-
-export default groupBy;
