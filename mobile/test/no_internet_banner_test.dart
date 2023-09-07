de029873028e4aede890fa6269bc3792dd233a67
Katende Jericho <katendejericho5@gmail.com>
2023-09-07 14:49:31 +0300

    added unit test for the InternetConnectionBannerCubit

--- mobile/test/no_internet_banner_test.dart
+++ mobile/test/no_internet_banner_test.dart
@@ -1,117 +1,48 @@
-import 'dart:async';
-import 'dart:ffi';
-
-import 'package:app/screens/home_page.dart';
-import 'package:app/screens/offline_banner.dart';
-import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:app/screens/offline_banner.dart'; // Import your cubit file.
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
-
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-
-class MockConnectivity extends Mock implements Connectivity {}
 
 void main() {
-  group('OfflineBanner', () {
-    late MockConnectivity connectivity;
+  group('InternetConnectionBannerCubit', () {
     late InternetConnectionBannerCubit cubit;
 
+    // Setting up the cubit
     setUp(() {
-      connectivity = MockConnectivity();
       cubit = InternetConnectionBannerCubit();
     });
 
-    testWidgets('shows banner when offline', (tester) async {
-      final key = GlobalKey();
-
-      await tester.runAsync(() async {
-        dynamic connectivityResult = true;
-        connectivityResult = connectivity.checkConnectivity();
-        // Set up the mock to return ConnectivityResult.none;
-        when(connectivityResult as FutureOr<ConnectivityResult>)
-            .thenAnswer((_) async => ConnectivityResult.none);
-
-        await tester.pumpWidget(const MaterialApp(
-          home: HomePage(),
-        ));
-        await tester.pumpAndSettle(const Duration(seconds: 2));
-
-        expect(find.byType(Container), findsOneWidget);
-        expect(find.byType(Positioned), findsOneWidget);
-        expect(
-          find.text(
-            AppLocalizations.of(key.currentContext!)!.internetConnectionLost,
-          ),
-          findsOneWidget,
-        );
-      });
+    // Closing the cubit .
+    tearDown(() {
+      cubit.close();
     });
 
-    testWidgets('hides banner when back online', (tester) async {
-      await tester.runAsync(() async {
-        // Set up the mock to return ConnectivityResult.none initially
-        when(connectivity.checkConnectivity())
-            .thenAnswer((_) async => ConnectivityResult.none);
-
-        await tester.pumpWidget(MaterialApp(
-          home: OfflineBanner(
-            child: Container(),
-          ),
-        ));
-
-        // Verify that the banner is initially displayed
-        expect(find.byType(Positioned), findsOneWidget);
-
-        // Change the mock to return ConnectivityResult.wifi
-        when(connectivity.checkConnectivity())
-            .thenAnswer((_) async => ConnectivityResult.wifi);
-
-        // Rebuild the widget
-        await tester.pump();
-
-        // Verify that the banner is hidden when back online
-        expect(find.byType(Positioned), findsNothing);
-      });
+    //check the initial state 
+    test('initial state is true', () {
+      expect(cubit.state, true);
     });
 
-    testWidgets('resets banner on connectivity change', (tester) async {
-      await tester.runAsync(() async {
-        // Set up the mock to return ConnectivityResult.none initially
-        when(connectivity.checkConnectivity())
-            .thenAnswer((_) async => ConnectivityResult.none);
+    // Test to check if the hideBanner method emits false.
+    blocTest<InternetConnectionBannerCubit, bool>(
+      'emits [false] when hideBanner is called',
+      build: () => cubit,
+      act: (cubit) => cubit.hideBanner(),
+      expect: () => [false],
+    );
 
-        // Set up the mock stream for connectivity changes
-        final connectivityStreamController =
-            StreamController<ConnectivityResult>();
-        when(connectivity.onConnectivityChanged)
-            .thenAnswer((_) => connectivityStreamController.stream);
+    // Test to check if the showBanner method emits true.
+    blocTest<InternetConnectionBannerCubit, bool>(
+      'emits [true] when showBanner is called',
+      build: () => cubit,
+      act: (cubit) => cubit.showBanner(),
+      expect: () => [true],
+    );
 
-        await tester.pumpWidget(MaterialApp(
-          home: BlocProvider.value(
-            value: cubit,
-            child: OfflineBanner(
-              child: Container(),
-            ),
-          ),
-        ));
-
-        // Verify that the banner is initially displayed
-        expect(find.byType(Positioned), findsOneWidget);
-
-        // Change the mock stream to emit ConnectivityResult.wifi
-        connectivityStreamController.add(ConnectivityResult.wifi);
-
-        // Wait for the widget to rebuild
-        await tester.pump();
-
-        // Verify that the banner is reset on connectivity change
-        verify(cubit.resetBanner()).called(1);
-
-        // Close the stream controller
-        connectivityStreamController.close();
-      });
-    });
+    // Test to check if the resetBanner method emits true.
+    blocTest<InternetConnectionBannerCubit, bool>(
+      'emits [true] when resetBanner is called',
+      build: () => cubit,
+      act: (cubit) => cubit.resetBanner(),
+      expect: () => [true],
+    );
   });
 }
