fafacc30daf112dd2f635f5bee2354e18d1957e6
FaithDaka <faithbusigu8@gmail.com>
2023-09-08 13:29:36 +0300

    Clean air page components

--- website/frontend/src/components/CleanAir/SecondaryNav/index.js
+++ website/frontend/src/components/CleanAir/SecondaryNav/index.js
@@ -1,29 +1,16 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import NavTab from '../../nav/NavTab';
 
-const SecondaryNav = () => {
+const SecondaryNavComponent = () => {
     return (
-        <div className="TopBar">
-            <div className="wrapper">
-                <div className="logo">
-                    <Link to='/clean-air'>
-                        <div style={{ border: '3px solid red' }}> CLEAN-Air logo</div>
-                    </Link>
-                </div>
-                <div className="nav-center" id="nav-center">
-                    <div className="nav-wrapper">
-                        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
-                            <span>Partners</span>
-                            <span>Events</span>
-                            <span>Press</span>
-                            <NavTab text="Get involved" hideArrow colored />
-                        </div>
-                    </div>
-                </div>
+        <div>
+            <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                <span>About</span>
+                <span>Memberships</span>
+                <span>Press</span>
+                <span>For You</span>
             </div>
         </div>
     )
 }
 
-export default SecondaryNav
+export default SecondaryNavComponent
--- website/frontend/src/pages/CleanAir/Page.js
+++ website/frontend/src/pages/CleanAir/Page.js
@@ -2,11 +2,13 @@ import React from 'react'
 import Footer from 'components/Footer';
 import TopBar from 'components/nav/TopBar';
 import BottomCTAs from 'components/CleanAir/CTAs/BottomCTAs';
+import HeaderComponent from 'components/CleanAir/HomePage/Header';
 
 const CleanAirPageContainer = ({ children }) => {
     return (
         <div className='Page Clean-air-page'>
             <TopBar />
+            <HeaderComponent/>
             <div className='page-wrapper page-container'>
                 {children}
             </div>
