3fcb335c3d91d1d105d278830543ba2914b86996
Ochieng Paul <ochiengpaul442@gmail.com>
2023-10-03 15:25:05 +0300

    included tests

--- netmanager/src/__tests__/DataDisplay/HostRegistry.test.js
+++ netmanager/src/__tests__/DataDisplay/HostRegistry.test.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import { render, fireEvent } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, combineReducers } from 'redux'; // Import combineReducers
 import { BrowserRouter as Router } from 'react-router-dom';
 import HostView from '../../views/components/Hosts/HostView';
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles';
 
 const theme = createMuiTheme();
-const store = createStore(() => ({}));
+
+// Create a rootReducer that includes the necessary reducers
+const rootReducer = combineReducers({
+  // Include the necessary reducers here, for example:
+  siteRegistry: (state = { sites: [] }, action) => state
+  // Add other reducers as needed
+});
+
+const store = createStore(rootReducer); // Use the rootReducer
 
 // Mock localStorage
 Storage.prototype.getItem = jest.fn(() => JSON.stringify({ net_name: 'test' }));
