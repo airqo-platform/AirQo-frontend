72aa80b0099aeef2c8e79ace2e6c94cf4a39f3dc
Katende Jericho <katendejericho5@gmail.com>
2023-10-04 18:01:24 +0300

    added flags

--- mobile/lib/models/language.dart
+++ mobile/lib/models/language.dart
@@ -1,16 +1,17 @@
 class Language {
   final int id;
+  final String flagEmoji; 
+
   final String name;
   final String languageCode;
-  final String flagEmoji; // Change the property name to flagEmoji
 
-  Language(this.id, this.name, this.languageCode, this.flagEmoji);
+  Language(this.id, this.flagEmoji, this.name, this.languageCode);
 
   static List<Language> languageList() {
     return <Language>[
-      Language(1, "🇺🇸","English", "en"), 
-      Language(2,  "🇫🇷","French", "fr"),  
-      Language(3,"🇵🇹", "Portuguese", "pt"), 
+      Language(1, "🇺🇸", "English", "en"),
+      Language(2, "🇫🇷", "French", "fr"),
+      Language(3, "🇵🇹", "Portuguese", "pt"),
     ];
   }
 }
---
+++ mobile/lib/screens/settings/language_switch.dart
@@ -0,0 +1,74 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:app/screens/offline_banner.dart';
+import 'package:app/widgets/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+import '../../constants/language_contants.dart';
+import '../../main_common.dart';
+import '../../models/language.dart';
+
+class LanguageList extends StatefulWidget {
+  const LanguageList({Key? key}) : super(key: key);
+
+  @override
+  LanguageListState createState() => LanguageListState();
+}
+
+class LanguageListState extends State<LanguageList> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return OfflineBanner(
+      child: Scaffold(
+        appBar: AppTopBar(
+          AppLocalizations.of(context)!.about,
+        ),
+        body: AppSafeArea(
+          child: Center(
+            child: ListView.builder(
+              itemCount: Language.languageList().length,
+              itemBuilder: (context, index) {
+                final language = Language.languageList()[index];
+                return Column(
+                  children: [
+                    AnimatedPadding(
+                      duration: const Duration(milliseconds: 100),
+                      padding: const EdgeInsets.symmetric(horizontal: 8),
+                      curve: Curves.easeIn,
+                      child: Card(
+                        margin: EdgeInsets.zero,
+                        child: ListTile(
+                          leading: Text(
+                              language.flagEmoji), // Display the flag emoji
+                          title: Text(
+                              "${language.name}"), // Display the flag emoji
+                          onTap: () async {
+                            Locale locale =
+                                await setLocale(language.languageCode);
+                            AirQoApp.setLocale(context, locale);
+                            Navigator.pop(context);
+                          },
+                        ),
+                      ),
+                    ),
+                    // include a separator between the list items;
+
+                    // const Divider(
+                    //   height: 1,
+                    // ),
+                  ],
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
