a88e02875b9b7e088600f678f9986ee4cbe8b046
FaithDaka <faithbusigu8@gmail.com>
2023-10-12 21:50:15 +0300

    cta section translation

--- website/frontend/src/components/MiniHighlights.js
+++ website/frontend/src/components/MiniHighlights.js
@@ -1,47 +1,54 @@
 import React from 'react';
 import { useDispatch } from "react-redux";
 import { showGetInvolvedModal } from "reduxStore/GetInvolved/operations";
-import engineerImg from  'src/assets/img/highlights/engineer.png'
-import GoogleOrgIcon from  'src/assets/img/highlights/google-org.svg'
+import engineerImg from 'src/assets/img/highlights/engineer.png'
+import GoogleOrgIcon from 'src/assets/img/highlights/google-org.svg'
 import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
-const MainSection = () => (
-    <div className="highlight-main">
-        <img src={engineerImg} />
-        <div className="main-content">
-            <GoogleOrgIcon />
-            <div className="title">Google.org Leaders to Watch 2022</div>
-            <div className="body">From expanding equity in education to addressing environmental issues, this year’s Leaders to Watch are building a better future for everyone.</div>
-            <div className="link"><a href={"https://www.google.org/leaders-to-watch/#engineer-bainomugisha"} target="_blank">Learn more --></a></div>
-        </div>
-    </div>
-)
-
-const SubSection = () => {
-    const dispatch = useDispatch();
-    const showModal = () => dispatch(showGetInvolvedModal(true));
-    
+const MainSection = () => {
+    const { t } = useTranslation();
     return (
-        <div className="highlight-sub">
-            <div className="content-wrapper blue-bg">
-                <div className="title white-color">Explore our digital tools. Learn about the quality of air around
-                    you.
-                </div>
-                <div className="link white-color"><Link to="/explore-data"><span>Explore data --></span></Link></div>
-            </div>
-            <div className="content-wrapper light-blue-bg" onClick={showModal}>
-                <div className="title blue-color">Get involved. Learn about ways you can support our vision.</div>
-                <div className="link blue-color" onClick={showModal}>Get Involved --></div>
+        <div className="highlight-main">
+            <img src={engineerImg} />
+            <div className="main-content">
+                <GoogleOrgIcon />
+                <div className="title">{t("homepage.ctaSection.leaders.title")}</div>
+                <div className="body">{t("homepage.ctaSection.leaders.subText")}</div>
+                <div className="link"><a href={"https://www.google.org/leaders-to-watch/#engineer-bainomugisha"} target="_blank">{t("homepage.ctaSection.leaders.cta")} {'-->'}</a></div>
             </div>
         </div>
     )
 }
 
-const MiniHighlights = () => (
-    <div className="Highlight">
-        <MainSection />
-        <SubSection />
-    </div>
-)
+const SubSection = () => {
+    const dispatch = useDispatch();
+    const showModal = () => dispatch(showGetInvolvedModal(true));
+
+    const { t } = useTranslation();
+
+    return (
+        <div className="highlight-sub">
+            <div className="content-wrapper blue-bg">
+                <div className="title white-color">{t("homepage.ctaSection.explore.title")}
+                </div>
+                <div className="link white-color"><Link to="/explore-data"><span>{t("homepage.ctaSection.explore.cta")} {'-->'}</span></Link></div>
+            </div>
+            <div className="content-wrapper light-blue-bg" onClick={showModal}>
+                <div className="title blue-color">{t("homepage.ctaSection.involved.title")}</div>
+                <div className="link blue-color" onClick={showModal}>{t("homepage.ctaSection.involved.cta")}{'-->'}</div>
+            </div>
+        </div>
+    )
+}
+
+const MiniHighlights = () => {
+    return (
+        <div className="Highlight">
+            <MainSection />
+            <SubSection />
+        </div>
+    )
+}
 
 export default MiniHighlights;
