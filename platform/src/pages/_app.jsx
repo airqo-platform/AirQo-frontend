d8b36adce6530e80ccafa588a364ebafc6b25c76
Ochieng Paul <ochiengpaul442@gmail.com>
2023-10-16 00:14:37 +0300

    introduced redux persist
    To solve the issue of the redux store clearing (empty) when a page redirect happens or when a page is refreshed.

--- platform/src/pages/_app.jsx
+++ platform/src/pages/_app.jsx
@@ -1,12 +1,17 @@
 import '@/styles/global.scss';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
 import { wrapper } from '@/lib/store';
 
 export default function App({ Component, ...rest }) {
   const { store, props } = wrapper.useWrappedStore(rest);
+  const persistor = persistStore(store);
   return (
     <Provider store={store}>
-      <Component {...props.pageProps} />
+      <PersistGate loading={null} persistor={persistor}>
+        <Component {...props.pageProps} />
+      </PersistGate>
     </Provider>
   );
 }
