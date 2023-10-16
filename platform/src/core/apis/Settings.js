ec011b87e9be7cbad1a450e5f99145296a192787
Ochieng Paul <ochiengpaul442@gmail.com>
2023-10-16 13:35:24 +0300

    Update Settings.js

--- platform/src/core/apis/Settings.js
+++ platform/src/core/apis/Settings.js
@@ -1,8 +1,5 @@
 import { UPDATE_PWD_URL } from '../urls/authentication';
-import { NEXT_PUBLIC_AUTHORISATION } from '../../lib/envConstants';
 import axios from 'axios';
-
-axios.defaults.headers.common.Authorization = `JWT ${NEXT_PUBLIC_AUTHORISATION}`;
 
 export const updateUserPasswordApi = async (userId, tenant, userData) => {
   return await axios
