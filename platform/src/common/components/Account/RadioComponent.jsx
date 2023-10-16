9f321c0045c3d9a978feddd668388b9f3eb5241a
FaithDaka <faithbusigu8@gmail.com>
2023-10-16 16:01:52 +0300

    refactored radio component

--- platform/src/common/components/Account/RadioComponent.jsx
+++ platform/src/common/components/Account/RadioComponent.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React from 'react';
 
-const RadioComponent = ({text}) => {
+const RadioComponent = ({ text, width, bgColor, padding, border }) => {
   return (
-    <div className='bg-form-input p-4 lg:w-fit w-11/12'>
+    <div
+      className={`${bgColor ? bgColor : 'bg-white'} ${padding ? padding : 'p-7'} ${
+        width ? width : 'lg:w-fit w-11/12'
+      } border ${border ? border : 'border-radio-border'} rounded-lg`}>
       <div className='flex flex-row items-center'>
         <div className='flex items-center'>
-          <input type='radio' name={text} className='border-radio-input bg-form-input checked:bg-blue-900' />
+          <input
+            type='checkbox'
+            name={text}
+            className='border rounded-2xl default:border-radio-border h-5 w-5 default:bg-white checked:bg-blue-900 active:border-blue-900'
+          />
         </div>
         <div className='ml-2'>{text}</div>
       </div>
     </div>
   );
-}
+};
 
-export default RadioComponent
+export default RadioComponent;
