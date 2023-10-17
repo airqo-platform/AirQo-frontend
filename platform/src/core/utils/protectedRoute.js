a2110d81b5cf9a01d4ec1470c3f6885affe7b021
Ochieng Paul <ochiengpaul442@gmail.com>
2023-10-17 20:01:43 +0300

    included session timeout

--- platform/src/core/utils/protectedRoute.js
+++ platform/src/core/utils/protectedRoute.js
@@ -1,11 +1,26 @@
 import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { resetStore } from '@/lib/store/services/account/LoginSlice';
 
 export default function withAuth(Component) {
   return () => {
+    const dispatch = useDispatch();
     const router = useRouter();
     const userCredentials = useSelector((state) => state.login);
+
+    // session timeout after 30 minutes
+    useEffect(() => {
+      if (userCredentials.success) {
+        setTimeout(() => {
+          localStorage.clear();
+          dispatch(resetStore());
+          router.push('/account/login');
+        }, 1800000); // 30 minutes
+      }
+    }, [userCredentials]);
+
     useEffect(() => {
       if (!userCredentials.success) {
         router.push('/account/login');
