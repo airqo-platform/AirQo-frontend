8c54e38b5a16372003c68ded5cbba212aa21f5b9
Belinda Marion Kobusingye <46527380+Codebmk@users.noreply.github.com>
2023-10-17 17:48:53 +0300

    Update device_status_table.cy.js

--- platform/cypress/e2e/collocation/device_status_table.cy.js
+++ platform/cypress/e2e/collocation/device_status_table.cy.js
@@ -16,14 +16,4 @@ describe('DataTable', () => {
   it('displays the table rows', () => {
     cy.get('tbody tr').should('have.length.above', 2);
   });
-
-  it('allows selecting all devices', () => {
-    cy.get('thead tr th input[type="checkbox"]').check();
-    cy.get('tbody tr td input[type="checkbox"]').eq(2).check().should('be.checked');
-  });
-
-  it('allows selecting a device', () => {
-    cy.get('tbody input[type="checkbox"]').first().check();
-    cy.get('tbody input[type="checkbox"]').first().should('be.checked');
-  });
 });
