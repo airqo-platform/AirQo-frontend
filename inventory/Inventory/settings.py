01320d21832b87cf2cd217befa4fabd2ba0628db
OlukaGibson <gibsonoluka7@gmail.com>
2023-10-20 12:49:22 +0300

    docker update

--- inventory/Inventory/settings.py
+++ inventory/Inventory/settings.py
@@ -79,7 +79,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-        },
+        }, 
     },
 ]
 
