e48e78571c4e98fb0e072f1777948b6766b93542
Ochieng Paul <ochiengpaul442@gmail.com>
2023-10-25 03:50:51 +0300

    include tab buttons and created a tabbutton component

--- platform/src/pages/analytics/index.jsx
+++ platform/src/pages/analytics/index.jsx
@@ -4,13 +4,21 @@ import Tab from '@/components/Tabs/Tab';
 import withAuth from '@/core/utils/protectedRoute';
 import Layout from '@/components/Layout';
 import OverView from './tabs/OverView';
+import TabButtons from '@/components/Button/TabButtons';
+import CalendarIcon from '@/icons/calendar.svg';
+import SettingsIcon from '@/icons/settings.svg';
 
 const AuthenticatedHomePage = () => {
   const renderChildrenRight = () => {
     return [
       {
         label: 'Overview',
-        children: <div className='flex'>{/* code goes here */}</div>,
+        children: (
+          <div className='flex mb-2'>
+            <TabButtons Icon={CalendarIcon} btnText='Last 7 days' dropdown />
+            <TabButtons Icon={SettingsIcon} btnText='Customize' />
+          </div>
+        ),
       },
       {
         label: 'Explore',
