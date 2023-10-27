39e4c51c0b4e62a4fa3872cf26a8969a420133b7
FaithDaka <faithbusigu8@gmail.com>
2023-10-27 13:02:04 +0300

    get grids url config

---
+++ platform/src/core/apis/DeviceRegistry.js
@@ -0,0 +1,6 @@
+import axios from 'axios';
+import { GRIDS_URL } from '../urls/deviceRegistry';
+
+export const getAllGridLocationsApi = async () => {
+    await axios.get(`${GRIDS_URL}`).then(response => response.data)
+}
