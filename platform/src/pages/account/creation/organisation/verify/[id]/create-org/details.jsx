8340ac65d118039507f42a7e4110bb1841ee33b1
FaithDaka <faithbusigu8@gmail.com>
2023-10-27 13:51:03 +0300

    create and update organisation implementation

--- platform/src/pages/account/creation/organisation/verify/[id]/create-org/details.jsx
+++ platform/src/pages/account/creation/organisation/verify/[id]/create-org/details.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import AccountPageLayout from '@/components/Account/Layout';
 import ProgressComponent from '@/components/Account/ProgressComponent';
 import HintIcon from '@/icons/Actions/exclamation.svg';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import SearchIcon from '@/icons/Actions/search.svg';
 import Spinner from '@/components/Spinner';
 import { useRouter } from 'next/router';
@@ -12,6 +12,12 @@ import {
 } from '@/lib/store/services/account/CreationSlice';
 import Toast from '@/components/Toast';
 import Link from 'next/link';
+import LocationIcon from '@/icons/SideBar/Sites.svg';
+import CloseIcon from '@/icons/Actions/close.svg';
+import {
+  setSelectedLocations,
+  getAllGridLocations,
+} from '@/lib/store/services/deviceRegistry/GridsSlice';
 
 const CreateOrganisationDetailsPageOne = ({ handleComponentSwitch }) => {
   const router = useRouter();
@@ -176,9 +182,12 @@ const CreateOrganisationDetailsPageOne = ({ handleComponentSwitch }) => {
 
 const CreateOrganisationDetailsPageTwo = ({ handleComponentSwitch }) => {
   const dispatch = useDispatch();
+  const router = useRouter();
+  const { id } = router.query;
   const [orgIndustry, setOrgIndustry] = useState('');
   const [orgCountry, setOrgCountry] = useState('');
   const [loading, setLoading] = useState(false);
+  const gridLocationsData = useSelector((state) => state.grids.gridsLocations) || [];
   const industryList = [
     'Textiles',
     'Transport',
@@ -214,12 +223,29 @@ const CreateOrganisationDetailsPageTwo = ({ handleComponentSwitch }) => {
     'Australia',
   ];
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     setLoading(true);
     e.preventDefault();
+    dispatch(setOrgDetails({ orgIndustry, orgCountry }));
+    try {
+      const response = await updateOrganisationDetails(
+        { grp_industry: orgIndustry, grp_country: orgCountry },
+        id,
+      );
+    } catch (error) {
+      return error;
+    }
     setLoading(false);
     handleComponentSwitch();
   };
+
+  useEffect(() => {
+    if (gridLocationsData.length <= 0) {
+      dispatch(getAllGridLocations());
+      console.log('Grid locations', gridLocationsData);
+    }
+  }, [gridLocationsData]);
+
   return (
     <div className='sm:ml-3 lg:ml-1'>
       <ProgressComponent colorFirst={true} colorSecond={true} />
@@ -280,6 +306,97 @@ const CreateOrganisationDetailsPageTwo = ({ handleComponentSwitch }) => {
 };
 
 const CreateOrganisationDetailsPageThree = () => {
+  const dispatch = useDispatch();
+  const gridLocationsData = useSelector((state) => state.grids.gridsLocations) || [];
+  // const gridLocationsData = [
+  //   {
+  //     _id: '6527d02625739700178e1d8b',
+  //     name: 'hellosss',
+  //   },
+  //   {
+  //     _id: '60d058c8048305120d2d6165',
+  //     name: 'Nansana west ward, Wakiso',
+  //   },
+  //   {
+  //     _id: '60d058c8048305120d2d6160',
+  //     name: 'Nansana east ward, Wakiso',
+  //   },
+  //   {
+  //     _id: '60d058c8048305120d2d617e',
+  //     name: 'Kawempe Industrial, Kawempe',
+  //   },
+  //   {
+  //     _id: '61092bd2ea763ff8bb8e3894',
+  //     name: 'Kawempe',
+  //   },
+  //   {
+  //     _id: '60d058c8048305120d2d614f',
+  //     name: 'Rubaga, Kampala',
+  //   },
+  //   {
+  //     _id: '60d058c8048305120d2d6154',
+  //     name: 'Kasubi, Rubaga',
+  //   },
+  //   {
+  //     _id: '60d058c8048305120d2d6157',
+  //     name: 'Seguku, Makindye-Ssabagabo',
+  //   },
+  //   {
+  //     _id: '60d058c8048305120d2d615b',
+  //     name: 'Mutundwe, Makindye Ssabagabo',
+  //   },
+  //   {
+  //     _id: '60d058c8048305120d2d616c',
+  //     name: 'Kasenge, Wakiso',
+  //   },
+  // ];
+  const [location, setLocation] = useState(undefined);
+  const [inputSelect, setInputSelect] = useState(false);
+  const [locationArray, setLocationArray] = useState([]);
+  const [filteredLocations, setFilteredLocations] = useState(gridLocationsData);
+  const [loading, setLoading] = useState(false);
+
+  const handleLocationEntry = (e) => {
+    setInputSelect(false);
+    filterBySearch(e);
+    setLocation(e.target.value);
+  };
+
+  const filterBySearch = (e) => {
+    const query = e.target.value;
+    let locationList = [...gridLocationsData];
+    locationList = locationList.filter((location) => {
+      return location.name.toLowerCase().indexOf(query.toLowerCase()) !== -1;
+    });
+    setFilteredLocations(locationList);
+  };
+
+  const handleLocationSelect = (name) => {
+    locationArray.includes(name)
+      ? setLocationArray(locationArray.filter((location) => location !== name))
+      : setLocationArray((locations) => [...locations, name]);
+    setInputSelect(true);
+    setLocation('');
+  };
+
+  const removeLocation = (name) => {
+    setLocationArray(locationArray.filter((location) => location !== name));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+    dispatch(setSelectedLocations(locationArray));
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (gridLocationsData.length <= 0) {
+      dispatch(getAllGridLocations());
+      console.log('Grid locations', gridLocationsData);
+    }
+  }, [gridLocationsData]);
+
   return (
     <div className='sm:ml-3 lg:ml-1'>
       <ProgressComponent colorFirst={true} colorSecond={true} colorThird={true} />
@@ -287,7 +404,7 @@ const CreateOrganisationDetailsPageThree = () => {
         <h2 className='text-3xl text-black font-semibold w-full lg:w-10/12 md:mt-20 lg:mt-2'>
           Choose locations you are interested in
         </h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div className='mt-6'>
             <div className='lg:w-11/12 sm:w-full md:w-full'>
               <div className='text-sm'>Add Locations</div>
@@ -296,16 +413,80 @@ const CreateOrganisationDetailsPageThree = () => {
                   <SearchIcon />
                 </div>
                 <input
+                  onChange={(e) => {
+                    handleLocationEntry(e);
+                  }}
+                  value={location}
                   placeholder='Search locations'
                   className='input text-sm w-full h-12 rounded-lg bg-white border-l-0 rounded-l-none border-input-light-outline focus:border-input-light-outline'
                 />
               </div>
+              {location !== undefined && (
+                <div
+                  className={`bg-white max-h-48 overflow-y-scroll px-3 pt-2 pr-1 my-1 border border-input-light-outline rounded-md ${
+                    inputSelect ? 'hidden' : 'relative'
+                  }`}>
+                  {filteredLocations.length > 0 ? (
+                    filteredLocations.map((location) => (
+                      <div className='flex flex-row justify-start items-center mb-0.5 text-sm w-full'>
+                        <LocationIcon />
+                        <div
+                          key={location._id}
+                          className='text-sm ml-1 text-black hover:cursor-pointer'
+                          onClick={() => {
+                            handleLocationSelect(location.name);
+                          }}>
+                          {location.name}
+                        </div>
+                      </div>
+                    ))
+                  ) : (
+                    <span />
+                  )}
+                </div>
+              )}
               <div className='mt-1 text-xs text-grey-350'>Minimum of 4 locations</div>
             </div>
           </div>
+          {inputSelect && (
+            <div className='mt-4 flex flex-row flex-wrap'>
+              {locationArray.length > 0 ? (
+                locationArray.map((location) => (
+                  <div className='bg-green-150 flex flex-row items-center mr-2 px-2 py-1 rounded-xl mb-2'>
+                    <span className='text-sm text-blue-600 font-semibold mr-1'>{location}</span>
+                    <div onClick={() => removeLocation(location)} className='hover:cursor-pointer'>
+                      <CloseIcon style={{ margin: '0 3px' }} />
+                    </div>
+                  </div>
+                ))
+              ) : (
+                <></>
+              )}
+            </div>
+          )}
+          <div className='mt-6'>
+            {locationArray.length >= 4 ? (
+              <div className='w-full'>
+                <button
+                  type='submit'
+                  onClick={handleSubmit}
+                  className='w-full btn bg-blue-900 rounded-none text-sm outline-none border-none hover:bg-blue-950'>
+                  {loading ? <Spinner data-testid='spinner' width={25} height={25} /> : 'Continue'}
+                </button>
+              </div>
+            ) : (
+              <div className='w-full'>
+                <button
+                  type='submit'
+                  className='w-full btn btn-disabled bg-white rounded-none text-sm outline-none border-none'>
+                  Continue
+                </button>
+              </div>
+            )}
+          </div>
         </form>
-        <div className='flex flex-row items-center justify-end mt-6'>
-          <Link href='/analytics'>
+        <div className='flex flex-row items-center justify-end mt-4'>
+          <Link href='/account/creation/get-started'>
             <span className='text-sm text-blue-900 font-medium hover:cursor-pointer hover:text-blue-950'>
               Complete this later
             </span>
@@ -329,10 +510,10 @@ const CreateOrganisationDetails = () => {
 
   return (
     <AccountPageLayout childrenHeight={'lg:h-[500]'} childrenTop={'mt-8'}>
-      {nextComponent === 'pageOne' && (
+      {nextComponent === 'pageTwo' && (
         <CreateOrganisationDetailsPageOne handleComponentSwitch={() => handleSwitchTo('pageTwo')} />
       )}
-      {nextComponent === 'pageTwo' && (
+      {nextComponent === 'pageOne' && (
         <CreateOrganisationDetailsPageTwo
           handleComponentSwitch={() => handleSwitchTo('pageThree')}
         />
