fb49b09bc73c50ffc8415eb16e56b94d7dbccbf2
Samantha <gloriaigadube@gmail.com>
2023-11-02 18:52:52 +0300

    settings edit

--- inventory/Inventory/settings.py
+++ inventory/Inventory/settings.py
@@ -35,7 +35,7 @@ dotenv.load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", default=False, cast=bool)
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = ['*']
 
