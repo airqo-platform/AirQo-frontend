9de8a0c5a6430d38084b7d050f52a283be80c50e
Samantha <gloriaigadube@gmail.com>
2023-11-02 14:51:51 +0300

    settings edit

--- inventory/Inventory/settings.py
+++ inventory/Inventory/settings.py
@@ -90,12 +90,12 @@ WSGI_APPLICATION = 'Inventory.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE' == 'django.db.backends.postgresql',
-        'NAME' =  env("DB_NAME"),
-        'USER' = env("DB_USER"),
-        'PASSWORD' = env("DB_PASSWORD"),
-        'HOST' = env("DB_HOST"),
-        'PORT' = env("DB_PORT"),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':  os.getenv("DB_NAME"),
+        'USER': os.getenv("DB_USER"),
+        'PASSWORD': os.getenv("DB_PASSWORD"),
+        'HOST': os.getenv("DB_HOST"),
+        'PORT': os.getenv("DB_PORT"),
     }
 }
 
