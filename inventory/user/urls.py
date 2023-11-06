a61dab3b78895821cf8d01a835f4dbef1332dc65
Samantha <gloriaigadube@gmail.com>
2023-11-06 19:54:31 +0300

    edited urls.py users

--- inventory/user/urls.py
+++ inventory/user/urls.py
@@ -3,7 +3,7 @@ from . import views
 from .views import logout_view
 
 urlpatterns = [
-    path('',  views.register, name='register'),
+    path('/inventory',  views.register, name='register'),
     path('profile',  views.profile, name='profile'),
     path('logout/', logout_view, name='logout'),
     path('activate/<uidb64>/<token>',  views.activate, name='activate'),
