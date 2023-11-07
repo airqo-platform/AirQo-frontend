e9ca86a697ea7429c1586f2f3d34c3ec20215b72
Belinda Marion Kobusingye <46527380+Codebmk@users.noreply.github.com>
2023-11-07 11:45:26 +0300

    fixed grid responsiveness

--- platform/src/common/components/AQNumberCard/index.jsx
+++ platform/src/common/components/AQNumberCard/index.jsx
@@ -38,22 +38,27 @@ const AQNumberCard = ({ reading, location }) => {
   }
 
   return (
-    <div className='md:max-w-[249px] sm:max-w-full w-full h-[164.48px] bg-white rounded-lg border border-gray-200 flex-col justify-start items-center inline-flex overflow-hidden'>
+    <div className='min-w-max lg:max-w-[249px] sm:max-w-full h-[164.48px] bg-white rounded-lg border border-gray-200 flex-col justify-start items-center inline-flex overflow-hidden'>
       <div className='self-stretch w-full h-[68.48px] px-4 pt-3.5 pb-[10.48px] bg-white border-b border-b-gray-200 flex-col justify-start items-start flex'>
         <div className='self-stretch justify-between items-center inline-flex'>
           <div className='flex-col justify-start items-start inline-flex'>
-            <div className='text-gray-700 text-base font-medium leading-normal'>{location}</div>
+            <div
+              className='text-gray-700 text-base font-medium leading-normal overflow-hidden whitespace-nowrap overflow-ellipsis'
+              title={location}
+            >
+              {location.length > 7 ? location.slice(0, 7) + '...' : location}
+            </div>
             <div className='text-slate-400 text-sm font-medium leading-tight'>Daily Avg.</div>
           </div>
-          <div className='pl-1 pr-2 bg-red-50 rounded-full justify-start items-center gap-0.5 flex'>
+          {/* <div className='pl-1 pr-2 bg-red-50 rounded-full justify-start items-center gap-0.5 flex'>
             <div className='p-1 bg-red-50 rounded-[900px] justify-start items-center gap-1 flex'>
               <div className='w-3.5 h-3.5 relative' />
             </div>
             <div className='text-red-500 text-sm font-medium leading-tight'>32%</div>
-          </div>
+          </div> */}
         </div>
       </div>
-      <div className='self-stretch w-full pl-4 pr-5 py-4 bg-white justify-between items-center inline-flex'>
+      <div className='self-stretch w-full pl-4 pr-5 py-4 bg-white justify-between items-center inline-flex gap-4'>
         <div className='flex-col justify-start items-start gap-0.5 inline-flex'>
           <div className='flex-col justify-start items-start gap-0.5 flex'>
             <div className='justify-start items-center gap-0.5 inline-flex'>
@@ -65,7 +70,7 @@ const AQNumberCard = ({ reading, location }) => {
             <div className='text-gray-700 text-[28px] font-extrabold leading-7'>{reading}</div>
           </div>
         </div>
-        <div className='w-16 h-16 justify-center items-center flex'>
+        <div className='w-16 h-16  justify-center items-center flex'>
           {AirQualityIcon && <AirQualityIcon />}
         </div>
       </div>
--- platform/src/pages/analytics/tabs/OverView.jsx
+++ platform/src/pages/analytics/tabs/OverView.jsx
@@ -1,29 +1,50 @@
 import React from 'react';
 import ChartContainer from '@/components/Charts/ChartContainer';
 import AQNumberCard from '@/components/AQNumberCard';
+import BorderlessContentBox from '@/components/Layout/borderless_content_box';
 
 const OverView = () => {
   return (
-    <div className='px-3 lg:px-16 pb-3'>
-      <div className='mb-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5'>
-        <div className='grid-col-1'>
-          <AQNumberCard location='Jinja' reading={'230'} />
-        </div>
-        <div className='grid-col-1'>
-          <AQNumberCard location='Nairobi' reading={'44'} />
-        </div>
-        <div className='grid-col-1'>
-          <AQNumberCard location='Kishasha' reading={'7'} />
-        </div>
-        <div className='grid-col-1'>
-          <AQNumberCard location='London' reading={'89'} />
-        </div>
+    <BorderlessContentBox>
+      <div
+        className='mb-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5 grid-flow-col-dense'
+        style={{ gridAutoFlow: 'dense' }}
+      >
+        <AQNumberCard location='Jinja city council hq' reading={'230'} />
+        <AQNumberCard location='Nairobi' reading={'44'} />
+        <AQNumberCard location='Kishasha' reading={'7'} />
+        <AQNumberCard location='London' reading={'89'} />
       </div>
       <div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
         <ChartContainer chartType='line' chartTitle='Air quality over time' />
         <ChartContainer chartType='bar' chartTitle='Air quality over time' />
       </div>
-    </div>
+      <style jsx>{`
+        .grid {
+          display: grid;
+          grid-template-columns: repeat(auto-fit, minmax(auto, 1fr));
+          grid-gap: 1rem;
+        }
+
+        @media (min-width: 640px) {
+          .grid {
+            grid-template-columns: repeat(auto-fit, minmax(250px, 1fr));
+          }
+        }
+
+        @media (min-width: 768px) {
+          .grid {
+            grid-template-columns: repeat(auto-fit, minmax(240px, 1fr));
+          }
+        }
+
+        @media (min-width: 1024px) {
+          .grid {
+            grid-template-columns: repeat(auto-fit, minmax(190px, 1fr));
+          }
+        }
+      `}</style>
+    </BorderlessContentBox>
   );
 };
 
