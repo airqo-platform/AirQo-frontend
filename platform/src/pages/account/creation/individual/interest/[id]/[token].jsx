6d36fa97da9fc92c34faa16b495bc83f7a033662
FaithDaka <faithbusigu8@gmail.com>
2023-11-07 13:43:32 +0300

    create user-default after user verification

--- platform/src/pages/account/creation/individual/interest/[id]/[token].jsx
+++ platform/src/pages/account/creation/individual/interest/[id]/[token].jsx
@@ -4,8 +4,12 @@ import RadioComponent from '@/components/Account/RadioComponent';
 import { updateUserCreationDetails } from '@/core/apis/Account';
 import { useRouter } from 'next/router';
 import Toast from '@/components/Toast';
+import Spinner from '@/components/Spinner';
+import { postUserDefaults } from '@/lib/store/services/account/UserDefaultsSlice';
+import { useDispatch } from 'react-redux';
 
 const IndividualAccountInterest = () => {
+  const dispatch = useDispatch();
   const router = useRouter();
   const { id } = router.query;
 
@@ -26,8 +30,10 @@ const IndividualAccountInterest = () => {
     state: false,
     message: '',
   });
+  const [loading, setLoading] = useState(false);
 
   const handleUpdate = async () => {
+    setLoading(true);
     setUpdateError({
       state: false,
       message: '',
@@ -36,25 +42,42 @@ const IndividualAccountInterest = () => {
       industry: clickedButton,
       interest,
     });
+    const userDefaults = {
+      user: id,
+    };
     try {
-      await updateUserCreationDetails(userData, id);
-      router.push('/account/creation/get-started');
+      const response = await updateUserCreationDetails(userData, id);
+      if (!response.success) {
+        setUpdateError({
+          state: true,
+          message: response.message,
+        });
+      }
     } catch (error) {
-      setUpdateError({
-        state: true,
-        message: error.response.data.message,
-      });
-      return error;
+      throw error;
     }
+
+    try {
+      const response = await dispatch(postUserDefaults(userDefaults));
+      if (response.payload.success) {
+        router.push('/account/creation/get-started');
+      } else {
+        setUpdateError({
+          state: true,
+          message: response.payload.message,
+        });
+      }
+    } catch (error) {
+      throw error;
+    }
+    setLoading(false);
   };
 
   return (
     <AccountPageLayout childrenHeight={'lg:h-[580]'}>
       {updateError.state && <Toast type={'error'} timeout={5000} message={updateError.message} />}
       <div className='w-full'>
-        <h2 className='text-3xl text-black-700 font-medium'>
-          Help us understand your interest
-        </h2>
+        <h2 className='text-3xl text-black-700 font-medium'>Help us understand your interest</h2>
         <p className='text-xl text-black-700 font-normal mt-3'>
           We will help you get started based on your response
         </p>
@@ -101,9 +124,8 @@ const IndividualAccountInterest = () => {
         <div className='mt-10'>
           <div className='lg:w-1/3 mt-6 md:mt-0 md:w-full'>
             {clickedButton === '' && interest === null ? (
-              <button
-                className='w-full btn btn-disabled bg-white rounded-none text-sm outline-none border-none'>
-                Continue
+              <button className='w-full btn btn-disabled bg-white rounded-none text-sm outline-none border-none'>
+                {loading ? <Spinner data-testid='spinner' width={25} height={25} /> : 'Continue'}
               </button>
             ) : (
               <button
