fa82c4a34b5be65303f94d7ac017098b16b79d83
Belinda Marion Kobusingye <46527380+Codebmk@users.noreply.github.com>
2023-11-08 14:36:50 +0300

    refactored Button component & analytics tab buttons

--- platform/src/common/components/Button/index.jsx
+++ platform/src/common/components/Button/index.jsx
@@ -1,12 +1,49 @@
-const Button = ({ onClick, className, path, children, dataTestId, rest, disabled, type }) => {
+const Button = ({
+  onClick,
+  className,
+  path,
+  children,
+  dataTestId,
+  rest,
+  disabled,
+  type,
+  variant,
+  color,
+  bgColor,
+  Icon,
+}) => {
+  let buttonClass = `flex justify-center items-center px-4 py-3 rounded sm:gap-1 ${className}`;
+  let textColor = '';
+  let backgroundColor = '';
+  let border = '';
+
+  if (variant === 'filled') {
+    backgroundColor = 'bg-blue-600';
+    textColor = 'text-white';
+  } else if (variant === 'outlined') {
+    border = 'border b-secondary-neutral-light-100';
+    textColor = 'text-secondary-neutral-light-600';
+  } else if (variant === 'text') {
+    textColor = 'text-secondary-neutral-light-600';
+  }
+
+  if (bgColor) {
+    backgroundColor = bgColor;
+  }
+
+  if (color) {
+    textColor = color;
+  }
+
   if (path) {
     // if the button has an href property, it should be a link button
     return (
       <a
         href={path}
-        className={`flex justify-center items-center px-3 py-2 ${className}`}
+        className={`${buttonClass} ${textColor} ${backgroundColor} ${border}`}
         data-testid={dataTestId}
       >
+        {Icon && <Icon className={`${textColor || 'text-black-900'} w-4 h-4`} />}
         {children}
       </a>
     );
@@ -15,12 +52,13 @@ const Button = ({ onClick, className, path, children, dataTestId, rest, disabled
     return (
       <button
         onClick={onClick}
-        className={`flex justify-center items-center px-3 py-2 ${className}`}
+        className={`${buttonClass} ${textColor} ${backgroundColor} ${border}`}
         data-testid={dataTestId}
         type={type}
         {...rest}
         disabled={disabled}
       >
+        {Icon && <Icon className={`${textColor || 'text-black-900 w-4 h-4'}`} />}
         {children}
       </button>
     );
--- platform/src/pages/analytics/index.jsx
+++ platform/src/pages/analytics/index.jsx
@@ -6,16 +6,19 @@ import Layout from '@/components/Layout';
 import OverView from './tabs/OverView';
 import Explore from './tabs/Explore';
 import CustomCalendar from '@/components/Calendar/CustomCalendar';
-import TabButtons from '@/components/Button/TabButtons';
+import Button from '@/components/Button';
 import SettingsIcon from '@/icons/settings.svg';
+import DownloadIcon from '@/icons/Common/download.svg';
+import { useWindowSize } from '@/lib/windowSize';
 
 const AuthenticatedHomePage = () => {
+  const isMobile = useWindowSize().width < 500;
   const renderChildrenRight = () => {
     return [
       {
         label: 'Overview',
         children: (
-          <div className='flex space-x-3 mt-2 md:mt-0 lg:mt-0 mb-2'>
+          <div className='flex space-x-3 mb-2'>
             <CustomCalendar
               initialStartDate={new Date()}
               initialEndDate={new Date()}
@@ -24,26 +27,49 @@ const AuthenticatedHomePage = () => {
               className='left-[60px] md:right-0 lg:right-0'
               dropdown
             />
-            <TabButtons Icon={SettingsIcon} btnText='Customize' />
+            <Button variant='outlined' className='text-sm font-medium' Icon={SettingsIcon}>
+              Customize
+            </Button>
           </div>
         ),
       },
       {
         label: 'Explore',
-        children: <div className='flex'>{/* code goes here */}</div>,
+        children: (
+          <div className='flex space-x-3 mb-3'>
+            <Button className='text-sm font-medium capitalize' variant='outlined'>
+              Print
+            </Button>
+            <Button className='text-sm font-medium capitalize' variant='filled' Icon={DownloadIcon}>
+              Export
+            </Button>
+          </div>
+        ),
       },
     ];
   };
   return (
     <Layout topbarTitle={'Analytics'} noBorderBottom>
-      <Tabs childrenRight={renderChildrenRight()}>
-        <Tab label='Overview'>
-          <OverView />
-        </Tab>
-        <Tab label='Explore'>
-          <Explore />
-        </Tab>
-      </Tabs>
+      <div className='pt-2'>
+        <Tabs childrenRight={!isMobile && renderChildrenRight()}>
+          <Tab label='Overview'>
+            {isMobile && (
+              <div className='flex justify-end px-3 lg:px-16'>
+                {renderChildrenRight()[0].children}
+              </div>
+            )}
+            <OverView />
+          </Tab>
+          <Tab label='Explore'>
+            {isMobile && (
+              <div className='flex justify-end px-3 lg:px-16'>
+                {renderChildrenRight()[1].children}
+              </div>
+            )}
+            <Explore />
+          </Tab>
+        </Tabs>
+      </div>
     </Layout>
   );
 };
