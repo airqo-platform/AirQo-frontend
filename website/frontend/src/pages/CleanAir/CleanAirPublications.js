e9650d4f675b6e250a03a5156dca321bcdcf77e0
Ochieng Paul <ochiengpaul442@gmail.com>
2023-11-09 17:15:51 +0300

    Update CleanAirPublications.js

--- website/frontend/src/pages/CleanAir/CleanAirPublications.js
+++ website/frontend/src/pages/CleanAir/CleanAirPublications.js
@@ -76,74 +76,76 @@ const CleanAirPublications = () => {
         description="CLEAN-Air Africa Network is a network of African cities and partners committed to improving air quality and reducing carbon emissions through knowledge sharing and capacity building."
       />
 
-      <div className="partners">
-        <div className="partners-wrapper">
-          <div className="resources-container">
-            <div className="resource-menu">
-              <div className="title-wrapper">
-                <h1 className="resource-menu-title">
-                  RESOURCE
-                  <br />
-                  <span>CENTER</span>
-                </h1>
+      <div>
+        <div className="partners">
+          <div className="partners-wrapper" style={{ marginTop: '-30px' }}>
+            <div className="resources-container">
+              <div className="resource-menu">
+                <div className="title-wrapper">
+                  <h1 className="resource-menu-title">
+                    RESOURCE
+                    <br />
+                    <span>CENTER</span>
+                  </h1>
 
-                <div className="resource-menu-icon" onClick={handleToggle}>
-                  {toggle ? <CloseIcon fontSize="large" /> : <ListIcon fontSize="large" />}
+                  <div className="resource-menu-icon" onClick={handleToggle}>
+                    {toggle ? <CloseIcon fontSize="large" /> : <ListIcon fontSize="large" />}
+                  </div>
+                </div>
+                <div className="menu-wrapper" ref={menuRef}>
+                  <ul className="resource-menu-item">
+                    {resources.map((resource) => (
+                      <ResourceMenuItem
+                        key={resource}
+                        activeResource={activeResource}
+                        resource={resource}
+                        dispatch={dispatch}
+                      />
+                    ))}
+                  </ul>
                 </div>
               </div>
-              <div className="menu-wrapper" ref={menuRef}>
-                <ul className="resource-menu-item">
-                  {resources.map((resource) => (
-                    <ResourceMenuItem
-                      key={resource}
-                      activeResource={activeResource}
-                      resource={resource}
-                      dispatch={dispatch}
-                    />
-                  ))}
-                </ul>
-              </div>
-            </div>
 
-            <div className="resource-body">
-              {activeResource === 'toolkits' && (
-                <CardComponent
-                  title="Air Quality Management in Africa: A Guide for Practitioners"
-                  authors="In this example, all list items except the first one will have a top margin of 10px. You can adjust the value as needed to suit your design. This is a simple and effective way to apply styles to middle items in a list. Note that this will also apply the margin to the last item. If you want to exclude the last item as well, you"
-                  link="https://www.cleanairafrica.org/wp-content/uploads/2021/09/"
-                  linkTitle="Read Journal"
-                  downloadLink="https://www.cleanairafrica.org/wp-content/uploads/2021/09/Air-Quality-Management-in-Africa-A-Guide-for-Practitioners.pdf"
-                />
-              )}
-              {activeResource === 'technical reports' && (
-                <ReportComponent
-                  title="Air Quality Management in Africa: A Guide for Practitioners"
-                  authors="led by the Clean Air Africa Network"
-                  link="https://www.cleanairafrica.org/wp-content/uploads/2021/09/Air-Quality-Management-in-Africa-A-Guide-for-Practitioners.pdf"
-                  linkTitle="Read Journal"
-                  showSecondAuthor={false}
-                  resourceFile="https://www.cleanairafrica.org/wp-content/uploads/2021/09/Air-Quality-Management-in-Africa-A-Guide-for-Practitioners.pdf"
-                />
-              )}
-              {activeResource === 'workshop reports' && (
-                <ReportComponent
-                  title="How to Develop an Air Quality Management Plan for Your City"
-                  authors="Led by the Clean Air Africa Network"
-                  link="https://www.cleanairafrica.org/wp-content/uploads/2021/09/Air-Quality-Management-in-Africa-A-Guide-for-Practitioners.pdf"
-                  linkTitle="Read Journal"
-                  showSecondAuthor={false}
-                  resourceFile="https://www.cleanairafrica.org/wp-content/uploads/2021/09/Air-Quality-Management-in-Africa-A-Guide-for-Practitioners.pdf"
-                />
-              )}
-              {activeResource === 'research publications' && (
-                <CardComponent
-                  title="Nairobi Air Quality Management Plan (2019-2024)"
-                  authors="Nairobi City County"
-                  link="https://www.cleanairafrica.org/wp-content/uploads/2021/09/Air-Quality-Management-in-Africa-A-Guide-for-Practitioners.pdf"
-                  linkTitle="Read More"
-                  downloadLink={null}
-                />
-              )}
+              <div className="resource-body">
+                {activeResource === 'toolkits' && (
+                  <CardComponent
+                    title="Air Quality Management in Africa: A Guide for Practitioners"
+                    authors="In this example, all list items except the first one will have a top margin of 10px. You can adjust the value as needed to suit your design. This is a simple and effective way to apply styles to middle items in a list. Note that this will also apply the margin to the last item. If you want to exclude the last item as well, you"
+                    link="https://www.cleanairafrica.org/wp-content/uploads/2021/09/"
+                    linkTitle="Read Journal"
+                    downloadLink="https://www.cleanairafrica.org/wp-content/uploads/2021/09/Air-Quality-Management-in-Africa-A-Guide-for-Practitioners.pdf"
+                  />
+                )}
+                {activeResource === 'technical reports' && (
+                  <ReportComponent
+                    title="Air Quality Management in Africa: A Guide for Practitioners"
+                    authors="led by the Clean Air Africa Network"
+                    link="https://www.cleanairafrica.org/wp-content/uploads/2021/09/Air-Quality-Management-in-Africa-A-Guide-for-Practitioners.pdf"
+                    linkTitle="Read Journal"
+                    showSecondAuthor={false}
+                    resourceFile="https://www.cleanairafrica.org/wp-content/uploads/2021/09/Air-Quality-Management-in-Africa-A-Guide-for-Practitioners.pdf"
+                  />
+                )}
+                {activeResource === 'workshop reports' && (
+                  <ReportComponent
+                    title="How to Develop an Air Quality Management Plan for Your City"
+                    authors="Led by the Clean Air Africa Network"
+                    link="https://www.cleanairafrica.org/wp-content/uploads/2021/09/Air-Quality-Management-in-Africa-A-Guide-for-Practitioners.pdf"
+                    linkTitle="Read Journal"
+                    showSecondAuthor={false}
+                    resourceFile="https://www.cleanairafrica.org/wp-content/uploads/2021/09/Air-Quality-Management-in-Africa-A-Guide-for-Practitioners.pdf"
+                  />
+                )}
+                {activeResource === 'research publications' && (
+                  <CardComponent
+                    title="Nairobi Air Quality Management Plan (2019-2024)"
+                    authors="Nairobi City County"
+                    link="https://www.cleanairafrica.org/wp-content/uploads/2021/09/Air-Quality-Management-in-Africa-A-Guide-for-Practitioners.pdf"
+                    linkTitle="Read More"
+                    downloadLink={null}
+                  />
+                )}
+              </div>
             </div>
           </div>
         </div>
