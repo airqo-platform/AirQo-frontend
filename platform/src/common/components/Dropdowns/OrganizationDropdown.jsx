0949c74ead9c44a342419cdb43c9e8dceca1477a
Belinda Marion Kobusingye <46527380+Codebmk@users.noreply.github.com>
2023-11-15 14:45:49 +0300

    add checkmark for selected dropdown item

--- platform/src/common/components/Dropdowns/OrganizationDropdown.jsx
+++ platform/src/common/components/Dropdowns/OrganizationDropdown.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import CustomDropdown from './CustomDropdown';
-import DotMenuIcon from '@/icons/Actions/three-dots-menu.svg';
+import CheckIcon from '@/icons/tickIcon';
+import ChevronDownIcon from '@/icons/Common/chevron_down.svg';
 
 const splitNameIntoList = (fullName) => {
   // split by space or underscore
@@ -37,9 +38,9 @@ const OrganizationDropdown = () => {
     <CustomDropdown
       trigger={
         <button>
-          <div className='w-56 h-12 pl-2 pr-3 py-2 bg-white rounded border border-gray-200 justify-start items-center gap-3 inline-flex'>
+          <div className='w-56 h-12 pl-2 pr-3 py-2 bg-white rounded border border-gray-200 justify-between items-center inline-flex'>
             <div className='justify-start items-center gap-3 flex'>
-              <div className='w-8 h-8 py-1.5 bg-gray-50 rounded-full justify-center items-center flex'>
+              <div className='w-8 h-8 py-1.5 bg-gray-50 rounded-full justify-center items-center flex gap-3'>
                 <div className='w-8 text-center text-slate-500 text-sm font-medium uppercase leading-tight'>
                   {splitNameIntoList(selectedOrganization?.org_name).length > 1
                     ? splitNameIntoList(selectedOrganization?.org_name)[0][0] +
@@ -52,24 +53,34 @@ const OrganizationDropdown = () => {
                   className='text-slate-500 text-sm font-medium capitalize leading-tight'
                   title={selectedOrganization?.org_name}
                 >
-                  {selectedOrganization?.org_name && selectedOrganization?.org_name.length > 18
-                    ? selectedOrganization?.org_name.slice(0, 18) + '...'
+                  {selectedOrganization?.org_name && selectedOrganization?.org_name.length > 16
+                    ? selectedOrganization?.org_name.slice(0, 16) + '...'
                     : selectedOrganization?.org_name}
                 </div>
               </div>
             </div>
+            <ChevronDownIcon />
           </div>
         </button>
       }
       id='options'
-      dropStyle={{ top: '40px', right: '0', zIndex: 999, width: '224px' }}
+      dropStyle={{
+        top: '41px',
+        zIndex: 999,
+        width: '224px',
+        maxHeight: '320px',
+        overflowY: 'scroll',
+        overflowX: 'hidden',
+      }}
     >
       {organizations.map((format) => (
         <a
           key={format}
           href='#'
           onClick={() => handleDropdownSelect(format)}
-          className='w-80 h-11 px-3.5 py-2.5 justify-start items-center gap-2 inline-flex'
+          className={`w-full h-11 px-3.5 py-2.5 justify-between items-center inline-flex ${
+            selectedOrganization?.org_name === format?.org_name && 'bg-secondary-neutral-light-50'
+          }`}
         >
           <div className='grow shrink basis-0 h-6 justify-start items-center gap-2 flex'>
             <div className='w-8 h-8 py-1.5 bg-gray-50 rounded-full justify-center items-center flex'>
@@ -80,10 +91,11 @@ const OrganizationDropdown = () => {
                   : splitNameIntoList(format?.org_name)[0][0]}
               </div>
             </div>
-            <div className="text-gray-700 text-sm font-normal font-['Inter'] leading-tight capitalize">
+            <div className='max-w-[120px] w-full text-gray-700 text-sm font-normal leading-tight capitalize'>
               {format.org_name}
             </div>
           </div>
+          {selectedOrganization?.org_name === format?.org_name && <CheckIcon fill='#145FFF' />}
         </a>
       ))}
     </CustomDropdown>
