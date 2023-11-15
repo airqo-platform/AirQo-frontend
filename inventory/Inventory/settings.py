569e45a9439e3e1d86438d86b7ef1072c91aa61c
OlukaGibson <gibsonoluka7@gmail.com>
2023-11-15 11:26:47 +0300

    Edits complete

--- inventory/Inventory/settings.py
+++ inventory/Inventory/settings.py
@@ -33,7 +33,8 @@ dotenv.load_dotenv()
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.getenv("SECRET_KEY")
-
+ 
+ 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
