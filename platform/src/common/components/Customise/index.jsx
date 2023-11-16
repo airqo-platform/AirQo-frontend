f2b6e288f0f2cc8cb612e2f097dba32ac27b1b86
FaithDaka <faithbusigu8@gmail.com>
2023-11-16 10:18:15 +0300

    submit only 4 locations

--- platform/src/common/components/Customise/index.jsx
+++ platform/src/common/components/Customise/index.jsx
@@ -1,9 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import CloseIcon from '@/icons/Actions/close.svg';
 import LocationsContentComponent from './LocationsContentComponent';
 import { useDispatch, useSelector } from 'react-redux';
 import Spinner from '@/components/Spinner';
-import { updateUserPreferences } from '@/lib/store/services/account/UserDefaultsSlice';
+import {
+  updateUserPreferences,
+  getIndividualUserPreferences,
+} from '@/lib/store/services/account/UserDefaultsSlice';
 import Toast from '@/components/Toast';
 
 const CustomiseLocationsComponent = ({ toggleCustomise }) => {
@@ -14,7 +17,10 @@ const CustomiseLocationsComponent = ({ toggleCustomise }) => {
     state: false,
     message: '',
   });
-  const selectedLocations = useSelector((state) => state.grids.selectedLocations) || [];
+  const selectedLocations = useSelector((state) => state.grids.selectedLocations);
+  const customisedLocations = useSelector(
+    (state) => state.defaults.individual_preferences.preferences[0].selected_sites,
+  );
   const id = useSelector((state) => state.login.userInfo._id);
 
   const handleSelectedTab = (tab) => {
@@ -23,26 +29,41 @@ const CustomiseLocationsComponent = ({ toggleCustomise }) => {
 
   const handleSubmit = async () => {
     setLoading(true);
-    const data = {
-      user_id: id,
-      selected_sites: selectedLocations,
-    };
-    try {
-      const response = await dispatch(updateUserPreferences(data));
-      if (!response.payload.success) {
-        setCreationErrors({
-          state: true,
-          message: response.payload.message,
-        });
-        setLoading(false);
-      } else {
-        toggleCustomise();
+    setCreationErrors({
+      state: false,
+      message:''
+    });
+    if (selectedLocations.length > 4) {
+      setCreationErrors({
+        state: true,
+        message: 'Please star only 4 locations',
+      });
+    } else {
+      const data = {
+        user_id: id,
+        selected_sites: selectedLocations,
+      };
+      try {
+        const response = await dispatch(updateUserPreferences(data));
+        if (!response.payload.success) {
+          setCreationErrors({
+            state: true,
+            message: response.payload.message,
+          });
+          setLoading(false);
+        } else {
+          toggleCustomise();
+        }
+      } catch (error) {
+        throw error;
       }
-    } catch (error) {
-      throw error;
     }
     setLoading(false);
   };
+
+  useEffect(() => {
+    dispatch(getIndividualUserPreferences(id));
+  }, []);
 
   return (
     <div>
@@ -50,7 +71,7 @@ const CustomiseLocationsComponent = ({ toggleCustomise }) => {
         <Toast type={'error'} timeout={6000} message={creationErrors.message} />
       )}
       <div
-        className='absolute right-0 top-0 w-full lg:w-3/12 h-full overflow-y-scroll bg-white z-20 border-l-grey-50 px-6'
+        className='absolute right-0 top-0 w-full lg:w-4/12 h-full overflow-y-scroll bg-white z-20 border-l-grey-50 px-6'
         style={{ boxShadow: '0px 16px 32px 0px rgba(83, 106, 135, 0.20)' }}>
         <div className='flex flex-row justify-between items-center mt-6'>
           <h3 className='text-xl text-black-800 font-semibold'>Customise</h3>
@@ -83,17 +104,18 @@ const CustomiseLocationsComponent = ({ toggleCustomise }) => {
             </div>
           </div>
         </div>
-        {selectedTab === 'locations' && <LocationsContentComponent />}
+        {selectedTab === 'locations' && (
+          <LocationsContentComponent selectedLocations={customisedLocations} />
+        )}
         {/* TODO: Pollutant component and post selection to user defaults */}
       </div>
-      <div className='absolute w-full lg:w-3/12 bg-white z-30 bottom-0 right-0 border-t border-input-light-outline py-4 px-6'>
+      <div className='absolute w-full lg:w-4/12 bg-white z-30 bottom-0 right-0 border-t border-input-light-outline py-4 px-6'>
         <div className='flex flex-row justify-end items-center'>
           <button
             className='btn bg-white mr-3 border border-input-light-outline text-sm text-secondary-neutral-light-800 font-medium py-3 px-4 rounded-lg hover:bg-white hover:border-input-light-outline'
             onClick={() => toggleCustomise()}>
             Cancel
           </button>
-          {/* TODO: Update user preferences onclick */}
           {selectedLocations.length >= 4 ? (
             <button
               className='btn bg-blue-900 text-sm border-none text-white font-medium py-3 px-4 rounded-lg hover:bg-primary-600'
@@ -101,7 +123,7 @@ const CustomiseLocationsComponent = ({ toggleCustomise }) => {
               {loading ? <Spinner data-testid='spinner' width={25} height={25} /> : 'Apply'}
             </button>
           ) : (
-            <button className='btn btn-disabled bg-white text-sm text-white font-medium py-3 px-4 rounded-lg'>
+            <button className='btn btn-disabled bg-white mr-3 border border-input-light-outline text-sm text-secondary-neutral-light-800 font-medium py-3 px-4 rounded-lg'>
               Apply
             </button>
           )}
