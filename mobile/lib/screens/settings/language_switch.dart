7fa3c50050fdb5f66dc5f29fd335f602f1bb016b
Katende Jericho <katendejericho5@gmail.com>
2023-11-20 17:17:28 +0300

    merge fix

--- mobile/lib/screens/settings/language_switch.dart
+++ mobile/lib/screens/settings/language_switch.dart
@@ -8,13 +8,12 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
-import '../../constants/language_contants.dart';
 import '../../themes/colors.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 // import 'package:restart_app/restart_app.dart';
 
 class LanguageList extends StatefulWidget {
-  const LanguageList({Key? key}) : super(key: key);
+  const LanguageList({super.key});
 
   @override
   LanguageListState createState() => LanguageListState();
