9e360c4d5aca7611ba26bd9f1c2136ead663ac1a
Ochieng Paul <ochiengpaul442@gmail.com>
2023-12-08 18:01:10 +0300

    route change for heatmap

--- netmanager/src/views/layouts/Landing.js
+++ netmanager/src/views/layouts/Landing.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import MapContainer from '../pages/Map/OverlayMap';
+import MapContainer from '../pages/Heatmap/HeatMapOverlay';
 import ErrorBoundary from 'views/ErrorBoundary/ErrorBoundary';
 import Main from './Main';
 
