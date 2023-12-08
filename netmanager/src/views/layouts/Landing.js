210ea8bc6726a0bdc3a28b51efc1391d4b24bad2
Ochieng Paul <ochiengpaul442@gmail.com>
2023-12-08 18:42:00 +0300

    Update Landing.js

--- netmanager/src/views/layouts/Landing.js
+++ netmanager/src/views/layouts/Landing.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import MapContainer from '../pages/Heatmap/HeatMapOverlay';
+import MapContainer from '../pages/Map/OverlayMap';
 import ErrorBoundary from 'views/ErrorBoundary/ErrorBoundary';
 import Main from './Main';
 
