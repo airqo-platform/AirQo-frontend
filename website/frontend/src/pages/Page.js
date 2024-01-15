3272a1fe5f123af4184e49428bca408eb96439ad
Ochieng Paul <ochiengpaul442@gmail.com>
2024-01-15 23:15:31 +0300

    worked on Language switcher component

--- website/frontend/src/pages/Page.js
+++ website/frontend/src/pages/Page.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import NotificationBanner from 'src/components/NotificationBanner';
+import LanguageSwitcher from 'src/components/LanguageSwitcher';
 import Footer from 'src/components/Footer';
 import TopBar from 'src/components/nav/TopBar';
-import NewsletterSection from "src/components/NewsletterSection/NewsletterSection";
-import GetInvolvedModal from "src/components/GetInvolvedModal";
+import NewsletterSection from 'src/components/NewsletterSection/NewsletterSection';
+import GetInvolvedModal from 'src/components/GetInvolvedModal';
 import MiniHighlights from '../components/MiniHighlights';
 
 const Page = ({ children }) => (
-    <div className='Page'>
-        {/* <NotificationBanner /> */}
-        <TopBar />
-        <div className='page-wrapper'>
-            {children}
-        </div>
-        <MiniHighlights />
-        <NewsletterSection />
-        <Footer />
-        <GetInvolvedModal />
-    </div>
+  <div className="Page">
+    {/* <NotificationBanner /> */}
+    <LanguageSwitcher />
+    <TopBar />
+    <div className="page-wrapper">{children}</div>
+    <MiniHighlights />
+    <NewsletterSection />
+    <Footer />
+    <GetInvolvedModal />
+  </div>
 );
 
 export default Page;
