f146f0344e244b247ce244e5e8ff97ccd71c5d56
Belinda Marion Kobusingye <46527380+Codebmk@users.noreply.github.com>
2024-02-13 19:44:29 +0300

    contact admin ui and functionality
    - created dialog modal wrapper

--- platform/src/pages/account/creation/organisation/verify/[id]/create-org/token-confirmation.jsx
+++ platform/src/pages/account/creation/organisation/verify/[id]/create-org/token-confirmation.jsx
@@ -7,16 +7,149 @@ import Spinner from '@/components/Spinner';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 import { verifyCohortID } from '@/core/apis/DeviceRegistry';
-import { useDispatch } from 'react-redux';
-import { addCohort } from '@/lib/store/services/deviceRegistry/CohortSlice';
+import { registerInquiry } from '@/core/apis/Inquiry';
+import DialogWrapper from '@/components/Modal/DialogWrapper';
+
+const ContactAdminModal = ({ open, onClose }) => {
+  const [fullName, setFullName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const isValidEmail = (email) => {
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (email && email.match(emailRegex)) {
+      return true;
+    }
+
+    return false;
+  };
+
+  const clearForm = () => {
+    setFullName('');
+    setEmail('');
+    setMessage('');
+  };
+
+  const handleContactAdmin = async () => {
+    if (fullName === '' || email === '' || message === '') return;
+
+    try {
+      setLoading(true);
+      const response = await registerInquiry({
+        fullName,
+        email,
+        message,
+        category: 'assistance',
+      });
+      if (response.success) {
+        setSuccessMessage('Your inquiry has been sent!');
+
+        setTimeout(() => {
+          setSuccessMessage('');
+          clearForm();
+          onClose();
+        }, 3000);
+      }
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      setErrorMessage(
+        error && error.response && error.response.data && error.response.data.message,
+      );
+      setTimeout(() => {
+        setErrorMessage('');
+      }, 5000);
+    }
+  };
+
+  return (
+    <DialogWrapper
+      handleClick={() => handleContactAdmin()}
+      open={open}
+      onClose={onClose}
+      loading={loading}
+    >
+      <div>
+        {successMessage && (
+          <div className='flex flex-row items-start text-xs text-green-600 bg-green-50 py-2 px-2 rounded'>
+            <span>{successMessage}</span>
+          </div>
+        )}
+        {errorMessage && (
+          <div className='flex flex-row items-start text-xs text-red-600 py-2'>
+            <HintIcon className='w-8 h-8 mr-2' />
+            <span>{errorMessage}</span>
+          </div>
+        )}
+        <div className='flex justify-between items-center'>
+          <h2 className='self-stretch text-gray-700 text-lg font-medium leading-relaxed'>
+            Contact Admin Support
+          </h2>
+        </div>
+        <div className='mt-4'>
+          <div className='w-full'>
+            <div className='text-sm text-secondary-neutral-light-600'>Full Name</div>
+            <div className='mt-2 w-full'>
+              <input
+                onChange={(e) => setFullName(e.target.value)}
+                type='text'
+                placeholder='Enter your full name'
+                className={`input w-full p-3 rounded-[4px] border-gray-300 focus:outline-none focus:ring-0 placeholder-gray-300 focus:border-green-500`}
+                required
+              />
+            </div>
+          </div>
+          <div className='mt-4'>
+            <div className='w-full'>
+              <div className='text-sm text-secondary-neutral-light-600'>Email</div>
+              <div className='mt-2 w-full'>
+                <input
+                  onChange={(e) => setEmail(e.target.value)}
+                  type='email'
+                  placeholder='Enter your email'
+                  className={`input w-full p-3 rounded-[4px] border-gray-300 focus:outline-none focus:ring-0 placeholder-gray-300 focus:border-green-500`}
+                  required
+                />
+              </div>
+              {email && email.length > 3 && !isValidEmail(email) && (
+                <div className='flex flex-row items-start text-xs text-red-600 py-2'>
+                  <HintIcon className='w-8 h-8' />
+                  <span>Please provide a valid email address!</span>
+                </div>
+              )}
+            </div>
+          </div>
+          <div className='mt-4'>
+            <div className='w-full'>
+              <div className='text-sm text-secondary-neutral-light-600'>Message</div>
+              <div className='mt-2 w-full'>
+                <textarea
+                  onChange={(e) => setMessage(e.target.value)}
+                  rows='3'
+                  placeholder='Enter your message'
+                  className={`textarea textarea-lg w-full p-3 rounded-[4px] border-gray-300 focus:outline-none focus:ring-0 placeholder-gray-300 focus:border-green-500`}
+                  required
+                ></textarea>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </DialogWrapper>
+  );
+};
 
 const ConfirmOrganizationCohortToken = () => {
   const router = useRouter();
-  const dispatch = useDispatch();
   const { id } = router.query;
   const [token, setToken] = useState('');
   const [tokenErrorMsg, setTokenErrorMsg] = useState('');
   const [loading, setLoading] = useState(false);
+  const [modalOpen, setModalOpen] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -108,10 +241,12 @@ const ConfirmOrganizationCohortToken = () => {
           <div className='mt-8 w-full flex justify-start'>
             <div className='flex flex-col gap-3'>
               <div className='text-sm text-blue-960 font-medium'>Don't have a token?</div>
-              {/* <div className='text-sm text-blue-900'>
+              <div className='text-sm text-blue-900'>
                 {' '}
-                <Link href='/account/login'>Contact admin</Link>
-              </div> */}
+                <button type='button' onClick={() => setModalOpen(true)}>
+                  Contact admin
+                </button>
+              </div>
               <div className='text-sm text-blue-900'>
                 {' '}
                 <Link href='/account/login'>Proceed as an individual</Link>
@@ -120,6 +255,7 @@ const ConfirmOrganizationCohortToken = () => {
           </div>
         </form>
       </div>
+      {modalOpen && <ContactAdminModal open={modalOpen} onClose={() => setModalOpen(false)} />}
     </AccountPageLayout>
   );
 };
