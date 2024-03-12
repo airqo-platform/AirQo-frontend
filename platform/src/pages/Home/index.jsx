7bbcbde3766505fb676438b880eb1905cfbed566
Ochieng Paul <ochiengpaul442@gmail.com>
2024-03-12 15:13:18 +0300

    Update index.jsx

--- platform/src/pages/Home/index.jsx
+++ platform/src/pages/Home/index.jsx
@@ -23,16 +23,11 @@ const Home = () => {
   const [open, setOpen] = useState(false);
 
   const [step, setStep] = useState(0);
-  const totalSteps = 4;
 
   useEffect(() => {
     const completedCards = cardCheckList.filter((card) => card.completed === true);
-    setStep(completedCards.length);
+    setStep(completedCards.length - 1);
   }, [cardCheckList]);
-
-  useEffect(() => {
-    dispatch(completeTask(1));
-  }, []);
 
   const handleModel = () => {
     setOpen(!open);
@@ -71,12 +66,12 @@ const Home = () => {
   };
 
   const steps = [
-    {
-      label: 'Introduction AirQo Analytics demo video',
-      time: '1 min',
-      link: '#',
-      func: () => handleModel(),
-    },
+    // {
+    //   label: 'Introduction AirQo Analytics demo video',
+    //   time: '1 min',
+    //   link: '#',
+    //   func: () => handleModel(),
+    // },
     {
       label: 'Choose location you most interested in',
       time: '2 min',
@@ -118,16 +113,12 @@ const Home = () => {
                 </p>
               </div>
               <div className='w-full'>
-                <StepProgress step={step} totalSteps={totalSteps} />
+                <StepProgress step={step} totalSteps={steps.length} />
               </div>
             </div>
 
             <div className='w-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5'>
               {steps.map((step, index) => {
-                if (index === 0) {
-                  return null; // Skip displaying the card for the first step
-                }
-
                 const card = cardCheckList.find((card) => card.id === index + 1);
 
                 const statusText = card && card.completed === true ? 'Done' : 'Start';
@@ -140,8 +131,7 @@ const Home = () => {
                   <div
                     key={index}
                     className='w-full h-[250px] flex flex-col justify-between items-start border-[0.5px] rounded-xl border-grey-150 py-5 px-3 space-y-5 focus:outline-blue-600 focus:ring-2 focus:shadow-lg focus:border-blue-600'
-                    tabIndex={0}
-                  >
+                    tabIndex={0}>
                     <div className='w-full'>
                       {card && card.completed === true ? (
                         <div className='w-14 h-14 flex justify-center items-center rounded-full bg-blue-900'>
@@ -150,8 +140,7 @@ const Home = () => {
                       ) : (
                         <div
                           className='text-base w-14 h-14 flex justify-center items-center font-medium rounded-full'
-                          style={{ background: '#F5F5FF' }}
-                        >
+                          style={{ background: '#F5F5FF' }}>
                           <span className='text-blue-600'>{index + 1}</span>
                         </div>
                       )}
@@ -167,8 +156,7 @@ const Home = () => {
                             <a
                               onClick={step.func}
                               className={statusColor}
-                              target={index === 3 ? '_blank' : ''}
-                            >
+                              target={index === 3 ? '_blank' : ''}>
                               {card && card.status === 'inProgress' ? 'Resume' : statusText}
                             </a>
                           </Link>
@@ -194,14 +182,12 @@ const Home = () => {
                   <Button
                     path='/analytics'
                     className='bg-blue-900 text-white rounded-lg w-32 h-12'
-                    dataTestId='get-started-button'
-                  >
+                    dataTestId='get-started-button'>
                     Start here
                   </Button>
                   <a
                     onClick={handleModel}
-                    className='text-blue-600 text-sm font-normal mt-2 cursor-pointer hidden'
-                  >
+                    className='text-blue-600 text-sm font-normal mt-2 cursor-pointer hidden'>
                     Show me how
                   </a>
                 </div>
@@ -210,12 +196,10 @@ const Home = () => {
                 className='rounded-md p-9 relative'
                 style={{
                   background: '#145DFF08',
-                }}
-              >
+                }}>
                 <div
                   onClick={handleModel}
-                  className='absolute z-50 inset-0 flex items-center justify-center cursor-pointer hidden'
-                >
+                  className='absolute z-50 inset-0 flex items-center justify-center cursor-pointer hidden'>
                   <PlayIcon />
                 </div>
                 <Image src={AnalyticsImage} alt='Analytics Image' width={600} height={350} />
