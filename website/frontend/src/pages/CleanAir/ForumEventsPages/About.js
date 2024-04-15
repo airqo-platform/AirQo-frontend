9c32ed759354e8151f5aa75813481a7f081a2500
Ochieng Paul <ochiengpaul442@gmail.com>
2024-04-15 17:46:42 +0300

    Update About.js and CleanAirPage.scss files in the CleanAir project

--- website/frontend/src/pages/CleanAir/ForumEventsPages/About.js
+++ website/frontend/src/pages/CleanAir/ForumEventsPages/About.js
@@ -19,24 +19,73 @@ const Index = ({ engagements, forumEvents }) => {
         </>
       )}
       {engagements && engagements.objectives.length > 0 && (
-        <section className="about">
-          <SplitTextSection
-            lists={[]}
-            content={
-              <div className="engagements_list">
-                {engagements.objectives.map((objective) => (
-                  <div key={objective.id}>
-                    <h3>{objective.title}</h3>
-                    <p>{objective.details}</p>
-                  </div>
-                ))}
-              </div>
-            }
-            title={<h2 className="section_title">{engagements.title}</h2>}
-            bgColor="#FFFFFF"
-          />
-        </section>
+        <>
+          <div className="separator" />
+          <section className="about">
+            <SplitTextSection
+              lists={[]}
+              content={
+                <div className="engagements_list">
+                  {engagements.objectives.map((objective) => (
+                    <div key={objective.id}>
+                      <h3>{objective.title}</h3>
+                      <p>{objective.details}</p>
+                    </div>
+                  ))}
+                </div>
+              }
+              title={<h2 className="section_title">{engagements.title}</h2>}
+              bgColor="#FFFFFF"
+            />
+          </section>
+        </>
       )}
+      <div className="separator" />
+      <section className="about">
+        <SplitTextSection
+          lists={[]}
+          content={
+            <div className="engagements_list">
+              This will be a largely in-person convening with limited hybrid sessions. You can{' '}
+              <a href="https://forms.gle/Bkx2iHM4gMRTLQ757" target="_blank">
+                register your interest here
+              </a>{' '}
+              and stay tuned for more details!{' '}
+              <b>
+                Attendance and participation is by invitation only, but there will be limited
+                opportunities for abstract submissions
+              </b>
+              . Details on call for abstracts will be shared separately.
+            </div>
+          }
+          title={<h2 className="section_title">Registration information</h2>}
+          bgColor="#FFFFFF"
+        />
+      </section>
+
+      <div className="separator" />
+      <section className="about">
+        <SplitTextSection
+          lists={[]}
+          content={
+            <div className="engagements_list">
+              <p style={{ paddingBottom: '15px' }}>
+                We have put together flexible sponsorship packages including travel scholarships,
+                session sponsorships, exhibitions, cocktail receptions and tailored options.
+                Sponsorship will help optimize the cost of hosting the forum, and more importantly,
+                support in-person participation of the target communities working for a clean air
+                future in Africa. We thank our funding partners for the continued funding support
+                and welcome more partners.
+              </p>
+              <p>
+                Please get in touch with <a>dokure@airqo.net</a> to discuss further details.
+              </p>
+            </div>
+          }
+          title={<h2 className="section_title">Sponsorship opportunities</h2>}
+          bgColor="#FFFFFF"
+        />
+      </section>
     </>
   );
 };
