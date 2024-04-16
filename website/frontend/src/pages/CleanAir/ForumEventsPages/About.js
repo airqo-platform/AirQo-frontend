d4d42027bbc8b167673030a8d2f3bfb31f27b665
Ochieng Paul <ochiengpaul442@gmail.com>
2024-04-17 01:47:26 +0300

    updates

--- website/frontend/src/pages/CleanAir/ForumEventsPages/About.js
+++ website/frontend/src/pages/CleanAir/ForumEventsPages/About.js
@@ -46,19 +46,28 @@ const Index = ({ engagements, forumEvents }) => {
           lists={[]}
           content={
             <div className="engagements_list">
-              This will be a largely in-person convening with limited hybrid sessions. You can{' '}
-              <a href="https://forms.gle/Bkx2iHM4gMRTLQ757" target="_blank">
-                register your interest here
-              </a>{' '}
-              and stay tuned for more details!{' '}
-              <b>
-                Attendance and participation is by invitation only, but there will be limited
-                opportunities for abstract submissions
-              </b>
-              . Details on call for abstracts will be shared separately.
+              <p style={{ marginBottom: '20px' }}>
+                This will be a largely in-person convening with limited hybrid sessions. You can{' '}
+                <a href="https://forms.gle/Bkx2iHM4gMRTLQ757" target="_blank">
+                  register your interest here
+                </a>{' '}
+                and stay tuned for more details!{' '}
+              </p>
+              <p style={{ marginBottom: '20px' }}>
+                <b>
+                  Attendance and participation is by invitation only, but there will be limited
+                  opportunities for abstract submissions
+                </b>
+              </p>
+              <p>Details on call for abstracts will be shared separately.</p>
             </div>
           }
-          title={<h2 className="section_title">Registration information</h2>}
+          title={
+            <h2 className="section_title">
+              Registration <br />
+              information
+            </h2>
+          }
           bgColor="#FFFFFF"
         />
       </section>
@@ -69,9 +78,11 @@ const Index = ({ engagements, forumEvents }) => {
           lists={[]}
           content={
             <div className="engagements_list">
-              <p style={{ paddingBottom: '15px' }}>
+              <p style={{ marginBottom: '20px' }}>
                 We have put together flexible sponsorship packages including travel scholarships,
                 session sponsorships, exhibitions, cocktail receptions and tailored options.
+              </p>
+              <p style={{ marginBottom: '20px' }}>
                 Sponsorship will help optimize the cost of hosting the forum, and more importantly,
                 support in-person participation of the target communities working for a clean air
                 future in Africa. We thank our funding partners for the continued funding support
@@ -83,7 +94,11 @@ const Index = ({ engagements, forumEvents }) => {
               </p>
             </div>
           }
-          title={<h2 className="section_title">Sponsorship opportunities</h2>}
+          title={
+            <h2 className="section_title">
+              Sponsorship <br /> opportunities
+            </h2>
+          }
           bgColor="#FFFFFF"
         />
       </section>
