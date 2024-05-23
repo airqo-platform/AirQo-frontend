1237372253fe3dd2f273f9c4b89bb711f0cbdfe0
Ochieng Paul <ochiengpaul442@gmail.com>
2024-05-23 22:16:58 +0300

    refactor: Improve lazy loading for images and handle PDF format restriction

--- platform/src/common/components/SideBar/SideBarItem.jsx
+++ platform/src/common/components/SideBar/SideBarItem.jsx
@@ -76,7 +76,7 @@ const SideBarItem = ({ Icon, label, dropdown, navPath, children, toggleMethod, t
   // get current route
   const currentRoute = router.pathname;
   // check if current route contains navPath
-  const isCurrentRoute = currentRoute.includes(navPath);
+  const isCurrentRoute = currentRoute === navPath || (navPath === '/Home' && currentRoute === '/');
 
   return (
     <div
