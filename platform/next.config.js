74ed4327c5cf35648b6a266f10554e9863ff7e15
Ochieng Paul <ochiengpaul442@gmail.com>
2024-07-16 12:02:55 +0300

    chore: Update Dockerfile to use yarn install with version 3.2.3 and clean yarn cache

--- platform/next.config.js
+++ platform/next.config.js
@@ -1,7 +1,11 @@
 const withVideos = require('next-videos');
+const PnpWebpackPlugin = require('pnp-webpack-plugin');
 
 module.exports = withVideos({
-  webpack(config, options) {
+  webpack: (config, options) => {
+    // Enables Webpack's support for Yarn PnP
+    PnpWebpackPlugin.setup();
+
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
