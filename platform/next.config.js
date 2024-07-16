b0a50d70877f593e1ee0ab7bc87de6c3d3888811
Ochieng Paul <ochiengpaul442@gmail.com>
2024-07-16 13:46:51 +0300

    chore: Update next.js to version 14.2.5 and optimize Dockerfile for yarn install

--- platform/next.config.js
+++ platform/next.config.js
@@ -1,23 +1,9 @@
 const withVideos = require('next-videos');
-const PnpWebpackPlugin = require('pnp-webpack-plugin');
 
 module.exports = withVideos({
-  webpack: (config, options) => {
-    // Enables Webpack's support for Yarn PnP
-    config.resolve = {
-      ...config.resolve,
-      alias: {
-        ...config.resolve.alias,
-        'pnpapi': require.resolve('pnpapi'),
-      },
-    };
-    config.resolveLoader = {
-      ...config.resolveLoader,
-      plugins: [
-        PnpWebpackPlugin.moduleLoader(module),
-      ],
-    };
-
+  // Adding support for Docker
+  output: 'standalone',
+  webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
