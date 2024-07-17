758b7c8fee552027a1a9b3a93a3bcdb42852e506
Ochieng Paul <ochiengpaul442@gmail.com>
2024-07-17 13:58:54 +0300

    Refactor SessionAndThemeProvider to use navigator.onLine directly for isOnline state

--- reports/src/app/SessionAndThemeProvider.tsx
+++ reports/src/app/SessionAndThemeProvider.tsx
@@ -1,10 +1,9 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
 import { useTheme } from "next-themes";
-import NetworkIssues from "./NetworkIssues";
 
 const SessionAndThemeProvider = ({
   children,
@@ -13,42 +12,14 @@ const SessionAndThemeProvider = ({
   children: React.ReactNode;
 } & ThemeProviderProps) => {
   const { theme } = useTheme();
-  const [isOnline, setIsOnline] = useState(navigator.onLine);
-  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
     document.body.classList.toggle("dark", theme === "dark");
   }, [theme]);
 
-  useEffect(() => {
-    const updateOnlineStatus = () => {
-      setIsOnline(navigator.onLine);
-    };
-
-    window.addEventListener("online", updateOnlineStatus);
-    window.addEventListener("offline", updateOnlineStatus);
-
-    return () => {
-      window.removeEventListener("online", updateOnlineStatus);
-      window.removeEventListener("offline", updateOnlineStatus);
-    };
-  }, []);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return null;
-
-  if (!isOnline) {
-    return <NetworkIssues />;
-  }
-
   return (
     <SessionProvider>
-      <>
-        <NextThemesProvider {...props}>{children}</NextThemesProvider>
-      </>
+      <NextThemesProvider {...props}>{children}</NextThemesProvider>
     </SessionProvider>
   );
 };
