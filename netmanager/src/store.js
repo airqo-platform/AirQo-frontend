66091ca4f8d55e5074df9b05184f50394f809c96
Daniel Ntege <danientege785@gmail.com>
2024-07-29 23:40:58 +0300

    commit

--- netmanager/src/store.js
+++ netmanager/src/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
-import rootReducer from "./reducer/index.js";
+import rootReducer from "./reducer";
 
 const initialState = {};
 const middleware = [thunk];
