251dc26ae9fa05ff09c20a57d85253bcd3c79e65
Daniel Ntege <danientege785@gmail.com>
2024-07-29 22:08:58 +0300

    commit

--- netmanager/webpack.prod.js
+++ netmanager/webpack.prod.js
@@ -1,11 +1,17 @@
 const { merge } = require("webpack-merge");
+const webpack = require("webpack");
 
 const dotenv = require("dotenv");
-dotenv.config({ path: `.env.production` });
-dotenv.config({ path: `.env.production.local`, override: true });
+dotenv.config({ path: `.env.` });
+dotenv.config({ path: `.env.production`, override: true });
 
 const common = require("./webpack.common.js");
 
 module.exports = merge(common, {
-    mode: "production"
+    mode: "production",
+    plugins: [
+        new webpack.DefinePlugin({
+          'process.env': JSON.stringify(process.env),
+        }),
+    ],
 });
