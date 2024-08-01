9de99e254de1e1ef96cda0169183ff33dfc0ad19
Daniel Ntege <danientege785@gmail.com>
2024-08-01 19:20:19 +0300

    Cloud Run and Deploy

--- netmanager/src/config/urls/dataManagement.js
+++ netmanager/src/config/urls/dataManagement.js
@@ -5,6 +5,6 @@ const BASE_DATA_MANAGEMENT_URL = stripTrailingSlash(
     process.env.REACT_APP_BASE_URL
 );
 
-export const ADD_MAINTENANCE_URI = `${BASE_DATA_MANAGEMENT_URL}/data/channels/maintenance/add`;
+export const ADD_MAINTENANCE_URI = `${BASE_DATA_MANAGEMENT_URL}data/channels/maintenance/add`;
 
-export const DEVICE_RECENT_FEEDS = `${BASE_DATA_MANAGEMENT_URL}/data/feeds/transform/recent`;
+export const DEVICE_RECENT_FEEDS = `${BASE_DATA_MANAGEMENT_URL}data/feeds/transform/recent`;
