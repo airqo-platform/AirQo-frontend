4d86fa738f4748f4d51d1a7cb73523d7e4418cad
Ochieng Paul <ochiengpaul442@gmail.com>
2024-09-09 16:09:27 +0300

    reversing recent changes

--- website/webpack.dev.config.js
+++ website/webpack.dev.config.js
@@ -6,17 +6,4 @@ config.output.publicPath = 'http://localhost:8081/static/frontend/';
 // Set the static public path to match what Django expects
 config.devServer.static.publicPath = '/static/';
 
-// Add HMR plugin
-const webpack = require('webpack');
-config.plugins.push(new webpack.HotModuleReplacementPlugin());
-
-// Optimize for development
-config.optimization = {
-  ...config.optimization,
-  minimize: false,
-  removeAvailableModules: false,
-  removeEmptyChunks: false,
-  splitChunks: false
-};
-
 module.exports = config;
--- website/webpack.fullapp.config.js
+++ website/webpack.fullapp.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const config = require('./webpack.config.js');
 
 // Update entrypoint
@@ -8,34 +9,8 @@ config.entry = './frontend/index.js';
 config.output.publicPath = '/';
 
 // Add HTMLWebpack plugin
-config.plugins.push(
-  new HtmlWebpackPlugin({
-    template: './frontend/standaloneIndex.html',
-    inject: 'body',
-    minify: {
-      removeComments: true,
-      collapseWhitespace: true
-    }
-  })
-);
-
-// Optimize for production
-const TerserPlugin = require('terser-webpack-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-
-config.optimization = {
-  ...config.optimization,
-  minimize: true,
-  minimizer: [
-    new TerserPlugin({
-      terserOptions: {
-        compress: {
-          drop_console: true
-        }
-      }
-    }),
-    new CssMinimizerPlugin()
-  ]
-};
+config.plugins = config.plugins.concat([
+  new HtmlWebpackPlugin({ template: './frontend/standaloneIndex.html' })
+]);
 
 module.exports = config;
