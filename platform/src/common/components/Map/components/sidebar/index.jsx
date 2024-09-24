15dee3f9cc235269ec9701ef818a95c1798a3298
Ochieng Paul <ochiengpaul442@gmail.com>
2024-09-24 13:40:01 +0300

    style changes and removed usestate

--- platform/src/common/components/Map/components/sidebar/index.jsx
+++ platform/src/common/components/Map/components/sidebar/index.jsx
@@ -67,7 +67,6 @@ const MapSidebar = ({ siteDetails, isAdmin }) => {
     (state) => state.locationSearch.searchTerm,
   );
   const suggestedSites = useSelector((state) => state.map.suggestedSites);
-  const selectedNode = useSelector((state) => state.map.selectedNode);
 
   const isSearchFocused = isFocused || reduxSearchTerm.length > 0;
 
