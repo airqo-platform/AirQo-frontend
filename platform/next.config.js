e2d964b0de5b7fd55e83048076c7be8a7ad6bd0e
Ochieng Paul <ochiengpaul442@gmail.com>
2024-09-27 13:28:39 +0300

    updated nex.config file

--- platform/next.config.js
+++ platform/next.config.js
@@ -1,16 +1,24 @@
+/* eslint-disable */
 const withVideos = require('next-videos');
 
 module.exports = withVideos({
+  // Image configuration: Add patterns here as necessary
   images: {
     remotePatterns: [],
   },
   reactStrictMode: true,
+
+  // Specify directories for ESLint linting
   eslint: {
     dirs: ['pages', 'components', 'lib', 'utils', 'hooks'],
   },
-  // Adding support for Docker
+
+  // Docker support (Standalone output for optimized Docker builds)
   output: 'standalone',
+
+  // Webpack custom configuration
   webpack(config) {
+    // Add rule for handling SVGs using @svgr/webpack
     config.module.rules.push({
       test: /\.svg$/,
       use: ['@svgr/webpack'],
