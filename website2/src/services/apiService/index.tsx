91bc0de584fc302586adfb9805fb43118c4b3cfb
Ochieng Paul <ochiengpaul442@gmail.com>
2024-11-14 11:22:22 +0300

    api service update

--- website2/src/services/apiService/index.tsx
+++ website2/src/services/apiService/index.tsx
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = (process.env.NEXT_PUBLIC_API_URL || '') + '/website';
+import { removeTrailingSlash } from '@/utils';
+
+const API_BASE_URL = `${removeTrailingSlash(process.env.NEXT_PUBLIC_API_URL || '')}/website`;
 
 // Axios instance to include any necessary headers
 const apiClient = axios.create({
