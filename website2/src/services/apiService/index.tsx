812639ee1253d315f4d6a67e488214650853dd93
Ochieng Paul <ochiengpaul442@gmail.com>
2024-11-14 11:08:01 +0300

    url update for the django service

--- website2/src/services/apiService/index.tsx
+++ website2/src/services/apiService/index.tsx
@@ -1,7 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL =
-  process.env.NEXT_PUBLIC_DJANGO_API_URL || 'http://127.0.0.1:8000/api';
+const API_BASE_URL = (process.env.NEXT_PUBLIC_API_URL || '') + '/website';
 
 // Axios instance to include any necessary headers
 const apiClient = axios.create({
