5202804abbf4a6c5cd9e18928093f9099af70273
Belinda Marion Kobusingye <46527380+Codebmk@users.noreply.github.com>
2024-12-04 16:47:09 +0300

    fix date time

--- platform/src/common/components/MaintenanceBanner/index.jsx
+++ platform/src/common/components/MaintenanceBanner/index.jsx
@@ -1,13 +1,9 @@
 import React from 'react';
 import { IoWarningOutline } from 'react-icons/io5';
-import { format } from 'date-fns';
 import dayjs from 'dayjs';
 
 const MaintenanceBanner = ({ maintenance }) => {
   if (!maintenance?.isActive) return null;
-
-  const startTime = format(new Date(maintenance.startDate), 'HH:mm');
-  const endTime = format(new Date(maintenance.endDate), 'HH:mm');
 
   return (
     <div className="bg-yellow-50 border-b border-yellow-200 mb-2">
@@ -24,8 +20,8 @@ const MaintenanceBanner = ({ maintenance }) => {
               {maintenance.message}
               <span className="ml-2 text-yellow-600">
                 (Estimated downtime:{' '}
-                {dayjs(startTime).format('DDD MM, YYYY HH:mm')} -{' '}
-                {dayjs(endTime).format('DDD MM, YYYY HH:mm')})
+                {dayjs(maintenance.startDate).format('DDD MM, YYYY HH:mm')} -{' '}
+                {dayjs(maintenance.endDate).format('DDD MM, YYYY HH:mm')})
               </span>
             </p>
           </div>
