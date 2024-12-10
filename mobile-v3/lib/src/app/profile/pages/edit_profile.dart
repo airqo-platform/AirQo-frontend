25a240031e4591d1f7069f9166a64e996dc7ab25
Peter Kyeyune <peterkayk@gmail.com>
2024-12-10 20:49:15 +0300

    Refactor EditProfile page UI

--- mobile-v3/lib/src/app/profile/pages/edit_profile.dart
+++ mobile-v3/lib/src/app/profile/pages/edit_profile.dart
@@ -1,14 +1,151 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
-class EditProfile extends StatelessWidget {
-  const EditProfile({super.key});
+class EditProfile extends StatefulWidget {
+  @override
+  _EditProfileState createState() => _EditProfileState();
+}
 
-  void something(){
-  
-  }
+class _EditProfileState extends State<EditProfile> {
+  final _firstNameController = TextEditingController(text: 'Nagawa');
+  final _lastNameController = TextEditingController(text: 'Alice');
+  final _emailController = TextEditingController(text: 'nagawaalice@gmail.com');
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    final screenWidth = MediaQuery.of(context).size.width;
+    final screenHeight = MediaQuery.of(context).size.height;
+
+    final avatarRadius = screenWidth * 0.15;
+    final padding = screenWidth * 0.05;
+    final iconSize = screenWidth * 0.07; 
+
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+        title: Center(
+          child: Text(
+            'Edit Profile',
+            style: TextStyle(fontSize: 20),
+          ),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {
+              
+            },
+            child: Text(
+              'Done',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 16.0,
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(padding),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Stack(
+                    children: [
+                      CircleAvatar(
+                        backgroundColor: Theme.of(context).highlightColor,
+                        radius: avatarRadius,
+                        child: SvgPicture.asset(
+                          'assets/icons/user_icon.svg',
+                          width: avatarRadius * 1.5, 
+                          height: avatarRadius * 1.5,
+                        ),
+                      ),
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            color: Colors.blue,
+                            shape: BoxShape.circle,
+                          ),
+                          padding: EdgeInsets.all(iconSize * 0.4),
+                          child: Icon(
+                            Icons.edit,
+                            color: Colors.white,
+                            size: iconSize,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(width: padding),
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                      vertical: screenHeight * 0.05,
+                    ),
+                    child: Expanded(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Edit your name, email and add',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: screenWidth * 0.035,
+                            ),
+                          ),
+                          SizedBox(height: screenHeight * 0.005),
+                          Text(
+                            'an optional profile picture',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: screenWidth * 0.035,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: screenHeight * 0.05),
+              TextField(
+                controller: _firstNameController,
+                decoration: InputDecoration(
+                  labelText: 'First Name',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: screenHeight * 0.03),
+              TextField(
+                controller: _lastNameController,
+                decoration: InputDecoration(
+                  labelText: 'Last Name',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+              SizedBox(height: screenHeight * 0.03),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
