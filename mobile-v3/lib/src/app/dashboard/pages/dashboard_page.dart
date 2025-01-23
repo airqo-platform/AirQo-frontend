a2586bb717569ebde9a230403aec7066aac65384
Peter Kyeyune <peterkayk@gmail.com>
2025-01-23 15:37:08 +0300

    Remove unused ProfilePage navigation code from DashboardPage

--- mobile-v3/lib/src/app/dashboard/pages/dashboard_page.dart
+++ mobile-v3/lib/src/app/dashboard/pages/dashboard_page.dart
@@ -13,7 +13,6 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_sticky_header/flutter_sticky_header.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:intl/intl.dart';
-import 'package:airqo/src/app/profile/pages/profile_page.dart';
 
 import '../models/airquality_response.dart';
 
@@ -97,13 +96,13 @@ class _DashboardPageState extends State<DashboardPage> {
               ),
               SizedBox(width: 8),
               GestureDetector(
-                onTap: () => Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return ProfilePage();
-                    },
-                  ),
-                ),
+                // onTap: () => Navigator.of(context).push(
+                //   MaterialPageRoute(
+                //     builder: (context) {
+                //       return ProfilePage();
+                //     },
+                //   ),
+                // ),
                 child: BlocBuilder<UserBloc, UserState>(
                   builder: (context, state) {
                     if (state is UserLoaded) {
