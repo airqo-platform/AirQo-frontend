7bc0bc354190c6adc0c1b547bfb93359fdb0fda5
Daniel Ntege <danientege785@gmail.com>
2025-01-24 11:55:57 +0300

    commit

--- netmanager-app/app/(authenticated)/clients/page.tsx
+++ netmanager-app/app/(authenticated)/clients/page.tsx
@@ -8,6 +8,7 @@ import { ActivateClientDialog, DeactivateClientDialog } from "./dialogs"
 import { getClientsApi, activateUserClientApi } from "@/core/apis/analytics"
 import { useToast } from "@/components/ui/use-toast"
 import type { Client } from "@/app/types/clients"
+import withPermission from "@/app/pageAccess"
 
 const formatDate = (dateString: string | undefined): string => {
   if (!dateString) return "N/A"
@@ -151,5 +152,5 @@ const ClientManagement = () => {
   )
 }
 
-export default ClientManagement
+export default withPermission(ClientManagement, 'CREATE_UPDATE_AND_DELETE_NETWORK_DEVICES');
 
