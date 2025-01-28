dabe3f06226b2d0bfa25d277522a6a6147eea037
Daniel Ntege <danientege785@gmail.com>
2025-01-28 22:52:10 +0300

    commit

---
+++ netmanager-app/core/apis/settings.ts
@@ -0,0 +1,17 @@
+import createAxiosInstance from "./axiosConfig";
+import { USERS_MGT_URL} from "@/core/urls";
+import { Client } from "@/app/types/clients";
+
+const axiosInstance = createAxiosInstance();
+
+export const getClientsApi = async (userID: string): Promise<Client[]> => {
+    try {
+      const response = await axiosInstance.get<Client[]>(USERS_MGT_URL, {
+        params: { user_id: userID },
+      });
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching clients:", error);
+      throw error;
+    }
+};
