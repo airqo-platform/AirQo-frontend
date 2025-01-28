691dee775ac77f1e0e35fc084c9be8ab1302cfb3
Ochieng Paul <ochiengpaul442@gmail.com>
2025-01-28 09:27:11 +0300

    updates

--- website2/src/app/explore-data/layout.tsx
+++ website2/src/app/explore-data/layout.tsx
@@ -10,32 +10,11 @@ export const metadata: Metadata = {
     title: 'Explore Air Quality Data - AirQo Analytics and Mobile App',
     description:
       'Access real-time air quality data through AirQo’s mobile app and analytics tools. Learn how AirQo’s platforms are helping improve air quality monitoring in Africa.',
-    url: 'https://yourdomain.com/explore-data',
+
     siteName: 'AirQo',
-    images: [
-      {
-        url: 'https://yourdomain.com/static/explore-data-og-image.jpg',
-        width: 1200,
-        height: 630,
-        alt: 'Explore Air Quality Data - AirQo Analytics and Mobile App',
-      },
-    ],
+
     locale: 'en_US',
     type: 'website',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    site: '@AirQo',
-    title: 'Explore Air Quality Data - AirQo Analytics and Mobile App',
-    description:
-      'Discover AirQo’s analytics and mobile app for real-time air quality data across Africa. Monitor air pollution and access detailed environmental data.',
-  },
-  robots: {
-    index: true,
-    follow: true,
-  },
-  alternates: {
-    canonical: 'https://yourdomain.com/explore-data',
   },
 };
 
