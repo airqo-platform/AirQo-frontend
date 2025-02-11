4466980331d54db08487ac9f4d12bc3bfd475780
Ochieng Paul <ochiengpaul442@gmail.com>
2025-02-11 12:19:12 +0300

    updated button

--- platform/src/pages/account/creation/DemoBookingSuccess.jsx
+++ platform/src/pages/account/creation/DemoBookingSuccess.jsx
@@ -3,12 +3,12 @@ import CongratsImage from '@/images/EventHandling/congratulations.svg';
 
 const DemoBookingSuccess = () => {
   return (
-    <div className="min-h-screen bg-white flex flex-col items-center justify-center relative overflow-hidden">
-      <div className="">
-        <CongratsImage className="w-auto h-auto mb-8" />
+    <div className="min-h-screen bg-white flex flex-col items-center justify-center relative overflow-hidden px-6">
+      <div className="mb-8">
+        <CongratsImage className="w-auto h-auto" />
       </div>
       {/* Main content */}
-      <div className="relative z-10 text-center px-6 md:px-48">
+      <div className="relative z-10 text-center md:px-48">
         <h3 className="text-3xl text-black-700 font-semibold">
           Congratulations!
         </h3>
@@ -16,6 +16,15 @@ const DemoBookingSuccess = () => {
           Your demo request has been submitted successfully. We will contact you
           shortly with the next steps.
         </p>
+      </div>
+      {/* Button to take the user to the AirQo website */}
+      <div className="mt-12">
+        <a
+          href="https://www.airqo.net"
+          className="inline-block btn bg-blue-900 text-white rounded-none text-sm outline-none border-none hover:bg-blue-950 px-12 py-3"
+        >
+          Visit AirQo Website
+        </a>
       </div>
     </div>
   );
