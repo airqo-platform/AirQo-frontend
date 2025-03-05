52389a0fa77e5f62f2db313d5a5c42b59c6beedb
Elia Light <141272836+AKATWIJUKA-ELIA@users.noreply.github.com>
2025-03-05 18:32:13 +0300

    moved urls from .env

--- src/netmanager-app/core/apis/MapData.ts
+++ src/netmanager-app/core/apis/MapData.ts
@@ -1,9 +1,7 @@
 import axios from "axios";
-const AirQuoData = process.env.NEXT_PUBLIC_AIRQUO_DATA
-const SUGGESTIONS = process.env.NEXT_PUBLIC_SUGGESTIONS
-const SEARCH = process.env.NEXT_PUBLIC_SEARCH
 export const token = process.env.NEXT_PUBLIC_MAP_API_TOKEN
 export const AirQoToken = process.env.NEXT_PUBLIC_AIRQO_DATA_TOKEN
+import {AIRQUO_DATA,MAP_BOX_SUGGESTIONS,MAP_BOX_SEARCH} from "../urls"
 import Node from '@/public/images/map/Node.webp';
 import Emoji from '@/public/images/map/Emoji.webp';
 import Heatmap from '@/public/images/map/Heatmap.webp';
@@ -18,7 +16,7 @@ import StreetsMode from '@/public/images/map/street.webp';
 export const GetAirQuoData = async (token: string) =>{
         try {
                 if(token){
-                        const response = await axios.get(`${AirQuoData}?token=${token}`);
+                        const response = await axios.get(`${AIRQUO_DATA}?token=${token}`);
                 const data = response.data;
                 return data;
                 }
@@ -43,7 +41,7 @@ export  const FetchSuggestions=async(value:string,access_token:string,sessionTok
         if (latitude !== undefined && longitude !== undefined) {
                 try{
                 const proximityParam = `${longitude},${latitude}`;
-                const response = await axios.get(`${SUGGESTIONS}?q=${value.toLowerCase()}&access_token=${access_token}&proximity=${proximityParam}&session_token=${sessionToken}`)
+                const response = await axios.get(`${MAP_BOX_SUGGESTIONS}?q=${value.toLowerCase()}&access_token=${access_token}&proximity=${proximityParam}&session_token=${sessionToken}`)
                 const data = await response.data;
                 
                 console.log("data :",data.suggestions)
@@ -64,7 +62,7 @@ export const UserClick = async(access_token:string,sessionToken:string,locationi
                 return;
         }
 
-        const response = await axios.get((`${SEARCH}/${locationid}?&access_token=${access_token}&session_token=${sessionToken}`))
+        const response = await axios.get((`${MAP_BOX_SEARCH}/${locationid}?&access_token=${access_token}&session_token=${sessionToken}`))
         const data = response.data;
         if(data){
                 return data;
@@ -115,7 +113,7 @@ export const mapStyles = [
       export const UserGroups = async (token: string) =>{
         try {
                 if(token){
-                        const response = await axios.get(`${AirQuoData}?token=${token}`);
+                        const response = await axios.get(`${AIRQUO_DATA}?token=${token}`);
                 const data = response.data;
                 return data;
                 }
