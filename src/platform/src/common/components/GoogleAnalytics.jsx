8bfe07093479304c4aa9b21835ecfcd78c7d2476
Belinda Marion Kobusingye <46527380+Codebmk@users.noreply.github.com>
2025-03-12 12:58:19 +0300

    merge files

--- src/platform/src/common/components/GoogleAnalytics.jsx
+++ src/platform/src/common/components/GoogleAnalytics.jsx
@@ -1,29 +1,36 @@
-'use strict';
+'use client';
 
 import Script from 'next/script';
 import { GA_MEASUREMENT_ID } from '@/core/hooks/useGoogleAnalytics';
 
-const GoogleAnalytics = () => {
+export default function GoogleAnalytics() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (pathname) {
+      window.gtag('config', GA_MEASUREMENT_ID, {
+        page_path: pathname + searchParams.toString(),
+      });
+    }
+  }, [pathname, searchParams]);
+
   return (
     <>
       <Script
-        strategy="afterInteractive"
         src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
-      />
-      <Script
-        id="google-analytics"
         strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${GA_MEASUREMENT_ID}');
-          `,
-        }}
       />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_MEASUREMENT_ID}', {
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
     </>
   );
-};
-
-export default GoogleAnalytics;
+}
