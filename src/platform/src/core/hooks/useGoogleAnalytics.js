bfa6e969ee168912b55ebd043f85d5a49a393707
Belinda Marion Kobusingye <46527380+Codebmk@users.noreply.github.com>
2025-03-12 12:51:27 +0300

    fixes

--- src/platform/src/core/hooks/useGoogleAnalytics.js
+++ src/platform/src/core/hooks/useGoogleAnalytics.js
@@ -2,20 +2,16 @@
 
 export const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
 
-export const pageview = (url) => {
-  if (typeof window.gtag === 'function') {
-    window.gtag('config', GA_MEASUREMENT_ID, {
-      page_path: url,
-    });
-  }
-};
-
 export const event = ({ action, category, label, value }) => {
-  if (typeof window.gtag === 'function') {
-    window.gtag('event', action, {
-      event_category: category,
-      event_label: label,
-      value: value,
-    });
+  try {
+    if (typeof window !== 'undefined' && typeof window.gtag === 'function') {
+      window.gtag('event', action, {
+        event_category: category,
+        event_label: label,
+        value: value,
+      });
+    }
+  } catch (error) {
+    console.error('Google Analytics event error:', error);
   }
 }; 
