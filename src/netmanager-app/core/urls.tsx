9479a694f9b6d46d3ed0a34264db040ae4752e6a
Daniel Ntege <danientege785@gmail.com>
2025-04-14 21:02:26 +0300

    Fix EXCEEDANCES_DATA_URI to point to the correct dashboard path

--- src/netmanager-app/core/urls.tsx
+++ src/netmanager-app/core/urls.tsx
@@ -6,7 +6,7 @@ export const USERS_MGT_URL = `${BASE_API_URL}/users`;
 export const DEVICES_MGT_URL = `${BASE_API_URL}/devices`;
 export const SITES_MGT_URL = `${BASE_API_URL}/devices/sites`;
 export const ANALYTICS_MGT_URL = `${BASE_API_URL}/analytics`;
-export const EXCEEDANCES_DATA_URI = `${ANALYTICS_MGT_URL}/exceedances`;
+export const EXCEEDANCES_DATA_URI = `${ANALYTICS_MGT_URL}/dashboard/exceedances`;
 export const DEVICE_EXCEEDANCES_URI = `${ANALYTICS_MGT_URL}/dashboard/exceedances-devices`;
 
 export const forgotPasswordUrl = `${process.env.NEXT_PUBLIC_ANALYTICS_URL}/account/forgotPwd`;
