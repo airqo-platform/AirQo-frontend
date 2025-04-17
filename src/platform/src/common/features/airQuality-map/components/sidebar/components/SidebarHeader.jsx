ce44b115bd81544909d5f78a68df79853455052f
Ochieng Paul <ochiengpaul442@gmail.com>
2025-04-17 20:11:53 +0300

    updates

--- src/platform/src/common/features/airQuality-map/components/sidebar/components/SidebarHeader.jsx
+++ src/platform/src/common/features/airQuality-map/components/sidebar/components/SidebarHeader.jsx
@@ -9,7 +9,7 @@ const SidebarHeader = ({
   handleHeaderClick = () => {},
 }) => {
   return (
-    <div className="p-4">
+    <div className="p-3 md:p-4">
       <div className="w-full flex justify-between items-center">
         <label className="font-medium text-xl text-gray-900 dark:text-gray-100">
           Air Quality Map
@@ -17,7 +17,7 @@ const SidebarHeader = ({
         {isFocused && (
           <button
             onClick={handleHeaderClick}
-            className="focus:outline-none border border-gray-300 dark:border-gray-600 rounded-xl hover:cursor-pointer p-2 hidden md:block bg-white dark:bg-gray-800"
+            className="focus:outline-none border border-gray-300 dark:border-gray-600 rounded-xl hover:cursor-pointer p-2 bg-white dark:bg-gray-800"
           >
             <CloseIcon fill="#536A87" />
           </button>
