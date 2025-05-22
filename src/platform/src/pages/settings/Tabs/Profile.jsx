e5302179fb0b48121c4388e2e046add33291d4b9
Paul Ochieng Levi <ochieng442@outlook.com>
2025-05-23 00:18:47 +0300

    refactor: update environment variable configuration and clean up Profile component imports

--- src/platform/src/pages/settings/Tabs/Profile.jsx
+++ src/platform/src/pages/settings/Tabs/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import * as Yup from 'yup';
 import Modal from '@/components/Modal/Modal';
