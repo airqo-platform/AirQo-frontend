64b594b10a464b5c4db5609d29a3f7eddc33fc23
Paul Ochieng Levi <ochieng442@outlook.com>
2025-05-22 23:33:57 +0300

    refactor: replace apiClient with secureApiProxy for improved security across API calls

--- src/platform/src/core/apis/predict.js
+++ src/platform/src/core/apis/predict.js
@@ -1,8 +1,11 @@
-import { publicApi } from '../utils/apiClient';
+import { secureApiProxy, AUTH_TYPES } from '../utils/secureApiProxyClient';
 import { DAILY_PREDICTIONS_URL } from '../urls/predict';
 
 // Get daily predictions for a site
 export const dailyPredictionsApi = (siteID) =>
-  publicApi
-    .get(DAILY_PREDICTIONS_URL, { params: { site_id: siteID } })
+  secureApiProxy
+    .get(DAILY_PREDICTIONS_URL, {
+      params: { site_id: siteID },
+      authType: AUTH_TYPES.NONE,
+    })
     .then((response) => response.data);
