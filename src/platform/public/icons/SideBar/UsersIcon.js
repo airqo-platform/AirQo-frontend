f05d0825d800d7a187c9a189ac95d98d8fb7f495
Paul Ochieng Levi <ochieng442@outlook.com>
2025-06-06 21:10:59 +0300

    refactor: update UsersIcon SVG structure and styling for consistency

--- src/platform/public/icons/SideBar/UsersIcon.js
+++ src/platform/public/icons/SideBar/UsersIcon.js
@@ -10,36 +10,15 @@ const UsersIcon = ({
     <svg
       width={size}
       height={size}
-      viewBox="0 0 24 24"
+      viewBox="0 0 22 20"
       fill="none"
-      className={className}
-      {...props}
+      className={`inline-block ${className}`}
       xmlns="http://www.w3.org/2000/svg"
+      {...props}
     >
       <path
-        d="M18 21a8 8 0 0 0-16 0"
-        stroke="currentColor"
-        strokeWidth={strokeWidth}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M14 9a4 4 0 1 0-8 0 4 4 0 0 0 8 0z"
-        stroke="currentColor"
-        strokeWidth={strokeWidth}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M22 21a8 8 0 0 0-6.4-7.8"
-        stroke="currentColor"
-        strokeWidth={strokeWidth}
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-      <path
-        d="M20 9a4 4 0 1 0-4 0"
-        stroke="currentColor"
+        d="M21 19v-2a4.002 4.002 0 00-3-3.874M14.5 1.291a4.001 4.001 0 010 7.418M16 19c0-1.864 0-2.796-.305-3.53a4 4 0 00-2.164-2.165C12.796 13 11.864 13 10 13H7c-1.864 0-2.796 0-3.53.305a4 4 0 00-2.166 2.164C1 16.204 1 17.136 1 19M12.5 5a4 4 0 11-8 0 4 4 0 018 0z"
+        stroke="#1C1D20"
         strokeWidth={strokeWidth}
         strokeLinecap="round"
         strokeLinejoin="round"
