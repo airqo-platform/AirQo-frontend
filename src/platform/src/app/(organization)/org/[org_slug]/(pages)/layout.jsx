375eff72d8298f4651920279da4bb0d9e40a2620
Paul Ochieng Levi <ochieng442@outlook.com>
2025-06-06 20:24:21 +0300

    refactor: simplify OrganizationPagesLayoutWrapper by removing unnecessary layout component

--- src/platform/src/app/(organization)/org/[org_slug]/(pages)/layout.jsx
+++ src/platform/src/app/(organization)/org/[org_slug]/(pages)/layout.jsx
@@ -2,15 +2,12 @@
 
 import { useParams } from 'next/navigation';
 import { OrganizationProvider } from '@/app/providers/OrganizationProvider';
-import OrganizationPagesLayout from '@/layouts/OrganizationPagesLayout';
 
 export default function OrganizationPagesLayoutWrapper({ children }) {
   const params = useParams();
   const orgSlug = params?.org_slug || params?.org_slug?.join('/') || '';
 
   return (
-    <OrganizationProvider orgSlug={orgSlug}>
-      <OrganizationPagesLayout>{children}</OrganizationPagesLayout>
-    </OrganizationProvider>
+    <OrganizationProvider orgSlug={orgSlug}>{children}</OrganizationProvider>
   );
 }
