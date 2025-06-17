578b4b210ab7b41047c05e8c3b51cf8b7a2cd4f8
Belinda Marion Kobusingye <46527380+Codebmk@users.noreply.github.com>
2025-06-17 15:04:15 +0300

    Update DialogWrapper.jsx

--- src/platform/src/common/components/Modal/DialogWrapper.jsx
+++ src/platform/src/common/components/Modal/DialogWrapper.jsx
@@ -42,49 +42,50 @@ export const DialogWrapper = ({
         open ? 'visible modal modal-open' : 'hidden'
       } w-screen h-screen flex items-center justify-center`}
     >
-      <div className="fixed inset-0" onClick={onClose} />
-      <Card
-        ref={dialogRef}
-        width={width}
-        height="h-auto"
-        className="mx-auto p-2"
-        contentClassName="overflow-hidden"
-        header={
-          ModalIcon && (
-            <div className="flex justify-between items-center w-full">
-              <div className="p-5 bg-indigo-50 rounded-full border border-indigo-50 flex justify-center items-center">
-                <ModalIcon className="w-6 h-6" />
+      <div className="fixed inset-0 bg-black/50" onClick={onClose} />
+      <div ref={dialogRef} className="relative z-50">
+        <Card
+          width={width}
+          height="h-auto"
+          className="mx-auto p-2"
+          contentClassName="overflow-hidden"
+          header={
+            ModalIcon && (
+              <div className="flex justify-between items-center w-full">
+                <div className="p-5 bg-indigo-50 rounded-full border border-indigo-50 flex justify-center items-center">
+                  <ModalIcon className="w-6 h-6" />
+                </div>
               </div>
-            </div>
-          )
-        }
-        footer={
-          footer !== undefined ? (
-            footer
-          ) : (
-            <div className="flex justify-end items-center gap-3 w-full">
-              <Button
-                onClick={handleCancel}
-                variant="outlined"
-                className="text-sm font-medium dark:bg-transparent"
-              >
-                Cancel
-              </Button>
-              <Button
-                variant={loading ? 'disabled' : 'filled'}
-                className="text-sm font-medium"
-                onClick={handleClick}
-              >
-                {loading ? 'Loading...' : primaryButtonText}
-              </Button>
-            </div>
-          )
-        }
-        headerProps={{ className: 'pb-4' }}
-        footerProps={{ className: 'pt-4' }}
-      >
-        <div className="w-full">{children}</div>
-      </Card>
+            )
+          }
+          footer={
+            footer !== undefined ? (
+              footer
+            ) : (
+              <div className="flex justify-end items-center gap-3 w-full">
+                <Button
+                  onClick={handleCancel}
+                  variant="outlined"
+                  className="text-sm font-medium dark:bg-transparent"
+                >
+                  Cancel
+                </Button>
+                <Button
+                  variant={loading ? 'disabled' : 'filled'}
+                  className="text-sm font-medium"
+                  onClick={handleClick}
+                >
+                  {loading ? 'Loading...' : primaryButtonText}
+                </Button>
+              </div>
+            )
+          }
+          headerProps={{ className: 'pb-4' }}
+          footerProps={{ className: 'pt-4' }}
+        >
+          <div className="w-full">{children}</div>
+        </Card>
+      </div>
     </dialog>
   );
 };
