96488c32873b43ced2fad0a2d848c7cb580efc51
Paul Ochieng Levi <ochiengpaul442@gmail.com>
2025-08-05 14:45:43 +0300

    feat: refactor DataExportPage and DataDownload components to enhance modal handling and pass additional props

--- src/platform/src/app/(individual)/user/(pages)/(layout-1)/data-export/page.jsx
+++ src/platform/src/app/(individual)/user/(pages)/(layout-1)/data-export/page.jsx
@@ -1,9 +1,25 @@
 'use client';
-
+import { useCallback } from 'react';
+import Modal from '@/features/download-insights-locations';
+import { setOpenModal } from '@/lib/store/services/downloadModal';
+import { useDispatch, useSelector } from 'react-redux';
 import DataExportPage from '@/common/features/download-insights-locations/data-export';
 
 const Page = () => {
-  return <DataExportPage />;
+  const dispatch = useDispatch();
+  const isModalOpen = useSelector((state) => state.modal.openModal);
+
+  const handleCloseModal = useCallback(() => {
+    dispatch(setOpenModal(false));
+  }, [dispatch]);
+
+  return (
+    <div>
+      <DataExportPage backToDownload={false} />
+
+      <Modal isOpen={isModalOpen} onClose={handleCloseModal} />
+    </div>
+  );
 };
 
 export default Page;
