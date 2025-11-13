58cde8c96fd7c288df6cc146316442b4fb7b69b2
Codebmk <belindamarion0@gmail.com>
2025-11-13 18:43:03 +0300

    Refactor device hook to use useAppSelector
    Replaced useSelector with useAppSelector in useDevices hook for consistency and improved type safety. Removed unused import of useSelector.

--- src/vertex/core/hooks/useDevices.ts
+++ src/vertex/core/hooks/useDevices.ts
@@ -24,7 +24,6 @@ import { useDispatch } from "react-redux";
 import ReusableToast from "@/components/shared/toast/ReusableToast";
 import { getApiErrorMessage } from "../utils/getApiErrorMessage";
 import logger from "@/lib/logger";
-import { useSelector } from "react-redux";
 
 interface ErrorResponse {
   message: string;
@@ -42,8 +41,8 @@ export interface DeviceListingOptions {
 }
 
 export const useDevices = (options: DeviceListingOptions = {}) => {
-  const activeNetwork = useSelector((state:any) => state.user.activeNetwork);
-  const activeGroup = useSelector((state:any) => state.user.activeGroup);
+  const activeNetwork = useAppSelector((state) => state.user.activeNetwork);
+  const activeGroup = useAppSelector((state:any) => state.user.activeGroup);
   const isAirQoGroup = activeGroup?.grp_title === "airqo";
 
   const { data: groupCohortIds, isLoading: isLoadingCohorts } = useGroupCohorts(activeGroup?._id, {
