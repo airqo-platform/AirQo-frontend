a4c5a50db08f1d5a53369b45d3d2a0551d6b1c0e
Codebmk <belindamarion0@gmail.com>
2025-11-29 14:06:53 +0300

    Stray closing brace in className template.

--- src/vertex/components/layout/layout.tsx
+++ src/vertex/components/layout/layout.tsx
@@ -101,8 +101,7 @@ export default function Layout({ children }: LayoutProps) {
         activeModule={activeModule}
       />
       <main
-        className={`flex-1 transition-all duration-300 ease-in-out bg-background w-full flex flex-col ${isSecondarySidebarCollapsed ? 'lg:ml-[88px]' : 'lg:ml-[256px]'} overflow-y-auto mt-[50px] md:mt-[50px] lg:mt-12 pb-20 md:pb-0
-        }`}
+        className={`flex-1 transition-all duration-300 ease-in-out bg-background w-full flex flex-col ${isSecondarySidebarCollapsed ? 'lg:ml-[88px]' : 'lg:ml-[256px]'} overflow-y-auto mt-[50px] md:mt-[50px] lg:mt-12 pb-20 md:pb-0`}
       >
         <div
           className={`flex-1 w-full bg-background max-w-7xl mx-auto flex flex-col gap-4 md:gap-8 px-3 py-3 md:px-2 lg:py-6 lg:px-6`}
