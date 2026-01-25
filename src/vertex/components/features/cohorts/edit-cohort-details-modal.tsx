8b51aadd6718195af8c84a05af466ef6741b4134
Codebmk <belindamarion0@gmail.com>
2026-01-25 04:23:12 +0300

    Remove unused Select import from edit cohort modal
    Cleaned up the import statements in edit-cohort-details-modal.tsx by removing the unused Select-related components.

--- src/vertex/components/features/cohorts/edit-cohort-details-modal.tsx
+++ src/vertex/components/features/cohorts/edit-cohort-details-modal.tsx
@@ -5,7 +5,7 @@ import ReusableDialog from "@/components/shared/dialog/ReusableDialog";
 import ReusableButton from "@/components/shared/button/ReusableButton";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
+
 import { useUpdateCohortDetails } from "@/core/hooks/useCohorts";
 import { PERMISSIONS } from "@/core/permissions/constants";
 import logger from "@/lib/logger";
